8354e1161ab726f4ce5ecc773adb9f4eeb058f4c
Florjan Bartol <florjan.bartol@gmail.com>
2014-06-04 13:35:08 +0200

    implemented basic authentication workflow

--- server/dash/urls.py
+++ server/dash/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from dash import views
 
 urlpatterns = [
-    url(r'^login$', 'django.contrib.auth.views.login', {'template_name': 'dash/login.html'})
+    url(r'^$', views.index, name='index'),
+    url(r'^signin$', 'django.contrib.auth.views.login', {'template_name': 'dash/signin.html'}),
+    url(r'^signout$', 'django.contrib.auth.views.logout_then_login', {'login_url': '/signin'})
 ]
--- server/dash/views.py
+++ server/dash/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect
 
 # Create your views here.
+def index(request):
+	if request.user.is_authenticated():
+		return HttpResponse("Hello, world. This is the home of the angular app.")
+	else:
+		return redirect('/signin')
