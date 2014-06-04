cdcb6843f36b8a8a4f1a8bd108809ae733d626d3
Tomaz Kovacic <tomaz.kovacic@zemanta.com>
2014-06-04 12:26:14 +0200

    adding yaml fixtures instead of JSON

--- server/manage.py
+++ server/manage.py
@@ -2,9 +2,14 @@
 import os
 import sys
 
+import warnings # filtering out warnings when loading YAML fixtures with USE_TZ = True
+warnings.filterwarnings('ignore', r"DateTimeField .* received a naive datetime", RuntimeWarning, r'django\.db\.models\.fields')
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
+
+    
