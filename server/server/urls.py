291fe265c0c310b9d7b6ef6b65d1ead431c71303
Florjan Bartol <florjan.bartol@gmail.com>
2014-06-09 17:38:09 +0200

    moved auth code from dash to zemauth

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    url(r'^', include('dash.urls')),
+from dash import views as dash_views
+from zemauth.forms import AuthenticationForm
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', dash_views.index, name='index'),
+    url(r'^signin$',
+        'django.contrib.auth.views.login',
+        {'authentication_form': AuthenticationForm, 'template_name': 'zemauth/signin.html'}),
+    url(r'^signout$', 'django.contrib.auth.views.logout_then_login'),
     url(r'^admin/', include(admin.site.urls))
 )
