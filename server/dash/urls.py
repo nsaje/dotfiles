e489559338dd5a7a181137a01ee1fe7c5aa2ee6d
Florjan Bartol <florjan.bartol@gmail.com>
2014-06-09 16:50:32 +0200

    added signin frontend code, auth form and backend

--- server/dash/urls.py
+++ server/dash/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 
 from dash import views
+from dash.auth.forms import AuthenticationForm
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^signin$', 'django.contrib.auth.views.login', {'template_name': 'dash/signin.html'}),
+    url(r'^signin$',
+        'django.contrib.auth.views.login',
+        {'authentication_form': AuthenticationForm, 'template_name': 'dash/signin.html'}),
     url(r'^signout$', 'django.contrib.auth.views.logout_then_login')
 ]
