39251c215bae444e9c873b3a270cc04fbd614608
Florjan Bartol <florjan.bartol@gmail.com>
2014-06-16 13:33:04 +0200

    removed unnecessary authentication checks

--- server/dash/views.py
+++ server/dash/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.shortcuts import redirect
 
 from django.conf import settings
 
@@ -8,7 +7,4 @@ from django.conf import settings
 # Create your views here.
 @login_required(login_url=settings.LOGIN_URL)
 def index(request):
-    if request.user.is_authenticated():
-        return render(request, 'index.html', {'staticUrl': settings.CLIENT_STATIC_URL})
-    else:
-        return redirect(settings.LOGIN_URL)
+    return render(request, 'index.html', {'staticUrl': settings.CLIENT_STATIC_URL})
