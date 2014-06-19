72700f710af3835ee984773f007e997891772846
Matic Žgur <mr.zgur@gmail.com>
2014-06-19 17:49:19 +0200

    client: Implemented header, breadcrumb, sidebar and footer.

--- client/app/js/controllers/main.js
+++ client/app/js/controllers/main.js
@@ -5,6 +5,9 @@ oneApp.controller('MainCtrl', ['$scope', '$state', 'api', function ($scope, $sta
         {heading: 'Networks', route: 'adGroups.networks', active: false},
         {heading: 'Settings', route: 'adGroups.settings', active: false}
     ];
+    $scope.accounts = [];
+
+    $scope.breadcrumb = [];
     
     $scope.$on("$stateChangeSuccess", function() {
         $scope.tabs.forEach(function(tab) {
@@ -12,7 +15,19 @@ oneApp.controller('MainCtrl', ['$scope', '$state', 'api', function ($scope, $sta
         });
     });
 
-    api.navData.list().then(function(data) {
+    api.navData.list().then(function (data) {
         $scope.accounts = data;
+    });
+
+    $scope.$watch('accounts', function (newValue, oldValue) {
+        $scope.accounts.forEach(function (account) {
+            account.campaigns.forEach(function (campaign) {
+                campaign.adGroups.forEach(function (adGroup) {
+                    if (adGroup.id.toString() === $state.params.id) {
+                        $scope.breadcrumb = [account.name, campaign.name, adGroup.name];
+                    }
+                });
+            });
+        });
     });
 }]);
