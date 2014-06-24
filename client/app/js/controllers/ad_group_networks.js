2b717ec03d8113c5b7b64d3bf47e0cea92510bbe
Florjan Bartol <florjan.bartol@gmail.com>
2014-06-24 18:17:02 +0200

    added css for table, loader, minor fixes

--- client/app/js/controllers/ad_group_networks.js
+++ client/app/js/controllers/ad_group_networks.js
@@ -1,5 +1,9 @@
 oneApp.controller('AdGroupNetworksCtrl', ['$scope', '$state', 'api', function ($scope, $state, api) {
+    $scope.loadRequestInProgress = false;
+
     $scope.getTableData = function (id) {
+        $scope.loadRequestInProgress = true;
+
         api.adGroupNetworksTable.get(id).then(
             function (data) {
                 $scope.rows = data.rows;
@@ -9,7 +13,9 @@ oneApp.controller('AdGroupNetworksCtrl', ['$scope', '$state', 'api', function ($
                 // error
                 return;
             }
-        );
+        ).finally(function () {
+            $scope.loadRequestInProgress = false;
+        });
     };
 
     $scope.getTableData($state.params.id);
