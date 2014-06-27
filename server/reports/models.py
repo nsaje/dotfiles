22be55b8ec28e5abc41fe9b2abcdc45020f25677
Luka Silovinac <luka.silovinac@zemanta.com>
2014-06-27 19:08:53 +0200

    Fix upsert and remove cpc_cc field from reports model

--- server/reports/models.py
+++ server/reports/models.py
@@ -11,7 +11,6 @@ class ArticleStats(models.Model):
 
     impressions = models.IntegerField(default=0, blank=False, null=False)
     clicks = models.IntegerField(default=0, blank=False, null=False)
-    cpc_cc = models.IntegerField(default=0, blank=False, null=False)
     cost_cc = models.IntegerField(default=0, blank=False, null=False)
     created_dt = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
 
