db76b4d6253c09948a59f690a051a22c6f04419b
Matic Žgur <mr.zgur@gmail.com>
2014-07-03 11:41:35 +0200

    server: Changed a few more occurrences of ActionLog.action_status to state.

--- server/actionlog/models.py
+++ server/actionlog/models.py
@@ -53,10 +53,10 @@ class ActionLog(models.Model):
     )
 
     def __str__(self):
-        return '{cn}(action={action}, action_status={status}, ad_group_network={agn}, id={id})'.format(
+        return '{cn}(action={action}, state={state}, ad_group_network={agn}, id={id})'.format(
             cn=self.__class__.__name__,
             action=self.action,
-            status=self.action_status,
+            status=self.state,
             agn=self.ad_group_network,
             id=self.id,
         )
