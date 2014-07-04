29c17c9378f5eaf3c3c9bc18d31ac62f5a7b5fd2
Jure Ham <jure@hamsworld.net>
2014-07-04 11:03:13 +0200

    Cleaned up dash and actionlog signals

--- server/actionlog/signals.py
+++ server/actionlog/signals.py
@@ -1,26 +1,11 @@
 from django.db.models.signals import pre_save
-from . import models
-from gadjo.requestprovider.signals import get_request
+
+from utils import signal_handlers
+
+from actionlog import models
 
 
-def modified_by_pre_save_signal_handler(sender, instance, **kwargs):
-    try:
-        request = get_request()
-        instance.modified_by = request.user
-    except IndexError:
-        instance.modified_by = None
-
-
-def created_by_pre_save_signal_handler(sender, instance, **kwargs):
-    try:
-        if instance.pk is None:
-            request = get_request()
-            instance.created_by = request.user
-    except IndexError:
-        instance.created_by = None
-
-
-pre_save.connect(modified_by_pre_save_signal_handler, sender=models.ActionLog)
-pre_save.connect(created_by_pre_save_signal_handler, sender=models.ActionLog)
-pre_save.connect(modified_by_pre_save_signal_handler, sender=models.ActionLogOrder)
-pre_save.connect(created_by_pre_save_signal_handler, sender=models.ActionLogOrder)
+pre_save.connect(signal_handlers.modified_by_pre_save_signal_handler, sender=models.ActionLog)
+pre_save.connect(signal_handlers.created_by_pre_save_signal_handler, sender=models.ActionLog)
+pre_save.connect(signal_handlers.modified_by_pre_save_signal_handler, sender=models.ActionLogOrder)
+pre_save.connect(signal_handlers.created_by_pre_save_signal_handler, sender=models.ActionLogOrder)
