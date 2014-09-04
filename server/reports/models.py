0db97d2d4056b4aa344a1d44618ba0003b7c306a
Lorand Dali <lorand@veronel.zemanta.com>
2014-09-04 08:55:30 +0200

    added has_conversion_metrics to ArticleStats

--- server/reports/models.py
+++ server/reports/models.py
@@ -24,6 +24,7 @@ class ArticleStats(models.Model):
 
     has_traffic_metrics = models.IntegerField(default=0, blank=False, null=False)
     has_postclick_metrics = models.IntegerField(default=0, blank=False, null=False)
+    has_conversion_metrics = models.IntegerField(default=0, blank=False, null=False)
 
     created_dt = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
 
@@ -66,8 +67,6 @@ class GoalConversionStats(models.Model):
     # conversion metrics
     conversions = models.IntegerField(default=0, blank=False, null=False)
     conversions_value_cc = models.IntegerField(default=0, blank=False, null=False)
-
-    has_conversion_metrics = models.IntegerField(default=0, blank=False, null=False)
 
     class Meta:
         unique_together = (
