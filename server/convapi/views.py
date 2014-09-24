acd1632e171bfc93b16fa1bb91fb5dc8c7577bf2
Lorand Dali <lorand@veronel.zemanta.com>
2014-09-24 17:13:53 +0200

    monitoring number of ga report requests and reported vs aggregated visits

--- server/convapi/views.py
+++ server/convapi/views.py
@@ -9,6 +9,7 @@ from django.db import transaction
 from auth import MailGunRequestAuth, GASourceAuth
 from parse import CsvReport
 from aggregate import ReportEmail
+from utils.statsd_helper import statsd_incr
 
 
 logger = logging.getLogger(__name__)
@@ -22,13 +23,17 @@ def mailgun_gareps(request):
         return HttpResponse(status=406)
     if not MailGunRequestAuth(request).is_authorised():
         logger.error('ERROR: authenticity of request could not be verified')
+        statsd_incr('convapi.invalid_request_signature')
         return HttpResponse(status=406)
 
     recipient = request.POST['recipient']
     
     if not GASourceAuth(recipient).is_authorised():
         logger.error('ERROR: sender is not authorised')
+        statsd_incr('convapi.invalid_email_sender')
         return HttpResponse(status=406)
+
+    statsd_incr('convapi.accepted_emails')
 
     if int(request.POST.get('attachment-count', 0)) != 1:
         logger.error('ERROR: single attachment expected, several received')
@@ -62,5 +67,7 @@ def mailgun_gareps(request):
 
     report_email.save_raw()
     report_email.aggregate()
+
+    statsd_incr('convapi.aggregated_emails')
     
     return HttpResponse(status=200)
