943113febef9f8f3b9f67a0e528162940c551df9
Lorand Dali <lorand@veronel.zemanta.com>
2014-09-25 15:02:33 +0200

    fixed cron job for monitoring ga reports

--- server/convapi/management/commands/monitor_ga_reports.py
+++ server/convapi/management/commands/monitor_ga_reports.py
@@ -1,7 +1,7 @@
 import datetime
 
 from django.core.management.base import BaseCommand
-from django.db.models import Sum
+from django.db.models import Sum, Max
 
 import reports.api
 import convapi.models
@@ -12,10 +12,10 @@ from utils.statsd_helper import statsd_gauge
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        today = datetime.datetime.utcnow().date()
-        n_aggregated_visits = reports.api.query(today, today).get('visits') or 0
+        max_date = convapi.models.RawPostclickStats.objects.aggregate(max_date=Max('datetime'))['max_date']
+        n_aggregated_visits = reports.api.query(max_date, max_date).get('visits') or 0
         n_reported_visits = convapi.models.RawPostclickStats.objects.filter(
-            datetime=today
+            datetime=max_date
         ).aggregate(visits_sum=Sum('visits')).get('visits_sum') or 0
         statsd_gauge('convapi.reported_visits', n_reported_visits)
         statsd_gauge('convapi.aggregated_visits', n_aggregated_visits)
