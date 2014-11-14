b56f630447f07edd3d0fc9f1cc1bdbfc41e13bf0
Florjan Bartol <florjan.bartol@gmail.com>
2014-11-14 17:39:30 +0100

    Added autofocus on editable currency field.

--- client/one/js/directives/zem_editable_currency_field.js
+++ client/one/js/directives/zem_editable_currency_field.js
@@ -11,13 +11,21 @@ oneApp.directive('zemEditableCurrencyField', function() {
             fractionSize: '='
         },
         templateUrl: '/partials/zem_editable_currency_field.html',
-        controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
+        controller: ['$scope', '$element', '$attrs', '$timeout', function ($scope, $element, $attrs, $timeout) {
             $scope.originalValue = $scope.value;
             $scope.editFormActive = false;
             $scope.errors = null;
 
             $scope.onError = function (errors) {
                 $scope.errors = errors;
+            };
+
+            $scope.showEditForm = function () {
+                $scope.editFormActive = true;
+
+                $timeout(function () {
+                    $element.find('.currency-input')[0].focus();
+                });
             };
 
             $scope.cancel = function () {
