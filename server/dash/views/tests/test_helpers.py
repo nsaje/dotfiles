290791f69f4e28f078e4ca50849273a8083c7d14
Florjan Bartol <florjan.bartol@gmail.com>
2015-01-14 15:55:00 +0100

    Updated tests.

--- server/dash/views/tests/test_helpers.py
+++ server/dash/views/tests/test_helpers.py
@@ -1,4 +1,8 @@
+import datetime
+import pytz
+
 from django.test import TestCase
+from django.conf import settings
 
 from dash.views import helpers
 from dash import models
@@ -19,37 +23,56 @@ class ViewHelpersTestCase(TestCase):
 
         self.assertEqual(
             data_status[ad_group_source1.source_id]['message'],
-            '<b>Status</b> for this Media Source differs from Status in the Media Source\'s 3rd party dashboard.<br/>Last OK sync was on: <b>06/10/2014 5:58 AM</b>'
+            '<b>Status</b> for this Media Source differs from Status in the Media Source\'s 3rd party dashboard.<br/>Reporting data is stale. Last OK sync was on: <b>06/10/2014 5:58 AM</b>'
         )
 
         self.assertEqual(
             data_status[ad_group_source2.source_id]['message'],
-            '<b>Bid CPC</b> for this Media Source differs from Bid CPC in the Media Source\'s 3rd party dashboard.<br/><b>Daily Budget</b> for this Media Source differs from Daily Budget in the Media Source\'s 3rd party dashboard.<br/>Last OK sync was on: <b>06/10/2014 5:58 AM</b>'
+            '<b>Bid CPC</b> for this Media Source differs from Bid CPC in the Media Source\'s 3rd party dashboard.<br/><b>Daily Budget</b> for this Media Source differs from Daily Budget in the Media Source\'s 3rd party dashboard.<br/>Reporting data is stale. Last OK sync was on: <b>06/10/2014 5:58 AM</b>'
         )
 
         self.assertEqual(
             data_status[ad_group_source3.source_id]['message'],
-            'Everything is OK. Last OK sync was on: <b>06/10/2014 5:58 AM</b>'
+            'Reporting data is stale. Last OK sync was on: <b>06/10/2014 5:58 AM</b>'
         )
 
-    def test_get_ad_group_sources_data_status_no_settings(self):
-        ad_group_source = models.AdGroupSource.objects.get(pk=4)
+    def test_get_ad_group_sources_data_status_not_stale(self):
+        ad_group_source = models.AdGroupSource.objects.get(pk=3)
+
+        last_sync = datetime.datetime.now()
+        ad_group_source.last_successful_sync_dt = last_sync
+
         data_status = helpers.get_ad_group_sources_data_status([ad_group_source])
 
         self.assertEqual(data_status[ad_group_source.source_id]['ok'], True)
 
+        datetime_string = pytz.utc.localize(last_sync).astimezone(pytz.timezone(settings.DEFAULT_TIME_ZONE))
+
         self.assertEqual(
             data_status[ad_group_source.source_id]['message'],
-            'Everything is OK. Last OK sync was on: <b>06/10/2014 5:58 AM</b>'
+            'All data is OK. Last OK sync was on: <b>{}</b>'.format(
+                datetime_string.strftime('%m/%d/%Y %-I:%M %p'))
+        )
+
+    def test_get_ad_group_sources_data_status_no_settings(self):
+        ad_group_source = models.AdGroupSource.objects.get(pk=4)
+
+        data_status = helpers.get_ad_group_sources_data_status([ad_group_source])
+
+        self.assertEqual(data_status[ad_group_source.source_id]['ok'], False)
+
+        self.assertEqual(
+            data_status[ad_group_source.source_id]['message'],
+            'Reporting data is stale. Last OK sync was on: <b>06/10/2014 5:58 AM</b>'
         )
 
     def test_get_ad_group_sources_data_status_property_none(self):
         ad_group_source = models.AdGroupSource.objects.get(pk=4)
         data_status = helpers.get_ad_group_sources_data_status([ad_group_source])
 
-        self.assertEqual(data_status[ad_group_source.source_id]['ok'], True)
+        self.assertEqual(data_status[ad_group_source.source_id]['ok'], False)
 
         self.assertEqual(
             data_status[ad_group_source.source_id]['message'],
-            'Everything is OK. Last OK sync was on: <b>06/10/2014 5:58 AM</b>'
+            'Reporting data is stale. Last OK sync was on: <b>06/10/2014 5:58 AM</b>'
         )
