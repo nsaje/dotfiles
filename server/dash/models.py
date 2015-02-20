de9a5cf65673363abfb4284803cb531501d3add5
Luka Šilovinac <luka.silovinac@zemanta.com>
2015-02-20 14:00:37 +0100

    Add a bidder slug field to Source model

--- server/dash/models.py
+++ server/dash/models.py
@@ -516,6 +516,13 @@ class Source(models.Model):
         unique=True,
         verbose_name='Tracking slug'
     )
+    bidder_slug = models.CharField(
+        max_length=50,
+        null=True,
+        blank=False,
+        unique=True,
+        verbose_name='B1 Slug'
+    )
     maintenance = models.BooleanField(default=True)
     created_dt = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
     modified_dt = models.DateTimeField(auto_now=True, verbose_name='Modified at')
