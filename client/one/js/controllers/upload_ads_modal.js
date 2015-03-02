f32ea3c15f84488c0a863a415e858c3c47587f86
Florjan Bartol <florjan.bartol@gmail.com>
2015-03-02 19:03:34 +0100

    Added default batch name.

--- client/one/js/controllers/upload_ads_modal.js
+++ client/one/js/controllers/upload_ads_modal.js
@@ -1,18 +1,13 @@
 /* globals oneApp */
-oneApp.controller('UploadAdsModalCtrl', ['$scope', '$modalInstance', 'api', '$state', '$timeout', function($scope, $modalInstance, api, $state, $timeout) {
-    $scope.formData = {};
+oneApp.controller('UploadAdsModalCtrl', ['$scope', '$modalInstance', 'api', '$state', '$timeout', '$filter', function($scope, $modalInstance, api, $state, $timeout, $filter) {
+    var getCurrentTimeString = function() {
+        var datetime = new Date();  // get current local time
 
-    $scope.upload = function() {
-        $scope.isInProgress = true;
+        // add UTC timezone offset to simulate time in UTC timezone
+        var timestamp = datetime.getTime() + datetime.getTimezoneOffset() * 60 * 1000;
 
-        api.adGroupAdsPlusUpload.upload(
-            $state.params.id, $scope.formData.file, $scope.formData.batchName
-        ).then(function(batchId) {
-            pollBatchStatus(batchId);
-        }, function(errors) {
-            $scope.isInProgress = false;
-            $scope.errors = errors;
-        });
+        datetime = new Date(timestamp + $scope.user.timezoneOffset * 1000);
+        return $filter('date')(datetime, 'M/d/yyyy h:mm a');
     };
 
     var pollBatchStatus = function(batchId) {
@@ -36,5 +31,22 @@ oneApp.controller('UploadAdsModalCtrl', ['$scope', '$modalInstance', 'api', '$st
                 });
             }, 1000);
         }
+    };
+
+    $scope.formData = {
+        batchName: getCurrentTimeString()
+    };
+
+    $scope.upload = function() {
+        $scope.isInProgress = true;
+
+        api.adGroupAdsPlusUpload.upload(
+            $state.params.id, $scope.formData.file, $scope.formData.batchName
+        ).then(function(batchId) {
+            pollBatchStatus(batchId);
+        }, function(errors) {
+            $scope.isInProgress = false;
+            $scope.errors = errors;
+        });
     };
 }]);
