f23475d9ce2df0b02eee9fc6b1afc148f96ac209
Miha Miklič <miha.miklic@zemanta.com>
2015-04-13 16:16:15 +0200

    Compressing/Decompressing SQS messages.

--- server/convapi/views.py
+++ server/convapi/views.py
@@ -1,5 +1,7 @@
 import logging
 import time
+import zlib
+import pickle
 from threading import Thread
 
 from django.views.decorators.csrf import csrf_exempt
@@ -70,7 +72,8 @@ def mailgun_gareps(request):
                                              request.FILES.get('attachment-1').name,
                                              request.POST.get('attachment-count', 0))
 
-        tasks.process_ga_report.apply_async((ga_report_task, ),
+        compressed_ga_report_task = zlib.compress(pickle.dumps(ga_report_task))
+        tasks.process_ga_report.apply_async((compressed_ga_report_task, ),
                                              queue=settings.CELERY_DEFAULT_CONVAPI_QUEUE)
     except Exception as e:
         report_log = models.GAReportLog()
