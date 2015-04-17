b2bc7e3d8dd6531f764fa4edbb38773c7dad09b0
Simon Mihevc <simon.mihevc@zemanta.com>
2015-04-17 15:02:40 +0200

    Migration fixes

--- server/dash/migrations/0032_auto_20150417_1253.py
+++ server/dash/migrations/0032_auto_20150417_1253.py
@@ -1,7 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import dash.constants
+
 from django.db import models, migrations
+
+# custom code to get source actions up to date in db
+def source_action_initial_data_forwards_code(apps, schema_editor):
+    SourceAction = apps.get_model('dash', 'SourceAction')
+    for source_action in dash.constants.SourceAction.get_all():
+        try:
+            SourceAction.objects.get(action=source_action)
+        except SourceAction.DoesNotExist:
+            SourceAction.objects.create(action=source_action)
+
+
+def source_action_initial_data_reverse_code(apps, schema_editor):
+    pass
+
 
 
 class Migration(migrations.Migration):
