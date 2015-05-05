d44abac55ae5f182ce8293226ab417fcc562d56b
Florjan Bartol <florjan.bartol@gmail.com>
2015-05-05 18:13:49 +0200

    Prevented adiant from showing errors

--- server/dash/views/helpers.py
+++ server/dash/views/helpers.py
@@ -354,14 +354,13 @@ def get_content_ad_submission_status(content_ad_sources):
             'status': cas_submission_status,
         }
 
-        if cas_submission_status == constants.ContentAdSubmissionStatus.REJECTED:
-            text = '{} ({})'.format(
-                constants.ContentAdSubmissionStatus.get_text(cas_submission_status),
-                content_ad_source.submission_errors)
+        text = constants.ContentAdSubmissionStatus.get_text(cas_submission_status)
+        if (cas_submission_status == constants.ContentAdSubmissionStatus.REJECTED and
+                content_ad_source.submission_errors is not None and
+                content_ad_source.source.id != 22):  # temp hack to avoid showing Adiant errors
+            text = '{} ({})'.format(text, content_ad_source.submission_errors)
         else:
-            text = '{} / {}'.format(
-                constants.ContentAdSubmissionStatus.get_text(cas_submission_status),
-                constants.ContentAdSourceState.get_text(cas_source_state))
+            text = '{} / {}'.format(text, constants.ContentAdSourceState.get_text(cas_source_state))
 
         status['text'] = text
         submission_status.append(status)
