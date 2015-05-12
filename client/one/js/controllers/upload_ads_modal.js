8f24adaf536646847dc93c63ba10683e457c1ade
Florjan Bartol <florjan.bartol@gmail.com>
2015-05-12 11:12:32 +0200

    Added char counts and textarea to upload modal

--- client/one/js/controllers/upload_ads_modal.js
+++ client/one/js/controllers/upload_ads_modal.js
@@ -47,6 +47,13 @@ oneApp.controller('UploadAdsModalCtrl', ['$scope', '$modalInstance', 'api', '$st
         data.displayUrl = data.displayUrl.replace(replaceEnd, '');
     };
 
+    $scope.clearErrors = function(name) {
+        if (!$scope.errors) {
+            return;
+        }
+        delete $scope.errors[name];
+    }
+
     $scope.upload = function() {
         $scope.isInProgress = true;
         $scope.errors = null;
@@ -58,6 +65,7 @@ oneApp.controller('UploadAdsModalCtrl', ['$scope', '$modalInstance', 'api', '$st
         ).then(function(batchId) {
             $scope.pollBatchStatus(batchId);
         }, function(data) {
+            console.log(data);
             $scope.isInProgress = false;
             $scope.errors = data.errors;
         });
