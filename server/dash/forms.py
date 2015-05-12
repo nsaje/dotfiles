d670c4be7baf2d2f1e23dc22f652d4eb43e89186
Simon Mihevc <simon.mihevc@zemanta.com>
2015-05-12 17:40:02 +0200

    Check for title too long

--- server/dash/forms.py
+++ server/dash/forms.py
@@ -13,6 +13,8 @@ from django.core import validators
 from dash import constants
 from zemauth.models import User as ZemUser
 
+MAX_CSV_TITLE_LENGTH = 256
+
 
 class BaseApiForm(forms.Form):
     def get_errors():
@@ -374,7 +376,11 @@ class AdGroupAdsPlusUploadForm(forms.Form):
                 if None in row:
                     del row[None]
 
+                if row['title'] is None or len(row['title']) > MAX_CSV_TITLE_LENGTH:
+                    raise forms.ValidationError('Title too long. Must be less than %d characters.' % MAX_CSV_TITLE_LENGTH)
+
                 count_rows += 1
+
                 data.append(row)
 
             if count_rows == 0:
