c7c5ea67b9437315fe07c794a1664e88d66c1171
Simon Mihevc <simon.mihevc@zemanta.com>
2015-05-13 10:10:17 +0200

    Added command for resending a batch of actions.

---
+++ server/actionlog/management/commands/resend_action.py
@@ -0,0 +1,30 @@
+import logging
+import actionlog
+
+from django.core.management.base import BaseCommand
+from utils.command_helpers import parse_id_list
+from optparse import make_option
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+
+    option_list = BaseCommand.option_list + (
+        make_option('--ids', help='Comma separated list of action logs'),
+    )
+
+    def handle(self, *args, **options):
+        ids = parse_id_list(options, 'ids')
+
+        for action_id in ids:
+            actions = actionlog.models.ActionLog.objects.filter(pk=action_id)
+            logger.info('Sending action log %s' % action_id)
+            count_sent = 0
+            for action in actions:
+                action.state = actionlog.constants.ActionState.WAITING
+                action.save()
+                actionlog.send_multiple([action])
+                count_sent += 1
+            if count_sent == 0:
+                logger.info("Action log %s doesn't exist" % action_id)
