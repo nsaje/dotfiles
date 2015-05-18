45160b782eebfda407ec5531e9ac7b2160c96d07
Florjan Bartol <florjan.bartol@gmail.com>
2015-05-18 13:35:14 +0200

    Fixed log message

--- server/actionlog/api.py
+++ server/actionlog/api.py
@@ -527,10 +527,10 @@ def _init_fetch_reports(ad_group_source, date, order, request=None):
 
 
 def _init_set_campaign_property(ad_group_source, prop, value, order, request):
-    msg = "_init_set_campaign_property started: ad_group_source.id: {}, prop: {}, value: {}, order.id: {}".format(
+    msg = u"_init_set_campaign_property started: ad_group_source.id: {}, prop: {}, value: {}, order.id: {}".format(
         ad_group_source.id,
         prop,
-        value.encode('utf-8'),
+        value,
         order.id if order else ''
     )
     logger.info(msg)
