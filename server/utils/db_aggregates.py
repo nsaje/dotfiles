14ac155d7e6e97636e01568193d6dcd85ee3f55c
Florjan Bartol <florjan.bartol@gmail.com>
2015-05-23 12:28:50 +0200

    Migrated DB aggregates

--- server/utils/db_aggregates.py
+++ server/utils/db_aggregates.py
@@ -1,58 +1,29 @@
-from django.db.models import aggregates, sql
-
-
-class SumDivisionAggregate(sql.aggregates.Aggregate):
-    is_ordinal = False
-    is_computed = True
-    # Not used
-    sql_function = 'SUM_DIVISION'
-    sql_template = 'CASE WHEN SUM(%(divisor)s) <> 0 THEN SUM(CAST(%(field)s AS FLOAT)) / SUM(%(divisor)s) ELSE NULL END'
+from django.db.models import aggregates, FloatField
 
 
 class SumDivision(aggregates.Aggregate):
-    name = 'SumDivision'
+    function = 'SUM_DIVISION'
+    template = 'CASE WHEN SUM(%(divisor)s) <> 0 THEN SUM(CAST(%(expressions)s AS FLOAT)) / SUM(%(divisor)s) ELSE NULL END'
 
-    def __init__(self, col, divisor, **extra):
-        super(SumDivision, self).__init__(col, divisor=divisor, **extra)
-
-    def add_to_query(self, query, alias, col, source, is_summary):
-        query.aggregates[alias] = SumDivisionAggregate(
-            col, source=source, is_summary=is_summary, **self.extra)
-
-
-class IsSumDivisionNullAggregate(sql.aggregates.Aggregate):
-    is_ordinal = True
-    is_computed = False
-    # Not used
-    sql_function = 'IS_NULL'
-    sql_template = 'CASE WHEN SUM(%(divisor)s) IS NULL OR SUM(%(divisor)s) = 0 OR SUM(%(field)s) IS NULL THEN 1 ELSE 0 END'
+    def __init__(self, field, divisor, **extra):
+        super(SumDivision, self).__init__(
+            field,
+            divisor=divisor,
+            output_field=FloatField(),
+            **extra)
 
 
 class IsSumDivisionNull(aggregates.Aggregate):
-    name = 'IsSumDivisionNull'
+    function = 'IS_NULL'
+    template = 'CASE WHEN SUM(%(divisor)s) IS NULL OR SUM(%(divisor)s) = 0 OR SUM(%(expressions)s) IS NULL THEN 1 ELSE 0 END'
 
     def __init__(self, col, divisor, **extra):
         super(IsSumDivisionNull, self).__init__(col, divisor=divisor, **extra)
 
-    def add_to_query(self, query, alias, col, source, is_summary):
-        query.aggregates[alias] = IsSumDivisionNullAggregate(
-            col, source=source, is_summary=is_summary, **self.extra)
-
-
-class IsSumNullAggregate(sql.aggregates.Aggregate):
-    is_ordinal = True
-    is_computed = False
-    # Not used
-    sql_function = 'IS_NULL'
-    sql_template = 'CASE WHEN SUM(%(field)s) IS NULL THEN 1 ELSE 0 END'
-
 
 class IsSumNull(aggregates.Aggregate):
-    name = 'IsSumNull'
+    function = 'IS_NULL'
+    template = 'CASE WHEN SUM(%(expressions)s) IS NULL THEN 1 ELSE 0 END'
 
     def __init__(self, col, **extra):
         super(IsSumNull, self).__init__(col, **extra)
-
-    def add_to_query(self, query, alias, col, source, is_summary):
-        query.aggregates[alias] = IsSumNullAggregate(
-            col, source=source, is_summary=is_summary, **self.extra)
