74fb0fde6e3bff6e53c9ab8d4fd6780cceb74468
Florjan Bartol <florjan.bartol@gmail.com>
2015-05-23 12:27:26 +0200

    Migrated test runner and removed obsolete test decorators

--- server/utils/test_runner.py
+++ server/utils/test_runner.py
@@ -1,73 +1,32 @@
 import os
 import logging
-import time
-from optparse import make_option
 
 from xmlrunner.extra.djangotestrunner import XMLTestRunner
 
 from django.test import runner
-from django.conf import settings
 
 
 class SplitTestsRunner(runner.DiscoverRunner):
-    option_list = runner.DiscoverRunner.option_list + (
-        make_option(
+    @classmethod
+    def add_arguments(cls, parser):
+        super(SplitTestsRunner, cls).add_arguments(parser)
+
+        parser.add_argument(
             '--integration-tests',
             action='store_true',
             dest='integration_tests',
             default=False,
             help='Run integration tests.'
-        ),
-        make_option(
-            '--ui-tests',
-            action='store_true',
-            dest='ui_tests',
-            default=False,
-            help='Run UI tests.'
-        ),
-        make_option(
-            '--health-check',
-            action='store_true',
-            dest='health_check',
-            default=False,
-            help='Run health check tests in production during test execution.'
-        ),
-    )
+        )
 
-    def __init__(self, integration_tests=None, ui_tests=None, health_check=None, *args, **kwargs):
-        self.skip_db = False
-
+    def __init__(self, integration_tests=None, *args, **kwargs):
         logging.disable(logging.CRITICAL)
 
         if integration_tests:
             os.environ['INTEGRATION_TESTS'] = '1'
 
-        if ui_tests:
-            os.environ['UI_TESTS'] = '1'
-
-        if health_check:
-            self.skip_db = True
-            os.environ['HEALTH_CHECK'] = '1'
-
         super(SplitTestsRunner, self).__init__(*args, **kwargs)
-
-    def setup_databases(self, **kwargs):
-        if not self.skip_db:
-            return super(SplitTestsRunner, self).setup_databases(**kwargs)
-
-    def teardown_databases(self, old_config, **kwargs):
-        if not self.skip_db:
-            return super(SplitTestsRunner, self).teardown_databases(old_config, **kwargs)
 
 
 class CustomRunner(XMLTestRunner, SplitTestsRunner):
     pass
-
-
-
-
-
-
-
-
-
