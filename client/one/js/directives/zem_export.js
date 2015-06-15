8afa635a3af12652ba10cc854895234b45aea9df
Florjan Bartol <florjan.bartol@gmail.com>
2015-06-15 13:58:04 +0200

    Fixed export dropdown width in some browsers

--- client/one/js/directives/zem_export.js
+++ client/one/js/directives/zem_export.js
@@ -70,7 +70,7 @@ oneApp.directive('zemExport', function() {
 
                     return results;
                 },
-                width: 'resolve'
+                width: '11em'
             };
 
             $scope.downloadReport = function() {
