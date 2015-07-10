c882f0dd59ce44cc3000f3992c4a42065e46f006
Luka Šilovinac <luka.silovinac@zemanta.com>
2015-07-10 10:59:43 +0200

    Update newrelic metric name according to docs

--- server/dash/views/table.py
+++ server/dash/views/table.py
@@ -671,7 +671,7 @@ class SourcesTable(api_common.BaseApiView):
             ad_group_level=False):
         rows = []
         for i, source in enumerate(sources):
-            newrelic.agent.record_custom_metric('get_rows loop', i)
+            newrelic.agent.record_custom_metric('Custom/GetRowsLoop', i)
             states = [s for s in sources_states if s.ad_group_source.source_id == source.id]
 
             source_settings = None
