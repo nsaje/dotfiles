96dd4fcb9459af49bdaf4babeea576e36f342cfb
Matic Žgur <mr.zgur@gmail.com>
2015-07-14 14:40:46 +0200

    Reports: removed tests from command.

--- server/reports/management/commands/tests/test_send_supply_report_emails.py
+++
@@ -1,8 +0,0 @@
-from django import test
-
-
-class SupplyReportEmailTestCase(test.TestCase):
-    fixtures = ['test_reports_base.yaml', 'test_article_stats.yaml']
-
-    def setUp(self):
-        refresh.refresh_adgroup_stats()
