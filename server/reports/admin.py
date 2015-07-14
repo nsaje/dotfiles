94b65fcb6ae8e836f9ff1192d20e6fd483c9fb35
Matic Žgur <mr.zgur@gmail.com>
2015-07-14 14:38:22 +0200

    Reports: added supply report e-mails.

--- server/reports/admin.py
+++ server/reports/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+import models
+
+
+class SupplyReportRecipientAdmin(admin.ModelAdmin):
+    search_fields = ['first_name', 'last_name', 'email', 'source']
+    list_display = (
+        'email',
+        'first_name',
+        'last_name',
+        'source',
+        'created_dt',
+        'modified_dt',
+    )
+    readonly_fields = ('created_dt', 'modified_dt')
+
+admin.site.register(models.SupplyReportRecipient, SupplyReportRecipientAdmin)
---
+++ server/reports/management/commands/send_supply_report_emails.py
@@ -0,0 +1,56 @@
+import datetime
+import logging
+
+import pytz
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+import reports.models
+import reports.api
+import utils.email_helper
+
+logger = logging.getLogger(__name__)
+
+
+email_subject = 'Zemanta Daily Stats'
+email_body = '''
+Hello,
+
+Statistics for %(date)s.
+
+Impressions: %(impressions)d
+Spend: %(cost)s
+
+Best regards,
+Zemanta
+'''
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        today_utc = pytz.UTC.localize(datetime.datetime.utcnow())
+        today = today_utc.astimezone(pytz.timezone(settings.DEFAULT_TIME_ZONE)).replace(tzinfo=None)
+        today = datetime.datetime(today.year, today.month, today.day)
+        yesterday = today - datetime.timedelta(days=1)
+
+        recipients = reports.models.SupplyReportRecipient.objects.all()
+        if len(recipients) == 0:
+            print('No recipients.')
+            return
+
+        source_ids = [recipient.source.pk for recipient in recipients]
+        stats = reports.api.query(yesterday, yesterday, ['source'], source=source_ids)
+
+        source_stats = {stat['source']: {'impressions': stat['impressions'], 'cost': stat['cost']} for stat in stats}
+
+        for recipient in recipients:
+            if recipient.source.pk not in source_stats:
+                continue
+
+            utils.email_helper.send_supply_report_email(
+                    recipient.email,
+                    yesterday,
+                    source_stats[recipient.source.pk]['impressions'],
+                    source_stats[recipient.source.pk]['cost']
+            )
