6302f581f0be7ec691a075ea25124f4a37280f35
Simon Mihevc <simon.mihevc@zemanta.com>
2015-08-11 10:54:20 +0200

    Fixed action list command

--- server/actionlog/management/commands/list_action.py
+++ server/actionlog/management/commands/list_action.py
@@ -13,8 +13,8 @@ class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option('--state', help='Action state.'),
         make_option('--name', help='Action name filter.'),
-        make_option('--limit', help='How many action log entries to return.'),
-        make_option('--verbose', help='Write out as much information as possible.', type=bool),
+        make_option('--limit', help='How many action log entries to return.', type="int"),
+        make_option('--verbose', help='Write out as much information as possible.', action='store_true'),
         make_option('--ids', help='Comma separated list of action log ids'),
     )
 
@@ -23,9 +23,9 @@ class Command(BaseCommand):
         try:
             state = int(options['state']) if options['state'] is not None else None
             name = options['name']
-            limit = max(int(options.get('limit', 100)), 1)
+            limit = max(int(options.get('limit', 100)), 1) if options['limit'] is not None else 100
             verbose = bool(options.get('verbose', False))
-            ids = parse_id_list(options, 'ids')
+            ids = parse_id_list(options, 'ids') if options['ids'] is not None else []
         except:
             logging.exception("Failed parsing command line arguments")
             sys.exit(1)
@@ -36,8 +36,8 @@ class Command(BaseCommand):
             actions = []
         actions_with_ids = actionlog.models.ActionLog.objects.filter(id__in=ids)[:]
 
-        for action in actions + actions_with_ids:
+        for action in [a for a in actions] + [a for a in actions_with_ids]:
             if verbose:
-                print('{id}\t{payload}\t{created}'.format(id=action.id, payload=action.payload, created=action.created_dt.isoformat()))
+                print('{id}\t{state}\t{payload}\t{created}'.format(id=action.id, state=action.state, payload=action.payload, created=action.created_dt.isoformat()))
             else:
                 print(action.id)
