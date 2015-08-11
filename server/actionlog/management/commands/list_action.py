3fa4ab4eaab099c7dd905f0851fb9680d90fa453
Simon Mihevc <simon.mihevc@zemanta.com>
2015-08-11 10:01:50 +0200

    Script improvements.

--- server/actionlog/management/commands/list_action.py
+++ server/actionlog/management/commands/list_action.py
@@ -3,6 +3,7 @@ import logging
 import actionlog
 
 from optparse import make_option
+from utils.command_helpers import parse_id_list
 from django.core.management.base import BaseCommand
 
 logger = logging.getLogger(__name__)
@@ -13,22 +14,29 @@ class Command(BaseCommand):
         make_option('--state', help='Action state.'),
         make_option('--name', help='Action name filter.'),
         make_option('--limit', help='How many action log entries to return.'),
-        make_option('--verbose', help='Write out as much information as possible.'),
+        make_option('--verbose', help='Write out as much information as possible.', type=bool),
+        make_option('--ids', help='Comma separated list of action log ids'),
     )
 
     def handle(self, *args, **options):
 
         try:
-            state = int(options['state'])
+            state = int(options['state']) if options['state'] is not None else None
             name = options['name']
             limit = max(int(options.get('limit', 100)), 1)
             verbose = bool(options.get('verbose', False))
+            ids = parse_id_list(options, 'ids')
         except:
             logging.exception("Failed parsing command line arguments")
             sys.exit(1)
 
-        actions = actionlog.models.ActionLog.objects.filter(state=state, action=name).order_by('-created_dt')[:limit]
-        for action in actions:
+        if state is not None:
+            actions = actionlog.models.ActionLog.objects.filter(state=state, action=name).order_by('-created_dt')[:limit]
+        else:
+            actions = []
+        actions_with_ids = actionlog.models.ActionLog.objects.filter(id__in=ids)[:]
+
+        for action in actions + actions_with_ids:
             if verbose:
                 print('{id}\t{payload}\t{created}'.format(id=action.id, payload=action.payload, created=action.created_dt.isoformat()))
             else:
