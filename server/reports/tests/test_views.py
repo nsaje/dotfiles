d6f4630a98828b8ce759b81e3cfee7d0aa4d25e2
Simon Mihevc <simon.mihevc@zemanta.com>
2015-08-12 16:37:57 +0200

    API endpoint created

--- server/reports/tests/test_views.py
+++ server/reports/tests/test_views.py
@@ -9,15 +9,15 @@ from zemauth.models import User
 class GaContentAdReportTest(test.TestCase):
     fixtures = ['test_reports_base.yaml', 'test_article_stats.yaml']
 
-    sample_data = json.loads("""[
+    sample_data = [
         {
-            "content_ad_id": null,
+            "content_ad_id": 1000,
             "ga_report": {
                 "% New Sessions": "96.02%",
                 "Avg. Session Duration": "00:00:12",
                 "Bounce Rate": "92.41%",
                 "Device Category": "mobile",
-                "Landing Page": "/lasko?_z1_adgid=1&_z1_msid=lasko",
+                "Landing Page": "/lasko?_z1_caid=1000&_z1_msid=lasko",
                 "New Users": "531",
                 "Pages / Session": "1.12",
                 "Sessions": "553",
@@ -47,13 +47,13 @@ class GaContentAdReportTest(test.TestCase):
             "report_date": "2015-04-16T00:00:00",
             "source_param": "lasko"
         }
-    ]""")
+    ]
 
     def setUp(self):
         self.rf = test.RequestFactory()
 
     def test_post(self):
-        request = self.rf.post('reports/ga/contentadstats', data=GaContentAdReportTest.sample_data, content_type='application/json')
+        request = self.rf.post('reports/ga/contentadstats', data=json.dumps(GaContentAdReportTest.sample_data), content_type='application/json')
         request.user = User.objects.get(pk=1)
         content_ad_report = views.GaContentAdReport()
         response = content_ad_report.post(request)
--- server/reports/views.py
+++ server/reports/views.py
@@ -1,7 +1,15 @@
+import datetime
+import json
+import logging
+
 from utils import api_common
 from utils import exc
 from utils import statsd_helper
 
+import dash.models
+import reports.models
+
+logger = logging.getLogger(__name__)
 
 class GaContentAdReport(api_common.BaseApiView):
     @statsd_helper.statsd_timer('reports.api', 'gacontentadreport_post')
@@ -9,7 +17,58 @@ class GaContentAdReport(api_common.BaseApiView):
         if not request.user.has_perm('zemauth.contentadstats'):
             raise exc.AuthorizationError()
 
+        # assuming objects are well formed here
 
+        # get all sources and their corresponding slugs
+        # construct a dict from source tracking param to it's id
+        sources = dash.models.Source.objects.all()
+        track_source_map = {}
+        for source in sources:
+            track_source_map[source.tracking_slug] = source.id
 
+        created_dt = datetime.datetime.utcnow()
+
+        bulk_objects = []
+        print type(request.body)
+        print request.body
+        try:
+            data_blob = json.loads(request.body)
+        except:
+            logger.exception("Failed parsing request body")
+            raise
+
+        for entry in data_blob:
+            print entry
+            ga_entry = entry['ga_report']
+            try:
+                report_date = datetime.datetime.strptime(entry['report_date'], "%Y-%m-%dT%H:%M:%S")
+
+                visits = int(ga_report['Sessions'])
+                bounce_rate = float(entry['Bounce Rate'].replace('%', '').replace(',', '')) / 100
+
+                stats = reports.models.ContentAdPostclickStats(
+                    date=report_date,
+                    created_dt=created_dt,
+                    visits=visits,
+                    new_visits=int(entry['New Users']),
+                    bounced_visits=int(bounce_rate * visits),
+                    pageviews=int(round(float(ga_entry['Pages / Session']) * visits)),
+                    total_time_on_site=visits * _parse_duration(ga_entry['Avg. Session Duration']),
+                )
+                stats.source_id = track_source_map[entry['source_param']]
+                stats.content_ad_id = int(entry['content_ad_id'])
+                bulk_objects.append(stats)
+            except:
+                logging.exception("Failed parsing content ad {blob}".format(blob=json.dumps(entry)))
+                pass
+
+        # TODO: setup transaction
+        reports.models.ContentAdPostclickStats.objects.bulk_create(bulk_objects)
 
         return self.create_api_response({})
+
+
+def _parse_duration(self, durstr):
+    hours_str, minutes_str, seconds_str = durstr.replace('<', '').split(':')
+    return int(seconds_str) + 60 * int(minutes_str) + 60 * 60 * int(hours_str)
+
