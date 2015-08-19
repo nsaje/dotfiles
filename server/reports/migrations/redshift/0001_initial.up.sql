6352f5a0aace0a8d9177028d9495e90b144f9816
Florjan Bartol <florjan.bartol@gmail.com>
2015-08-19 18:32:34 +0200

    Added initial migration/sql script

---
+++ server/reports/migrations/redshift/0001_initial.up.sql
@@ -0,0 +1,25 @@
+CREATE TABLE contentadstats (
+    id integer IDENTITY PRIMARY KEY,
+    datetime timestamp,
+    content_ad_id integer,
+    adgroup_id integer,
+    source_id integer,
+    campaign_id integer,
+    account_id integer,
+
+    impressions integer,
+    clicks integer,
+    cost_cc integer,
+    data_cost_cc integer,
+
+    visits integer,
+    new_visits integer,
+    bounced_visits integer,
+    pageviews integer,
+    total_time_on_site integer,
+
+    conversions varchar(256),
+    touchpoints varchar(256)
+)
+DISTSTYLE EVEN
+SORTKEY (datetime);
