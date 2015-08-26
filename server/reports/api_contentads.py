c1342d017c6c91fc4e763b785b6381de19f6f8dc
Simon Mihevc <simon.mihevc@zemanta.com>
2015-08-26 17:36:23 +0200

    Fixed tests and aggregation query

--- server/reports/api_contentads.py
+++ server/reports/api_contentads.py
@@ -8,7 +8,7 @@ from utils import exc
 import dash.models
 import reports.models
 
-from django.db import transaction
+from django.db import connections, transaction
 from reports import models
 from reports import aggregate_fields
 from reports import api_helpers
@@ -31,6 +31,76 @@ def query(start_date, end_date, breakdown=None, **constraints):
     stats = stats.aggregate(**aggregate_fields.AGGREGATE_FIELDS)
 
     return _transform_row(stats)
+
+
+def _sumdiv(expr, divisor, name):
+    return "CASE WHEN SUM({divis}) <> 0 THEN SUM(CAST({expr} AS FLOAT)) / SUM({divis}) ELSE NULL END as {name}".format(
+        expr=expr,
+        divis=divisor,
+        name=name
+    )
+
+
+# TODO: queries will reside here for now
+# they will work pretty much the same as current queries to database
+def query_redshift(start_date, end_date, breakdown=None):
+
+    proper_breakdown = None
+    if breakdown:
+        proper_breakdown = []
+        for col in breakdown:
+            if col == 'date':
+                proper_breakdown.append('datetime')
+            elif col == 'content_ad':
+                proper_breakdown.append('content_ad_id')
+            elif col == 'source':
+                proper_breakdown.append('source_id')
+            elif col == 'campaign':
+                proper_breakdown.append('campaign_id')
+            elif col == 'account':
+                proper_breakdown.append('account_id')
+            elif col == 'ad_group':
+                proper_breakdown.append('adgroup_id')
+            else:
+                proper_breakdown.append(col)
+
+
+    aggregate_columns = ','.join(proper_breakdown or [])
+    if aggregate_columns != '':
+        aggregate_columns = ",{ex}".format(ex=aggregate_columns)
+    if aggregate_columns != '':
+        group_by_clause = 'GROUP BY content_ad_id {cols}'.format(cols=aggregate_columns)
+    else:
+        group_by_clause = ''
+
+    postclick_columns = ','.join([
+        "SUM(visits) AS visits_sum",
+        "SUM(new_visits) AS new_visits_sum",
+        "SUM(pageviews) AS pageviews_sum",
+        _sumdiv('new_visits', 'visits', 'percent_new_users'),
+        _sumdiv('bounced_visits', 'visits', 'bounce_rate'),
+        _sumdiv('pageviews', 'visits', 'pv_per_visit'),
+        _sumdiv('total_time_on_site', 'visits', 'avg_tos')
+    ])
+    if postclick_columns != '':
+        postclick_columns = ',{ex}'.format(ex=postclick_columns)
+
+    query = "SELECT content_ad_id {postclick_cols} FROM contentadstats WHERE \
+        datetime >= '{date_from}' AND datetime <= '{date_to}' \
+        {group_clause}".format(
+            postclick_cols=postclick_columns,
+            date_from=start_date,
+            date_to=end_date,
+            group_clause=group_by_clause
+        )
+
+    db = connections['redshift']
+    if not db:
+        raise Exception('Redshift DB not configured')
+    cursor = db.cursor()
+    cursor.execute(query)
+
+    return cursor.fetchall()
 
 
 def _preprocess_breakdown(breakdown):
