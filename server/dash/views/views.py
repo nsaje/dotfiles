5f90587c21b9d855d19903887c693e449b36db8d
Luka Šilovinac <luka.silovinac@zemanta.com>
2015-09-04 18:08:35 +0200

    Add a csrft exempt for sharethrough

--- server/dash/views/views.py
+++ server/dash/views/views.py
@@ -23,6 +23,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
 from django.views.decorators.http import require_GET
+from django.views.decorators.csrf import csrf_exempt
 
 from dash.views import helpers
 
@@ -1039,6 +1040,7 @@ def oauth_redirect(request, source_name):
 
 
 @statsd_helper.statsd_timer('dash', 'sharethrough_approval')
+@csrf_exempt
 def sharethrough_approval(request):
     data = json.loads(request.body)
 
