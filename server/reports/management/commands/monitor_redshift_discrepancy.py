84072b35dc003eee319e205a770c3f19aed37a34
Florjan Bartol <florjan.bartol@gmail.com>
2015-09-04 14:45:25 +0200

    Changed metric names

--- server/reports/management/commands/monitor_redshift_discrepancy.py
+++ server/reports/management/commands/monitor_redshift_discrepancy.py
@@ -10,15 +10,15 @@ from utils.statsd_helper import statsd_gauge
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        n_impressions_contentads = reports.models.ContentAdStats.objects\
+        n_impressions = reports.models.ContentAdStats.objects\
             .aggregate(impressions=Sum('impressions')).get('impressions') or 0
-        n_visits_contentads = reports.models.ContentAdPostclickStats.objects\
+        n_visits = reports.models.ContentAdPostclickStats.objects\
             .aggregate(impressions=Sum('visits')).get('visits') or 0
 
-        contentads_redshift = redshift.sum_contentadstats()
+        redshift_sums = redshift.sum_contentadstats()
 
-        statsd_gauge('reports.impressions_contentads_total', n_impressions_contentads)
-        statsd_gauge('reports.impressions_contentads_total_aggr', contentads_redshift['impressions'])
+        statsd_gauge('reports.contentadstats.impressions_total', n_impressions)
+        statsd_gauge('reports.contentadstats.impressions_total_aggr', redshift_sums['impressions'])
 
-        statsd_gauge('reports.visits_contentads_total', n_visits_contentads)
-        statsd_gauge('reports.visits_contentads_total_aggr', contentads_redshift['visits'])
+        statsd_gauge('reports.contentadstats.visits_contentads_total', n_visits)
+        statsd_gauge('reports.contentadstats.visits_contentads_total_aggr', redshift_sums['visits'])
