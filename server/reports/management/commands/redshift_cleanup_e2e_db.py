6fddb02f5f5e8378e4da1ae001b2ed83c4ef35ca
Gregor Ratajc <gregor.ratajc@zemanta.com>
2015-09-09 09:48:47 +0200

    Replaces logger.debug with logger.info when running redshift management commands

--- server/reports/management/commands/redshift_cleanup_e2e_db.py
+++ server/reports/management/commands/redshift_cleanup_e2e_db.py
@@ -11,15 +11,15 @@ logger = logging.getLogger(__name__)
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        logger.debug('Cleaning up Redshift E2E DB')
+        logger.info('Cleaning up Redshift E2E DB')
 
-        logger.debug('Connecting to Redshift DB {}::{}'.format(settings.STATS_E2E_DB_NAME,
+        logger.info('Connecting to Redshift DB {}::{}'.format(settings.STATS_E2E_DB_NAME,
                                                                settings.DATABASES[settings.STATS_E2E_DB_NAME]['NAME']))
 
         cursor = connections[settings.STATS_E2E_DB_NAME].cursor()
-        logger.debug('Connected successfully. Droping the temporary e2e database.')
+        logger.info('Connected successfully. Droping the temporary e2e database.')
 
         cursor.execute('DROP DATABASE {}'.format(settings.DATABASES[settings.STATS_DB_NAME]['NAME']))
         cursor.close()
 
-        logger.debug('Database successfully dropped. All done.')
+        logger.info('Database successfully dropped. All done.')
--- server/reports/management/commands/redshift_create_e2e_db.py
+++ server/reports/management/commands/redshift_create_e2e_db.py
@@ -12,35 +12,35 @@ logger = logging.getLogger(__name__)
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        logger.debug('Creating a new Redshift DB')
+        logger.info('Creating a new Redshift DB')
 
         migrations_dir = os.path.join(os.path.dirname(__file__), '../../migrations/redshift')
 
         # sort migrations alphabetically
         migration_files = sorted(os.listdir(migrations_dir))
-        logger.debug('Found {} migrations in {}'.format(len(migration_files), migrations_dir))
+        logger.info('Found {} migrations in {}'.format(len(migration_files), migrations_dir))
 
         # create redshift db
-        logger.debug('Connecting to Redshift DB {}::{}'.format(settings.STATS_E2E_DB_NAME,
-                                                               settings.DATABASES[settings.STATS_E2E_DB_NAME]['NAME']))
+        logger.info('Connecting to Redshift DB {}::{}'.format(settings.STATS_E2E_DB_NAME,
+                                                              settings.DATABASES[settings.STATS_E2E_DB_NAME]['NAME']))
 
         cursor = connections[settings.STATS_E2E_DB_NAME].cursor()
-        logger.debug('Connected successfully')
+        logger.info('Connected successfully')
 
         new_db_settings = settings.DATABASES[settings.STATS_DB_NAME]
-        logger.debug('Creating database {}'.format(new_db_settings['NAME']))
+        logger.info('Creating database {}'.format(new_db_settings['NAME']))
         cursor.execute('CREATE DATABASE {} WITH OWNER {}'.format(new_db_settings['NAME'],
                                                                  new_db_settings['USER']))
         cursor.close()
-        logger.debug('Database created. Connecting to the newly created database.')
+        logger.info('Database created. Connecting to the newly created database.')
 
         cursor = connections[settings.STATS_DB_NAME].cursor()
 
-        logger.debug('Applying migrations')
+        logger.info('Applying migrations')
         for i, mf in enumerate(migration_files):
             with open(os.path.join(migrations_dir, mf), 'r') as f:
-                logger.debug('Applying migration {}/{} {}'.format(i, len(migration_files), mf))
+                logger.info('Applying migration {}/{} {}'.format(i, len(migration_files), mf))
                 cursor.execute(f.read())
 
         cursor.close()
-        logger.debug('New database created and initialized')
+        logger.info('New database created and initialized')
