6b09d0c944f74b69946b0f328cdd5177143bc18f
Tomaz Kovacic <tomaz.kovacic@zemanta.com>
2015-09-10 17:06:05 +0200

    bugfix when permission is not available

--- client/one/js/services/zem_intercom_service.js
+++ client/one/js/services/zem_intercom_service.js
@@ -5,6 +5,10 @@ oneApp.factory('zemIntercomService', ['$window', function($window) {
     var INTERCOM_APP_ID = 'anyekw96';
 
     function boot(user) {
+        if ($window.Intercom === undefined) {
+            return;
+        }
+
         $window.Intercom('boot',{
               app_id: INTERCOM_APP_ID,
               name: user.name,
