9e4f033651f0c9701c76b90dcc7200527d80b9fd
Tomaz Kovacic <tomaz.kovacic@zemanta.com>
2015-09-10 17:42:36 +0200

    another bugfix for intercom

--- server/zemauth/models.py
+++ server/zemauth/models.py
@@ -187,8 +187,7 @@ class User(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
 
             public_permissions_ids = [x.pk for x in public_permissions]
 
-            permissions = {'{}.{}'.format(x.content_type.app_label, x.codename): x.pk
-                           in public_permissions_ids for x in perms}
+            permissions = {'{}.{}'.format(x.content_type.app_label, x.codename): x.pk in public_permissions_ids for x in perms}
 
             setattr(self, perm_cache_name, permissions)
 
