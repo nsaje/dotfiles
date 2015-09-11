3cea5f4c2105123d679b6547c289acf20530595a
davorink <davorin.kopic@zemanta.com>
2015-09-11 15:44:01 +0200

    Added migrations

---
+++ server/dash/migrations/0073_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('dash', '0072_auto_20150911_1004'),
+        ('dash', '0066_adgroupsourcesettings_autopilot'),
+    ]
+
+    operations = [
+    ]
