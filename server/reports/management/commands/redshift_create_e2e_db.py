2f827e53d98e3afc7f2ee717538fc0e8133ff363
Gregor Ratajc <gregor.ratajc@zemanta.com>
2015-09-14 14:30:40 +0200

    Update description

--- server/reports/management/commands/redshift_create_e2e_db.py
+++ server/reports/management/commands/redshift_create_e2e_db.py
@@ -11,10 +11,7 @@ logger = logging.getLogger(__name__)
 """
 This command creates a new database for end-to-end tests. The new database is created
 from the STATS_E2E_DB_NAME database connection and is created with parameters that
-are set for the STATS_DB_NAME database.
-
-If the new database is set to be owned by some other user than the STATS_E2E_DB_NAME database,
-then the STATS_E2E_DB_NAME user needs to have the "createuser" privilege.
+are at this point set for the STATS_DB_NAME database.
 """
 
 
