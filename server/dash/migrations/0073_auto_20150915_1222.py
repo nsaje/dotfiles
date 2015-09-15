5ebcd7ccf417053e92b2d68df55f0851615dc777
Gregor Ratajc <gregor.ratajc@zemanta.com>
2015-09-15 14:23:45 +0200

    Merges migrations

--- server/dash/migrations/0073_auto_20150915_1222.py
+++ server/dash/migrations/0073_auto_20150915_1222.py
@@ -7,10 +7,15 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dash', '0071_remove_batch_fields'),
+        ('dash', '0072_auto_20150911_1004'),
     ]
 
     operations = [
+        migrations.AddField(
+            model_name='defaultsourcesettings',
+            name='auto_add',
+            field=models.BooleanField(default=False, verbose_name=b'Automatically add this source to ad group at creation'),
+        ),
         migrations.AddField(
             model_name='defaultsourcesettings',
             name='daily_budget_cc',
@@ -18,12 +23,12 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name='defaultsourcesettings',
-            name='desktop_cpc_cc',
-            field=models.DecimalField(null=True, verbose_name=b'Default CPC (desktop/default)', max_digits=10, decimal_places=4, blank=True),
+            name='default_cpc_cc',
+            field=models.DecimalField(null=True, verbose_name=b'Default CPC', max_digits=10, decimal_places=4, blank=True),
         ),
         migrations.AddField(
             model_name='defaultsourcesettings',
             name='mobile_cpc_cc',
-            field=models.DecimalField(null=True, verbose_name=b'Default CPC (if mobile)', max_digits=10, decimal_places=4, blank=True),
+            field=models.DecimalField(null=True, verbose_name=b'Default CPC (if ad group is targeting mobile only)', max_digits=10, decimal_places=4, blank=True),
         ),
     ]
