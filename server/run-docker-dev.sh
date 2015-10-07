4588b9d2ad6658414aed665723890c5b4a9ae0bc
marko <marko.celan@zemanta.com>
2015-10-07 13:33:33 +0200

    docker: minor changes + default admin user created from fixtures (user/pass = dev@zemanta.com/admin)

--- server/run-docker-dev.sh
+++ server/run-docker-dev.sh
@@ -3,10 +3,10 @@
 echo "Wait for PostgreSQL"
 retval=0
 while [[ "$retval" != "52" ]]; do
-    curl -q http://${DB_PORT_5432_TCP_ADDR}:${DB_PORT_5432_TCP_PORT}/
+    curl -q http://${DB_PORT_5432_TCP_ADDR}:${DB_PORT_5432_TCP_PORT}/ 2> /dev/null
     retval=$?
     echo -n "."
-    sleep 0.5
+    sleep 1
 done
 echo "PostgreSQL opened port"
 
