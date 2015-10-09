5b738e2184978a29ef07d36dc118a08f770bc1cf
Simon Mihevc <simon.mihevc@zemanta.com>
2015-10-09 11:58:41 +0200

    Fixed command, fixed issues with parsing specific onstar reports that contani div/0 cells

--- server/convapi/management/commands/reprocess_report.py
+++ server/convapi/management/commands/reprocess_report.py
@@ -12,6 +12,7 @@ from reports import api_contentads
 from reports import constants
 from django.core.management.base import BaseCommand
 from optparse import make_option
+from utils.csv_utils import convert_to_xls
 
 logger = logging.getLogger(__name__)
 
@@ -20,6 +21,7 @@ class Command(BaseCommand):
 
     option_list = BaseCommand.option_list + (
         make_option('-f', '--filename', help='file', dest='filename'),
+        make_option('-t', '--report_type', help='Report type - omniture or ga', dest='report_type'),
     )
 
     def handle(self, *args, **options):
@@ -28,6 +30,7 @@ class Command(BaseCommand):
 
         try:
             filename = options['filename']
+            report_type = options['report_type']
         except:
             logger.exception("Failed parsing command line arguments")
             sys.exit(1)
@@ -40,10 +43,25 @@ class Command(BaseCommand):
             with open(filename) as f:
                 print("Processing file {}".format(filename))
                 try:
-                    csvreport = parse_v2.CsvReport(f.read())
+                    content = f.read()
+
+                    magic_string = '\xef\xbb\xbf'
+                    start_of_content = content[:3]
+                    if start_of_content == magic_string:
+                        content = content[len(magic_string):]
+
+                    if report_type == 'ga':
+                        report = parse_v2.GAReport(content)
+                    elif report_type == 'omniture':
+                        if filename.endswith('.csv'):
+                            content = convert_to_xls(content)
+                        report = parse_v2.OmnitureReport(content)
+                    else:
+                        raise Exception('Unknown report type {}'.format(report_type))
+
                     # parse will throw exceptions in case of errors
-                    csvreport.parse()
-                    logger.info(csvreport.debug_parsing_overview())
+                    report.parse()
+                    report.validate()
                     # serialize report
                     # api_contentads.process_report(csvreport.entries, constants.ReportType.GOOGLE_ANALYTICS)
                 except:
