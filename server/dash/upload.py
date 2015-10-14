07aa94831b7f3cf8ca184e52fadbfc1aec258b75
Gregor Ratajc <gregor.ratajc@zemanta.com>
2015-10-14 15:53:48 +0200

    Adds a bit of logging to upload content ads

--- server/dash/upload.py
+++ server/dash/upload.py
@@ -56,6 +56,7 @@ def _process_callback(batch, ad_group, ad_group_sources, filename, request, resu
             all_content_ad_sources = []
             num_errors = 0
 
+            logger.info('Inserting uploaded content ads for batch %s', batch)
             for row, cleaned_data, errors in results:
                 if not errors:
                     content_ad, content_ad_sources = _create_objects(
@@ -76,6 +77,7 @@ def _process_callback(batch, ad_group, ad_group_sources, filename, request, resu
                 # raise exception to rollback transaction
                 raise UploadFailedException()
 
+            logger.info('Submitting uploaded content ads for batch %s', batch)
             actions = api.submit_content_ads(all_content_ad_sources, request)
 
             batch.status = constants.UploadBatchStatus.DONE
@@ -95,6 +97,7 @@ def _process_callback(batch, ad_group, ad_group_sources, filename, request, resu
         batch.save()
         return
 
+    logger.info('Sending uploaded content ads for batch %s', batch)
     actionlog.zwei_actions.send(actions)
 
 
