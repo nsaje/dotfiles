9b263b2ae27a4806c0958c93d6d3eba5d26689cc
Luka Šilovinac <luka.silovinac@zemanta.com>
2015-10-19 15:49:24 +0200

    Fix redirector helper

--- server/utils/redirector_helper.py
+++ server/utils/redirector_helper.py
@@ -58,7 +58,7 @@ def fetch_redirects_impressions(date, account_id=None, timeout=300):
     url = settings.R1_CONVERSION_STATS_URL
     if account_id:
         url = url + '?' + urllib.urlencode({'account': account_id})
-    job_id = _call_api_retry(settings.R1_CONVERSION_STATS_URL.format(date=date.strftime('%Y-%m-%d')), method='GET')
+    job_id = _call_api_retry(url.format(date=date.strftime('%Y-%m-%d')), method='GET')
 
     start_time = time.time()
     while (time.time() - start_time) < timeout:
