c330817a245a0f7575987a7c9b99a3c016b87be2
Luka Šilovinac <luka.silovinac@zemanta.com>
2015-10-19 15:43:00 +0200

    Correct urllib2 handling

--- server/utils/redirector_helper.py
+++ server/utils/redirector_helper.py
@@ -55,9 +55,9 @@ def insert_adgroup(ad_group_id, tracking_codes, enable_ga_tracking, enable_adobe
 
 @statsd_helper.statsd_timer('redirector_helper', 'fetch_redirects_impressions')
 def fetch_redirects_impressions(date, account_id=None, timeout=300):
-    data = None
+    url = settings.R1_CONVERSION_STATS_URL
     if account_id:
-        data = urllib.urlencode({'account': account_id})
+        data = url + '?' + urllib.urlencode({'account': account_id})
     job_id = _call_api_retry(settings.R1_CONVERSION_STATS_URL.format(date=date.strftime('%Y-%m-%d')),
                              data=data, method='GET')
 
