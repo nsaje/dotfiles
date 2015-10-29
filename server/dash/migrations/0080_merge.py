89fb97d3d75fac50d042334a5d370b5901df27ca
Ziga Stopinsek <sigi.kajzer@gmail.com>
2015-10-29 12:12:48 +0100

    added migration

---
+++ server/dash/migrations/0080_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('dash', '0079_auto_20151014_1305'),
+        ('dash', '0079_auto_20151026_1543'),
+    ]
+
+    operations = [
+    ]
