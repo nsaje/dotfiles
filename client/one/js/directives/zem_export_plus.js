716ac00db9357e547b222b73e6be3662214355ee
davorink <davorin.kopic@gmail.com>
2015-11-02 18:45:35 +0100

    Created zem_export_plus partial

---
+++ client/one/js/directives/zem_export_plus.js
@@ -0,0 +1,93 @@
+"use strict";
+
+oneApp.directive('zemExportPlus', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            disabledOptions: '=',
+            baseUrl: '=',
+            startDate: '=',
+            endDate: '=',
+            options: '=',
+            columns: '=',
+            order: '='
+        },
+        templateUrl: '/partials/zem_export_plus.html',
+        controller: ['$scope', '$window', '$compile', 'zemFilterService', function($scope, $window, $compile, zemFilterService) {
+            $scope.exportType = '';
+
+            function getOptionByValue(value) {
+                var option = null;
+                $scope.options.forEach(function(opt) {
+                    if (opt.value === value) {
+                        option = opt;
+                    }
+                });
+
+                return option;
+            }
+
+            $scope.config = {
+                minimumResultsForSearch: -1,
+                formatResult: function(object) {
+                    if (!object.disabled) {
+                        return angular.element(document.createElement('span')).text(object.text);
+                    }
+
+                    var popoverEl = angular.element(document.createElement('div'));
+                    var option = getOptionByValue(object.id);
+
+                    var popoverText = 'This report is not available for download due to the volume of content. Please contact your account manager for assistance.';
+
+                    popoverEl.attr('popover', popoverText);
+                    popoverEl.attr('popover-trigger', 'mouseenter');
+                    popoverEl.attr('popover-placement', 'right');
+                    popoverEl.attr('popover-append-to-body', 'true');
+                    popoverEl.text(object.text);
+
+                    return $compile(popoverEl)($scope);
+                },
+                sortResults: function(results) {
+                    var option = null;
+
+                    // used to set disabled property on results
+                    results = results.map(function(result) {
+                        option = getOptionByValue(result.id);
+
+                        if (option.disabled) {
+                            result.disabled = true;
+                        }
+                        return result;
+                    });
+
+                    return results;
+                },
+                width: '12em'
+            };
+
+            $scope.downloadReport = function() {
+                var url = $scope.baseUrl + 'export_plus/?type=' + $scope.exportType +
+                          '&start_date=' + $scope.startDate.format() +
+                          '&end_date=' + $scope.endDate.format() +
+                          '&order=' + $scope.order;
+
+                if (zemFilterService.isSourceFilterOn()) {
+                    url += '&filtered_sources=' + zemFilterService.getFilteredSources().join(',');
+                }
+
+                var export_columns = []
+                for (var i = 0; i < $scope.columns.length; i++) {
+                  var col = $scope.columns[i]
+                  if (col.shown && col.checked && !col.unselectable){
+                    export_columns.push(col.field)
+                  }
+                }
+                url += '&additional_fields=' + export_columns.join(',');
+
+                $window.open(url, '_blank');
+
+                $scope.exportType = '';
+            };
+        }]
+    };
+});
