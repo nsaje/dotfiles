5ed3fb9515d8a7b3fb607121fb59ddf6af3370e9
Luka Šilovinac <luka.silovinac@zemanta.com>
2015-11-03 15:09:39 +0100

    Fix a bug where chart metrics weren't set from url

--- client/one/js/services/zem_local_storage_service.js
+++ client/one/js/services/zem_local_storage_service.js
@@ -27,7 +27,7 @@ oneApp.factory('zemLocalStorageService', function() {
             getKey(key, namespace)
         );
 
-        if (!value) {
+        if (!value || typeof value === "undefinded") {
             return;
         }
 
