4ffa8e9961bd20d4164e76ba6843a7b00342e753
Luka Šilovinac <luka.silovinac@zemanta.com>
2015-11-06 17:45:02 +0100

    Bigger field for crop areas

--- server/dash/migrations/0084_contentad_crop_areas.py
+++ server/dash/migrations/0084_contentad_crop_areas.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='contentad',
             name='crop_areas',
-            field=models.CharField(max_length=64, null=True),
+            field=models.CharField(max_length=128, null=True),
         ),
     ]
