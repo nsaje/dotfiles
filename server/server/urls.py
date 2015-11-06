88694c719e5dc8287e76ca614711adfa3b6c9ffd
matematik7 <domen@ipavec.net>
2015-11-06 14:24:53 +0100

    Added crossvalidation api

--- server/server/urls.py
+++ server/server/urls.py
@@ -387,6 +387,15 @@ urlpatterns += patterns(
     )
 )
 
+# Crossvalidation Api
+urlpatterns += patterns(
+    '',
+    url(
+        r'^api/crossvalidation$',
+        reports.views.crossvalidation,
+        name='api.crossvalidation',
+    )
+)
 
 # Conversion Api
 urlpatterns += patterns(
