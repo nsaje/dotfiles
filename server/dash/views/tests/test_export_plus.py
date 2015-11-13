13934116a57e8dd718011cad4e20cb8e5cc7ef14
davorink <davorin.kopic@gmail.com>
2015-11-13 19:01:56 +0100

    Fixed test

--- server/dash/views/tests/test_export_plus.py
+++ server/dash/views/tests/test_export_plus.py
@@ -88,7 +88,7 @@ class AdGroupAdsPlusExportTestCase(AssertRowMixin, test.TestCase):
         expected_content = '''Start Date,End Date,Account,Campaign,Ad Group,Title,Image URL,URL,Average CPC,Clicks,Visits\r
 2014-06-30,2014-07-01,test account 1 \xc4\x8c\xc5\xbe\xc5\xa1,test campaign 1 \xc4\x8c\xc5\xbe\xc5\xa1,test adgroup 1 \xc4\x8c\xc5\xbe\xc5\xa1,Test Article unicode \xc4\x8c\xc5\xbe\xc5\xa1,/123456789/200x300.jpg,http://testurl.com,10.230,103,40\r
 2014-06-30,2014-07-01,test account 1 \xc4\x8c\xc5\xbe\xc5\xa1,test campaign 1 \xc4\x8c\xc5\xbe\xc5\xa1,test adgroup 1 \xc4\x8c\xc5\xbe\xc5\xa1,Test Article with no content_ad_sources 1,/123456789/200x300.jpg,http://testurl.com,20.230,203,30\r\n'''
-        filename = '{0}_{1}_{2}_report_2014-06-30_2014-07-01.csv'.format(
+        filename = '{0}_{1}_{2}_-_by_content_ad_report_2014-06-30_2014-07-01.csv'.format(
             slugify.slugify(self.account_name),
             slugify.slugify(self.campaign),
             slugify.slugify(self.ad_group_name)
