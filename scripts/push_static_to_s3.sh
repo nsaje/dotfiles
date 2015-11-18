2a9a25b792cfa696783907d81ddb0d75444ebc0b
Wessel Rossing <wessel.rossing@zemanta.com>
2015-11-18 11:50:38 +0100

    Testing working directory

--- scripts/push_static_to_s3.sh
+++ scripts/push_static_to_s3.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-echo "build-$CIRCLE_BUILD_NUM"
-echo $PWD
+sudo pip install awscli
+
+S3_PATH="s3://z1-static/build-$CIRCLE_BUILD_NUM/"
+
+echo "Syncing static files to $S3_PATH"
+echo "Syncing client/one/assets"
+
+ls client/one/assets
