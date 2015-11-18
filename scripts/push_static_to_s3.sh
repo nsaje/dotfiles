6a72dc9a05bb07184a0943730dea083df3ff5b06
Wessel Rossing <wessel.rossing@zemanta.com>
2015-11-18 14:50:59 +0100

    Pushing dist to s3 now on succesful build

--- scripts/push_static_to_s3.sh
+++ scripts/push_static_to_s3.sh
@@ -4,7 +4,4 @@ sudo pip install awscli
 
 S3_PATH="s3://z1-static/build-$CIRCLE_BUILD_NUM"
 
-echo "Syncing static files to $S3_PATH"
-
-aws s3 sync --acl public-read client/one/assets $S3_PATH/assets
-aws s3 sync --acl public-read client/one/img $S3_PATH/img
+aws s3 sync --acl public-read client/dist $S3_PATH/dist
