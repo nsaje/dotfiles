ab7a76cb854e73548d5ac0d33c78743a76ff4631
Wessel Rossing <wessel.rossing@zemanta.com>
2015-11-18 15:41:28 +0100

    Moved conditional for circle.ci parallelism from circle.yml to push_static_to_s3.sh

--- scripts/push_static_to_s3.sh
+++ scripts/push_static_to_s3.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-sudo pip install awscli
-
-S3_PATH="s3://z1-static/build-$CIRCLE_BUILD_NUM"
-
-aws s3 sync --acl public-read client/dist $S3_PATH/dist
+if [ -d client/dist ]
+then
+	sudo pip install awscli
+	
+	S3_PATH="s3://z1-static/build-$CIRCLE_BUILD_NUM"
+	
+	aws s3 sync --acl public-read client/dist $S3_PATH/dist
+fi
