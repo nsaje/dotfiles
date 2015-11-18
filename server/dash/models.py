b74b0195db9696f66d5ec4b2bdeb7550fb00e211
Tomaz Kovacic <tomaz.kovacic@zemanta.com>
2015-11-18 16:27:16 +0100

    making sure we can only edit if we have the permission

--- server/dash/models.py
+++ server/dash/models.py
@@ -753,6 +753,7 @@ class Source(models.Model):
     deprecated = models.BooleanField(default=False, null=False)
     created_dt = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
     modified_dt = models.DateTimeField(auto_now=True, verbose_name='Modified at')
+    released = models.BooleanField(default=True)
     content_ad_submission_type = models.IntegerField(
         default=constants.SourceSubmissionType.DEFAULT,
         choices=constants.SourceSubmissionType.get_choices()
