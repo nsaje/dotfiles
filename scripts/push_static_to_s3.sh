bcab89cf393c8db4c4f9498fc3e801931191a271
Wessel Rossing <wessel.rossing@zemanta.com>
2015-11-18 11:56:37 +0100

    Proof of concept with assets and img directory

--- scripts/push_static_to_s3.sh
+++ scripts/push_static_to_s3.sh
@@ -5,6 +5,6 @@ sudo pip install awscli
 S3_PATH="s3://z1-static/build-$CIRCLE_BUILD_NUM/"
 
 echo "Syncing static files to $S3_PATH"
-echo "Syncing client/one/assets"
 
-ls client/one/assets
+aws s3 sync client/one/assets $S3_PATH/assets
+aws s3 sync client/one/img $S3_PATH/img
