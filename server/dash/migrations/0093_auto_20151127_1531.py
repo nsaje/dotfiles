2651cc82c45b75c5610a989278eb9d8fb4ed3b69
Tomaz Kovacic <tomaz.kovacic@zemanta.com>
2015-11-27 17:20:17 +0100

    remove redundant comment

--- server/dash/migrations/0093_auto_20151127_1531.py
+++ server/dash/migrations/0093_auto_20151127_1531.py
@@ -3,17 +3,6 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
-'''
-def combine_names(apps, schema_editor):
-    # We can't import the Person model directly as it may be a newer
-    # version than this migration expects. We use the historical version.
-    Person = apps.get_model("yourappname", "Person")
-    for person in Person.objects.all():
-        person.name = "%s %s" % (person.first_name, person.last_name)
-        person.save()
-
-'''
-
 # looks like get_model does not return 
 # the model defined in dash/models.py
 # so I had to write my own
