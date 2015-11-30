06b85f2037d1b9249ac02151ba9abc4b8fd9202d
Simon Mihevc <simon.mihevc@zemanta.com>
2015-11-30 17:58:23 +0100

    Added some more variables to scope

--- client/one/js/directives/zem_infobox_setting.js
+++ client/one/js/directives/zem_infobox_setting.js
@@ -7,10 +7,29 @@ oneApp.directive('zemInfoboxSetting', ['config', '$window', function(config, $wi
         restrict: 'E',
         scope: {
             label: '=',
-            value: '='
+            value: '=',
+            detailsVisible: '=',
+            detailsLabel: '=',
+            detailsContent: '=',
+            icon: '='
         },
         templateUrl: '/partials/zem_infobox_setting.html',
-        controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {}]
+        controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
+        
+            $scope.icon = null;
+            $scope.detailsVisible = false;
+            $scope.detailsLabel = null;
+            $scope.detailsContent = null;
+
+            $scope.isSet = function (val) {
+                if (typeof val === "undefined") {
+                    return false;
+                }
+
+                return val !== null;
+            };
+        
+        }]
     }
 
 }]);
