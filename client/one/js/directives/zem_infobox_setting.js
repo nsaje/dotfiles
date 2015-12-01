47b5ba54a0e2852a03bd81296fedf5d09dc50b02
Simon Mihevc <simon.mihevc@zemanta.com>
2015-12-01 15:10:41 +0100

    Work in progress - elements are now sensibly positioned.

--- client/one/js/directives/zem_infobox_setting.js
+++ client/one/js/directives/zem_infobox_setting.js
@@ -8,18 +8,21 @@ oneApp.directive('zemInfoboxSetting', ['config', '$window', function(config, $wi
         scope: {
             label: '=',
             value: '=',
+            valueDescription: '=',
             detailsVisible: '=',
             detailsLabel: '=',
             detailsContent: '=',
+            tooltip: '=',
             icon: '='
         },
         templateUrl: '/partials/zem_infobox_setting.html',
         controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
         
-            $scope.icon = null;
+            $scope.icon = "happy";
             $scope.detailsVisible = false;
-            $scope.detailsLabel = null;
-            $scope.detailsContent = null;
+            $scope.detailsLabel = "Kick me";
+            $scope.detailsContent = "BAM";
+            $scope.tooltip = "Test"
 
             $scope.isSet = function (val) {
                 if (typeof val === "undefined") {
@@ -27,6 +30,14 @@ oneApp.directive('zemInfoboxSetting', ['config', '$window', function(config, $wi
                 }
 
                 return val !== null;
+            };
+
+            $scope.showDetails = function () {
+                $scope.detailsVisible = true;
+            };
+
+            $scope.hideDetails = function () {
+                $scope.detailsVisible = false;
             };
         
         }]
