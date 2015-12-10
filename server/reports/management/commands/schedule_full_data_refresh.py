c58e02c8da0cc9730f420ea95b003aa4495a8ffc
Luka Šilovinac <luka.silovinac@zemanta.com>
2015-12-10 16:53:41 +0100

    Refresh for one day more than sync

--- server/reports/management/commands/schedule_full_data_refresh.py
+++ server/reports/management/commands/schedule_full_data_refresh.py
@@ -16,5 +16,5 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         today = dates_helper.local_today()
         for campaign in dash.models.Campaign.objects.exclude_archived():
-            for days in range(settings.LAST_N_DAY_REPORTS):
+            for days in range(settings.LAST_N_DAY_REPORTS + 1):
                 reports.refresh.notify_contentadstats_change(today - datetime.timedelta(days=days), campaign.id)
