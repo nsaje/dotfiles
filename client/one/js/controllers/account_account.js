1cd057f00c43cfb3f5216c4253b076010ce24a19
Luka Šilovinac <luka.silovinac@zemanta.com>
2015-12-11 17:12:05 +0100

    Add account tab on account level

---
+++ client/one/js/controllers/account_account.js
@@ -0,0 +1,3 @@
+/*globals oneApp,constants,options,moment*/
+oneApp.controller('AccountAccountCtrl', ['$scope', '$state', '$q', '$timeout', 'api', function ($scope, $state, $q, $timeout, api) {
+}]);
