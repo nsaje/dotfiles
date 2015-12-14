467209f1460b0573ed9ba3bb56a3eac3edc78a8c
Marko Novak <marko.novak@zemanta.com>
2015-12-14 16:29:09 +0100

    Fixed the unit test that was failing.

--- server/utils/tests/test_list_helper.py
+++ server/utils/tests/test_list_helper.py
@@ -6,4 +6,4 @@ class SortHelperTestCase(unittest.TestCase):
     def test_chunk_list(self):
         sample_list = ['cat', 'dog', 'rabbit', 'duck', 'bird', 'cow', 'gnu', 'fish']
         chunked_list = list_chunker(sample_list, 3)
-        self.assertEqual(chunked_list, [['cat', 'dog', 'rabbit'], ['duck', 'bird', 'cow'], ['gnu', 'fish']])
+        self.assertEqual(list(chunked_list), [['cat', 'dog', 'rabbit'], ['duck', 'bird', 'cow'], ['gnu', 'fish']])
