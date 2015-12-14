d1eab7f18032363b6dc12662c1950a0bc380c781
Luka Šilovinac <luka.silovinac@gmail.com>
2015-12-14 15:31:23 +0100

    Update zemConversionPixelsSpec.js

--- client/test/unit/directives/zemConversionPixelsSpec.js
+++ client/test/unit/directives/zemConversionPixelsSpec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('AccountAgencyCtrl', function () {
+describe('zemConversionPixels', function () {
     var $scope, $q, element, isolate;
 
     var mockApiFunc = function() {
