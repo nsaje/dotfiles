369c89ab23ca55e32a1614e68f1946bf6f5ccd16
Tomaz Kovacic <tomaz.kovacic@zemanta.com>
2015-12-15 11:16:36 +0100

    merge migrations

---
+++ server/dash/migrations/0099_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('dash', '0098_auto_20151211_1319'),
+        ('dash', '0098_auto_20151214_1042'),
+    ]
+
+    operations = [
+    ]
