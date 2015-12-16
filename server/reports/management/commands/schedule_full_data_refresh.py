bb7b91281662313b4116a5c6e9cdb4ac11e678eb
Luka Šilovinac <luka.silovinac@zemanta.com>
2015-12-16 17:47:42 +0100

    Fix schedule full refresh cron job

--- server/reports/management/commands/schedule_full_data_refresh.py
+++ server/reports/management/commands/schedule_full_data_refresh.py
@@ -15,6 +15,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         today = dates_helper.local_today()
-        for campaign in dash.models.Campaign.objects.exclude_archived():
+        for campaign in dash.models.Campaign.objects.all().exclude_archived():
             for days in range(settings.LAST_N_DAY_REPORTS + 1):
                 reports.refresh.notify_contentadstats_change(today - datetime.timedelta(days=days), campaign.id)
