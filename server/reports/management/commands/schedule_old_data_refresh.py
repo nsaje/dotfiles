05a1748c072f8b1f51018df287cc0f95b7e261c8
Luka Šilovinac <luka.silovinac@zemanta.com>
2015-12-16 19:16:57 +0100

    Make scheduling refreshes of old data less frequent

--- server/reports/management/commands/schedule_old_data_refresh.py
+++ server/reports/management/commands/schedule_old_data_refresh.py
@@ -16,5 +16,5 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         today = dates_helper.local_today()
         for campaign in dash.models.Campaign.objects.all().exclude_archived():
-            for days in range(settings.LAST_N_DAY_REPORTS + 1):
+            for days in range(1, settings.LAST_N_DAY_REPORTS + 1):
                 reports.refresh.notify_contentadstats_change(today - datetime.timedelta(days=days), campaign.id)
