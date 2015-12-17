fd507f69c78dbed3d99c48d2df3fbb2c66658bb7
Marko Novak <marko.novak@zemanta.com>
2015-12-17 10:16:33 +0100

    Added the migration for Google Analytics tables.

---
+++ server/dash/migrations/0005_auto_20151217_0912.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('dash', '0004_auto_20151216_0950'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='GAAnalyticsAccount',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True)),
+                ('ga_account_id', models.CharField(max_length=127)),
+                ('ga_web_property_id', models.CharField(max_length=127)),
+                ('account', models.ForeignKey(to='dash.Account', on_delete=django.db.models.deletion.PROTECT)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='adgroupsettings',
+            name='ga_tracking_type',
+            field=models.IntegerField(default=1, choices=[(2, b'API'), (1, b'Email')]),
+        ),
+    ]
