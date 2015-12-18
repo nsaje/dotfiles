79a64ab8d2868c5b4da237acf1c23a4a7391e77e
Luka Šilovinac <luka.silovinac@zemanta.com>
2015-12-18 13:02:18 +0100

    Add an endpoint that returns spends pcts for all ad groups

--- server/reports/views.py
+++ server/reports/views.py
@@ -1,5 +1,4 @@
 import datetime
-import json
 import logging
 
 from django.conf import settings
@@ -10,9 +9,45 @@ import newrelic.agent
 
 from utils import request_signer
 import reports.api_contentads
+import reports.daily_statements
 import dash.models
 
 logger = logging.getLogger(__name__)
+
+
+@csrf_exempt
+def ad_group_spend_pcts(request):
+    newrelic.agent.set_background_task(flag=True)
+
+    try:
+        request_signer.verify_wsgi_request(request, settings.BIDDER_API_SIGN_KEY)
+    except request_signer.SignatureError:
+        logger.exception('Invalid signature.')
+        return _error_response('Invalid signature.', status=401)
+
+    try:
+        date = datetime.datetime.strptime(request.GET['date'], '%Y-%m-%d').date()
+    except Exception as e:
+        logger.exception('Invalid input parameters')
+        return _error_response(str(e), status=400)
+
+    result = {}
+
+    campaigns = dash.models.Campaign.objects.all()
+    for campaign in campaigns:
+        pct_actual_spend, pct_license_fee = reports.daily_statements.get_effective_spend_pcts(date, campaign)
+        for ad_group in campaign.adgroup_set.all():
+            result[ad_group.id] = {
+                'pct_actual_spend': str(pct_actual_spend),
+                'pct_license_fee': str(pct_license_fee)
+            }
+
+    response_data = {
+        'status': 'OK',
+        'data': result
+    }
+
+    return JsonResponse(response_data)
 
 
 @csrf_exempt
