2d287cdc52c4ac899d1d1d8b028b49185fb174a5
Florjan Bartol <florjan.bartol@gmail.com>
2015-12-24 10:26:32 +0100

    Fixed calculate_flight_time for campaigns with no ad groups

--- server/dash/infobox_helpers.py
+++ server/dash/infobox_helpers.py
@@ -52,7 +52,7 @@ def get_reports_api_module(user):
 
 
 def calculate_flight_time(start_date, end_date):
-    start_date_str = start_date.strftime('%m/%d')
+    start_date_str = start_date.strftime('%m/%d') if start_date else ''
     end_date_str = end_date.strftime('%m/%d') if end_date else ''
 
     flight_time = "{start_date} - {end_date}".format(
