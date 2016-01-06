417fe61bdbf77e93f3a3e3f4a6f20e1219045879
Simon Mihevc <simon.mihevc@zemanta.com>
2016-01-06 17:53:06 +0100

    Test fix

--- server/reports/tests/test_api_publishers.py
+++ server/reports/tests/test_api_publishers.py
@@ -272,8 +272,8 @@ class ApiPublishersInsertTest(TestCase):
 
         mock_cursor.execute.assert_has_calls([
             mock.call('DELETE FROM "ob_publishers_2" WHERE (adgroup_id=%s AND date=%s AND exchange=%s)', [3, datetime.date(2015, 2, 1), 'outbrain']),
-            mock.call('INSERT INTO ob_publishers_2 (date,adgroup_id,exchange,domain,name,clicks,cost_micro,ob_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s),(%s,%s,%s,%s,%s,%s,%s,%s)',
-                      [datetime.date(2015, 2, 1), 3, 'outbrain', 'money.cnn.com', 'CNN money', 20, 40000000000.0, 'AAAABBBBB', datetime.date(2015, 2, 1), 3, 'outbrain', 'money.cnn.com', 'CNN money', 80, 160000000000.0, 'AAAABBBBB'])
+            mock.call('INSERT INTO ob_publishers_2 (date,adgroup_id,exchange,name,clicks,cost_micro,ob_id) VALUES (%s,%s,%s,%s,%s,%s,%s),(%s,%s,%s,%s,%s,%s,%s)',
+                      [datetime.date(2015, 2, 1), 3, 'outbrain', 'CNN money', 20, 40000000000.0, 'AAAABBBBB', datetime.date(2015, 2, 1), 3, 'outbrain', 'CNN money', 80, 160000000000.0, 'AAAABBBBB'])
         ])
 
 
