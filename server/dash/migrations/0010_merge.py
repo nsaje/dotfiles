7a2b719fef60b11e017ca6bb2daaba5093573e81
Simon Mihevc <simon.mihevc@zemanta.com>
2016-01-06 16:45:37 +0100

    Added merge migration

---
+++ server/dash/migrations/0010_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('dash', '0009_campaignsettings_campaign_manager'),
+        ('dash', '0008_publisherblacklist_external_id'),
+    ]
+
+    operations = [
+    ]
