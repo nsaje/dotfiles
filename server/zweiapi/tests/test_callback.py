1c70a50832f8398bec66b30101b2cb30671b1ba7
Simon Mihevc <simon.mihevc@zemanta.com>
2016-01-08 14:07:14 +0100

    Test fix

--- server/zweiapi/tests/test_callback.py
+++ server/zweiapi/tests/test_callback.py
@@ -827,7 +827,7 @@ class FetchReportsByPublisherTestCase(TestCase):
         ob_insert_adgroup_date.assert_has_calls ([mock.call(
                                               datetime.date(2014,6,4),
                                               1,
-                                              "Outbrain",
+                                              "outbrain",
                                               [article_row],
                                               1.9043	# daily cost
                                               )])
