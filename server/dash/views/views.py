a315ab73ebf6782eab9c3022da5a06b3885d8e91
Simon Mihevc <simon.mihevc@zemanta.com>
2016-01-15 11:07:07 +0100

    Revert "Fixed level at adgroup since all changes are logged there"
    This reverts commit 3e9f6f7901e6a00da599c9d997228054bbffe0b5.

--- server/dash/views/views.py
+++ server/dash/views/views.py
@@ -1899,7 +1899,7 @@ class PublishersBlacklistStatus(api_common.BaseApiView):
         # TODO: revise this if making separate views per level
         helpers.log_useraction_if_necessary(
             request,
-            constants.PublisherBlacklistLevel.ADGROUP,
+            publisher_helpers.get_useractiontype(level),
             ad_group=ad_group
         )
 
