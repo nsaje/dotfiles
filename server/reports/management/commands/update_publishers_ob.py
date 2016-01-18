332905e69653bb044b5bef486406e3e599b7db6b
Luka Šilovinac <luka.silovinac@zemanta.com>
2016-01-18 14:48:52 +0100

    Add default option for date_parse

--- server/reports/management/commands/update_publishers_ob.py
+++ server/reports/management/commands/update_publishers_ob.py
@@ -21,9 +21,11 @@ class Command(ExceptionCommand):
     )
 
     def handle(self, *args, **options):
+        yesterday = datetime.date.today() - datetime.timedelta(days=1)
+
         try:
-            start_date = self._get_start_date(options)
-            end_date = self._get_end_date(options)
+            start_date = command_helpers.parse_date(options, field_name='start_date', default=yesterday)
+            end_date = command_helpers.parse_date(options, field_name='end_date', default=yesterday)
         except:
             logger.info('Failed parsing command line arguments')
             sys.exit(1)
@@ -35,17 +37,3 @@ class Command(ExceptionCommand):
 
             reports.refresh.refresh_ob_publishers_data(start_date)
             start_date = start_date + datetime.timedelta(days=1)
-
-    def _get_start_date(self, options):
-        start_date = command_helpers.parse_date(options, 'start_date')
-        if start_date is None:
-            logger.info("No start date was provided. Using yesterday's date as default value.")
-            start_date = datetime.date.today() - datetime.timedelta(days=1)
-        return start_date
-
-    def _get_end_date(self, options):
-        end_date = command_helpers.parse_date(options, 'end_date')
-        if end_date is None:
-            logger.info("No end date was provided. Using yesterday's date as default value.")
-            end_date = datetime.date.today() - datetime.timedelta(days=1)
-        return end_date
