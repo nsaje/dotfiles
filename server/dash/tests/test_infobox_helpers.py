0d62794fe455fa148b050dd8dabab71a8c858f51
Simon Mihevc <simon.mihevc@zemanta.com>
2016-01-18 17:41:31 +0100

    Stubs added

---
+++ server/dash/tests/test_infobox_helpers.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+
+
+class InfoBoxHelpersTest(TestCase):
+
+    def test_calculate_flight_time(self):
+        pass
+
+    def test_get_ideal_campaign_spend(self):
+        pass
+
+    def test_get_total_campaign_spend(self):
+        pass
+
+    def test_get_yesterday_total_cost(self):
+        pass
+
+    def test_get_goal_value(self):
+        pass
+
+    def test_get_goal_difference(self):
+        pass
