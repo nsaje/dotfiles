6b19853242d8d257c8890d2924a15562167590d5
Jure Polutnik <jure.polutnik@gmail.com>
2016-01-26 12:19:57 +0100

    Add migrations

---
+++ server/dash/migrations/0017_auto_20160126_1118.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('dash', '0016_auto_20160126_0933'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='adgroupsettings',
+            name='cpc_cc',
+            field=models.DecimalField(null=True, verbose_name=b'Maximum CPC', max_digits=10, decimal_places=4, blank=True),
+        ),
+    ]
