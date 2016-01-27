dbc01e0b62569ff34b8fe72349a1ad97dc70eaa9
Marko Novak <marko.novak@zemanta.com>
2016-01-27 10:54:32 +0100

    Added database migration

--- server/dash/migrations/0017_auto_20160127_0953.py
+++ server/dash/migrations/0017_auto_20160127_0953.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dash', '0011_remove_campaignsettings_account_manager'),
+        ('dash', '0016_auto_20160126_0933'),
     ]
 
     operations = [
