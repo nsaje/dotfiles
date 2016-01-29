29eb9368ef1235d9607985ac17db9e1ab4fc1d1e
Wessel Rossing <wessel.rossing@zemanta.com>
2016-01-29 11:05:31 +0100

    Addressed pointer from code review

--- server/zweiapi/views.py
+++ server/zweiapi/views.py
@@ -202,7 +202,7 @@ def _process_zwei_response(action, data, request):
         elif action.action == actionlog.constants.Action.SET_PUBLISHER_BLACKLIST:
             args = action.payload['args']
             dash.api.update_publisher_blacklist_state(args)
-            actionlog.api.send_delayed_actionlogs()
+            actions.extend(actionlog.api.send_delayed_actionlogs(send=False))
 
         elif action.action == actionlog.constants.Action.CREATE_CAMPAIGN:
             dash.api.create_campaign_callback(
