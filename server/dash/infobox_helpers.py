1d0ac391b7fe7563c424d792d1f46e08f984a1b0
Simon Mihevc <simon.mihevc@zemanta.com>
2016-02-05 12:05:52 +0100

    Added space between description and parentheses

--- server/dash/infobox_helpers.py
+++ server/dash/infobox_helpers.py
@@ -40,8 +40,11 @@ class OverviewSetting(object):
     def as_dict(self):
         ret = {}
         for key, value in self.__dict__.iteritems():
-            if value is not None:
-                ret[key] = value
+            adjusted_value = value
+            if value and key == 'description':
+                adjusted_value = ' {} '.format(value)
+            if adjusted_value is not None:
+                ret[key] = adjusted_value
         return ret
 
 
