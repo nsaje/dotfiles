db5d78bfcbe6a99af49051ea6acd55af6b0a4fd3
marko <marko.celan@zemanta.com>
2016-02-10 17:27:17 +0100

    latest.txt pushed to a wrong place

--- scripts/push_artifact_to_s3.sh
+++ scripts/push_artifact_to_s3.sh
@@ -3,11 +3,11 @@
 PROJECT="zemanta-eins"
 
 if [ -f "$1" ]; then
-    S3_PATH="s3://zemanta-artifacts/${PROJECT}/build-${CIRCLE_BUILD_NUM}"
+    S3_PATH="s3://zemanta-artifacts/${PROJECT}"
     BASENAME=$(basename $1)
 
 
-    aws s3 cp --acl private "$1" "${S3_PATH}/${BASENAME}"
+    aws s3 cp --acl private "$1" "${S3_PATH}/build-${CIRCLE_BUILD_NUM}/${BASENAME}"
 
     if [[ "$CIRCLE_BRANCH" == "master" ]]; then
         echo "${CIRCLE_BUILD_NUM}" > latest.txt
