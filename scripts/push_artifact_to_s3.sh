afc34c1e90036f4c96ca648341c771816524123e
marko <marko.celan@zemanta.com>
2016-02-10 16:50:53 +0100

    make CircleCI push artifacts to S3: cleanup

--- scripts/push_artifact_to_s3.sh
+++ scripts/push_artifact_to_s3.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -x
 
+PROJECT="zemanta-eins"
+
 if [ -f "$1" ]; then
-    S3_PATH="s3://zemanta-artifacts/z1/build-${CIRCLE_BUILD_NUM}"
+    S3_PATH="s3://zemanta-artifacts/${PROJECT}/build-${CIRCLE_BUILD_NUM}"
     BASENAME=$(basename $1)
+
 
     aws s3 cp --acl private "$1" "${S3_PATH}/${BASENAME}"
 
     if [[ "$CIRCLE_BRANCH" == "master" ]]; then
         echo "${CIRCLE_BUILD_NUM}" > latest.txt
-        aws s3api put-object --bucket zemanta-artifacts --key z1/latest.txt --body latest.txt
+        aws s3 cp --acl private latest.txt "${S3_PATH}/"
     fi
 fi
