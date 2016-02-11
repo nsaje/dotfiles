ffd25d840417659ce041536a30fb7f400a060479
Simon Mihevc <simon.mihevc@zemanta.com>
2016-02-11 15:20:47 +0100

    Show/hide chart button is now available via permissions

--- server/zemauth/models.py
+++ server/zemauth/models.py
@@ -174,6 +174,7 @@ class User(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
             ('can_control_ad_group_state_in_table', 'Can control ad group state in Campaign / Ad Groups table'),
             ('can_see_campaign_goals', 'Can see and manage campaign goals'),
             ('can_see_managers_in_accounts_table', "Can see Account Manager and Sales Representative in accounts table."),
+            ('can_hide_chart', 'Can show or hide chart'),
         )
 
     def get_full_name(self):
