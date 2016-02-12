49a2b3300aae9410b7f64674b6e8e812860e7b65
Gregor Ratajc <gregor.ratajc@zemanta.com>
2016-02-12 11:10:44 +0100

    Adds merge migration

---
+++ server/dash/migrations/0021_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('dash', '0019_uploadbatch_cancelled'),
+        ('dash', '0020_auto_20160201_1505'),
+    ]
+
+    operations = [
+    ]
