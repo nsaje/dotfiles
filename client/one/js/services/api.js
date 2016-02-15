ac4faad17f51fee16576eb8cdb54158a75a40534
Florjan Bartol <florjan.bartol@gmail.com>
2016-02-15 11:23:13 +0100

    Added api code in client

--- client/one/js/services/api.js
+++ client/one/js/services/api.js
@@ -1,4 +1,5 @@
-/*globals angular,oneApp,constants,options,moment*/
+/* globals angular,oneApp,constants,options,moment */
+/* eslint-disable camelcase */
 'use strict';
 
 oneApp.factory('api', ['$http', '$q', 'zemFilterService', function ($http, $q, zemFilterService) {
@@ -28,7 +29,7 @@ oneApp.factory('api', ['$http', '$q', 'zemFilterService', function ($http, $q, z
             var device = {
                 name: item.name,
                 value: item.value,
-                checked: false
+                checked: false,
             };
 
             if (targetDevices && targetDevices.indexOf(item.value) > -1) {
@@ -945,7 +946,8 @@ oneApp.factory('api', ['$http', '$q', 'zemFilterService', function ($http, $q, z
                 enableAdobeTracking: settings.enable_adobe_tracking,
                 adobeTrackingParam: settings.adobe_tracking_param,
                 autopilotState: settings.autopilot_state,
-                autopilotBudget: settings.autopilot_daily_budget
+                autopilotBudget: settings.autopilot_daily_budget,
+                retargetingAdGroups: settings.retargeting_ad_groups,
             };
         }
 
@@ -965,7 +967,8 @@ oneApp.factory('api', ['$http', '$q', 'zemFilterService', function ($http, $q, z
                 enable_adobe_tracking: settings.enableAdobeTracking,
                 adobe_tracking_param: settings.adobeTrackingParam,
                 autopilot_state: settings.autopilotState,
-                autopilot_daily_budget: settings.autopilotBudget
+                autopilot_daily_budget: settings.autopilotBudget,
+                retargeting_ad_groups: settings.retargetingAdGroups,
             };
 
             return result;
@@ -990,7 +993,8 @@ oneApp.factory('api', ['$http', '$q', 'zemFilterService', function ($http, $q, z
                 enableAdobeTracking: errors.enable_adobe_tracking,
                 adobeTrackingParam: errors.adobe_tracking_param,
                 autopilotState: errors.autopilot_state,
-                autopilotBudget: errors.autopilot_daily_budget
+                autopilotBudget: errors.autopilot_daily_budget,
+                retargetingAdGroups: errors.retargeting_ad_groups,
             };
 
             return result;
@@ -1007,11 +1011,11 @@ oneApp.factory('api', ['$http', '$q', 'zemFilterService', function ($http, $q, z
             var deferred = $q.defer();
             var url = '/api/ad_groups/' + id + '/settings/';
             var config = {
-                params: {}
+                params: {},
             };
 
             $http.get(url, config).
-                success(function (data, status) {
+                success(function (data) {
                     var settings, defaultSettings;
                     if (data && data.data && data.data.settings) {
                         settings = convertFromApi(data.data.settings);
@@ -1022,10 +1026,10 @@ oneApp.factory('api', ['$http', '$q', 'zemFilterService', function ($http, $q, z
                     deferred.resolve({
                         settings: settings,
                         defaultSettings: defaultSettings,
-                        actionIsWaiting: data.data.action_is_waiting
+                        actionIsWaiting: data.data.action_is_waiting,
                     });
                 }).
-                error(function (data, status, headers, config) {
+                error(function (data) {
                     deferred.reject(data);
                 });
 
@@ -1036,15 +1040,15 @@ oneApp.factory('api', ['$http', '$q', 'zemFilterService', function ($http, $q, z
             var deferred = $q.defer();
             var url = '/api/ad_groups/' + settings.id + '/settings/';
             var config = {
-                params: {}
+                params: {},
             };
 
             var data = {
-                'settings': convertToApi(settings)
+                'settings': convertToApi(settings),
             };
 
             $http.put(url, data, config).
-                success(function (data, status) {
+                success(function (data) {
                     var settings, defaultSettings;
                     if (data && data.data && data.data.settings) {
                         settings = convertFromApi(data.data.settings);
@@ -1055,10 +1059,10 @@ oneApp.factory('api', ['$http', '$q', 'zemFilterService', function ($http, $q, z
                     deferred.resolve({
                         settings: settings,
                         defaultSettings: defaultSettings,
-                        actionIsWaiting: data.data.action_is_waiting
+                        actionIsWaiting: data.data.action_is_waiting,
                     });
                 }).
-                error(function (data, status, headers, config) {
+                error(function (data, status) {
                     var resource;
                     if (status === 400 && data && data.data.error_code === 'ValidationError') {
                         resource = convertValidationErrorFromApi(data.data.errors);
