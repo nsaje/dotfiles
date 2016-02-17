06e6570d4aee932dbf6fd05d8b9c59ff74e71c65
Gregor Ratajc <gregor.ratajc@zemanta.com>
2016-02-17 22:20:28 +0100

    Always write batch upload status after processing

--- server/dash/threads.py
+++ server/dash/threads.py
@@ -49,25 +49,31 @@ class UpdateUploadBatchThread(Thread):
         self.check_exception()
 
     def finish(self):
-        self.bump_queue.put('die')
+        self.bump_queue.put_nowait('die')
+
+    def _get_upload_batch_attr(self, attr_name):
+        return models.UploadBatch.objects.filter(pk=self.batch_id).values(attr_name)[0].get(attr_name)
 
     def run(self):
-        batch = models.UploadBatch.objects.get(pk=self.batch_id)
-        while True:
+        messages_todo = self._get_upload_batch_attr('batch_size') * 2
+        while messages_todo > 0:
+            messages_todo -= 1
+
             message = self.bump_queue.get()
-            batch.refresh_from_db()
+
+            if self._get_upload_batch_attr('cancelled'):
+                self.exception_queue.put_nowait(exceptions.UploadCancelledException())
+                break
+
             if message == 'inserted_content_ads':
-                models.UploadBatch.objects.filter(pk=batch.id).update(
+                # update atomically
+                models.UploadBatch.objects.filter(pk=self.batch_id).update(
                     inserted_content_ads=F('inserted_content_ads') + 1)
             elif message == 'propagated_content_ads':
-                models.UploadBatch.objects.filter(pk=batch.id).update(
+                models.UploadBatch.objects.filter(pk=self.batch_id).update(
                     propagated_content_ads=F('propagated_content_ads') + 1)
             else:
                 # die
-                break
-
-            if batch.cancelled:
-                self.exception_queue.put_nowait(exceptions.UploadCancelledException())
                 break
 
 
