856e6bc4f8487a7d58a00b6e64526c5795229e30
Simon Mihevc <simon.mihevc@zemanta.com>
2016-02-24 15:47:17 +0100

    renamed region_targeting_helper to targeting_helper

--- server/dash/views/views.py
+++ server/dash/views/views.py
@@ -45,7 +45,7 @@ import actionlog.zwei_actions
 import actionlog.models
 import actionlog.constants
 
-from dash import models, region_targeting_helper
+from dash import models, targeting_helper
 from dash import constants
 from dash import api
 from dash import forms
@@ -510,7 +510,7 @@ class CampaignAdGroups(api_common.BaseApiView):
 
         ad_group_source = helpers.add_source_to_ad_group(source_settings, ad_group)
         ad_group_source.save(request)
-        active_source_state = region_targeting_helper.can_target_existing_regions(source, ad_group_settings)
+        active_source_state = targeting_helper.can_target_existing_regions(source, ad_group_settings)
         helpers.set_ad_group_source_settings(request, ad_group_source, source_settings,
                                              mobile_only=ad_group_settings.is_mobile_only(),
                                              active=active_source_state)
@@ -861,7 +861,7 @@ class AdGroupSources(api_common.BaseApiView):
             sources.append({
                 'id': source.id,
                 'name': source.name,
-                'can_target_existing_regions': region_targeting_helper.can_target_existing_regions(
+                'can_target_existing_regions': targeting_helper.can_target_existing_regions(
                         source, ad_group_settings)
             })
 
@@ -887,7 +887,7 @@ class AdGroupSources(api_common.BaseApiView):
             raise exc.ValidationError(
                 '{} media source for ad group {} already exists.'.format(source.name, ad_group_id))
 
-        if not region_targeting_helper.can_target_existing_regions(source, ad_group.get_current_settings()):
+        if not targeting_helper.can_target_existing_regions(source, ad_group.get_current_settings()):
             raise exc.ValidationError('{} media source can not be added because it does not support selected region targeting.'
                                       .format(source.name))
 
