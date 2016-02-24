218a5e62d3dc7c2d7938c11ab63a08f79e0ac7bd
Simon Mihevc <simon.mihevc@zemanta.com>
2016-02-24 12:16:39 +0100

    made dropdown extend contents and resolved issues with api call and js errors

--- client/one/js/directives/zem_retargeting.js
+++ client/one/js/directives/zem_retargeting.js
@@ -14,16 +14,7 @@ oneApp.directive('zemRetargeting', ['config', 'zemFilterService', '$state', 'zem
             $scope.config = config;
             $scope.selected = {adgroup: undefined};
             $scope.campaigns = angular.copy($scope.account.campaigns) || [];
-
-            /*
-            // TODO: campaigns will be set on ad group settings controller init
-            zemNavigationService.onUpdate($scope, function () {
-                zemNavigationService.getAccount($scope.account.id).then(function (data) {
-                    // make a copy of campaigns so we don't change original objects
-                    $scope.campaigns = angular.copy(data.account.campaigns);
-                });
-            });
-            */
+            $scope.selectAdgroupIds = [];
 
             $scope.selectedAdgroups = function () {
                 var result = [];
@@ -36,26 +27,19 @@ oneApp.directive('zemRetargeting', ['config', 'zemFilterService', '$state', 'zem
             };
 
             $scope.availableAdgroups = function () {
-                return $scope.retargetableAdgroups.reduce(function (result, adgroup) {
-                    // TODO: refactor this to use adgroups directly
-                    var adgroups = campaign.adGroups.filter(function (adgroup) {
-                        return !adgroup.archived || zemFilterService.isArchivedFilterOn();
-                    }).filter(function (adgroup) {
-                        return $scope.selectedAdgroupIds.indexOf(adgroup.id) < 0;
-                    }).filter(function (adgroup) {
-                        return adgroup.id !== parseInt($state.params.id);
-                    }).map(function (adgroup) {
-                        // add campaign info to each item for grouping
-                        adgroup.campaign = campaign;
-                        return adgroup;
-                    });
+                var adgroups = $scope.retargetableAdgroups.filter(function (adgroup) {
+                    return !adgroup.archived || zemFilterService.isArchivedFilterOn();
+                }).filter(function (adgroup) {
+                    return $scope.selectedAdgroupIds.indexOf(adgroup.id) < 0;
+                }).filter(function (adgroup) {
+                    return adgroup.id !== parseInt($state.params.id);
+                });
 
-                    return result.concat(adgroups);
-                }, []);
+                return adgroups;
             };
 
             $scope.groupByCampaign = function (adgroup) {
-                return adgroup.campaign.name;
+                return adgroup.campaign_name;
             };
 
             $scope.removeSelectedAdgroup = function (adgroup) {
