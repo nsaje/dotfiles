5956a9b88b45d95a838f4e86fd0996b7a4c7e24a
Simon Mihevc <simon.mihevc@zemanta.com>
2016-03-09 11:37:00 +0100

    added stub for campaign goal values extraction

---
+++ server/dash/campaign_goal_helpers.py
@@ -0,0 +1,10 @@
+def create_goals(data):
+    return {
+        'total_seconds': 0,
+        'unbounced_visits': 0,
+        'total_pageviews': 0,
+        'avg_cost_per_second': 0,
+        'avg_cost_per_pageview': 0,
+        'avg_cost_per_non_bounced_visitor': 0,
+        'cpa': 0,
+    }
