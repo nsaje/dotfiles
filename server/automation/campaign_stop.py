81887f4b61361a96594002eb01125193c07e098a
Luka Šilovinac <luka.silovinac@zemanta.com>
2016-03-15 14:05:07 +0100

    Add an optimization to mrb calculation

--- server/automation/campaign_stop.py
+++ server/automation/campaign_stop.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from collections import defaultdict
 import datetime
 import logging
 
@@ -62,32 +63,37 @@ def _get_budgets_active_on_date(date, campaign):
     ).select_related('credit')
 
 
+def _get_ags_settings_dict(date, ad_group_sources):
+    ad_group_sources_settings = dash.models.AdGroupSourceSettings.objects.filter(
+        ad_group_source__in=ad_group_sources,
+        created_dt__lt=date+datetime.timedelta(days=1)
+    ).order_by('-created_dt')
+
+    ret = defaultdict(list)
+    for ags_sett in ad_group_sources_settings:
+        ret[ags_sett.ad_group_source_id].append(ags_sett)
+
+    return ret
+
+
 def _get_max_daily_budget(date, campaign):
     ad_groups = _get_ad_groups_active_on_date(date, campaign)
     ad_group_sources = dash.models.AdGroupSource.objects.filter(
         ad_group__in=ad_groups,
     ).select_related('source__source_type')
-    ad_group_sources_settings = dash.models.AdGroupSourceSettings.objects.filter(
-        ad_group_source__in=ad_group_sources,
-        created_dt__lt=date+datetime.timedelta(days=1)
-    )
+    ad_group_sources_settings = _get_ags_settings_dict(date, ad_group_sources)
 
     max_daily_budget = 0
-    for ad_group_source in ad_group_sources:
-        max_daily_budget += _get_source_max_daily_budget(date, ad_group_source, ad_group_sources_settings)
+    for ags in ad_group_sources:
+        max_daily_budget += _get_source_max_daily_budget(date, ags, ad_group_sources_settings[ags.id])
 
     return max_daily_budget
 
 
-def _get_source_max_daily_budget(date, ad_group_source, ad_group_sources_settings):
-    ad_group_sources_settings = ad_group_sources_settings.order_by('-created_dt')
-
+def _get_source_max_daily_budget(date, ad_group_source, ad_group_source_settings):
     ags_max_daily_budget = 0
     reached_day_before = False
-    for sett in ad_group_sources_settings:
-        if sett.ad_group_source_id != ad_group_source.id:
-            continue
-
+    for sett in ad_group_source_settings:
         if reached_day_before:
             break
 
