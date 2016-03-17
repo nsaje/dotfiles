bec34a1527439803142444d1b79fd495279d7860
Simon Mihevc <simon.mihevc@zemanta.com>
2016-03-17 16:00:53 +0100

    test for campaign goals

--- server/dash/tests/test_campaign_goal_helpers.py
+++ server/dash/tests/test_campaign_goal_helpers.py
@@ -1,9 +1,40 @@
-from mock import patch
 from django.test import TestCase
+from decimal import Decimal
+
+import dash.constants
+import dash.models
+import zemauth.models
+import dash.campaign_goal_helpers
 
 
 class CampaignGoalHelpersTestCase(TestCase):
     fixtures = ['test_models.yaml']
+
+    def setUp(self):
+        # make up some goals
+        self.campaign = dash.models.Campaign.objects.get(pk=1)
+        self.user = zemauth.models.User.objects.get(pk=1)
+
+        all_goal_types = dash.constants.CampaignGoalKPI.get_all()
+        for goal_type in all_goal_types:
+            dash.models.CampaignGoal.objects.create(
+                campaign=self.campaign,
+                type=goal_type,
+                created_by=self.user,
+            )
+
+    def _goal(self, goal_type):
+        return dash.models.CampaignGoal.objects.filter(
+            type=goal_type
+        ).first()
+
+    def _add_value(self, goal_type, value):
+        goal = self._goal(goal_type)
+        dash.models.CampaignGoalValue.objects.create(
+            campaign_goal=goal,
+            value=Decimal(value),
+            created_by=self.user
+        )
 
     def test_create_goals(self):
         # campaign, data, cost):
@@ -26,5 +57,28 @@ class CampaignGoalHelpersTestCase(TestCase):
         pass
 
     def test_get_campaign_goals(self):
-        # campaign):
-        pass
+        self._add_value(dash.constants.CampaignGoalKPI.MAX_BOUNCE_RATE, 0.75)
+        self._add_value(dash.constants.CampaignGoalKPI.PAGES_PER_SESSION, 5)
+        self._add_value(dash.constants.CampaignGoalKPI.TIME_ON_SITE, 60)
+
+        campaign_goals = dash.campaign_goal_helpers.get_campaign_goals(self.campaign)
+
+        result = [
+            {
+                'name': 'time on site in seconds',
+                'value': 60,
+                'fields': {'total_seconds': True, 'avg_cost_per_second': True},
+            },
+            {
+                'name': 'pages per session',
+                'value': 5,
+                'fields': {'total_pageviews': True, 'avg_cost_per_pageview': True},
+            },
+            {
+                'name': 'max bounce rate %',
+                'value': 0.75,
+                'fields': {'unbounced_visits': True, 'avg_cost_per_non_bounced_visitor': True},
+            }
+        ]
+
+        self.assertItemsEqual(result, campaign_goals)
