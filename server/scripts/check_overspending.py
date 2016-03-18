e44a20af365e05b58b77954e226c4d8ba36ac18a
Matija Vižintin <matija.vizintin@gmail.com>
2016-03-18 15:11:15 +0100

    improvements

--- server/scripts/check_overspending.py
+++ server/scripts/check_overspending.py
@@ -9,16 +9,16 @@ from optparse import OptionParser
 from dateutil.parser import parse
 from django.db.models import Sum
 from dash import constants
-from dash.models import AdGroup, ContentAd, AdGroupSourceSettings, AdGroupSource
+from dash.models import AdGroup, AdGroupSourceSettings, AdGroupSource
 from reports.models import ContentAdStats
 from automation import campaign_stop
 
 
 def create_overspend_report(date, ad_group_id, debug_print):
     if ad_group_id:
-        ad_groups = AdGroup.objects.filter(id=ad_group_id)
+        ad_groups = AdGroup.objects.filter(id=ad_group_id).prefetch_related('sources')
     else:
-        ad_groups = AdGroup.objects.all()
+        ad_groups = AdGroup.objects.all().prefetch_related('sources')
 
     # all ad groups
     for ad_group in ad_groups.iterator():
@@ -27,20 +27,21 @@ def create_overspend_report(date, ad_group_id, debug_print):
 
         ad_group_name = unicode(ad_group.name)
         media_sources = ad_group.sources.filter(source_type__type=constants.SourceType.B1)
-        content_ads = ContentAd.objects.filter(ad_group=ad_group)
+        content_ads = ad_group.contentad_set.all()
+
+        ad_group_sources = AdGroupSource.objects.filter(ad_group=ad_group, source__in=media_sources)
+        ad_group_sources_map = {ags.source: ags for ags in ad_group_sources}
 
         # all media source for this ad group
         for media_source in media_sources:
             media_source_name = unicode(media_source.name)
 
             #  daily budget
-            ad_group_source = AdGroupSource.objects.get(ad_group=ad_group, source=media_source)
+            ad_group_source = ad_group_sources_map[media_source]
             next_day = date + datetime.timedelta(days=1)
             ad_group_settings = AdGroupSourceSettings.objects.filter(ad_group_source=ad_group_source,
                                                                      created_dt__lt=next_day)
             daily_budget = campaign_stop._get_source_max_daily_budget(date, ad_group_source, ad_group_settings)
-
-            # daily_budget = ad_group_settings.daily_budget_cc
             if daily_budget == 0:
                 continue
 
@@ -53,11 +54,14 @@ def create_overspend_report(date, ad_group_id, debug_print):
             # diff
             diff = daily_spent - float(daily_budget)
             if diff > 0:
-                result_string = 'OVERSPENT: AdGroup {} [id={}], MediaSource: {} [id={}], Daily budget: {}, ' \
-                                'Daily spent: {}, DIFF: {}'.format(
-                                    ad_group_name, ad_group.id, media_source_name, media_source.id, daily_budget,
-                                    daily_spent, diff)
-                print(result_string)
+                try:
+                    result_string = 'OVERSPENT: AdGroup {} [id={}], MediaSource: {} [id={}], Daily budget: {}, ' \
+                                    'Daily spent: {}, DIFF: {}'.format(
+                                        ad_group_name, ad_group.id, media_source_name, media_source.id, daily_budget,
+                                        daily_spent, diff)
+                    print(result_string)
+                except UnicodeEncodeError:
+                    print('Error printing AdGroup.id=' + ad_group.id)
             elif debug_print:
                 result_string = 'OK: AdGroup {} [id={}], MediaSource: {} [id={}], Daily budget: {}, ' \
                                 'Daily spent: {}'.format(
@@ -80,10 +84,9 @@ if len(args) >= 2:
     print('Checking overspending only for AdGroup with id ' + str(ad_group_id))
 
 if date:
-    create_overspend_report(date, ad_group_id, options.debug)
+    create_overspend_report(date.date(), ad_group_id, options.debug)
 else:
-    date = datetime.datetime.now()
-    date = datetime.date(date.year, date.month, date.day) - datetime.timedelta(days=1)
+    date = datetime.datetime.utcnow().date() - datetime.timedelta(days=1)
 
     while True:
         print('Checking overspending for day ' + str(date))
