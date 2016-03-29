8698f61f1c65c553eaa18b111b9b5929cd5a2080
Jure Polutnik <jure.polutnik@gmail.com>
2016-03-29 10:06:05 +0200

    Style fixes

--- client/one/components/zem-grid/zem_grid_row.js
+++ client/one/components/zem-grid/zem_grid_row.js
@@ -13,13 +13,22 @@ oneApp.directive('zemGridRow', ['config', function (config) {
             row: '=',
         },
         template: '' +
-            '<div class="zem-grid-row">' +
+            '<div class="zem-grid-row" ng-class="getRowClass()">' +
                 '<zem-grid-cell ng-repeat="col in row.data.stats track by $index" ' +
                     'cell="col" ' +
                     'options="options" ' +
                 '></zem-grid-cell>' +
             '</div>',
         controller: ['$scope', function ($scope) {
+            $scope.getRowClass = function () {
+                var classes = [];
+                classes.push('level-' + this.row.level);
+
+                if (this.row.level === $scope.dataSource.breakdowns.length) {
+                    classes.push('level-last');
+                }
+                return classes;
+            };
         }],
     };
 }]);
