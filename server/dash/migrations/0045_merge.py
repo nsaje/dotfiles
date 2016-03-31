6b9ffbab3bfc513f0a88af7ef0557c1d65c6349c
Jure Polutnik <jure.polutnik@gmail.com>
2016-03-31 15:48:46 +0200

    Merge migrations

---
+++ server/dash/migrations/0045_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('dash', '0044_exportreport_include_model_ids'),
+        ('dash', '0044_merge'),
+    ]
+
+    operations = [
+    ]
