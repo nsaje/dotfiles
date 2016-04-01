56869096b1130c6fe5c68669deed4fd111bcbd64
Gregor Ratajc <gregor.ratajc@zemanta.com>
2016-04-01 16:12:10 +0200

    Adds tests

--- server/dash/tests/test_stats_helper.py
+++ server/dash/tests/test_stats_helper.py
@@ -82,8 +82,6 @@ class GetStatsWithConversionsTestCase(test.TestCase):
 
         _update_with_defaults(touchpoint_conversion_stats, breakdown + ['slug', 'account', 'conversion_window'],
                               defaults=defaults)
-        import json
-        print 'ASD', breakdown, json.dumps(touchpoint_conversion_stats, indent=4)
         return touchpoint_conversion_stats
 
     def setUp(self):
@@ -508,6 +506,85 @@ class GetStatsWithConversionsTestCase(test.TestCase):
             'conversion_goal_1': 5
         }], stats)
 
+    def test_multiple_conversion_goals_with_same_pixel(self, mock_as_query, mock_ca_query, mock_tp_query):
+        mock_ca_query.side_effect = self._get_content_ad_stats
+        mock_tp_query.side_effect = [
+            [{
+                "account": 1,
+                "ad_group": 9999,
+                "conversion_count": 5,
+                "slug": "test",
+                "touchpoint_count": 10,
+                "conversion_window": 168,
+            }, {
+                "account": 1,
+                "ad_group": 9999,
+                "conversion_count": 4,
+                "slug": "test",
+                "touchpoint_count": 9,
+                "conversion_window": 1,
+            }, {
+                "account": 1,
+                "ad_group": 9999,
+                "conversion_count": 11,
+                "slug": "test",
+                "touchpoint_count": 8,
+                "conversion_window": 7,
+            }],
+        ]
+
+
+        self.use_separate_rows_for_tp_conversions = True
+
+        # add another 2 conversion goals with the same pixel but different windows
+        extra1 = models.ConversionGoal.objects.get(pk=1)
+        extra1.pk = None
+        extra1.name = 'extra conversion goal 1'
+        extra1.conversion_window = 1
+        extra1.goal_id = '100'
+        extra1.save()
+
+        extra2 = models.ConversionGoal.objects.get(pk=1)
+        extra2.pk = None
+        extra2.name = 'extra conversion goal 2'
+        extra2.conversion_window = 7
+        extra2.goal_id = '101'
+        extra2.save()
+
+        conversion_goals = models.ConversionGoal.objects.filter(pk__in=[1, 2, extra1.pk, extra2.pk])
+        stats = stats_helper.get_stats_with_conversions(self.superuser, datetime.date(2015, 10, 1),
+                                                        datetime.date(2015, 10, 31), breakdown=['ad_group'], order=[],
+                                                        conversion_goals=conversion_goals)
+
+        self.assertFalse(mock_as_query.called)
+        self.assertTrue(mock_ca_query.called)
+        self.assertTrue(mock_tp_query.called)
+
+        self.assertEqual([{
+            'ad_group': 1,
+            'impressions': 10,
+            'clicks': 1,
+            'cost': decimal.Decimal('10.00'),
+            'cpc': decimal.Decimal('10.00'),
+            'ctr': 0.1,
+            'visits': 1,
+            'click_discrepancy': 0,
+            'pageviews': 5,
+            'percent_new_users': 100,
+            'bounce_rate': 0,
+            'pv_per_visit': 5,
+            'avg_tos': 0,
+            'conversion_goal_1': 0,
+            'conversion_goal_2': 1,
+            'conversion_goal_3': 0,
+            'conversion_goal_4': 0,
+        }, {
+            'ad_group': 9999,
+            'conversion_goal_1': 5,
+            'conversion_goal_3': 4,
+            'conversion_goal_4': 11,
+        }], stats)
+
 
 @patch('dash.table.reports.api_touchpointconversions.query')
 @patch('dash.table.reports.api_publishers.query')
@@ -845,3 +922,126 @@ class GetPublishersDataAndConversionGoalsTestCase(test.TestCase):
 
         result_mock = self._mock_publishers_and_touchpoint_data(date)
         self.assertEqual(result_mock, publisher_data)
+
+    @patch('dash.table.reports.api_publishers.query_active_publishers')
+    def test_multiple_conversion_goals_with_same_pixel(self, mock_active, mock_query, mock_touchpointconversions_query):
+        # add another 2 conversion goals with the same pixel but different windows
+        extra1 = models.ConversionGoal.objects.get(pk=1)
+        extra1.pk = None
+        extra1.name = 'extra conversion goal 1'
+        extra1.conversion_window = 1
+        extra1.goal_id = '100'
+        extra1.save()
+
+        extra2 = models.ConversionGoal.objects.get(pk=1)
+        extra2.pk = None
+        extra2.name = 'extra conversion goal 2'
+        extra2.conversion_window = 7
+        extra2.goal_id = '101'
+        extra2.save()
+
+        ad_group = AdGroup.objects.get(pk=1)
+        constraints = {'ad_group': ad_group.id}
+        conversion_goals = ad_group.campaign.conversiongoal_set.all()
+        touchpoint_conversion_goals = ad_group.campaign.conversiongoal_set.filter(type=conversions_helper.PIXEL_GOAL_TYPE)
+        date = datetime.date(2016, 3, 14)
+
+        publisher_data_mock = self._mock_publisher_data(date)
+        mock_active.side_effect = [publisher_data_mock]
+
+        mock_stats = [{
+            'date': date.isoformat(),
+            'conversion_count': 1168,
+            'slug': 'test',
+            'source': 7,
+            'publisher': 'example.com',
+            'conversion_window': 168,
+            'account': 1,
+        }, {
+            'date': date.isoformat(),
+            'conversion_count': 11,
+            'slug': 'test',
+            'source': 7,
+            'publisher': 'example.com',
+            'conversion_window': 1,
+            'account': 1,
+        }, {
+            'date': date.isoformat(),
+            'conversion_count': 77,
+            'slug': 'test',
+            'source': 7,
+            'publisher': 'example.com',
+            'conversion_window': 7,
+            'account': 1,
+        }]
+        touchpoint_data_mock = mock_stats
+
+        mock_touchpointconversions_query.side_effect = [touchpoint_data_mock]
+
+        publishers_breakdown_fields = ['domain', 'exchange']
+        touchpoint_breakdown_fields = ['publisher', 'source']
+        publisher_data = stats_helper.get_publishers_data_and_conversion_goals(
+            self.superuser,
+            reports.api_publishers.query_active_publishers,
+            date,
+            date,
+            constraints,
+            conversion_goals,
+            publishers_breakdown_fields,
+            touchpoint_breakdown_fields,
+            show_blacklisted_publishers=constants.PublisherBlacklistFilter.SHOW_ACTIVE)
+
+        mock_active.assert_any_call(
+            date,
+            date,
+            breakdown_fields=publishers_breakdown_fields,
+            order_fields=[],
+            constraints=constraints,
+            conversion_goals=ListMatcher(['omniture__5', 'omniture__4', 'ga__3', 'ga__2']),
+            constraints_list=[],
+        )
+
+        mock_touchpointconversions_query.assert_any_call(
+            date,
+            date,
+            breakdown=touchpoint_breakdown_fields,
+            conversion_goals=list(touchpoint_conversion_goals),
+            constraints=constraints,
+            constraints_list=[],
+        )
+
+        self.assertFalse(mock_query.called)
+
+        self.assertEqual([{
+            'clicks': 123,
+            'cost': 2.4,
+            'data_cost': 0,
+            'e_data_cost': 0,
+            'external_id': '12345',
+            'total_cost': 3,
+            'billing_cost': 3,
+            'media_cost': 2.4,
+            'e_media_cost': 2.4,
+            'license_fee': 0.6,
+            'cpc': 1.3,
+            'ctr': 100.0,
+            'impressions': 10560,
+            'date': date.isoformat(),
+            'visits': 15,
+            'click_discrepancy': 3,
+            'pageviews': 100,
+            'new_visits': 50,
+            'percent_new_users': 0.5,
+            'bounce_rate': 0.3,
+            'pv_per_visit': 10,
+            'avg_tos': 20,
+            'domain': 'example.com',
+            'exchange': 'adiant',
+            'conversion_goal_1': 1168,
+            'conversion_goal_2': None,
+            'conversion_goal_3': None,
+            'conversion_goal_4': None,
+            'conversion_goal_5': None,
+            'conversion_goal_6': 11,
+            'conversion_goal_7': 77,
+        }], publisher_data)
