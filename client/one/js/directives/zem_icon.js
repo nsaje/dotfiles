14831777af12c670fe588ed3ac0c71d7d8866bb8
Ziga Stopinsek <sigi.kajzer@gmail.com>
2016-04-01 13:42:03 +0200

    fixing icons and goal performance table bug

--- client/one/js/directives/zem_icon.js
+++ client/one/js/directives/zem_icon.js
@@ -5,16 +5,16 @@ oneApp.directive('zemIcon', ['config', function (config) {
     return {
         restrict: 'E',
         scope: true,
-        template: '<img class="{{ classes }}" title="{{ title }}" ng-src="{{ config.static_url }}/one/img/{{file}}" />',
-        controller: ['$scope', '$attrs', function ($scope, $attrs) {
-            $scope.config = config;
-            $scope.title = $attrs.title || '';
-            $scope.file = $attrs.file;
-            $scope.classes = 'zem-icon';
-            if ($attrs.imgClass) {
-                $scope.classes += ' ' + $attrs.imgClass;
+        template: '<img class="{{ classes }}" title="{{ title }}" ng-src="{{ config.static_url }}/one/img/{{ file }}" />',
+        link: function (scope, element, attrs) {
+            scope.config = config;
+            scope.title = attrs.title || '';
+            scope.file = attrs.file;
+            scope.classes = 'zem-icon';
+            if (attrs.imgClass) {
+                scope.classes += ' ' + attrs.imgClass;
             }
-        }],
+        },
     };
 
 }]);
