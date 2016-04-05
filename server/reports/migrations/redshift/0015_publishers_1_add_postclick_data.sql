869b071214e4bc14e0e2088842327d63b19467dd
Gregor Ratajc <gregor.ratajc@zemanta.com>
2016-04-05 16:18:35 +0200

    Changed publishers table definition

--- server/reports/migrations/redshift/0015_publishers_1_add_postclick_data.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE publishers_1 ADD COLUMN visits integer DEFAULT NULL;
-ALTER TABLE publishers_1 ADD COLUMN new_visits integer DEFAULT NULL;
-ALTER TABLE publishers_1 ADD COLUMN bounced_visits integer DEFAULT NULL;
-ALTER TABLE publishers_1 ADD COLUMN pageviews integer DEFAULT NULL;
-ALTER TABLE publishers_1 ADD COLUMN total_time_on_site integer DEFAULT NULL;
-ALTER TABLE publishers_1 ADD COLUMN conversions varchar(2048) DEFAULT NULL;
