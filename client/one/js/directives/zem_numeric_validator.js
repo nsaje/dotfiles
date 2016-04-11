5c20ed04c8725e8c0589c6f6c85605f1e60c3841
Simon Mihevc <simon.mihevc@zemanta.com>
2016-04-11 13:54:04 +0200

    cc: some formatting fixes

--- client/one/js/directives/zem_numeric_validator.js
+++ client/one/js/directives/zem_numeric_validator.js
@@ -1,19 +1,19 @@
-/*globals oneApp,$*/
+/* globals oneApp */
 'use strict';
 
 oneApp.directive('zemNumericValidator', function(){
-   return {
-     require: 'ngModel',
-     link: function(scope, element, attrs, modelCtrl) {
-
-       modelCtrl.$parsers.push(function (inputValue) {
-           var transformedInput = inputValue.toLowerCase().replace(/[^.0-9]/g, ''); 
-           if (transformedInput !== inputValue) {
-               modelCtrl.$setViewValue(transformedInput);
-               modelCtrl.$render();
-           }         
-           return transformedInput;
-       });
-     }
-   };
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, modelCtrl) {
+            
+            modelCtrl.$parsers.push(function (inputValue) {
+                var transformedInput = inputValue.toLowerCase().replace(/[^.0-9]/g, ''); 
+                if (transformedInput !== inputValue) {
+                    modelCtrl.$setViewValue(transformedInput);
+                    modelCtrl.$render();
+                }         
+                return transformedInput;
+            });
+        }
+    };
 });
