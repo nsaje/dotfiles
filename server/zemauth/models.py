f36ca0ee8b047addfea2c8e43fa7418e0ca2560b
Wessel Rossing <wessel.rossing@zemanta.com>
2016-04-15 15:44:42 +0200

    Removed Drift snippet

--- server/zemauth/models.py
+++ server/zemauth/models.py
@@ -192,7 +192,6 @@ class User(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
             ('campaign_goal_optimization', 'Can view aggregate campaign goal optimisation metrics'),
             ('campaign_goal_performance', 'Can view goal performance information'),
             ('can_include_model_ids_in_reports', 'Can include model ids in reports'),
-            ('has_drift', 'Has Drift snippet'),
             ('has_supporthero', 'Has Supporthero snippet'),
         )
 
