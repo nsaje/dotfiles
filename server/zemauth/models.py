f44a92a2668ce65f63ae3c029ec4b860cb4361ae
Florjan Bartol <florjan.bartol@gmail.com>
2016-04-15 15:43:53 +0200

    Removed campaign_ad_groups_add_ad_group

--- server/zemauth/models.py
+++ server/zemauth/models.py
@@ -116,7 +116,6 @@ class User(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
             ('campaign_sources_view', 'Can view campaign sources view.'),
             ('account_sources_view', 'Can view account sources view.'),
             ('all_accounts_sources_view', 'Can view all accounts sources view.'),
-            ('campaign_ad_groups_add_ad_group', 'Can add ad groups.'),
             ('account_campaigns_add_campaign', 'Can add campaigns.'),
             ('all_accounts_accounts_add_account', 'Can add accounts.'),
             ('all_new_sidebar', 'Can see new sidebar.'),
