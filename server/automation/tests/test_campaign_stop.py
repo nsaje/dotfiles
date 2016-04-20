178c8bc35b8a9b0395cd611110eb552024253a85
Ziga Stopinsek <sigi.kajzer@gmail.com>
2016-04-20 16:18:17 +0200

    update ad group settings when going into landing, auto set autopilot and disable editing

--- server/automation/tests/test_campaign_stop.py
+++ server/automation/tests/test_campaign_stop.py
@@ -627,6 +627,68 @@ class CalculateDailySpendsTestCase(TestCase):
         self.assertEqual(820, daily_caps[1])
 
 
+class UpdateAdGroupSettingsTestCase(TestCase):
+
+    fixtures = ['test_campaign_stop.yaml']
+
+    def test_ad_group_settings_values(self):
+        ag1 = dash.models.AdGroup.objects.get(id=1)
+        ag2 = dash.models.AdGroup.objects.get(id=2)
+
+        ag1_settings = ag1.get_current_settings()
+        self.assertEqual(ag1_settings.landing_mode, False)
+        self.assertEqual(ag1_settings.autopilot_state,
+                         dash.constants.AdGroupSettingsAutopilotState.INACTIVE)
+        self.assertEqual(ag1_settings.autopilot_daily_budget, Decimal('0.0000'))
+
+        ag2_settings = ag2.get_current_settings()
+        self.assertEqual(ag2_settings.landing_mode, False)
+        self.assertEqual(ag2_settings.autopilot_state,
+                         dash.constants.AdGroupSettingsAutopilotState.INACTIVE)
+        self.assertEqual(ag2_settings.autopilot_daily_budget, Decimal('0.0000'))
+
+        daily_caps = {
+            1: 12,
+            2: 15,
+        }
+        campaign_stop._update_ad_group_settings_to_landing(daily_caps)
+
+        ag1_settings = ag1.get_current_settings()
+        self.assertEqual(ag1_settings.landing_mode, True)
+        self.assertEqual(ag1_settings.autopilot_state,
+                         dash.constants.AdGroupSettingsAutopilotState.ACTIVE_CPC_BUDGET)
+        self.assertEqual(ag1_settings.autopilot_daily_budget, Decimal('12'))
+
+        ag2_settings = ag2.get_current_settings()
+        self.assertEqual(ag2_settings.landing_mode, True)
+        self.assertEqual(ag2_settings.autopilot_state,
+                         dash.constants.AdGroupSettingsAutopilotState.ACTIVE_CPC_BUDGET)
+        self.assertEqual(ag2_settings.autopilot_daily_budget, Decimal('15'))
+
+    @patch('automation.campaign_stop._wrap_up_landing')
+    @patch('automation.campaign_stop._stop_non_spending_sources')
+    @patch('automation.campaign_stop._wrap_up_landing')
+    @patch('automation.campaign_stop._run_autopilot')
+    @patch('automation.campaign_stop._set_end_date_to_today')
+    @patch('automation.campaign_stop._prepare_for_autopilot')
+    @patch('automation.campaign_stop._get_past_7_days_data')
+    @patch('automation.campaign_stop._calculate_daily_caps')
+    @patch('automation.campaign_stop._update_ad_group_settings_to_landing')
+    def test_update_ad_group_settings_called(self, mock_update_ags, mock_calc_caps,
+                                             mock_past7, mock_prepare_ap, *mocks):
+        caps = {
+            1: 12,
+            2: 15,
+        }
+        mock_past7.return_value = None, None
+        mock_prepare_ap.return_value = [], None
+        mock_calc_caps.return_value = caps
+        campaign = dash.models.Campaign.objects.get(pk=1)
+        campaign_stop._update_landing_campaign(campaign)
+        self.assertTrue(mock_update_ags.called)
+        mock_update_ags.assert_called_once_with(caps)
+
+
 class PrepareActiveSourceForAutopilotTestCase(TestCase):
 
     fixtures = ['test_campaign_stop.yaml']
