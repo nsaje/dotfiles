55f4c1dfe58078caf6e45247a603541e5056efca
Simon Mihevc <simon.mihevc@zemanta.com>
2016-04-22 10:11:03 +0200

    test fix

--- server/dash/views/tests/test_table.py
+++ server/dash/views/tests/test_table.py
@@ -1966,7 +1966,7 @@ class AdGroupPublishersTableTest(TestCase):
             breakdown_fields=['domain', 'exchange'],
             order_fields=[],
             constraints={'ad_group': ad_group.id, },
-            conversion_goals=[],
+            conversion_goals=[u'omniture__5', u'omniture__4', u'ga__3', u'ga__2'],
             constraints_list=[],
         )
 
@@ -1976,7 +1976,7 @@ class AdGroupPublishersTableTest(TestCase):
             breakdown_fields=[],
             order_fields=[],
             constraints={"ad_group": ad_group.id, },
-            conversion_goals=[],
+            conversion_goals=[u'omniture__5', u'omniture__4', u'ga__3', u'ga__2'],
             constraints_list=[],
         )
 
@@ -2005,6 +2005,11 @@ class AdGroupPublishersTableTest(TestCase):
             u'license_fee': 0.6,
             u'impressions': 10560,
             u'clicks': 123,
+            u'conversion_goal_1': 0,
+            u'conversion_goal_2': None,
+            u'conversion_goal_3': None,
+            u'conversion_goal_4': None,
+            u'conversion_goal_5': None,
             u'source_id': 7,
         })
 
