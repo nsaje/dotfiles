23bf67f38aa8cb61e81938443e7af5157b2ae042
Simon Mihevc <simon.mihevc@zemanta.com>
2016-04-28 12:31:09 +0200

    added permission for agency column

--- server/zemauth/models.py
+++ server/zemauth/models.py
@@ -167,6 +167,7 @@ class User(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
             ('can_include_model_ids_in_reports', 'Can include model ids in reports'),
             ('has_supporthero', 'Has Supporthero snippet'),
             ('can_filter_sources_through_table', 'Can filter sources through sources table'),
+            ('can_view_account_agency_information', 'Can view relations between account and agency'),
         )
 
     def get_full_name(self):
