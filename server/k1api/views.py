c3da6aa01887930e23bcb7c470a15f8efa6521a5
matematik7 <domen@ipavec.net>
2016-04-29 11:09:53 +0200

    Fix adgroup query

--- server/k1api/views.py
+++ server/k1api/views.py
@@ -320,8 +320,9 @@ def get_accounts_slugs_ad_groups(request):
     for conversion_pixel in conversion_pixels:
         data[conversion_pixel.account_id]['slugs'].append(conversion_pixel.slug)
 
-    ad_groups = (dash.models.AdGroup.objects
-                 .select_related('campaign__account')
+    ad_groups = (dash.models.AdGroup.objects.all()
+                 .exclude_archived()
+                 .select_related('campaign')
                  .filter(campaign__account_id__in=accounts))
     for ad_group in ad_groups:
         data[ad_group.campaign.account_id]['ad_groups'][ad_group.id] = {
