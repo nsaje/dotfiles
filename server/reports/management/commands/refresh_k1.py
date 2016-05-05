40c7e391353d63a1b05ef5de4f5c753b5a1b572e
Jure Ham <jure@hamsworld.net>
2016-05-05 11:38:02 +0200

    Refresh k1 data command supports relative days

--- server/reports/management/commands/refresh_k1.py
+++ server/reports/management/commands/refresh_k1.py
@@ -10,9 +10,25 @@ class Command(ExceptionCommand):
     help = "Refreshes k1 daily statements and materialized views"
 
     def add_arguments(self, parser):
-        parser.add_argument('from_date', type=str)
+        parser.add_argument('from', type=str)
 
     def handle(self, *args, **options):
-        since = datetime.datetime.strptime(options['from_date'], '%Y-%m-%d')
+        err = []
+        since = None
+
+        try:
+            since = datetime.datetime.strptime(options['from'], '%Y-%m-%d')
+        except Exception as e:
+            err.append(e)
+
+            try:
+                delta = int(options['from'])
+                since = datetime.datetime.today() - datetime.timedelta(days=delta)
+            except Exception as e:
+                err.append(e)
+
+        if since is None:
+            print err
+            return
 
         reports.refresh_k1.refresh_k1_reports(since)
