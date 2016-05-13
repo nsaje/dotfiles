094406b1425d0fab1a8a0e7e125b2ea7b5265d3a
Gregor Ratajc <gregor.ratajc@zemanta.com>
2016-05-13 10:46:39 +0200

    Extended augment api

--- server/stats/api_breakdowns.py
+++ server/stats/api_breakdowns.py
@@ -4,20 +4,6 @@ from stats import helpers
 
 import redshiftapi.api_breakdowns
 
-"""
-Enhancments: futures for database queries
-   While RS is loading we can also be loading dash data.
-
-1. get report with ALL columns
-2. extra columns get stripped out later
-
-
-PROBLEM:
-  - how to get level data
-
-Example breakdown: [campaign, source]
-"""
-
 # TODO handle 'other' rows
 # TODO level specific api
 # TODO which columns should be queried (what exists per level)
@@ -27,6 +13,7 @@ DEFAULT_ORDER = ['-clicks']  # this is level specific
 
 def query(user, breakdown, constraints, breakdown_constraints, order=DEFAULT_ORDER, page=1, page_size=10):
     # TODO page, page_size should be outsorced to view - based on which view, exports have a bigger one
+    # TODO if sort is in dash than this should be sorted by dash data (fetch before)
 
     stats_rows = redshiftapi.api_breakdowns.query(
         breakdown,
@@ -36,28 +23,12 @@ def query(user, breakdown, constraints, breakdown_constraints, order=DEFAULT_ORD
         page,
         page_size)
 
+    helpers.augment(stats_rows)
+    _secure(stats_rows)
+
     return stats_rows
 
 
-def get_dash_data(breakdown, stats_data):
-    augmented_dimension = breakdown[-1]
-
-    if augmented_dimension == 'source':
-        # get all sources that reside in the stats
-        pass
-
-
-def _merge():
-    # merge dash and stats rows
-    pass
-
-
-def _augment(breakdown, stats_rows, dash_rows):
-    # we augment with the last level data
-    # add names, stats, constants etc to rows
-    pass
-
-
-def _secure():
+def _secure(stats_rows):
     # remove columns without permission
-    pass
+    return stats_rows
--- server/stats/helpers.py
+++ server/stats/helpers.py
@@ -1,6 +1,7 @@
 import copy
+import collections
 
-# remove colums without permissions
+from dash import models
 
 
 def extract_stats_constraints(constraints):
@@ -12,3 +13,109 @@ def extract_stats_constraints(constraints):
     del constraints['show_archived']
 
     return constraints
+
+
+def get_rows_by_obj_ids(stats_rows):
+
+    rows_by_obj_ids = collections.defaultdict(lambda: collections.defaultdict(list))
+
+    # collect objects that need to be augmented
+    for row in stats_rows:
+
+        # TODO could this sorting be a memory bottleneck?
+        # should rows be indexed and only indices stored?
+        # but if references are stored than it should be no problem
+
+        if 'account_id' in row:
+            rows_by_obj_ids['account_id'][row['account_id']].append(row)
+
+        if 'campaign_id' in row:
+            rows_by_obj_ids['campaign_id'][row['campaign_id']].append(row)
+
+        if 'ad_group_id' in row:
+            rows_by_obj_ids['ad_group_id'][row['ad_group_id']].append(row)
+
+        if 'content_ad_id' in row:
+            rows_by_obj_ids['content_ad_id'][row['content_ad_id']].append(row)
+
+        if 'source_id' in row:
+            rows_by_obj_ids['source_id'][row['source_id']].append(row)
+
+        if 'publisher' in row:
+            rows_by_obj_ids['publisher'][row['publisher']].append(row)
+
+    return rows_by_obj_ids
+
+
+def get_needed_objs(rows_by_obj_ids):
+
+    objects_dict = collections.defaultdict(set)
+
+    accounts = None
+    campaigns = None
+    ad_groups = None
+    content_ads = None
+    sources = None
+
+    if 'account_id' in rows_by_obj_ids:
+        ids = rows_by_obj_ids['account_id']
+        accounts = models.Account.objects.filter(pk__in=ids)
+        objects_dict['account_id'] = {x.id: x for x in accounts}
+
+    if 'campaign_id' in rows_by_obj_ids:
+        ids = rows_by_obj_ids['campaign_id']
+        campaigns = models.Campaign.objects.filter(pk__in=ids)
+        objects_dict['campaign_id'] = {x.id: x for x in campaigns}
+
+    if 'ad_group_id' in rows_by_obj_ids:
+        ids = rows_by_obj_ids['ad_group_id']
+        ad_groups = models.AdGroup.objects.filter(pk__in=ids)
+        objects_dict['ad_group_id'] = {x.id: x for x in ad_groups}
+
+    if 'content_ad_id' in rows_by_obj_ids:
+        ids = rows_by_obj_ids['content_ad_id']
+        content_ads = models.ContentAd.objects.filter(pk__in=ids)
+        objects_dict['content_ad_id'] = {x.id: x for x in content_ads}
+
+    if 'source_id' in rows_by_obj_ids:
+        ids = rows_by_obj_ids['source_id']
+        sources = models.Source.objects.filter(pk__in=ids)
+        objects_dict['source_id'] = {x.id: x for x in sources}
+
+    # TODO data for status and running status
+
+    return objects_dict
+
+
+def augment(stats_rows):
+    # TODO constants for other dimensions (age?), unknown
+    # TODO can data between dimensions be shared? I think not - as
+    # upper dimension needs a superset
+
+    rows_by_obj_ids = get_rows_by_obj_ids(stats_rows)
+    objs_by_id = get_needed_objs(rows_by_obj_ids)
+
+    def rows(dimension):
+        for obj_id, rows in rows_by_obj_ids[dimension].iteritems():
+            for row in rows:
+                yield row, objs_by_id[dimension][obj_id], obj_id
+
+    if 'account_id' in rows_by_obj_ids:
+        for row, account, _ in rows('account_id'):
+            row['account_name'] = account.name
+
+    if 'campaign_id' in rows_by_obj_ids:
+        for row, campaign, _ in rows('campaign_id'):
+            row['campaign_name'] = campaign.name
+
+    if 'ad_group_id' in rows_by_obj_ids:
+        for row, ad_group, _ in rows('ad_group_id'):
+            row['ad_group_name'] = ad_group.name
+
+    if 'content_ad_id' in rows_by_obj_ids:
+        for row, content_ad, _ in rows('content_ad_id'):
+            row['content_ad_title'] = content_ad.title
+
+    if 'source_id' in rows_by_obj_ids:
+        for row, source, _ in rows('source_id'):
+            row['source_name'] = source.name
