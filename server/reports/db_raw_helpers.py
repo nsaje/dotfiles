6dd0ebd5b94189a475cf9a885e69ea6dceeced34
Gregor Ratajc <gregor.ratajc@zemanta.com>
2016-05-16 14:33:13 +0200

    Revert changes to redshift.py module

--- server/reports/db_raw_helpers.py
+++ server/reports/db_raw_helpers.py
@@ -4,6 +4,15 @@ from django.db import models
 from django.db.models.query import QuerySet
 
 
+def dictfetchall(cursor):
+    desc = cursor.description
+
+    return [
+        dict(zip([col[0] for col in desc], row))
+        for row in cursor.fetchall()
+    ]
+
+
 def is_collection(value):
     return ((isinstance(value, collections.Iterable) or isinstance(value, QuerySet))
             and type(value) not in (str, unicode))
@@ -21,7 +30,7 @@ def extract_obj_ids(objects):
     elif is_collection(objects):
         return [get_obj_id(item) for item in objects]
 
-    return get_obj_id(objects)
+    return objects
 
 
 def get_obj_id(obj):
@@ -30,3 +39,27 @@ def get_obj_id(obj):
     return obj
 
 
+class MyCursor(object):
+    def __init__(self, cursor):
+        self.cursor = cursor
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def execute(self, statement, params):
+        self.cursor.execute(statement, params)
+
+    def fetchall(self):
+        return self.cursor.fetchall()
+
+    def dictfetchall(self):
+        return dictfetchall(self.cursor)
+
+    def close(self):
+        self.cursor.close()
+
+    def mogrify(self, *args, **kwargs):
+        return self.cursor.mogrify(*args, **kwargs)
--- server/reports/redshift.py
+++ server/reports/redshift.py
@@ -8,10 +8,7 @@ import influx
 from utils.statsd_helper import statsd_timer
 
 from reports import exc
-from reports.db_raw_helpers import is_collection
-
-from redshiftapi.db import get_cursor
-
+from reports.db_raw_helpers import MyCursor, is_collection
 
 JSON_KEY_DELIMITER = '--'
 S3_FILE_URI = 's3://{bucket_name}/{key}'
@@ -37,6 +34,10 @@ def delete_contentadstats(date, campaign_id):
 def _execute(query, params):
     with get_cursor() as cursor:
         cursor.execute(query, params)
+
+
+def get_cursor():
+    return MyCursor(connections[settings.STATS_DB_NAME].cursor())
 
 
 @statsd_timer('reports.redshift', 'delete_contentadstatsdiff')
