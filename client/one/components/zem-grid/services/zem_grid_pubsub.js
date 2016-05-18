88bd8419da9b113ea61d0f578f83852a3bbfa7bd
Jure Polutnik <jure.polutnik@gmail.com>
2016-05-18 22:40:24 +0200

    Minor cleanup.

--- client/one/components/zem-grid/services/zem_grid_pubsub.js
+++ client/one/components/zem-grid/services/zem_grid_pubsub.js
@@ -2,11 +2,6 @@
 'use strict';
 
 oneApp.factory('zemGridPubSub', [function () {
-
-    // PubSub that hides AngularJS scope events mechanism and
-    // provides some simplifications over it (debouncing, etc.)
-    // and removes the need for child directives to work with scopes.
-
     var EVENTS = {
         BODY_HORIZONTAL_SCROLL: 'zem-grid-pubsub-bodyHorizontalScroll',
         BODY_VERTICAL_SCROLL: 'zem-grid-pubsub-bodyVerticalScroll',
