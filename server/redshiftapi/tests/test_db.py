7b09b44dc1b790abce15e5a959e37fcf8a7daeec
Gregor Ratajc <gregor.ratajc@zemanta.com>
2016-05-20 18:15:56 +0200

    Fixes db tests

--- server/redshiftapi/tests/test_db.py
+++ server/redshiftapi/tests/test_db.py
@@ -9,9 +9,19 @@ class DBTest(TestCase):
 
     def test_dictfetchall(self):
         mock_cursor = Mock()
-        mock_cursor.description = ['foo', 'bar']
+        mock_cursor.description = [['foo'], ['bar']]
         mock_cursor.fetchall.return_value = [['a', 1]]
 
         result = db.dictfetchall(mock_cursor)
 
-        self.assertEqual(result, [{'b': 1, 'f': 'a'}])
+        self.assertEqual(result, [{'bar': 1, 'foo': 'a'}])
+
+    def test_namedtuplefetchall(self):
+        mock_cursor = Mock()
+        mock_cursor.description = [['foo'], ['bar']]
+        mock_cursor.fetchall.return_value = [['a', 1]]
+
+        result = db.namedtuplefetchall(mock_cursor)
+
+        self.assertEqual(result[0].bar, 1)
+        self.assertEqual(result[0].foo, 'a')
