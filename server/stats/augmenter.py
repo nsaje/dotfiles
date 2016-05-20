2106d87cd99998e803d79fc1071c9b54a253ead3
Gregor Ratajc <gregor.ratajc@zemanta.com>
2016-05-20 10:14:24 +0200

    Fix augmenter cyc complexity

--- server/stats/augmenter.py
+++ server/stats/augmenter.py
@@ -6,92 +6,91 @@ from stats import constants
 from stats import helpers
 
 
+UNKNOWN = 'Unknown'
+
+
 def augment(breakdown, stats_rows, target_dimension):
-    rows_by_obj_ids = _get_rows_by_obj_ids(stats_rows, target_dimension)
-    objs_by_id = _get_needed_objs(rows_by_obj_ids)
 
-    def rows(dimension):
-        # helper for iterating through rows of the selected dimension
-        rows_by_dimension_id = rows_by_obj_ids.get(dimension, {})
-        for obj_id, rows in rows_by_dimension_id.iteritems():
-            for row in rows:
-                yield row, objs_by_id[dimension][obj_id], obj_id
+    if target_dimension == constants.StructureDimension.ACCOUNT:
+        augment_accounts(stats_rows)
 
-    # only the targeted dimension will be selected here
-    for row, account, _ in rows('account_id'):
-        row['account_name'] = account.name if account else 'Unknown'
+    if target_dimension == constants.StructureDimension.CAMPAIGN:
+        augment_campaigns(stats_rows)
 
-    for row, campaign, _ in rows('campaign_id'):
-        row['campaign_name'] = campaign.name if campaign else 'Unknown'
+    if target_dimension == constants.StructureDimension.AD_GROUP:
+        augment_ad_groups(stats_rows)
 
-    for row, ad_group, _ in rows('ad_group_id'):
-        row['ad_group_name'] = ad_group.name if ad_group else 'Unknown'
+    if target_dimension == constants.StructureDimension.CONTENT_AD:
+        augment_content_ads(stats_rows)
 
-    for row, content_ad, _ in rows('content_ad_id'):
-        row['content_ad_title'] = content_ad.title if content_ad else 'Unknown'
-
-    for row, source, _ in rows('source_id'):
-        row['source_name'] = source.name if source else 'Unknown'
+    if target_dimension == constants.StructureDimension.SOURCE:
+        augment_source(stats_rows)
 
     for row in stats_rows:
         row['breakdown_id'] = helpers.create_breakdown_id(breakdown, row)
         row['parent_breakdown_id'] = helpers.create_breakdown_id(breakdown[:-1], row) if breakdown else None
 
-def _get_rows_by_obj_ids(stats_rows, target_dimension):
-    """
-    Collect rows by targeted dimension id.
-    """
 
-    rows_by_obj_ids = collections.defaultdict(lambda: collections.defaultdict(list))
-    dimension_identifier = constants.get_dimension_identifier(target_dimension)
-
+def augment_accounts(stats_rows):
+    rows_by_id = collections.defaultdict(list)
     for row in stats_rows:
+        rows_by_id[row['account_id']].append(row)
 
-        if dimension_identifier in row:
-            rows_by_obj_ids[dimension_identifier][row[dimension_identifier]].append(row)
+    accounts = models.Account.objects.filter(pk__in=rows_by_id.keys())
+    account_by_id = {x.pk: x for x in accounts}
 
-    return rows_by_obj_ids
+    for account_id, row in rows_by_id.items():
+        account = account_by_id.get(account_id)
+        row['account_name'] = account.name if account else UNKNOWN
 
 
-def _get_needed_objs(rows_by_obj_ids):
-    """
-    Load objects for rows by dimension ids.
-    """
+def augment_campaigns(stats_rows):
+    rows_by_id = collections.defaultdict(list)
+    for row in stats_rows:
+        rows_by_id[row['campaign_id']].append(row)
 
-    objects_dict = collections.defaultdict(set)
+    campaigns = models.Campaign.objects.filter(pk__in=rows_by_id.keys())
+    campaign_by_id = {x.pk: x for x in campaigns}
 
-    accounts = None
-    campaigns = None
-    ad_groups = None
-    content_ads = None
-    sources = None
+    for campaign_id, row in rows_by_id.items():
+        campaign = campaign_by_id.get(campaign_id)
+        row['campaign_name'] = campaign.name if campaign else UNKNOWN
 
-    if 'account_id' in rows_by_obj_ids:
-        ids = rows_by_obj_ids['account_id']
-        accounts = models.Account.objects.filter(pk__in=ids)
-        objects_dict['account_id'] = {x.id: x for x in accounts}
 
-    if 'campaign_id' in rows_by_obj_ids:
-        ids = rows_by_obj_ids['campaign_id']
-        campaigns = models.Campaign.objects.filter(pk__in=ids)
-        objects_dict['campaign_id'] = {x.id: x for x in campaigns}
+def augment_ad_groups(stats_rows):
+    rows_by_id = collections.defaultdict(list)
+    for row in stats_rows:
+        rows_by_id[row['ad_group_id']].append(row)
 
-    if 'ad_group_id' in rows_by_obj_ids:
-        ids = rows_by_obj_ids['ad_group_id']
-        ad_groups = models.AdGroup.objects.filter(pk__in=ids)
-        objects_dict['ad_group_id'] = {x.id: x for x in ad_groups}
+    ad_groups = models.AdGroup.objects.filter(pk__in=rows_by_id.keys())
+    ad_group_by_id = {x.pk: x for x in ad_groups}
 
-    if 'content_ad_id' in rows_by_obj_ids:
-        ids = rows_by_obj_ids['content_ad_id']
-        content_ads = models.ContentAd.objects.filter(pk__in=ids)
-        objects_dict['content_ad_id'] = {x.id: x for x in content_ads}
+    for ad_group_id, row in rows_by_id.items():
+        ad_group = ad_group_by_id.get(ad_group_id)
+        row['ad_group_name'] = ad_group.name if ad_group else UNKNOWN
 
-    if 'source_id' in rows_by_obj_ids:
-        ids = rows_by_obj_ids['source_id']
-        sources = models.Source.objects.filter(pk__in=ids)
-        objects_dict['source_id'] = {x.id: x for x in sources}
 
-    # TODO data for status and running status
+def augment_content_ads(stats_rows):
+    rows_by_id = collections.defaultdict(list)
+    for row in stats_rows:
+        rows_by_id[row['content_ad_id']].append(row)
 
-    return objects_dict
+    content_ads = models.ContentAd.objects.filter(pk__in=rows_by_id.keys())
+    content_ad_by_id = {x.pk: x for x in content_ad_by_id}
 
+    for content_ad_id, row in rows_by_id.items():
+        content_ad = content_ad_by_id.get(content_ad_id)
+        row['content_ad_title'] = content_ad.title if content_ad else UNKNOWN
+
+
+def augment_source(stats_rows):
+    rows_by_id = collections.defaultdict(list)
+    for row in stats_rows:
+        rows_by_id[row['source_id']].append(row)
+
+    sources = models.Source.objects.filter(pk__in=rows_by_id.keys())
+    source_by_id = {x.pk: x for x in sources}
+
+    for source_id, row in rows_by_id.items():
+        source = source_by_id.get(source_id)
+        row['source_name'] = source.name if source else UNKNOWN
