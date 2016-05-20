0dfd85c853dfe18373aaf87fab849d19c5f22c71
Simon Mihevc <simon.mihevc@zemanta.com>
2016-05-20 14:27:31 +0200

    styled a basic best performers table

--- client/one/js/controllers/content_insights.js
+++ client/one/js/controllers/content_insights.js
@@ -1,6 +1,18 @@
 /* globals oneApp */
 oneApp.controller('ContentInsightsCtrl', ['$scope', '$state', 'api', 'zemNavigationService', function ($scope, $state, api, zemNavigationService) { // eslint-disable-line max-len
     $scope.infoboxLinkTo = 'main.campaigns.settings';
+    $scope.summary = 'Titles';
+    $scope.metric = 'CTR';
+    $scope.rows = [
+            {
+                summary: 'Test title',
+                metric: 10.5,
+            },
+            {
+                summary: 'Test title 2',
+                metric: 0.1,
+            },
+        ];
 
     $scope.getInfoboxData = function () {
         api.campaignOverview.get(
