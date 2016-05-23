ba104bfdd0c08248164bd64dccd6e720a178f3c8
Gregor Ratajc <gregor.ratajc@zemanta.com>
2016-05-23 18:16:13 +0200

    Use as_kw tag in templates instead of ifs

--- server/redshiftapi/templates/part_trunc_month.sql
+++ server/redshiftapi/templates/part_trunc_month.sql
@@ -1,2 +1,2 @@
 {% load backtosql_tags %}
-DATE_TRUNC('month', {{ p }}{{ column_name }}) {{ alias|as_kw }}
+TRUNC(DATE_TRUNC('month', {{ p }}{{ column_name }})) {{ alias|as_kw }}
--- server/redshiftapi/templates/part_trunc_week.sql
+++ server/redshiftapi/templates/part_trunc_week.sql
@@ -1,3 +1,3 @@
 -- TODO note this is monday-based, not start_date based
 {% load backtosql_tags %}
-DATE_TRUNC('week', {{ p }}{{ column_name }}) {{ alias|as_kw }}
+TRUNC(DATE_TRUNC('week', {{ p }}{{ column_name }})) {{ alias|as_kw }}
