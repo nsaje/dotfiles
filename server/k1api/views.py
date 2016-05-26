5b55c24cb40320b41f7a40dcfe8b4014eadded02
Simon Mihevc <simon.mihevc@zemanta.com>
2016-05-26 10:38:48 +0200

    master merge

--- server/k1api/views.py
+++ server/k1api/views.py
@@ -556,10 +556,10 @@ def get_ad_groups_exchanges(request):
     if ad_group_id:
         ad_groups_settings_query = ad_groups_settings_query.filter(ad_group__id=ad_group_id)
     else:
-        ad_groups_settings_query = ad_groups_settings_query.all()
+        ad_groups_settings_query = ad_groups_settings_query.filter(archived=False)
 
-    ad_groups_settings = ad_groups_settings_query.group_current_settings().select_related('ad_group')
-    ad_group_settings_map = {ags.ad_group: ags for ags in ad_groups_settings if ad_group_id or not ags.archived}
+    ad_groups_settings = ad_groups_settings_query.group_current_settings()
+    ad_group_settings_map = {ags.ad_group_id: ags for ags in ad_groups_settings}
 
     ad_group_sources_settings = (dash.models.AdGroupSourceSettings.objects
                                  .filter(ad_group_source__ad_group__in=ad_group_settings_map.keys())
@@ -567,13 +567,12 @@ def get_ad_groups_exchanges(request):
                                  .filter(ad_group_source__source__deprecated=False)
                                  .group_current_settings()
                                  .select_related('ad_group_source',
-                                                 'ad_group_source__ad_group',
                                                  'ad_group_source__source'))
 
     ad_group_sources = defaultdict(list)
 
     for ad_group_source_settings in ad_group_sources_settings:
-        ad_group_settings = ad_group_settings_map[ad_group_source_settings.ad_group_source.ad_group]
+        ad_group_settings = ad_group_settings_map[ad_group_source_settings.ad_group_source.ad_group_id]
         if (ad_group_settings.state == constants.AdGroupSettingsState.ACTIVE and
                 ad_group_source_settings.state == constants.AdGroupSourceSettingsState.ACTIVE):
             source_state = constants.AdGroupSettingsState.ACTIVE
@@ -586,7 +585,7 @@ def get_ad_groups_exchanges(request):
             'cpc_cc': ad_group_source_settings.cpc_cc,
             'daily_budget_cc': ad_group_source_settings.daily_budget_cc,
         }
-        ad_group_sources[ad_group_settings.ad_group.id].append(source)
+        ad_group_sources[ad_group_settings.ad_group_id].append(source)
 
     return _response_ok(ad_group_sources)
 
