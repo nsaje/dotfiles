4b00f866d3117de30f1a1839f64e734b577100ce
Jure Polutnik <jure.polutnik@gmail.com>
2016-05-27 15:11:36 +0200

    Minor fix

--- client/one/components/zem-grid/services/zem_grid_parser.js
+++ client/one/components/zem-grid/services/zem_grid_parser.js
@@ -70,7 +70,8 @@ oneApp.factory('zemGridParser', ['$filter', 'zemGridConstants', function ($filte
 
     function parseStats (grid, stats) {
         // Parse (pre-filter) all stats that does not require special handling to remove the
-        // need for filters in the templates (huge performance gain)
+        // need for filters in the templates (huge performance gain). If field can't be parsed
+        // here just use the same value as in data stats
         var parsedStats = {};
         grid.header.columns.forEach(function (col) {
             var parsedValue;
@@ -98,7 +99,7 @@ oneApp.factory('zemGridParser', ['$filter', 'zemGridConstants', function ($filte
     }
 
     function parseDateTime (value) {
-        if (!value) 'N/A';
+        if (!value) return 'N/A';
         return $filter('date')(value, 'M/d/yyyy h:mm a');
     }
 
