112aef289cf7d29856c48bee7bd0f1eab8243395
Gregor Ratajc <gregor.ratajc@zemanta.com>
2016-05-31 10:43:07 +0200

    Extracts model helpers from redshiftapi

--- server/redshiftapi/models.py
+++ server/redshiftapi/models.py
@@ -1,29 +1,6 @@
 import backtosql
 
-
-BREAKDOWN = 1
-AGGREGATES = 2
-
-
-class RSBreakdownMixin(object):
-    """
-    Mixin that defines breakdowns specific model features.
-    """
-
-    @classmethod
-    def get_best_view(cls, breakdown):
-        """ Returns the SQL view that best fits the breakdown """
-        raise NotImplementedError()
-
-    @classmethod
-    def get_breakdown(cls, breakdown):
-        """ Selects breakdown subset of columns """
-        return cls.select_columns(subset=breakdown)
-
-    @classmethod
-    def get_aggregates(cls):
-        """ Returns all the aggregate columns """
-        return cls.select_columns(group=AGGREGATES)
+from redshiftapi.model_helpers import RSBreakdownMixin, AGGREGATES, BREAKDOWN
 
 
 class RSContentAdStats(backtosql.Model, RSBreakdownMixin):
