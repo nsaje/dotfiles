289404b3ce79635c0ac89caf0434829c342fd1fa
Nejc Saje <nejc@saje.info>
2016-05-31 10:19:23 +0200

    refactor for multiple possible accounts

--- server/dash/management/commands/create_demo_snapshot.py
+++ server/dash/management/commands/create_demo_snapshot.py
@@ -22,6 +22,11 @@ logger = logging.getLogger(__name__)
 
 ACCOUNT_DUMP_SETTINGS = {
     'primary': 'dash.account',  # This is our reference model.
+    'prefetch_related': [ # prefetch fields
+        'campaign_set__adgroup_set__contentad_set__contentadsource_set__source',
+        'campaign_set__adgroup_set__adgroupsource_set__source__source_type',
+        'campaign_set__adgroup_set__adgroupsource_set__source_credentials',
+    ],
     'dependents': [  # These are the attributes/methods of the model that we wish to dump.
         'get_current_settings',
         {
@@ -50,44 +55,35 @@ ACCOUNT_DUMP_SETTINGS = {
 class Command(ExceptionCommand):
     help = """ Create a DB snapshot for demo deploys. """
 
-    def __init__(self):
-        super(ExceptionCommand, self).__init__()
+    def handle(self, *args, **options):
         self.serialize_list = collections.OrderedDict()
-        self.name_pools = demo_anonymizer.DemoNamePools(
-            ['Top 4 Mobile Carrier'],
-            ['Brand Awareness Campaign', 'Earned Media Promotion & Retargeting', 'The Quiz'],
-            ['Audio & Connected audience segment', 'Connected Family audience segment',
-             'Full Feature audience segment', '4G LTE', 'Best Value for International Travel',
-             'Waive early termination fee', 'Digital Moms', 'Hipsters', 'Teenagers'])
-        self.demo_users_set = None
-
-        demo_anonymizer.set_name_pools(self.name_pools)
+        real_account_pks = [279]
         demo_anonymizer.set_fake_factory(faker.Faker())
 
-    def handle(self, *args, **options):
+        self.demo_users_set = None
         demo_users = loading.get_model('zemauth', 'User').objects.filter(email__endswith='test@test.com').prefetch_related(
             'groups__permissions__content_type',
             'user_permissions__content_type',
         )
         self.demo_users_set = set(demo_users)
 
-        # pks = [168]
-        pks = [279]
-        # pks = [203]
         (app_label, model_name) = ACCOUNT_DUMP_SETTINGS['primary'].split('.')
         model_to_dump = loading.get_model(app_label, model_name)
-        real_accounts = model_to_dump.objects.filter(pk__in=[int(i) for i in pks]).prefetch_related(
-            'campaign_set__adgroup_set__contentad_set__contentadsource_set__source',
-            'campaign_set__adgroup_set__adgroupsource_set__source__source_type',
-            'campaign_set__adgroup_set__adgroupsource_set__source_credentials',
-        )
-        for account in real_accounts:
-            account.users = demo_users
-            self._add_object_dependencies(account, ACCOUNT_DUMP_SETTINGS['dependents'])
-        self._serialize_fully()
-        data = serialize('json', [o for o in self.serialize_list if o is not None],
-                         indent=4)
-        #  use_natural_foreign_keys=True, use_natural_primary_keys=True,
+        for real_account_pk in real_account_pks:
+            name_pools = demo_anonymizer.DemoNamePools(
+                ['Top 4 Mobile Carrier'],
+                ['Brand Awareness Campaign', 'Earned Media Promotion & Retargeting', 'The Quiz'],
+                ['Audio & Connected audience segment', 'Connected Family audience segment',
+                 'Full Feature audience segment', '4G LTE', 'Best Value for International Travel',
+                 'Waive early termination fee', 'Digital Moms', 'Hipsters', 'Teenagers'])
+            demo_anonymizer.set_name_pools(name_pools)
+            real_account = model_to_dump.objects.filter(pk=real_account_pk).prefetch_related(
+                ACCOUNT_DUMP_SETTINGS['prefetch_related']
+            )
+            real_account.users = demo_users
+            self._add_object_dependencies(real_account, ACCOUNT_DUMP_SETTINGS['dependents'])
+            self._serialize_fully()
+        data = serialize('json', [o for o in self.serialize_list if o is not None], indent=4)
 
         self.stdout.write(data)
         # logger.info("QUERIES: %s", str(connection.queries))
