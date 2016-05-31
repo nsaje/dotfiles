6bf657e6e1c6aaf6e7c072b4b3716e15b0e0cdea
Nejc Saje <nejc@saje.info>
2016-05-31 14:06:14 +0200

    Fix typo

--- docker/prepare-demo.sh
+++ docker/prepare-demo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "$CONF_ENV" -ne "demo" || "${DB_PORT_5432_TCP_ADDR}" -ne "db" ]]; do
+if [[ "$CONF_ENV" -ne "demo" || "${DB_PORT_5432_TCP_ADDR}" -ne "db" ]]; then
     echo "ERROR: Running prepare-demo in non-demo environment, which would drop the DB! Exiting."
     exit 1
 fi
