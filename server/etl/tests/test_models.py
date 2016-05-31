e3f9158bef00badbf7ac1c4abf663c450c13d073
Gregor Ratajc <gregor.ratajc@zemanta.com>
2016-05-31 13:51:01 +0200

    Fix cc

--- server/etl/tests/test_models.py
+++ server/etl/tests/test_models.py
@@ -8,7 +8,7 @@ from etl import models
 class ColumnsTest(TestCase, backtosql.TestSQLMixin):
 
     def test_conversion_window(self):
-        column = models.K1Conversions.get_column('conversion_window')
+        column = models.K1Conversions.conversion_window
 
         self.assertSQLEquals(column.column_as_alias('T'), """\
         CASE \
@@ -18,11 +18,12 @@ class ColumnsTest(TestCase, backtosql.TestSQLMixin):
         END AS conversion_window""")
 
     def test_count(self):
-        column = models.K1Conversions.get_column('count')
+        column = models.K1Conversions.count
 
         self.assertSQLEquals(column.column_as_alias('T'), "COUNT(*) AS count")
 
     def test_json_dict_sum(self):
         column = models.K1PostclickStats.conversions
 
-        self.assertSQLEquals(column.column_as_alias('T'), "json_dict_sum(listagg(T.conversions, ';'), ';') as conversions")
+        self.assertSQLEquals(column.column_as_alias('T'),
+                             "json_dict_sum(listagg(T.conversions, ';'), ';') as conversions")
