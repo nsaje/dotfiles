1cb5b65367768e324785be5a950e860a9369a96b
Nejc Saje <nejc@saje.info>
2016-06-01 11:47:34 +0200

    clean up requirements and settings

--- server/server/settings.py
+++ server/server/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fixture_magic',
     'import_export',
     'dash',
     'zemauth',
