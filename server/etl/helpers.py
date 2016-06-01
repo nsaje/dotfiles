75e6d96f97c04d13f5b05c3f1618fa0d0ba4eeeb
Gregor Ratajc <gregor.ratajc@zemanta.com>
2016-06-01 14:22:43 +0200

    Adds helpers to extract age, gender, source_slug

--- server/etl/helpers.py
+++ server/etl/helpers.py
@@ -41,3 +41,79 @@ def calculate_effective_cost(cost, data_cost, factors):
     license_fee = (effective_cost + effective_data_cost) * pct_license_fee
 
     return effective_cost, effective_data_cost, license_fee
+
+
+def extract_source_slug(source_slug):
+    if source_slug.startswith('b1_'):
+        return source_slug[3:]
+    return source_slug
+
+
+def extract_device_type(device_type):
+    if device_type == 1:
+        return constants.DeviceType.MOBILE
+    elif device_type == 2:
+        return constants.DeviceType.DESKTOP
+    elif device_type == 5:
+        return constants.DeviceType.TABLET
+    return constants.DeviceType.UNDEFINED
+
+
+def extract_age(age):
+    age = age.strip()
+    if age == '18-20':
+        return constants.AgeGroup.AGE_18_20
+    elif age == '21-29':
+        return constants.AgeGroup.AGE_21_29
+    elif age == '30-39':
+        return constants.AgeGroup.AGE_30_39
+    elif age == '40-49':
+        return constants.AgeGroup.AGE_40_49
+    elif age == '50-64':
+        return constants.AgeGroup.AGE_50_64
+    elif age == '65+':
+        return constants.AgeGroup.AGE_65_MORE
+    return constants.AgeGroup.UNDEFINED
+
+
+def extract_gender(gender):
+    gender = gender.strip()
+    if gender == 'female':
+        return constants.Gender.WOMEN
+    elif gender == 'male':
+        return constants.Gender.MEN
+    return constants.Gender.UNDEFINED
+
+
+def extract_age_gender(age, gender):
+    mapping = {
+        constants.Gender.WOMEN: {
+            constants.AgeGroup.AGE_18_20: constants.AgeGenderGroup.AGE_18_20_WOMEN,
+            constants.AgeGroup.AGE_21_29: constants.AgeGenderGroup.AGE_21_29_WOMEN,
+            constants.AgeGroup.AGE_30_39: constants.AgeGenderGroup.AGE_30_39_WOMEN,
+            constants.AgeGroup.AGE_40_49: constants.AgeGenderGroup.AGE_40_49_WOMEN,
+            constants.AgeGroup.AGE_50_64: constants.AgeGenderGroup.AGE_50_64_WOMEN,
+            constants.AgeGroup.AGE_65_MORE: constants.AgeGenderGroup.AGE_65_MORE_WOMEN,
+        },
+        constants.Gender.MEN: {
+            constants.AgeGroup.AGE_18_20: constants.AgeGenderGroup.AGE_18_20_MEN,
+            constants.AgeGroup.AGE_21_29: constants.AgeGenderGroup.AGE_21_29_MEN,
+            constants.AgeGroup.AGE_30_39: constants.AgeGenderGroup.AGE_30_39_MEN,
+            constants.AgeGroup.AGE_40_49: constants.AgeGenderGroup.AGE_40_49_MEN,
+            constants.AgeGroup.AGE_50_64: constants.AgeGenderGroup.AGE_50_64_MEN,
+            constants.AgeGroup.AGE_65_MORE: constants.AgeGenderGroup.AGE_65_MORE_MEN,
+        },
+        constants.Gender.UNDEFINED: {
+            constants.AgeGroup.AGE_18_20: constants.AgeGenderGroup.AGE_18_20_UNDEFINED,
+            constants.AgeGroup.AGE_21_29: constants.AgeGenderGroup.AGE_21_29_UNDEFINED,
+            constants.AgeGroup.AGE_30_39: constants.AgeGenderGroup.AGE_30_39_UNDEFINED,
+            constants.AgeGroup.AGE_40_49: constants.AgeGenderGroup.AGE_40_49_UNDEFINED,
+            constants.AgeGroup.AGE_50_64: constants.AgeGenderGroup.AGE_50_64_UNDEFINED,
+            constants.AgeGroup.AGE_65_MORE: constants.AgeGenderGroup.AGE_65_MORE_UNDEFINED,
+        },
+    }
+
+    if gender in mapping:
+        return mapping[gender].get(age, constants.AgeGenderGroup.UNDEFINED)
+
+    return constants.AgeGenderGroup.UNDEFINED
--- server/etl/tests/test_helpers.py
+++ server/etl/tests/test_helpers.py
@@ -4,6 +4,8 @@ import decimal
 
 from django.test import TestCase
 
+import dash.constants
+
 from etl import helpers
 
 
@@ -39,3 +41,39 @@ class HelpersTest(TestCase, backtosql.TestSQLMixin):
         self.assertEqual(effective_cost, 50.0)
         self.assertEqual(effective_data_cost, 60.0)
         self.assertEqual(license_fee, 11.0)
+
+    def test_extract_source_slug(self):
+        self.assertEqual(helpers.extract_source_slug('b1_outbrain'), 'outbrain')
+        self.assertEqual(helpers.extract_source_slug('outbrain'), 'outbrain')
+
+    def test_extract_device_type(self):
+        self.assertEqual(helpers.extract_device_type(1), dash.constants.DeviceType.MOBILE)
+        self.assertEqual(helpers.extract_device_type(2), dash.constants.DeviceType.DESKTOP)
+        self.assertEqual(helpers.extract_device_type(5), dash.constants.DeviceType.TABLET)
+
+        self.assertEqual(helpers.extract_device_type(None), dash.constants.DeviceType.UNDEFINED)
+        self.assertEqual(helpers.extract_device_type(0), dash.constants.DeviceType.UNDEFINED)
+        self.assertEqual(helpers.extract_device_type(3), dash.constants.DeviceType.UNDEFINED)
+
+    def test_extract_age(self):
+        self.assertEqual(helpers.extract_age('18-20'), dash.constants.AgeGroup.AGE_18_20)
+        self.assertEqual(helpers.extract_age('21-29'), dash.constants.AgeGroup.AGE_21_29)
+        self.assertEqual(helpers.extract_age('30-39'), dash.constants.AgeGroup.AGE_30_39)
+        self.assertEqual(helpers.extract_age('40-49'), dash.constants.AgeGroup.AGE_40_49)
+        self.assertEqual(helpers.extract_age('50-64'), dash.constants.AgeGroup.AGE_50_64)
+        self.assertEqual(helpers.extract_age('65+'), dash.constants.AgeGroup.AGE_65_MORE)
+
+        self.assertEqual(helpers.extract_age('Lol'), dash.constants.AgeGroup.UNDEFINED)
+
+    def test_extract_gender(self):
+        self.assertEqual(helpers.extract_gender('male'), dash.constants.Gender.MEN)
+        self.assertEqual(helpers.extract_gender('female'), dash.constants.Gender.WOMEN)
+        self.assertEqual(helpers.extract_gender('nesto'), dash.constants.Gender.UNDEFINED)
+
+    def test_extract_age_gender(self):
+        self.assertEqual(helpers.extract_age_gender(dash.constants.AgeGroup.AGE_50_64, dash.constants.Gender.WOMEN),
+                         dash.constants.AgeGenderGroup.AGE_50_64_WOMEN)
+        self.assertEqual(helpers.extract_age_gender(dash.constants.AgeGroup.AGE_21_29, dash.constants.Gender.UNDEFINED),
+                         dash.constants.AgeGenderGroup.AGE_21_29_UNDEFINED)
+        self.assertEqual(helpers.extract_age_gender(dash.constants.AgeGroup.UNDEFINED, dash.constants.Gender.WOMEN),
+                         dash.constants.AgeGenderGroup.UNDEFINED)
