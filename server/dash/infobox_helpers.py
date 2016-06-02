4038cc99e94e71a1150689d756a57d7db37ede25
Gregor Ratajc <gregor.ratajc@zemanta.com>
2016-06-02 11:38:11 +0200

    Fix missing function

--- server/dash/infobox_helpers.py
+++ server/dash/infobox_helpers.py
@@ -262,7 +262,7 @@ def calculate_allocated_and_available_credit(account):
     credits = _retrieve_active_creditlineitems(account, today)
     credit_total = credits.aggregate(
         amount_sum=Sum('amount'),
-        flat_fee_sum=dash.models.CC_TO_DEC_MULTIPLIER * Sum('flat_fee_cc'))
+        flat_fee_sum=converters.CC_TO_DECIMAL_DOLAR * Sum('flat_fee_cc'))
     budget_total = dash.models.BudgetLineItem.objects.filter(
         credit__in=credits
     ).aggregate(
