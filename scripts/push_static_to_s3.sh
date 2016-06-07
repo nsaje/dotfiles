456ceff637a8a61171dd7df8e36d8179ce2794f0
marko <marko.celan@zemanta.com>
2016-06-07 11:01:45 +0200

    add content-disposition to assets/ on S3 so Safari can handle it properly. See: https://trello.com/c/sy8UHKGO/302-example-csv-opens-in-safari-browser

--- scripts/push_static_to_s3.sh
+++ scripts/push_static_to_s3.sh
@@ -3,12 +3,15 @@
 if [ -d client/dist ]
 then
 	sudo pip install awscli
-	
+
 	S3_PATH="s3://z1-static/build-$CIRCLE_BUILD_NUM"
-	
-	aws s3 sync --acl public-read client/dist "${S3_PATH}/client"
+
+	# sync assets separately
+	aws s3 sync --acl public-read --exclude 'assets/*' client/dist "${S3_PATH}/client"
 	aws s3 sync --acl public-read server/static "${S3_PATH}/server"
-	
-	echo $CIRCLE_BUILD_NUM > latest.txt
-	aws s3api put-object --bucket z1-static --key latest.txt --body latest.txt
+
+	# Need to set content-disposition for attachments
+	aws s3 sync --acl public-read --content-disposition 'attachment' client/dist/one/assets "${S3_PATH}/client/one/assets"
+
+	echo $CIRCLE_BUILD_NUM | aws s3 cp - s3://z1-static/latest.txt
 fi
