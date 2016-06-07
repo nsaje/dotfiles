42dabb12d268c3453461659361929680e0e71eef
marko <marko.celan@zemanta.com>
2016-06-07 11:17:03 +0200

    --except param not behaving as expected

--- scripts/push_static_to_s3.sh
+++ scripts/push_static_to_s3.sh
@@ -6,12 +6,12 @@ then
 
 	S3_PATH="s3://z1-static/build-$CIRCLE_BUILD_NUM"
 
-	# sync assets separately
-	aws s3 sync --acl public-read --exclude 'assets/*' client/dist "${S3_PATH}/client"
+	# need to set content-disposition for attachments sync assets separately
+	aws s3 sync --acl public-read --content-disposition 'attachment' client/dist/one/assets "${S3_PATH}/client/one/assets"
+
+	aws s3 sync --acl public-read --exclude 'assets/' client/dist "${S3_PATH}/client"
 	aws s3 sync --acl public-read server/static "${S3_PATH}/server"
 
-	# Need to set content-disposition for attachments
-	aws s3 sync --acl public-read --content-disposition 'attachment' client/dist/one/assets "${S3_PATH}/client/one/assets"
 
 	echo $CIRCLE_BUILD_NUM | aws s3 cp - s3://z1-static/latest.txt
 fi
