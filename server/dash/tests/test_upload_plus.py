3979e5ddb3cd98ac1e92835b9a239b51fffcbd3d
Luka Šilovinac <luka.silovinac@zemanta.com>
2016-06-08 15:46:35 +0200

    Fix tests

--- server/dash/tests/test_upload_plus.py
+++ server/dash/tests/test_upload_plus.py
@@ -147,7 +147,7 @@ class PersistCandidatesTestCase(TestCase):
         self.assertTrue(s3_key.startswith('contentads/errors/3/test_upload'))
         self.assertTrue(s3_key.endswith('.csv'))
         self.assertEqual(
-            'url,title,image_url,tracker_urls,display_url,brand_name,description,call_to_action,label,image_crop,errors'
+            'url,title,image_url,impression_trackers,display_url,brand_name,description,call_to_action,label,image_crop,errors'
             '\r\nhttp://zemanta.com/blog,Zemanta blog,http://zemanta.com/img.jpg,,zemanta.com,Zemanta,Zemanta blog,Read'
             ' more,content ad 1,entropy,"Content unreachable, Image could not be processed"\r\n', content)
 
