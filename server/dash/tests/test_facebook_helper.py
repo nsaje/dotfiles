cb51815dd7f3f771660d8fd03c555ec68f600928
Matija Vižintin <matija.vizintin@gmail.com>
2016-06-08 20:05:06 +0200

    additional fb helper tests

--- server/dash/tests/test_facebook_helper.py
+++ server/dash/tests/test_facebook_helper.py
@@ -1,11 +1,10 @@
 import json
 
-from django.http import HttpResponse
 from django.test import TestCase, override_settings
 from mock import patch
 from requests import Response
 
-from dash import facebook_helper, constants
+from dash import facebook_helper, constants, models
 
 
 @override_settings(
@@ -18,7 +17,6 @@ class FacebookPageAccessTest(TestCase):
     def _create_response(status_code, content):
         response = Response()
         response.status_code = status_code
-        # response.encoding = 'utf-8'
         response._content = content
         return response
 
@@ -41,6 +39,40 @@ class FacebookPageAccessTest(TestCase):
     def _get_fb_api_url():
         return 'https://graph.facebook.com/%s/%s/pages'
 
+    @staticmethod
+    def _get_fb_account(page_id):
+        fb_account = models.FacebookAccount()
+        fb_account.page_url = page_id
+        return fb_account
+
+    @patch('requests.post')
+    def test_unchanged_page(self, mock):
+        page_id = 'existing_page_id'
+        fb_account = self._get_fb_account(page_id)
+        facebook_helper.update_facebook_account(fb_account, page_id)
+
+        self.assertEqual(fb_account.get_page_id(), page_id)
+        self.assertFalse(mock.called)
+
+    @patch('requests.post')
+    def test_changed_page(self, mock):
+        page_id = 'new_page_id'
+        fb_account = self._get_fb_account('old_page_id')
+        facebook_helper.update_facebook_account(fb_account, page_id)
+
+        self.assertEqual(fb_account.get_page_id(), page_id)
+        self.assertTrue(mock.called)
+
+    @patch('requests.post')
+    def test_clear_page(self, mock):
+        page_id = 'existing_page_id'
+        fb_account = self._get_fb_account(page_id)
+        facebook_helper.update_facebook_account(fb_account, None)
+
+        self.assertEqual(fb_account.get_page_id(), None)
+        self.assertEqual(fb_account.status, constants.FacebookPageRequestType.EMPTY)
+        self.assertFalse(mock.called)
+
     @patch('requests.post')
     def test_invalid_page(self, mock):
         mock.return_value = self._create_response(
@@ -49,11 +81,12 @@ class FacebookPageAccessTest(TestCase):
         )
 
         page_id = 'invalid_page_id'
-        status = facebook_helper.send_page_access_request(page_id)
+        fb_account = self._get_fb_account(page_id)
+        status = facebook_helper._send_page_access_request(fb_account)
 
         mock.assert_called_once_with(self._get_fb_api_url() % (self._get_version(), 'fake_app_id'),
                                      data=json.dumps(self._get_params(page_id)), headers=self._get_headers())
-        self.assertEqual(status, constants.FacebookPageRequestType.INVALID_PAGE)
+        self.assertEqual(status, constants.FacebookPageRequestType.INVALID)
 
     @patch('requests.post')
     def test_already_pending(self, mock):
@@ -63,7 +96,8 @@ class FacebookPageAccessTest(TestCase):
         )
 
         page_id = 'pending_page_id'
-        status = facebook_helper.send_page_access_request(page_id)
+        fb_account = self._get_fb_account(page_id)
+        status = facebook_helper._send_page_access_request(fb_account)
 
         mock.assert_called_once_with(self._get_fb_api_url() % (self._get_version(), 'fake_app_id'),
                                      data=json.dumps(self._get_params(page_id)), headers=self._get_headers())
@@ -77,12 +111,12 @@ class FacebookPageAccessTest(TestCase):
         )
 
         page_id = 'pending_page_id'
-        status = facebook_helper.send_page_access_request(page_id)
+        fb_account = self._get_fb_account(page_id)
+        status = facebook_helper._send_page_access_request(fb_account)
 
         mock.assert_called_once_with(self._get_fb_api_url() % (self._get_version(), 'fake_app_id'),
                                      data=json.dumps(self._get_params(page_id)), headers=self._get_headers())
         self.assertEqual(status, constants.FacebookPageRequestType.CONNECTED)
-
 
     @patch('requests.post')
     def test_approval_sent_successfully(self, mock):
@@ -92,7 +126,8 @@ class FacebookPageAccessTest(TestCase):
         )
 
         page_id = 'approve_me_page'
-        status = facebook_helper.send_page_access_request(page_id)
+        fb_account = self._get_fb_account(page_id)
+        status = facebook_helper._send_page_access_request(fb_account)
 
         mock.assert_called_once_with(self._get_fb_api_url() % (self._get_version(), 'fake_app_id'),
                                      data=json.dumps(self._get_params(page_id)), headers=self._get_headers())
@@ -106,7 +141,8 @@ class FacebookPageAccessTest(TestCase):
         )
 
         page_id = 'approve_me_page'
-        status = facebook_helper.send_page_access_request(page_id)
+        fb_account = self._get_fb_account(page_id)
+        status = facebook_helper._send_page_access_request(fb_account)
 
         mock.assert_called_once_with(self._get_fb_api_url() % (self._get_version(), 'fake_app_id'),
                                      data=json.dumps(self._get_params(page_id)), headers=self._get_headers())
