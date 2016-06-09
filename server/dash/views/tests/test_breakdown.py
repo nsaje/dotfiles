6a01c3d94c6e865ec7465dedaf44a395301353a5
Gregor Ratajc <gregor.ratajc@zemanta.com>
2016-06-09 14:46:41 +0200

    Adds tests

--- server/dash/views/tests/test_breakdown.py
+++ server/dash/views/tests/test_breakdown.py
@@ -11,6 +11,7 @@ from utils import test_helper
 
 from dash import models
 from dash.views import breakdown
+from dash import table
 
 
 class ExtractConstraintsTest(TestCase):
@@ -128,6 +129,154 @@ class AllAccountsBreakdownTestCase(TestCase):
             5
         )
 
+    @patch.object(table.AccountsAccountsTable, 'get')
+    def test_post_base_level(self, mock_table, mock_query):
+
+        mock_table.return_value = {
+            'order': '-clicks',
+            'pagination': {
+                'numPages': 10,
+                'count': 40,
+                'endIndex': 4,
+                'startIndex': 1,
+                'currentPage': 1,
+                'size': 4
+            },
+            'totals': None,
+            'rows': [{
+                'account': 11,
+                'account_type': 'Managed',
+                'agency': '',
+                'archived': False,
+                'click_discrepancy': 40.6292995154633,
+                'ctr': 0.746625150795136,
+                'default_account_manager': 'Ana Dejanovi\xc4\x87',
+                'default_sales_representative': 'John Castiglione',
+                'id': '11',
+                'impressions': 42541160,
+                'name': 'BuildDirect',
+                'pv_per_visit': 1.18423969243007,
+                'status': 1,
+            }, {
+                'account': 116,
+                'account_type': 'Self-managed',
+                'agency': 'MBuy',
+                'archived': False,
+                'click_discrepancy': None,
+                'ctr': 0.682212242285772,
+                'default_account_manager': 'Helen Wagner',
+                'default_sales_representative': 'David Kaplan',
+                'id': '116',
+                'impressions': 9484585,
+                'name': "Cat's Pride",
+                'pv_per_visit': None,
+                'status': 1,
+            }, {
+                'account': 305,
+                'account_type': 'Pilot',
+                'agency': '',
+                'archived': False,
+                'click_discrepancy': None,
+                'ctr': 0.445301342321372,
+                'default_account_manager': 'Tadej Pavli\xc4\x8d',
+                'default_sales_representative': 'David Kaplan',
+                'id': '305',
+                'impressions': 13136273,
+                'name': 'Outbrain',
+                'pv_per_visit': None,
+                'status': 1,
+            }, {
+                'account': 63,
+                'account_type': 'Managed',
+                'agency': '',
+                'archived': False,
+                'click_discrepancy': 28.166866857618,
+                'ctr': 0.374501558123542,
+                'default_account_manager': 'Ana Dejanovi\xc4\x87',  # TODO this should be unicode
+                'default_sales_representative': 'David Kaplan',
+                'id': '63',
+                'impressions': 23119797,
+                'name': 'Allstate',
+                'pv_per_visit': 1.22613994469098,
+                'status': 1,
+            }],
+        }
+
+        test_helper.add_permissions(self.user, ['can_access_table_breakdowns_feature'])
+
+        params = {
+            'limit': 2,
+            'offset': 1,
+            'order': '-clicks',
+            'start_date': '2016-01-01',
+            'end_date': '2016-02-03',
+            'filtered_sources': '1,3,4',
+            'show_archived': 'true',
+            'breakdown_page': None,
+        }
+
+        response = self.client.post(
+            reverse('breakdown_all_accounts', kwargs={
+                'breakdown': '/account'
+            }),
+            data=json.dumps({'params': params}),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        result = json.loads(response.content)
+
+        self.assertIsNone(result['data'][0]['breakdown_id'])
+
+        self.assertDictEqual(result['data'][0]['pagination'], {
+            'count': 40,
+            'limit': 2,
+            'offset': 1,
+        })
+
+        self.assertEqual(len(result['data']), 1)
+
+        self.assertItemsEqual(result['data'][0]['rows'], [{
+            u'account': 116,
+            u'account_id': 116,
+            u'account_name': u"Cat's Pride",
+            u'account_type': u'Self-managed',
+            u'agency': u'MBuy',
+            u'archived': False,
+            u'breakdown_id': u'116',
+            u'breakdown_name': u"Cat's Pride",
+            u'click_discrepancy': None,
+            u'ctr': 0.682212242285772,
+            u'default_account_manager': u'Helen Wagner',
+            u'default_sales_representative': u'David Kaplan',
+            u'id': u'116',
+            u'impressions': 9484585,
+            u'name': u"Cat's Pride",
+            u'parent_breakdown_id': None,
+            u'pv_per_visit': None,
+            u'status': 1,
+        }, {
+            u'account': 305,  # TODO remove when doing stats
+            u'account_id': 305,
+            u'account_name': u'Outbrain',
+            u'account_type': u'Pilot',
+            u'agency': u'',
+            u'archived': False,
+            u'breakdown_id': u'305',
+            u'breakdown_name': u'Outbrain',
+            u'click_discrepancy': None,
+            u'ctr': 0.445301342321372,
+            u'default_account_manager': u'Tadej Pavli\u010d',
+            u'default_sales_representative': u'David Kaplan',
+            u'id': u'305',  # TODO remove when doing stats
+            u'impressions': 13136273,
+            u'name': u'Outbrain',
+            u'parent_breakdown_id': None,
+            u'pv_per_visit': None,
+            u'status': 1,
+        }])
+
 
 @patch('stats.api_breakdowns.query')
 class AccountBreakdownTestCase(TestCase):
@@ -191,6 +340,124 @@ class AccountBreakdownTestCase(TestCase):
             33,
             5
         )
+
+    @patch.object(table.AccountCampaignsTable, 'get')
+    def test_post_base_level(self, mock_table, mock_query):
+        test_helper.add_permissions(self.user, ['can_access_table_breakdowns_feature'])
+
+        mock_table.return_value = {
+            'rows': [{
+                'pageviews': 92569,
+                'campaign': 350,
+                'cost': 10612.4835,
+                'impressions': 19996915,
+                'archived': False,
+                'state': 1,
+                'campaign_manager': 'Ana Dejanovi\xc4\x87',
+                'name': u'Blog Campaign [Mobile]',
+            }, {
+                'pageviews': 78853,
+                'campaign': 198,
+                'cost': 9196.1064,
+                'impressions': 9621740,
+                'archived': False,
+                'state': 1,
+                'campaign_manager': 'Ana Dejanovi\xc4\x87',
+                'name': u'Blog Campaign [Desktop]',
+            }, {
+                'pageviews': 51896,
+                'campaign': 413,
+                'cost': 7726.1054,
+                'impressions': 10441143,
+                'archived': False,
+                'state': 1,
+                'campaign_manager': 'Ana Dejanovi\xc4\x87',
+                'name': u'Learning Center',
+            }, {
+                'pageviews': None,
+                'campaign': 357,
+                'cost': 1766.137,
+                'impressions': 2481362,
+                'archived': False,
+                'state': 1,
+                'campaign_manager': 'Ana Dejanovi\xc4\x87',
+                'name': u'Earned Media',
+            }],
+            'incomplete_postclick_metrics': False,
+            'is_sync_recent': True,
+            'last_sync': None,
+            'is_sync_in_progress': False,
+            'totals': {
+                'pageviews': 223318,
+                'cost': 29300.8323,
+                'impressions': 42541160,
+            },
+            'order': u'-cost'
+        }
+
+        params = {
+            'limit': 2,
+            'offset': 1,
+            'order': '-clicks',
+            'start_date': '2016-01-01',
+            'end_date': '2016-02-03',
+            'filtered_sources': '1,3,4',
+            'show_archived': 'true',
+            'breakdown_page': None,
+        }
+
+        response = self.client.post(
+            reverse('breakdown_accounts', kwargs={
+                'account_id': 1,
+                'breakdown': '/campaign'
+            }),
+            data=json.dumps({'params': params}),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        result = json.loads(response.content)
+
+        self.assertIsNone(result['data'][0]['breakdown_id'])
+
+        self.assertDictEqual(result['data'][0]['pagination'], {
+            'count': None,
+            'limit': 2,
+            'offset': 1,
+        })
+
+        self.assertEqual(len(result['data']), 1)
+
+        self.assertItemsEqual(result['data'][0]['rows'], [{
+            u'archived': False,
+            u'breakdown_id': u'198',
+            u'breakdown_name': u'Blog Campaign [Desktop]',
+            u'campaign': 198,
+            u'campaign_id': 198,
+            u'campaign_manager': u'Ana Dejanovi\u0107',
+            u'campaign_name': u'Blog Campaign [Desktop]',
+            u'cost': 9196.1064,
+            u'impressions': 9621740,
+            u'name': u'Blog Campaign [Desktop]',
+            u'pageviews': 78853,
+            u'parent_breakdown_id': None,
+            u'state': 1,
+        }, {
+            u'archived': False,
+            u'breakdown_id': u'413',
+            u'breakdown_name': u'Learning Center',
+            u'campaign': 413,  # TODO remove when doing stats
+            u'campaign_id': 413,
+            u'campaign_manager': u'Ana Dejanovi\u0107',
+            u'campaign_name': u'Learning Center',
+            u'cost': 7726.1054,
+            u'impressions': 10441143,
+            u'name': u'Learning Center',  # TODO remove when doing stats
+            u'pageviews': 51896,
+            u'parent_breakdown_id': None,
+            u'state': 1,
+        }])
 
 
 @patch('stats.api_breakdowns.query')
