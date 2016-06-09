4032e407014b3d3230a16c061434c58e068ae938
Jure Polutnik <jure.polutnik@gmail.com>
2016-06-10 00:14:05 +0200

    Improve column resize (use styles)

--- client/one/components/zem-grid/services/zem_grid_ui_service.js
+++ client/one/components/zem-grid/services/zem_grid_ui_service.js
@@ -14,32 +14,68 @@ oneApp.factory('zemGridUIService', ['$timeout', 'zemGridConstants', function ($t
             };
     })();
 
-    function calculateColumnsWidths (grid, element) {
-        // TODO: Optimize element querying
-        var rows = element.find('.zem-grid-row');
-        rows.each(function (rowIndex, row) {
-            row = angular.element(row);
-            var cells = row.find('.zem-grid-cell');
-            cells.each(function (cellIndex, cell) {
-                angular.element(cell).css({
-                    'width': 'auto',
-                });
-                var cellWidth = cell.offsetWidth;
-                if (!grid.ui.columnsWidths[cellIndex] || grid.ui.columnsWidths[cellIndex] < cellWidth) {
-                    grid.ui.columnsWidths[cellIndex] = cellWidth;
-                }
+    function calculateColumnWidths (grid) {
+        // Calculate rendered column widths, based on grid contents,
+        // and configured styles (font, min/max widths, padding, etc.)
+        // Solution is sub-optimal, since it can only calculate text fields (including parsed values)
+
+        var headerCells = grid.header.ui.element.find('.zem-grid-cell');
+        var font = window.getComputedStyle(headerCells[0], null).getPropertyValue('font');
+        var padding = window.getComputedStyle(headerCells[0], null).getPropertyValue('padding-left');
+        padding = parseInt(padding) || 0;
+
+        var columnWidths = [];
+        var computedHeaderWidth = 0;
+        grid.header.visibleColumns.forEach(function (column, i) {
+            var width = getTextWidth(column.data.name, font);
+            if (column.data.help) width += 20; // TODO: find better solution for icon widths
+
+            grid.body.rows.forEach(function (row) {
+                if (row.type > 1) return;
+                var valueWidth = getTextWidth(row.stats[column.field], font);
+                width = Math.max(width, valueWidth);
             });
-            if (row.hasClass('breakdown')) {
-                var paginationCell = row.find('.zem-grid-cell.pagination-cell');
-                paginationCell.css({
-                    'width': 'auto',
-                });
-                var paginationCellWidth = paginationCell[0].offsetWidth;
-                if (!grid.ui.columnsWidths[0] || grid.ui.columnsWidths[0] < paginationCellWidth) {
-                    grid.ui.columnsWidths[0] = paginationCellWidth;
-                }
+
+            if (grid.footer.row) {
+                var valueWidth = getTextWidth(grid.footer.row.stats[column.field], font);
+                width = Math.max(width, valueWidth);
             }
+
+            // Final touch - use padding and check against min and max widths
+            width += 2 * padding;
+            var maxWidth = window.getComputedStyle(headerCells[0], null).getPropertyValue('max-width');
+            var minWidth = window.getComputedStyle(headerCells[0], null).getPropertyValue('min-width');
+            maxWidth = parseInt(maxWidth) || width;
+            minWidth = parseInt(minWidth) || width;
+            width = Math.min(maxWidth, width);
+            width = Math.max(minWidth, width);
+
+            computedHeaderWidth += width;
+            columnWidths[i] = width;
         });
+
+        // Stretch columns to fill available space, if there is any (keep ratio)
+        var scrollerWidth = 20; // TODO: find exact value (based on browser version)
+        var headerWidth = grid.header.ui.element[0].offsetWidth - scrollerWidth;
+        if (headerWidth > computedHeaderWidth) {
+            var ratio = headerWidth / computedHeaderWidth;
+            columnWidths.forEach(function (width, i) {
+                columnWidths[i] *= ratio;
+            });
+        }
+
+        return columnWidths;
+    }
+
+    function getTextWidth (text, font) {
+        if (typeof text !== 'string') return -1;
+
+        // re-use canvas object for better performance
+        var canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
+        var context = canvas.getContext("2d");
+        context.font = font;
+        var metrics = context.measureText(text);
+        return metrics.width;
     }
 
     function resizeCells (grid, element) {
@@ -64,57 +100,13 @@ oneApp.factory('zemGridUIService', ['$timeout', 'zemGridConstants', function ($t
         });
     }
 
-    function resizeGridColumns (grid) {
-        requestAnimationFrame(function () {
-            grid.ui.columnsWidths = [];
-
-            calculateColumnsWidths(grid, grid.header.ui.element);
-            calculateColumnsWidths(grid, grid.body.ui.element);
-            calculateColumnsWidths(grid, grid.footer.ui.element);
-
-            resizeCells(grid, grid.header.ui.element);
-            resizeCells(grid, grid.body.ui.element);
-            resizeCells(grid, grid.footer.ui.element);
-        });
-    }
-
     function resizeGridColumnsOptimized (grid) {
-        grid.ui.columnsWidths = [];
-
-        grid.header.visibleColumns.forEach(function (column, i) {
-
-            var width = getTextWidth(column.data.name, 'bold 13pt Roboto,sans-serif');
-            if (column.data.help) width += 20;
-
-            grid.body.rows.forEach(function (row) {
-                if (row.type > 1) return;
-                var valueWidth = getTextWidth(row.stats[column.field], '13pt Roboto,sans-serif');
-                width = Math.max(width, valueWidth);
-            });
-
-            if (grid.footer.row) {
-                var valueWidth = getTextWidth(grid.footer.row.stats[column.field], '13pt Roboto,sans-serif');
-                width = Math.max(width, valueWidth);
-            }
-            grid.ui.columnsWidths[i] = width;
-        });
+        grid.ui.columnsWidths = calculateColumnWidths(grid);
 
         resizeCells(grid, grid.header.ui.element);
         resizeCells(grid, grid.body.ui.element);
         resizeCells(grid, grid.footer.ui.element);
     }
-
-    function getTextWidth (text, font) {
-        if (typeof text !== 'string') return -1;
-
-        // re-use canvas object for better performance
-        var canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
-        var context = canvas.getContext("2d");
-        context.font = font;
-        var metrics = context.measureText(text);
-        return metrics.width;
-    }
-
 
     function getRowClass (grid, row) {
         var classes = [];
