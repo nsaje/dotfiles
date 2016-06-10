329ccf891981db849711068ca8c9972b5d3b1939
Ziga Stopinsek <sigi.kajzer@gmail.com>
2016-06-10 15:39:47 +0200

    command moved and cleaned

--- server/etl/management/commands/vacuum_redshift_db.py
+++ server/etl/management/commands/vacuum_redshift_db.py
@@ -1,23 +1,10 @@
-from django.core.management.base import BaseCommand
-
-from reports import redshift
-
 from utils.command_helpers import ExceptionCommand
-from utils.statsd_helper import statsd_gauge
 
-from etl import redshift
+from etl import maintenance
 
 class Command(ExceptionCommand):
 
     def handle(self, *args, **options):
-        redshift.vacuum_and_analyze('publishers_1')
-        redshift.vacuum_and_analyze('contentadstats')
-        redshift.vacuum_and_analyze('touchpointconversions')
-
-
-
-
-
-
-
-
+        maintenance.vacuum_and_analyze('publishers_1')
+        maintenance.vacuum_and_analyze('contentadstats')
+        maintenance.vacuum_and_analyze('touchpointconversions')
