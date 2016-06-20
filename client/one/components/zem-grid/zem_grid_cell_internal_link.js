bc044df351d978caa9401be17a5950cf6194a1e5
Jure Bajt <jure.bajt@gmail.com>
2016-06-20 12:01:04 +0200

    Refactor zem_grid_cell_internall_link to watch row object.

--- client/one/components/zem-grid/zem_grid_cell_internal_link.js
+++ client/one/components/zem-grid/zem_grid_cell_internal_link.js
@@ -3,6 +3,21 @@
 
 oneApp.directive('zemGridCellInternalLink', [function () {
 
+    function updateRow (ctrl) {
+        if (ctrl.data && ctrl.row.data.breakdownId) {
+            ctrl.id = ctrl.row.data.breakdownId;
+            ctrl.state = getState(ctrl.grid.meta.data.level);
+        }
+    }
+
+    function getState (level) {
+        switch (level) {
+        case constants.level.ALL_ACCOUNTS: return 'main.accounts.campaigns';
+        case constants.level.ACCOUNTS: return 'main.campaigns.ad_groups';
+        case constants.level.CAMPAIGNS: return 'main.adGroups.ads';
+        }
+    }
+
     return {
         restrict: 'E',
         replace: true,
@@ -15,22 +30,17 @@ oneApp.directive('zemGridCellInternalLink', [function () {
             grid: '=',
         },
         templateUrl: '/components/zem-grid/templates/zem_grid_cell_internal_link.html',
-        link: function (scope, element, attributes, ctrl) {
-            scope.$watch('ctrl.data', function () {
-                if (ctrl.data && ctrl.row.data.breakdownId) {
-                    ctrl.id = ctrl.row.data.breakdownId;
-                    if (ctrl.grid.meta.data.level === constants.level.ALL_ACCOUNTS) {
-                        ctrl.state = 'main.accounts.campaigns';
-                    }
-                    if (ctrl.grid.meta.data.level === constants.level.ACCOUNTS) {
-                        ctrl.state = 'main.campaigns.ad_groups';
-                    }
-                    if (ctrl.grid.meta.data.level === constants.level.CAMPAIGNS) {
-                        ctrl.state = 'main.adGroups.ads';
-                    }
-                    ctrl.zemInLinkReady = true;
-                }
-            });
+        link: {
+            pre: function (scope, element, attributes, ctrl) {
+                // Set some dummy values to initialize zem-in-link
+                ctrl.id = -1;
+                ctrl.state = 'unknown';
+            },
+            post: function (scope, element, attributes, ctrl) {
+                scope.$watch('ctrl.row', function () {
+                    updateRow(ctrl);
+                });
+            },
         },
         controller: [function () {}],
     };
