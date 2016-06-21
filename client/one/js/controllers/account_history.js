d28d4f85fa11fa8ecb6a48c3b00989cc29bfefa1
Simon Mihevc <simon.mihevc@zemanta.com>
2016-06-21 11:45:15 +0200

    replaced existing code with a directive

--- client/one/js/controllers/account_history.js
+++ client/one/js/controllers/account_history.js
@@ -1,53 +1,7 @@
 /* globals oneApp,constants */
 oneApp.controller('AccountHistoryCtrl', ['$scope', '$state', 'api', 'zemNavigationService', function ($scope, $state, api, zemNavigationService) { // eslint-disable-line max-len
-    $scope.history = [];
-    $scope.requestInProgress = false;
-    $scope.order = 'datetime';
-    $scope.orderAsc = false;
-
-    $scope.changeOrder = function (field) {
-        $scope.order = field;
-        $scope.orderAsc = !$scope.orderAsc;
-
-        $scope.getHistory();
-    };
-
-    $scope.getOrderClass = function (field) {
-        if ($scope.order !== field) {
-            return '';
-        }
-
-        if ($scope.orderAsc) {
-            return 'ordered-reverse';
-        }
-        return 'ordered';
-    };
-
-    $scope.getHistory = function () {
-        $scope.requestInProgress = true;
-
-        var order = (!$scope.orderAsc && '-' || '') + $scope.order,
-            params = {
-                account: $state.params.id,
-                level: constants.historyLevel.ACCOUNT,
-            };
-        api.history.get(params, order).then(
-            function (data) {
-                $scope.history = data.history;
-            }
-        ).finally(function () {
-            $scope.requestInProgress = false;
-        });
-    };
-
-    $scope.refreshPage = function () {
-        zemNavigationService.reload();
-        $scope.getHistory();
-    };
-
-    $scope.getHistory();
-
-    $scope.getName = function (user) {
-        return user.name;
+    $scope.params = {
+        account: $state.params.id,
+        level: constants.historyLevel.ACCOUNT,
     };
 }]);
--- client/one/js/controllers/campaign_history.js
+++ client/one/js/controllers/campaign_history.js
@@ -1,44 +1,7 @@
 /* globals oneApp,constants */
 oneApp.controller('CampaignHistoryCtrl', ['$scope', '$state', '$modal', 'api', function ($scope, $state, $modal, api) { // eslint-disable-line max-len
-    $scope.settings = {};
-    $scope.history = [];
-    $scope.requestInProgress = false;
-    $scope.order = 'datetime';
-    $scope.orderAsc = false;
-
-    $scope.changeOrder = function (field) {
-        $scope.order = field;
-        $scope.orderAsc = !$scope.orderAsc;
-
-        $scope.getHistory();
+    $scope.params = {
+        campaign: $state.params.id,
+        level: constants.historyLevel.CAMPAIGN,
     };
-
-    $scope.getOrderClass = function (field) {
-        if ($scope.order !== field) {
-            return '';
-        }
-
-        if ($scope.orderAsc) {
-            return 'ordered-reverse';
-        }
-        return 'ordered';
-    };
-
-    $scope.getHistory = function () {
-        $scope.requestInProgress = true;
-        var order = (!$scope.orderAsc && '-' || '') + $scope.order,
-            params = {
-                campaign: $state.params.id,
-                level: constants.historyLevel.CAMPAIGN,
-            };
-        api.history.get(params, order).then(
-            function (data) {
-                $scope.history = data.history;
-            }
-        ).finally(function () {
-            $scope.requestInProgress = false;
-        });
-    };
-
-    $scope.getHistory();
 }]);
