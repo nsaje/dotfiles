e049effa11d05b8c82aa4d5ea38079051c907071
Simon Mihevc <simon.mihevc@zemanta.com>
2016-06-21 11:41:29 +0200

    refacotored out a directive

--- client/one/js/controllers/ad_group_history.js
+++ client/one/js/controllers/ad_group_history.js
@@ -1,49 +1,7 @@
 /* globals oneApp,constants */
 oneApp.controller('AdGroupHistoryCtrl', ['$scope', '$state', 'api', 'zemNavigationService', function ($scope, $state, api, zemNavigationService) { // eslint-disable-line max-len
-    $scope.alerts = [];
-    $scope.history = [];
-    $scope.requestInProgress = false;
-    $scope.order = 'datetime';
-    $scope.orderAsc = false;
-
-    $scope.changeOrder = function (field) {
-        $scope.order = field;
-        $scope.orderAsc = !$scope.orderAsc;
-
-        $scope.getHistory();
+    $scope.params = {
+        adGroup: $state.params.id,
+        level: constants.historyLevel.AD_GROUP,
     };
-
-    $scope.getOrderClass = function (field) {
-        if ($scope.order !== field) {
-            return '';
-        }
-
-        if ($scope.orderAsc) {
-            return 'ordered-reverse';
-        }
-        return 'ordered';
-    };
-
-    $scope.getHistory = function (id) {
-        $scope.requestInProgress = true;
-        var order = (!$scope.orderAsc && '-' || '') + $scope.order,
-            params = {
-                adGroup: $state.params.id,
-                level: constants.historyLevel.AD_GROUP,
-            };
-        api.history.get(params, order).then(
-            function (data) {
-                $scope.history = data.history;
-            }
-        ).finally(function () {
-            $scope.requestInProgress = false;
-        });
-    };
-
-    $scope.refreshPage = function () {
-        zemNavigationService.reloadAdGroup($state.params.id);
-        $scope.getHistory($state.params.id);
-    };
-
-    $scope.getHistory($state.params.id);
 }]);
