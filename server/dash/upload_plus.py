2fcefbf50cab3e9edbccfe15954d1ddf4751290c
Luka Šilovinac <luka.silovinac@zemanta.com>
2016-06-23 18:12:50 +0200

    Revert accidentally commited dev code

--- server/dash/upload_plus.py
+++ server/dash/upload_plus.py
@@ -37,22 +37,22 @@ def insert_candidates(content_ads_data, ad_group, batch_name, filename):
 
 @transaction.atomic
 def invoke_external_validation(candidate, skip_url_validation=False):
-    # lambda_helper.invoke_lambda(
-    #     settings.LAMBDA_CONTENT_UPLOAD_FUNCTION_NAME,
-    #     {
-    #         'namespace': settings.LAMBDA_CONTENT_UPLOAD_NAMESPACE,
-    #         'candidateID': candidate.pk,
-    #         'pageUrl': candidate.url,
-    #         'adGroupID': candidate.ad_group.pk,
-    #         'batchID': candidate.batch.pk,
-    #         'imageUrl': candidate.image_url,
-    #         'callbackUrl': settings.LAMBDA_CONTENT_UPLOAD_CALLBACK_URL,
-    #         'skipUrlValidation': skip_url_validation,
-    #     },
-    #     async=True,
-    # )
-    candidate.image_status = constants.AsyncUploadJobStatus.FAILED
-    candidate.url_status = constants.AsyncUploadJobStatus.FAILED
+    lambda_helper.invoke_lambda(
+        settings.LAMBDA_CONTENT_UPLOAD_FUNCTION_NAME,
+        {
+            'namespace': settings.LAMBDA_CONTENT_UPLOAD_NAMESPACE,
+            'candidateID': candidate.pk,
+            'pageUrl': candidate.url,
+            'adGroupID': candidate.ad_group.pk,
+            'batchID': candidate.batch.pk,
+            'imageUrl': candidate.image_url,
+            'callbackUrl': settings.LAMBDA_CONTENT_UPLOAD_CALLBACK_URL,
+            'skipUrlValidation': skip_url_validation,
+        },
+        async=True,
+    )
+    candidate.image_status = constants.AsyncUploadJobStatus.WAITING_RESPONSE
+    candidate.url_status = constants.AsyncUploadJobStatus.WAITING_RESPONSE
     candidate.save()
 
 
