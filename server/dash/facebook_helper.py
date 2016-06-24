ef4ebbff327991680e4dc8158e409fa9e9f17989
Marko Novak <marko.novak@zemanta.com>
2016-06-24 12:05:18 +0200

    Added another error message to the condition for the assigning of the "connected" status to facebook account.

--- server/dash/facebook_helper.py
+++ server/dash/facebook_helper.py
@@ -23,6 +23,7 @@ CURRENCY_USD = 'USD'
 ERROR_INVALID_PAGE = 'Param page_id must be a valid page ID'
 ERROR_ALREADY_PENDING = 'There is already pending client request for page'
 ERROR_ALREADY_CONNECTED = 'You Already Have Access To This Page'
+ERROR_ALREADY_BELONGS_TO_BUSINESS_MANAGER = 'Asset already belongs to this Business Manager'
 
 
 def update_facebook_account(facebook_account, new_url, business_id, access_token):
@@ -84,7 +85,8 @@ def send_page_access_request(page_id, business_id, access_token):
     elif response.status_code == 400:
         error = response.json().get('error', {})
 
-        if ERROR_ALREADY_CONNECTED in error.get('error_user_title', ''):
+        if ERROR_ALREADY_CONNECTED in error.get('error_user_title', '') \
+                or ERROR_ALREADY_BELONGS_TO_BUSINESS_MANAGER in error.get('message', ''):
             return constants.FacebookPageRequestType.CONNECTED
         elif ERROR_ALREADY_PENDING in error.get('message', ''):
             return constants.FacebookPageRequestType.PENDING
@@ -152,7 +154,7 @@ def add_system_user_permissions(connected_object_id, role, business_id, system_u
 
 
 def get_credentials():
-    return json.loads(models.SourceCredentials.objects.get(source__source_type__type='facebook').decrypt())
+    return json.loads(models.SourceCredentials.objects.get(source__bidder_slug='facebook').decrypt())
 
 
 def stop_facebook_media_sources(account):
