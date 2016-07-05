b688f3360c305ef3024d4dc0b859d572f6a94581
Gregor Ratajc <gregor.ratajc@zemanta.com>
2016-07-05 15:48:40 +0200

    Query cost and data_cost as nano

--- server/redshiftapi/templates/part_total_cost.sql
+++ server/redshiftapi/templates/part_total_cost.sql
@@ -1,1 +1,1 @@
-(SUM({{ p }}license_fee_nano)+SUM({{ p }}cost_cc)*100000+SUM({{ p }}data_cost_cc)*100000)/1000000000.0 {{ alias }}
+(SUM({{ p }}license_fee_nano)+SUM({{ p }}cost_nano)*100+SUM({{ p }}data_cost_nano)*100)/1000000000.0 {{ alias }}
