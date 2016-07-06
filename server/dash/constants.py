93450275b6c8bfee67bceea8544972cc930ec6e0
romanorac <orac.roman@gmail.com>
2016-07-06 16:29:56 +0200

    remove revoked status

--- server/dash/constants.py
+++ server/dash/constants.py
@@ -1416,15 +1416,13 @@ class FacebookPageRequestType(ConstantBase):
     PENDING = 3
     INVALID = 4
     ERROR = 5
-    REVOKED = 6
 
     _VALUES = {
         EMPTY: 'Empty',
         CONNECTED: 'Connected',
         PENDING: 'Pending',
         INVALID: 'Invalid',
-        ERROR: 'Error',
-        REVOKED: 'Revoked'
+        ERROR: 'Error'
     }
 
 
