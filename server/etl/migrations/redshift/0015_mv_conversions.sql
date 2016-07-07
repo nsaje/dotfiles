37867b868ac6f08bd7160bac62d7593ea90ebf52
Gregor Ratajc <gregor.ratajc@zemanta.com>
2016-07-07 16:13:59 +0200

    Adds conversions sql table definition

---
+++ server/etl/migrations/redshift/0015_mv_conversions.sql
@@ -0,0 +1,15 @@
+CREATE TABLE mv_conversions (
+       date date not null encode delta,
+       source_id int2 encode bytedict,
+
+       agency_id int2 encode lzo,
+       account_id int2 encode lzo,
+       campaign_id int2 encode lzo,
+       ad_group_id int2 encode lzo,
+       content_ad_id integer encode lzo,
+       publisher varchar(255) encode lzo,
+
+       slug varchar(256) encode lzo,
+
+       conversion_count integer encode lzo
+) sortkey(date, source_id, account_id, campaign_id, ad_group_id, content_ad_id, publisher, slug);
