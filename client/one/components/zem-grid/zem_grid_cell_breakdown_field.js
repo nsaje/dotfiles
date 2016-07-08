b673bdddbfff46e261967e1d98ada0e5a53bb20a
Jure Bajt <jure.bajt@gmail.com>
2016-07-08 10:28:49 +0200

    zemGridCellBreakdownField refactor and tests (#1259)
    * Removed notification field type as it is not used anymore.
    
    * Moved zemGridCellBreakdownField functionality to controller.
    
    * Added tests for zemGridCellBreakdonField.

--- client/one/components/zem-grid/zem_grid_cell_breakdown_field.js
+++ client/one/components/zem-grid/zem_grid_cell_breakdown_field.js
@@ -1,24 +1,13 @@
 /* globals oneApp, constants */
 'use strict';
 
-oneApp.directive('zemGridCellBreakdownField', ['zemGridConstants', function (zemGridConstants) {
+oneApp.directive('zemGridCellBreakdownField', [function () {
 
     var BREAKDOWNS_WITH_INTERNAL_LINKS = [
         constants.breakdown.ACCOUNT,
         constants.breakdown.CAMPAIGN,
         constants.breakdown.AD_GROUP,
     ];
-
-    function getFieldType (breakdown, rowLevel) {
-        if (rowLevel === 0) {
-            return zemGridConstants.gridColumnTypes.TOTALS_LABEL;
-        }
-        // Display internal links for rows on first level in 'Account', 'Campaign' or 'Ad Group' breakdowns
-        if (BREAKDOWNS_WITH_INTERNAL_LINKS.indexOf(breakdown) !== -1 && rowLevel === 1) {
-            return zemGridConstants.gridColumnTypes.INTERNAL_LINK;
-        }
-        return zemGridConstants.gridColumnTypes.BASE_FIELD;
-    }
 
     return {
         restrict: 'E',
@@ -32,25 +21,35 @@ oneApp.directive('zemGridCellBreakdownField', ['zemGridConstants', function (zem
             grid: '=',
         },
         templateUrl: '/components/zem-grid/templates/zem_grid_cell_breakdown_field.html',
-        link: function (scope, element, attributes, ctrl) {
-            ctrl.types = zemGridConstants.gridColumnTypes;
-
-            scope.$watch('ctrl.row', update);
-            scope.$watch('ctrl.data', update);
-
-            function update () {
-                if (ctrl.row) {
-                    ctrl.fieldType = getFieldType(ctrl.grid.meta.data.breakdown, ctrl.row.level);
-                }
-            }
-        },
-        controller: ['zemGridUIService', function (zemGridUIService) {
+        controller: ['$scope', 'zemGridConstants', 'zemGridUIService', function ($scope, zemGridConstants, zemGridUIService) { // eslint-disable-line max-len
             var vm = this;
+            vm.types = zemGridConstants.gridColumnTypes;
             vm.getBreakdownColumnStyle = getBreakdownColumnStyle;
 
+            $scope.$watch('ctrl.row', update);
+            $scope.$watch('ctrl.data', update);
+
+            function update () {
+                if (vm.row) {
+                    vm.fieldType = getFieldType(vm.grid.meta.data.breakdown, vm.row.level);
+                }
+            }
+
             function getBreakdownColumnStyle () {
-                if (!this.row) return; // can happen because of virtual scroll; TODO: find better solution
+                if (!vm.row) return; // can happen because of virtual scroll; TODO: find better solution
                 return zemGridUIService.getBreakdownColumnStyle(vm.row);
+            }
+
+            function getFieldType (breakdown, rowLevel) {
+                // Footer row
+                if (rowLevel === 0) {
+                    return zemGridConstants.gridColumnTypes.TOTALS_LABEL;
+                }
+                // Display internal links for rows on first level in 'Account', 'Campaign' or 'Ad Group' breakdowns
+                if (BREAKDOWNS_WITH_INTERNAL_LINKS.indexOf(breakdown) !== -1 && rowLevel === 1) {
+                    return zemGridConstants.gridColumnTypes.INTERNAL_LINK;
+                }
+                return zemGridConstants.gridColumnTypes.BASE_FIELD;
             }
         }],
     };
