852e72f30a5b8781614ef15799b960dca5da1a32
Matic Žgur <mr.zgur@gmail.com>
2016-07-27 17:25:18 +0200

    K1 API ad groups: Only return agency id if agency is set.

--- server/k1api/views.py
+++ server/k1api/views.py
@@ -633,8 +633,12 @@ class get_ad_groups(K1APIView):
                 'retargeting': self._get_retargeting(ad_group_settings),
                 'campaign_id': ad_group_settings.ad_group.campaign.id,
                 'account_id': ad_group_settings.ad_group.campaign.account.id,
+                'agency_id': None,
                 'goal_types': campaign_goal_types[ad_group_settings.ad_group.campaign.id],
             }
+
+            if ad_group_settings.ad_group.campaign.account.agency:
+                ad_group['agency_id'] = ad_group_settings.ad_group.campaign.account.agency.id
 
             ad_groups.append(ad_group)
 
