004a1f13d5c142b4aa224d2b521a52b84a263daf
Luka Šilovinac <luka.silovinac@zemanta.com>
2016-08-01 11:30:22 +0200

    Add missing alias to redshiftapi template

--- server/redshiftapi/templates/part_returning_users.sql
+++ server/redshiftapi/templates/part_returning_users.sql
@@ -2,4 +2,4 @@ CASE
   WHEN SUM({{ p }}users) IS NULL OR SUM({{ p }}new_visits) IS NULL THEN NULL
   WHEN SUM({{ p }}new_visits) > SUM({{ p }}users) THEN 0
   ELSE SUM({{ p }}users) - SUM({{ p }}new_visits)
-END
+END {{ alias }}
