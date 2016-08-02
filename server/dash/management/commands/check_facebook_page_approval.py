dbf4adcceb8cc72123ad95c35907191be939e782
Matija Vižintin <matija.vizintin@gmail.com>
2016-08-02 16:28:04 +0200

    check fb page command improvements (#1357)

--- server/dash/management/commands/check_facebook_page_approval.py
+++ server/dash/management/commands/check_facebook_page_approval.py
@@ -26,19 +26,28 @@ class Command(ExceptionCommand):
                                                                     credentials['system_user_id'],
                                                                     credentials['access_token'])
                 if not added:
-                    raise CommandError('Error while adding system user to a connected object.')
+                    raise CommandError('Error while adding system user to a connected object for account %s.',
+                                       pending_account.name)
 
-                ad_account_id = facebook_helper.create_ad_account(pending_account.account.name, pending_account.page_id,
-                                                                  credentials['app_id'], credentials['business_id'],
-                                                                  credentials['access_token'])
-                if not ad_account_id:
-                    raise CommandError('Error while creating facebook account.')
-                added = facebook_helper.add_system_user_permissions(ad_account_id, 'ADMIN', credentials['business_id'],
+                if not pending_account.ad_account_id:
+                    ad_account_id = facebook_helper.create_ad_account(pending_account.account.name,
+                                                                      pending_account.page_id,
+                                                                      credentials['app_id'], credentials['business_id'],
+                                                                      credentials['access_token'])
+                    if not ad_account_id:
+                        raise CommandError('Error while creating facebook account for account %s.',
+                                           pending_account.name)
+                    pending_account.ad_account_id = ad_account_id
+                    pending_account.save()
+                    logger.info('Facebook ad account for account %s created with id %s.', pending_account.account.name,
+                                ad_account_id)
+
+                added = facebook_helper.add_system_user_permissions(pending_account.ad_account_id, 'ADMIN',
+                                                                    credentials['business_id'],
                                                                     credentials['system_user_id'],
                                                                     credentials['access_token'])
                 if not added:
                     raise CommandError('Error while adding system user to a connected object.')
 
-                pending_account.ad_account_id = ad_account_id
                 pending_account.status = constants.FacebookPageRequestType.CONNECTED
                 pending_account.save()
