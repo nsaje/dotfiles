3ab16247e582f8328c8c4700c66f79faac920bc3
markocelan <markocelan@users.noreply.github.com>
2016-08-18 16:09:18 +0200

    additional checks for DemoV3 (#1419)
    * additional checks for DemoV3
    
    * split monitor steps into functions
    
    * pep8
    
    * make monitor_demo run more often
    
    * rearranged code as suggested in comments

--- server/dash/management/commands/monitor_demo.py
+++ server/dash/management/commands/monitor_demo.py
@@ -3,6 +3,7 @@ import json
 import logging
 import time
 import urllib2
+import requests
 
 from django.conf import settings
 
@@ -18,7 +19,7 @@ class Command(ExceptionCommand):
 
     def handle(self, *args, **options):
         demo = self._start_demo()
-        self._check_demo_url(demo['url'])
+        self._check_demo_url(demo)
         self._stop_demo(demo['name'])
         influx.incr('demo.monitor', 1)
 
@@ -37,17 +38,53 @@ class Command(ExceptionCommand):
         return {
             'name': ret.get('instance_name'),
             'url': ret.get('instance_url'),
+            'username': ret.get('instance_username'),
+            'password': ret.get('instance_password'),
         }
 
-    def _check_demo_url(self, url):
-        error = None
+    def _find_string_in_login_page(session):
+        response = session.get(url)
+        body = response.text
+        csrf_token = session.cookies['csrftoken']
+        if 'Zemanta One' not in body or 'Sign in' not in body:
+            raise Exception('Invalid response from demo')
+        return session
+
+    def _try_to_login(session, demo_instance):
+        response = session.post(
+            url='%s/signin' % url,
+            data={
+                'username': demo_instance['username'],
+                'password': demo_instance['password'],
+                'csrfmiddlewaretoken': csrf_token
+                },
+            headers={'Referer': '%s/signin?next=/' % demo_instance['url']}
+        )
+        if response.status_code != 200:
+            raise Exception('Invalid response code from demo signin')
+        return session
+
+    def _fetch_all_accounts_nav(session, demo_instance):
+        response = session.get(
+            url='%s/api/all_accounts/nav/' % demo_instance['url'], headers={'Accept': 'application/json', }
+        )
+        if response.status_code != 200:
+            raise Exception('Invalid response code from demo nav')
+        data = json.loads(response.text)
+        if not data.get('success', False):
+            raise Exception("Couldn't get basic nav data")
+        return session
+
+    def _check_demo_url(self, demo_instance):
+        error = Exception("Automatic 'Request demoV3' check failed for unknown reason.")
         for _ in range(NUM_RETRIES):
             try:
-                response = urllib2.urlopen(url)
-                body = response.read()
-                if 'Zemanta One' not in body or 'Sign in' not in body:
-                    raise Exception('Invalid response from demo')
+                session = requests.Session()
+                _find_string_in_login_page(session)
+                _try_to_login(session, demo_instance)
+                _fetch_all_accounts_nav(session, demo_instance)
                 return
+
             except Exception as err:
                 error = err
                 time.sleep(5)
