62f023925dd9d0c1b390d7a5f850a24969535f34
marko <marko.celan@zemanta.com>
2016-08-30 15:18:04 +0200

    demo monitor did not shut down instance when check failed. This caused demo insances to grow in numbers, eventually causing OOM condition on host machine

--- server/dash/management/commands/monitor_demo.py
+++ server/dash/management/commands/monitor_demo.py
@@ -19,9 +19,14 @@ class Command(ExceptionCommand):
 
     def handle(self, *args, **options):
         demo = self._start_demo()
-        self._check_demo_url(demo)
-        self._stop_demo(demo['name'])
-        influx.incr('demo.monitor', 1)
+        try:
+            self._check_demo_url(demo)
+        except Exception as err:
+            influx.incr('demo.monitor', 1, status='error')
+            raise(err)
+        finally:
+            self._stop_demo(demo['name'])
+        influx.incr('demo.monitor', 1, status='ok')
 
     def _start_demo(self):
         request = urllib2.Request(settings.DK_DEMO_UP_ENDPOINT)
@@ -46,7 +51,7 @@ class Command(ExceptionCommand):
         response = session.get(demo_instance['url'])
         body = response.text
         if 'Zemanta One' not in body or 'Sign in' not in body:
-            raise Exception('Invalid response from demo')
+            raise Exception('Invalid response from demo({instance_name})'.format(instance_name=demo_instance['name']))
 
     def _try_to_login(self, session, demo_instance):
         response = session.post(
@@ -59,22 +64,22 @@ class Command(ExceptionCommand):
             headers={'Referer': '%s/signin?next=/' % demo_instance['url']}
         )
         if response.status_code != 200:
-            raise Exception('Invalid response code from demo signin')
+            raise Exception('Invalid response code from demo signin({instance_name})'.format(instance_name=demo_instance['name']))
 
     def _fetch_all_accounts_nav(self, session, demo_instance):
         response = session.get(
             url='%s/api/all_accounts/nav/' % demo_instance['url'], headers={'Accept': 'application/json', }
         )
         if response.status_code != 200:
-            raise Exception('Invalid response code from demo nav')
+            raise Exception('Invalid response code from demo nav({instance_name})'.format(instance_name=demo_instance['name']))
         data = json.loads(response.text)
         if not data.get('success', False):
-            raise Exception("Couldn't get basic nav data")
+            raise Exception('Could not get basic nav data({instance_name})'.format(instance_name=demo_instance['name']))
 
         return session
 
     def _check_demo_url(self, demo_instance):
-        error = Exception("Automatic 'Request demoV3' check failed for unknown reason.")
+        error = Exception("Automatic 'Request demoV3' check failed for unknown reason({instance_name}).".format(instance_name=demo_instance['name']))
         for _ in range(NUM_RETRIES):
             try:
                 session = requests.Session()
@@ -95,8 +100,8 @@ class Command(ExceptionCommand):
 
         status_code = response.getcode()
         if status_code != 200:
-            raise Exception('Invalid response status code. status code: {}'.format(status_code))
+            raise Exception('Invalid response status code. status code: {code} (name)'.format(code=status_code, name=name))
 
         ret = json.loads(response.read())
         if ret['status'] != 'pending':
-            raise Exception('Request not successful. status: {}'.format(ret['status']))
+            raise Exception('Request not successful. status: {status} ({name})'.format(status=ret['status'], name=name))
