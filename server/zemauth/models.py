20906650032c12e9381937fc26ad368ee337b0d4
Jure Bajt <jure.bajt@gmail.com>
2016-09-05 13:06:11 +0200

    Added permission for users to see new design (#1505)
    * Added permission for users to see new design.
    
    * Defined app global variables in eslint config file.

--- server/zemauth/models.py
+++ server/zemauth/models.py
@@ -220,6 +220,7 @@ class User(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
             ('group_agency_manager_add', 'Agency managers are added to this group when promoted'),
             ('can_set_agency_for_account', 'User can set agency for account'),
             ('can_use_single_ad_upload', 'User can use single content ad upload'),
+            ('can_see_new_design', 'User can see new design'),
         )
 
     def get_full_name(self):
