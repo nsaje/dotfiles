b3ec06125d1a96b1faab03e1549b6f6b081c4c51
Matic Žgur <mr.zgur@gmail.com>
2016-09-08 13:33:11 +0200

    Custom audiences: a fix to make tab active on click.

--- client/one/js/controllers/account_custom_audiences.js
+++ client/one/js/controllers/account_custom_audiences.js
@@ -3,4 +3,6 @@
 
 angular.module('one.legacy').controller('AccountCustomAudiencesCtrl', ['$scope', '$state', function ($scope, $state) { // eslint-disable-line max-len
     $scope.accountId = $state.params.id;
+
+    $scope.setActiveTab();
 }]);
