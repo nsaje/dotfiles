74a9e71e325e53249f4e58513bca26ec297ff5d4
Jure Bajt <jure.bajt@gmail.com>
2016-09-16 09:54:12 +0200

    ESLint.

--- client/one/app/widgets/zem-header/components/zem-header-menu/zemHeaderMenu.component.js
+++ client/one/app/widgets/zem-header/components/zem-header-menu/zemHeaderMenu.component.js
@@ -1,6 +1,6 @@
 angular.module('one.widgets').component('zemHeaderMenu', {
     templateUrl: '/app/widgets/zem-header/components/zem-header-menu/zemHeaderMenu.component.html',
-    controller: ['config', 'zemHeaderMenuService', 'zemUserService', function (config, zemHeaderMenuService, zemUserService) {
+    controller: ['config', 'zemHeaderMenuService', 'zemUserService', function (config, zemHeaderMenuService, zemUserService) { // eslint-disable-line max-len
         var $ctrl = this;
         $ctrl.config = config;
         $ctrl.getActions = zemHeaderMenuService.getAvailableActions;
