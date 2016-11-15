394ed49d883cceae55ee3a847e7bf0a518f74cc4
Luka Šilovinac <luka.silovinac@zemanta.com>
2016-11-15 14:08:55 +0100

    Add bizwire ad group to config

--- server/integrations/bizwire/config.py
+++ server/integrations/bizwire/config.py
@@ -1,5 +1,6 @@
 # NOTE: keep in sync with R1 (https://github.com/Zemanta/r1/blob/8335778196430425a770138435a59eaaae36e342/config/config.go#L69)
 TEST_AD_GROUP_IDS = [
+    2793,
     2539,
     2780,
     2591,  # click capping test in product campaign
