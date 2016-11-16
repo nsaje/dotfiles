a0efcc1cba51a258f4f0defe7080e5e75727500c
Luka Šilovinac <luka.silovinac@zemanta.com>
2016-11-16 11:18:05 +0100

    A bit better regex

--- server/integrations/bizwire/views.py
+++ server/integrations/bizwire/views.py
@@ -155,7 +155,7 @@ class PromotionExport(BizwireView):
         article_id = request.GET.get('article_id')
         article_url = request.GET.get('article_url')
         if article_url:
-            m = re.search('home/(\d*)/en', article_url)
+            m = re.search('news/home/(\d*)/', article_url)
             if m and m.groups():
                 article_id = m.groups()[0]
 
