c6f4132168912b618b8bcb7b5af6e8fb35f8c2ed
Florjan Bartol <florjan.bartol@gmail.com>
2016-11-18 14:33:56 +0100

    Changed from 3 days to 7 days

---
+++ server/dash/migrations/0144_change_demo_running_email.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+from dash.constants import EmailTemplateType
+
+
+def load_data(apps, schema_editor):
+    EmailTemplate = apps.get_model('dash', 'EmailTemplate')
+
+    EmailTemplate(
+        template_type=EmailTemplateType.DEMO_RUNNING,
+        subject=u'Demo is running',
+        body=u'''Hi,
+
+Demo is running.
+Log in to {url}
+u/p: regular.user+demo@zemanta.com / {password}
+
+Note: This instance will selfdestroy in 7 days
+
+Yours truly,
+Zemanta
+    '''
+    ).save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('dash', '0143_auto_20161110_1523'),
+    ]
+
+    operations = [
+        migrations.RunPython(load_data)
+    ]
