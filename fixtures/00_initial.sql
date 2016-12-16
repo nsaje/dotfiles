60785106a8b4d3480b96fadbcc46f056f1e75e4f
marko <marko.celan@zemanta.com>
2016-12-16 09:27:25 +0100

    one record too many in fixtures

--- fixtures/00_initial.sql
+++ fixtures/00_initial.sql
@@ -5338,7 +5338,6 @@ SELECT pg_catalog.setval('zemauth_internalgroup_id_seq', 1, false);
 --
 
 COPY zemauth_user (id, password, last_login, is_superuser, email, username, first_name, last_name, date_joined, is_staff, is_active, is_test_user) FROM stdin;
-1	pbkdf2_sha256$24000$GSWijssN2W0b$J0Hd1hjyMLM6BIn9xKPDSRlNU8IYx8QvqkcQPo+PI8Y=	2016-09-16 13:18:05.637437+00	t	dev@zemanta.com	\N			2016-09-16 13:18:05.637437+00	t	t	f
 \.
 
 
