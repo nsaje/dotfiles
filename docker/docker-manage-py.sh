dbee06d8084e32359d17fc572df1c055bca6292f
Domen Ipavec <domen@ipavec.net>
2017-01-11 15:00:46 +0100

    Jenkins push docker-compose (#1881)
    * Push production docker-compose files from jenkins
    
    * Push script fix
    
    * Push also docker-manage-py.sh
    
    * docker-manage-py fix
    
    * few fixes
    
    * removed obsolete setting

--- docker/docker-manage-py.sh
+++ docker/docker-manage-py.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
 TASK=${1:-there_is_no_default}
+shift
+TASK_PARAMS=$@
 if [[ "${TASK}" == "there_is_no_default" ]]; then
     echo "Please define manage.py command as parameter"
     exit -1
 fi
 
 CONTAINER_ID=$(docker ps -q -f status=exited -f name="$TASK")
-
 if [[ "$CONTAINER_ID" != "" ]]; then
-    echo "[WARN] Deleting leftover container"
+    echo "[WARN] Deleting leftover container [$TASK]"
     docker rm $CONTAINER_ID
 fi
 
-
-echo "running $0 $TASK"
+echo "running $0 $TASK $TASK_PARAMS"
 
 exec /usr/bin/docker run --rm -h $(hostname) \
     --name="${TASK}" \
-    -w /app/zemanta-eins \
     -e "ZWEI_CALLBACK_BASE_URL=https://one-cb.zemanta.com" \
-    --link=statsd \
+    -e "CONF_ENV=prod" \
     --link=memcached \
     --entrypoint=python \
-    z1-bundle:current manage.py "${TASK}" 2>&1 | sudo tee -a "/mnt/logs/eins/cron-${TASK}.log" > /dev/null
-
-
+    z1-bundle:current manage.py "${TASK}" $TASK_PARAMS 2>&1 | sudo tee -a "/mnt/logs/eins/cron-${TASK}.log" > /dev/null
