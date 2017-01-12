ec1bc46400734ff6ff3a4937cdff0812b9bd8eef
Luka Šilovinac <luka.silovinac@zemanta.com>
2017-01-12 17:06:08 +0100

    Remove actionlog references in lint check

--- lint_check.sh
+++ lint_check.sh
@@ -15,7 +15,7 @@ function green {
 blue "PEP8 lint in progress ..."
 pep8 \
   --first \
-  --exclude="__init__.py,test_api.py,settings.py,wsgi.py,migrations,actionlog,localsettings.py,regions.py" \
+  --exclude="__init__.py,test_api.py,settings.py,wsgi.py,migrations,localsettings.py,regions.py" \
   --max-line-length=700 \
   ./server/
 
