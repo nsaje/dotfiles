cf59885324bc3ec0b66a444539563468d2839fa3
Matic Žgur <mr.zgur@gmail.com>
2017-01-17 15:41:02 +0100

    Supply reports: add custom subject for emails. (#1900)

--- server/reports/models.py
+++ server/reports/models.py
@@ -159,6 +159,7 @@ class SupplyReportRecipient(models.Model):
     last_name = models.CharField('last name', max_length=30, blank=True)
     email = models.EmailField('email address', max_length=255)
     source = models.ForeignKey('dash.Source', on_delete=models.PROTECT)
+    custom_subject = models.CharField(max_length=100, blank=True)
     publishers_report = models.BooleanField(default=False)
     created_dt = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
     modified_dt = models.DateTimeField(auto_now=True, verbose_name='Modified at')
