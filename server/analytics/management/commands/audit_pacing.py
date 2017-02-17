7a187da1fa4c160666c1f2509cffedfaeffa424d
Ziga Stopinsek <sigi.kajzer@gmail.com>
2017-02-17 09:21:21 +0100

    pacing command improvements

--- server/analytics/management/commands/audit_pacing.py
+++ server/analytics/management/commands/audit_pacing.py
@@ -8,6 +8,12 @@ import zemauth.models
 import dash.models
 import analytics.monitor
 
+VALID_PACING_ACCOUNT_TYPES = (
+    dash.constants.AccountType.ACTIVATED,
+    dash.constants.AccountType.MANAGED,
+    dash.constants.AccountType.PILOT,
+)
+
 
 class Command(utils.command_helpers.ExceptionCommand):
     help = "Audit pacing"
@@ -21,6 +27,10 @@ class Command(utils.command_helpers.ExceptionCommand):
                             help='Min pacing threshold (default 50%)')
         parser.add_argument('--max-pacing', dest='max_pacing', default='150.0',
                             help='Max pacing threshold (default 150%)')
+        parser.add_argument('--date', '-d', dest='date',
+                            help='Date checked (default today)')
+        parser.add_argument('--days-running', dest='days_running', default=7,
+                            help='Minimal number of days that ad groups have to be running')
 
     def _print(self, msg):
         if not self.verbose:
@@ -29,13 +39,30 @@ class Command(utils.command_helpers.ExceptionCommand):
 
     def handle(self, *args, **options):
         self.verbose = options['verbose']
+        date = (options['date'] and
+                datetime.datetime.strptime(options['date'], "%Y-%m-%d").date() or
+                datetime.date.today())
+        days_running = int(options['days_running'])
+        flying_ad_groups = [
+            ad_group for ad_group in
+            dash.models.AdGroup.objects.all().filter_running().exclude_archived()
+            if (date - ad_group.get_current_settings().start_date).days >= days_running
+        ]
+        flying_campaigns = {
+            c.pk: c
+            for c in dash.models.Campaign.objects.filter(
+                id__in=set(adg.campaign_id for adg in flying_ad_groups)
+            ).exclude_landing().select_related('account')
+            if c.account.get_current_settings().account_type in VALID_PACING_ACCOUNT_TYPES
+        }
         alarms = analytics.monitor.audit_pacing(
-            date=options['date'] and datetime.datetime.strptime(options['date'], "%Y-%m-%d").date(),
+            date=date,
             max_pacing=Decimal(options['max_pacing']),
             min_pacing=Decimal(options['min_pacing']),
+            campaign__in=flying_campaigns.values(),
         )
         valid_emails = set(
-            user.email for user in zemauth.models.User.objects.all().get_users_with_perm(
+            user.email for user in zemauth.models.User.objects.get_users_with_perm(
                 'can_receive_pacing_email'
             )
         )
@@ -46,15 +73,15 @@ class Command(utils.command_helpers.ExceptionCommand):
         for campaign_id, pacing, alert in alarms:
             campaign = campaigns[campaign_id]
             emails = set(utils.email_helper.email_manager_list(campaign)) & valid_emails
-            self._print('Campaign {} ({}) has {} pacing {}: send to {}'.format(
+            self._print(u'Campaign {} ({}) has {} pacing {}: send to {}'.format(
                 campaign.pk,
                 campaign.name,
                 alert,
                 pacing,
-                ', '.join(emails) if emails else 'none'
+                u', '.join(emails) if emails else 'none'
             ))
             if not options['send_emails']:
-                return
+                continue
             utils.email_helper.send_pacing_notification_email(
                 campaign, emails, pacing, alert
             )
