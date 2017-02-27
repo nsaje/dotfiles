7391c96b91e68dcbf0967e78b967d63c3831c74a
Nejc Saje <nejc@saje.info>
2017-02-27 09:56:42 +0100

    add vim and less to docker base image (#2025)
    * add vim and less to docker base image
    
    * fix issue

--- docker/entrypoint_dev.sh
+++ docker/entrypoint_dev.sh
@@ -7,7 +7,7 @@ DB_NAME=${DB_ENV_POSTGRES_DB:-z1}
 DB_USER=${DB_ENV_POSTGRES_USER:-z1}
 DB_PASS=${DB_ENV_POSTGRES_PASSWORD:-devpwd}
 
-until PGPASSWORD="$DB_PASS" psql -w -h "$DB_HOST" -U "$DB_USER" "$DB_NAME" -c '\l'; do
+until PGPASSWORD="$DB_PASS" psql -w -h "$DB_HOST" -U "$DB_USER" "$DB_NAME" -c '\l' -P pager=off; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
