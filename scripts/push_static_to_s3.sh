bba43a87c88c907686d0209186251d27dd50b2c4
marko <marko.celan@zemanta.com>
2017-03-03 15:48:18 +0100

    Push static to one-static.zemanta.com

--- scripts/push_static_to_s3.sh
+++ scripts/push_static_to_s3.sh
@@ -21,3 +21,20 @@ if [ -d client/dist ]; then
 	aws s3 cp --acl public-read client/dist/one/zemanta-one.css s3://z1-static/styleguides/
 	aws s3 cp --acl public-read client/dist/one/zemanta-one.lib.min.css s3://z1-static/styleguides/
 fi
+
+if [ -d client/dist ]; then
+	S3_PATH="s3://one-static.zemanta.com/build-${BUILD_NUM}"
+
+	# need to set content-disposition for attachments sync assets separately
+	aws s3 sync --acl public-read --content-disposition 'attachment' client/dist/one/assets "${S3_PATH}/client/one/assets"
+
+	aws s3 sync --acl public-read --exclude 'assets/' client/dist "${S3_PATH}/client"
+	aws s3 sync --acl public-read server/static "${S3_PATH}/server"
+
+
+	echo ${BUILD_NUM} | aws s3 cp - s3://one-static.zemanta.com/latest.txt
+
+	# Push styleguides to well-known URL
+	aws s3 cp --acl public-read client/dist/one/zemanta-one.css s3://one-static.zemanta.com/styleguides/
+	aws s3 cp --acl public-read client/dist/one/zemanta-one.lib.min.css s3://one-static.zemanta.com/styleguides/
+fi
