6fac473e6f7e92ea448b3537bae450ffce5f53f0
Jure Polutnik <jure.polutnik@zemanta.com>
2017-03-21 15:52:45 +0100

    Add navigation keyboard shortcuts (#2105)
    Navigate through entities with CTRL+Arrows.
    CTRL + UP/DOWN - next/previous entity on the same level
    CTRL + LEFT/RIGHT - previous (parent entity) and next (first child) level

--- client/one/app/widgets/zem-header/components/zem-header-navigation/zemHeaderNavigation.component.js
+++ client/one/app/widgets/zem-header/components/zem-header-navigation/zemHeaderNavigation.component.js
@@ -1,6 +1,6 @@
 angular.module('one.widgets').component('zemHeaderNavigation', {
     templateUrl: '/app/widgets/zem-header/components/zem-header-navigation/zemHeaderNavigation.component.html',
-    controller: function ($scope, $element, $timeout, config, hotkeys) { // eslint-disable-line max-len
+    controller: function ($scope, $element, $timeout, config, hotkeys, zemHeaderNavigationService) { // eslint-disable-line max-len
         var $ctrl = this;
         $ctrl.config = config;
         $ctrl.open = false;
@@ -13,6 +13,11 @@ angular.module('one.widgets').component('zemHeaderNavigation', {
         function initializeShortcuts () {
             hotkeys.bindTo($scope).add({combo: 'f', callback: open});
             hotkeys.bindTo($scope).add({combo: 'enter', allowIn: ['INPUT'], callback: close});
+
+            hotkeys.bindTo($scope).add({combo: 'ctrl+up', callback: zemHeaderNavigationService.quickNavigate});
+            hotkeys.bindTo($scope).add({combo: 'ctrl+down', callback: zemHeaderNavigationService.quickNavigate});
+            hotkeys.bindTo($scope).add({combo: 'ctrl+left', callback: zemHeaderNavigationService.quickNavigate});
+            hotkeys.bindTo($scope).add({combo: 'ctrl+right', callback: zemHeaderNavigationService.quickNavigate});
         }
 
         function open () {
