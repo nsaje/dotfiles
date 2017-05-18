c567779a7af90c0aa1ab4dc7ca73d00aca00a680
Jure Bajt <jure.bajt@gmail.com>
2017-05-18 08:51:48 +0200

    Added entity check to ckeckStateChange promise chain. (#2246)

--- client/one/app/views/main/zemMainView.controller.js
+++ client/one/app/views/main/zemMainView.controller.js
@@ -13,8 +13,7 @@ angular.module('one.views').controller('zemMainView', function ($scope, $locatio
         if (checksPromise) return;
 
         $ctrl.mainStateAllowed = false;
-
-        checksPromise = $q.all([canActivateMainState(), checkRedirectToDefaultAccount()])
+        checksPromise = $q.all([canActivateMainState(), checkRedirectToDefaultAccount(), checkIfEntityExists()])
             .then(function () {
                 checksPromise = null;
                 $ctrl.mainStateAllowed = true;
@@ -64,5 +63,31 @@ angular.module('one.views').controller('zemMainView', function ($scope, $locatio
                 }
             });
         return deferred.promise;
+    }
+
+    function checkIfEntityExists () {
+        var entityGetter = getEntityGetter($state.params.level);
+        if (!entityGetter) return $q.resolve();
+
+        var deferred = $q.defer();
+        entityGetter($state.params.id)
+            .then(function () {
+                deferred.resolve();
+            })
+            .catch(function () {
+                deferred.reject({
+                    state: 'v2.analytics',
+                    params: {level: constants.levelStateParam.ACCOUNTS, id: null},
+                });
+            });
+        return deferred.promise;
+    }
+
+    function getEntityGetter (level) {
+        switch (level) {
+        case constants.levelStateParam.ACCOUNT: return zemNavigationService.getAccount;
+        case constants.levelStateParam.CAMPAIGN: return zemNavigationService.getCampaign;
+        case constants.levelStateParam.AD_GROUP: return zemNavigationService.getAdGroup;
+        }
     }
 });
