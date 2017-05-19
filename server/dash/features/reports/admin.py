b5c7bd3b56e7902b40f31f306daae3e979e59845
matematik7 <domen@ipavec.net>
2017-05-19 12:30:29 +0200

    Fix: improve reports job admin

--- server/dash/features/reports/admin.py
+++ server/dash/features/reports/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.core import urlresolvers
 
 import models
 
@@ -10,16 +11,36 @@ class ReportJobAdmin(admin.ModelAdmin):
         'created_dt',
         'user',
         'status',
-        'result'
+        'result',
+        'is_scheduled',
     )
 
     list_filter = (
         'status',
     )
 
-    readonly_fields = ('created_dt', 'user', 'query', 'result', 'scheduled_report')
+    readonly_fields = (
+        'created_dt',
+        'user',
+        'query',
+        'result',
+        'scheduled_report',
+        'link_to_scheduled_report',
+    )
 
     ordering = ('-created_dt', )
+
+    def link_to_scheduled_report(self, obj):
+        if obj.scheduled_report is None:
+            return ''
+        link = urlresolvers.reverse("admin:dash_scheduledreport_change", args=[obj.scheduled_report.id])
+        print(link)
+        return u'<a href="%s">%s</a>' % (link, obj.scheduled_report.name)
+    link_to_scheduled_report.allow_tags = True
+
+    def is_scheduled(self, obj):
+        return obj.scheduled_report is  not None
+    is_scheduled.boolean = True
 
 
 admin.site.register(models.ReportJob, ReportJobAdmin)
