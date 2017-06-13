b486d0c89287f9e5d1730d4fcaf510b325a3dd57
Nejc Saje <nejc@saje.info>
2017-06-13 14:40:17 +0200

    move restapi to gunicorn gthread

--- docker/start-server.sh
+++ docker/start-server.sh
@@ -6,6 +6,7 @@ msg() {
 
 GUNICORN_TO=${GUNICORN_TIMEOUT:-60}
 GUNICORN_WC=${GUNICORN_WORKERS:-4}
+GUNICORN_THREADS=${GUNICORN_THREADS:-4}
 GUNICORN_WORKER_CLASS=${GUNICORN_WORKER_CLASS:-gevent}
 GUNICORN_LOG_LEVEL=${GUNICORN_LOG_LEVEL:-info}
 
@@ -13,8 +14,8 @@ msg "booting container. ETCD: $ETCD"
 python manage.py collectstatic --noinput
 if [ -z "$NEW_RELIC_LICENSE_KEY" ]; then
     msg "starting app in $PWD"
-    gunicorn --timeout $GUNICORN_TO --workers=$GUNICORN_WC --worker-class=$GUNICORN_WORKER_CLASS --log-level=$GUNICORN_LOG_LEVEL --bind 0.0.0.0:8000 server.wsgi:application
+    gunicorn --timeout $GUNICORN_TO --workers=$GUNICORN_WC --threads=$GUNICORN_THREADS --worker-class=$GUNICORN_WORKER_CLASS --log-level=$GUNICORN_LOG_LEVEL --bind 0.0.0.0:8000 server.wsgi:application
 else
     msg "starting app with NewRelic in $PWD"
-    /usr/local/bin/newrelic-admin run-program /usr/local/bin/gunicorn --timeout $GUNICORN_TO --workers=$GUNICORN_WC --worker-class=$GUNICORN_WORKER_CLASS  --log-level=$GUNICORN_LOG_LEVEL --bind 0.0.0.0:8000 server.wsgi:application
+    /usr/local/bin/newrelic-admin run-program /usr/local/bin/gunicorn --timeout $GUNICORN_TO --workers=$GUNICORN_WC --threads=$GUNICORN_THREADS --worker-class=$GUNICORN_WORKER_CLASS  --log-level=$GUNICORN_LOG_LEVEL --bind 0.0.0.0:8000 server.wsgi:application
 fi
