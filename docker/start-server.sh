fa93611ad9d47e625c4eb787006ff101a2d1f41e
Nejc Saje <nejc@saje.info>
2017-06-13 12:11:25 +0200

    move gunicorn restapi to sync workers

--- docker/start-server.sh
+++ docker/start-server.sh
@@ -6,13 +6,15 @@ msg() {
 
 GUNICORN_TO=${GUNICORN_TIMEOUT:-60}
 GUNICORN_WC=${GUNICORN_WORKERS:-4}
+GUNICORN_WORKER_CLASS=${GUNICORN_WORKER_CLASS:-gevent}
+GUNICORN_LOG_LEVEL=${GUNICORN_LOG_LEVEL:-info}
 
 msg "booting container. ETCD: $ETCD"
 python manage.py collectstatic --noinput
 if [ -z "$NEW_RELIC_LICENSE_KEY" ]; then
     msg "starting app in $PWD"
-    gunicorn --timeout $GUNICORN_TO --workers=$GUNICORN_WC --worker-class=gevent --log-level=debug --bind 0.0.0.0:8000 server.wsgi:application
+    gunicorn --timeout $GUNICORN_TO --workers=$GUNICORN_WC --worker-class=$GUNICORN_WORKER_CLASS --log-level=$GUNICORN_LOG_LEVEL --bind 0.0.0.0:8000 server.wsgi:application
 else
     msg "starting app with NewRelic in $PWD"
-    /usr/local/bin/newrelic-admin run-program /usr/local/bin/gunicorn --timeout $GUNICORN_TO --workers=$GUNICORN_WC --worker-class=gevent  --log-level=debug --bind 0.0.0.0:8000 server.wsgi:application
+    /usr/local/bin/newrelic-admin run-program /usr/local/bin/gunicorn --timeout $GUNICORN_TO --workers=$GUNICORN_WC --worker-class=$GUNICORN_WORKER_CLASS  --log-level=$GUNICORN_LOG_LEVEL --bind 0.0.0.0:8000 server.wsgi:application
 fi
