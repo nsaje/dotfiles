a217a412457cd725ddae6cc7159c6ef494de12b7
Luka Šilovinac <luka.silovinac@zemanta.com>
2017-06-22 15:13:00 +0200

    Fix tests

--- server/redshiftapi/tests/test_api_audiences.py
+++ server/redshiftapi/tests/test_api_audiences.py
@@ -24,8 +24,9 @@ class AudiencesTestCase(TestCase):
         ]
         api_audiences.get_audience_sample_size(1, 'slug1', 10, rules)
 
-        expected_query = 'SELECT COUNT(DISTINCT(zuid)) FROM pixie_sample WHERE account_id = %s AND slug = %s AND timestamp > %s ' \
-                         'AND (referer ILIKE %s% OR referer ILIKE %s% OR referer ILIKE %%s% OR referer NOT ILIKE %s% OR referer NOT ILIKE %s%)'
+        expected_query = "SELECT COUNT(DISTINCT(zuid)) FROM pixie_sample WHERE account_id = %s AND slug = %s AND timestamp > %s " \
+                         "AND (referer ILIKE %s + '%%' OR referer ILIKE %s + '%%' OR referer ILIKE '%%' + %s + '%%' OR referer "\
+                         "NOT ILIKE %s + '%%' OR referer NOT ILIKE %s + '%%')"
         time = datetime.datetime.now().date() - datetime.timedelta(days=10)
         expected_params = [1, 'slug1', time.isoformat(), 'a', 'b', 'c', 'd', 'e']
         mock_cursor.execute.assert_called_once_with(backtosql.SQLMatcher(expected_query), expected_params)
