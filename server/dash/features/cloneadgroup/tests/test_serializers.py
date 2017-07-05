eaa33e238cd26b4e1a1e9d0846be0634fdce0ace
Ziga Stopinsek <sigi.kajzer@gmail.com>
2017-07-05 14:34:24 +0200

    fixed migrations and added a test that i forgot to ad earlier

---
+++ server/dash/features/cloneadgroup/tests/test_serializers.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from mock import patch
+from utils.magic_mixer import magic_mixer
+
+from .. import serializers
+import core.entity
+import core.source
+
+
+@patch.object(core.entity.ContentAd.objects, 'insert_redirects')
+@patch('automation.autopilot_plus.initialize_budget_autopilot_on_ad_group')
+@patch('utils.redirector_helper.insert_adgroup')
+class CloneForm(TestCase):
+
+    def test_clone_valid(self, mock_insert_adgroup, mock_redirects, mock_autopilot):
+        ad_group = magic_mixer.blend(core.entity.AdGroup)
+        magic_mixer.cycle(5).blend(core.entity.ContentAd, ad_group=ad_group, archived=False)
+        dest_campaign = magic_mixer.blend(core.entity.Campaign)
+
+        dest_campaign.get_current_settings().save()
+
+        form = serializers.CloneAdGroupSerializer(data={
+            'ad_group_id': ad_group.pk,
+            'destination_campaign_id': dest_campaign.pk,
+            'destination_ad_group_name': 'Test'
+        })
+        self.assertTrue(form.is_valid())
