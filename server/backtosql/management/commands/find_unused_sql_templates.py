895638094e4ad308ff88e39fc3a1fbcaeda18fc7
Gregor Ratajc <me@gregorratajc.com>
2017-07-25 11:51:38 +0200

    Adds command to find unused templates (#2366)
    * adds find_unused_templates command
    
    * comments

---
+++ server/backtosql/management/commands/find_unused_sql_templates.py
@@ -0,0 +1,41 @@
+import collections
+import fileinput
+import operator
+import os
+from django.core.management.base import BaseCommand
+
+
+TEMPLATES_PREFIX = "templates/"
+
+
+class Command(BaseCommand):
+    help = 'Finds all unused templates'
+
+    def handle(self, *args, **options):
+
+        templates = get_templates()
+        files = get_py_files()
+        usage = collections.defaultdict(int)
+
+        for line in fileinput.input(files):
+            for template in templates:
+                name = get_template_name(template)
+
+                if name in line:
+                    usage[template] += 1
+
+        print("Times used\tTemplate path")
+        for template, counter in sorted(usage.items(), reverse=True, key=operator.itemgetter(1, 0)):
+            print("{:>10}\t{}".format(counter, template))
+
+
+def get_template_name(path):
+    return path.rsplit(TEMPLATES_PREFIX)[1]
+
+
+def get_templates(extension=".sql"):
+    return [f[2:-1] for f in os.popen("find . -name '*{}' | sort".format(extension)).readlines() if TEMPLATES_PREFIX in f]
+
+
+def get_py_files():
+    return [f[2:-1] for f in os.popen("find . -name '*.py' | sort").readlines()]
