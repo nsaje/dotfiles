b096519b239f568a7dbd4647616228c821009cee
Žiga Stopinšek <sigi.kajzer@gmail.com>
2017-08-01 16:32:04 +0200

    Whitelabel - login support (#2396)
    * whitelabel custom login support prep
    
    * whitelabel assignment tag
    
    * improved whitelabel info
    
    * - added whitelabel tag
    
    * - 404, 500, tos fixes
    
    * - newscorp > zemanta

---
+++ server/zemauth/templatetags/whitelabel.py
@@ -0,0 +1,52 @@
+from django import template
+from django.db.models import Q
+
+import dash.constants
+import core.entity
+
+register = template.Library()
+
+
+def _get_whitelabel_from_host(context):
+    host = context['request'].META.get('HTTP_HOST', '')
+    if host == 'newscorp.zemanta.com':
+        return dash.constants.Whitelabel.NEWSCORP
+    return None
+
+
+def _get_user_agency(context):
+    user = context['request'].user
+    if user.is_anonymous():
+        return None
+    return core.entity.agency.Agency.objects.all().filter(
+        Q(users__id=user.id) | Q(account__users__id=user.id)
+    ).first()
+
+
+def _get_agency_from_host(context):
+    whitelabel = _get_whitelabel_from_host(context)
+    if not whitelabel:
+        return None
+    return core.entity.agency.Agency.objects.filter(
+        whitelabel=whitelabel,
+    ).first()
+
+
+@register.simple_tag(takes_context=True)
+def whitelabel_from_host(context):
+    return _get_whitelabel_from_host(context)
+
+
+@register.simple_tag(takes_context=True)
+def get_whitelabel_info(context):
+    info = {
+        'base': 'zemanta',
+        'favicon': None,
+        'dashboard_title': None,
+    }
+    agency = _get_user_agency(context) or _get_agency_from_host(context)
+    if agency and agency.whitelabel:
+        info['base'] = agency.whitelabel
+        info['favicon'] = agency.custom_favicon_url
+        info['dashboard_title'] = agency.custom_dashboard_title
+    return info
