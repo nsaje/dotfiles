4188042926177521b8c1547bca9e1ad0c58f8437
Gregor Ratajc <me@gregorratajc.com>
2017-08-21 11:00:54 +0200

    return first matching budget and credit line item

--- server/core/entity/adgroup/ad_group/bcm_mixin.py
+++ server/core/entity/adgroup/ad_group/bcm_mixin.py
@@ -3,18 +3,12 @@ import core.bcm
 
 class AdGroupBCMMixin(object):
     def get_todays_fee_and_margin(self):
-        try:
-            todays_budget = core.bcm.BudgetLineItem.objects.filter(
-                campaign_id=self.campaign_id).filter_today().get()
-            margin = todays_budget.margin
-        except core.bcm.BudgetLineItem.DoesNotExist:
-            margin = None
+        todays_budget = core.bcm.BudgetLineItem.objects.filter(
+            campaign_id=self.campaign_id).filter_today().first()
+        margin = todays_budget.margin if todays_budget else None
 
-        try:
-            todays_credit = core.bcm.CreditLineItem.objects.filter(
-                account_id=self.campaign.account_id).filter_active().get()
-            fee = todays_credit.license_fee
-        except core.bcm.CreditLineItem.DoesNotExist:
-            fee = None
+        todays_credit = core.bcm.CreditLineItem.objects.filter(
+            account_id=self.campaign.account_id).filter_active().first()
+        fee = todays_credit.license_fee if todays_credit else None
 
         return fee, margin
