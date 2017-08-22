dc1d739929214a3f3345eb2f6ae8a4bb81aa2bc0
Domen Ipavec <domen@ipavec.net>
2017-08-22 16:16:29 +0200

    Monitor blacklists log to slack (#2442)
    * monitor blacklists log to slack instead of sentry
    
    * Pretty print publisher ids
    
    * Ignore yahoo all publishers
    
    * Added comment

--- server/etl/management/commands/monitor_blacklists.py
+++ server/etl/management/commands/monitor_blacklists.py
@@ -15,6 +15,7 @@ from dash import publisher_group_helpers
 import redshiftapi.db
 
 from utils import list_helper
+from utils import slack
 
 logger = logging.getLogger(__name__)
 
@@ -33,6 +34,8 @@ class Command(ExceptionCommand):
         parser.add_argument(
             '--ad_group_ids',
             help='Comma separated ad group ids')
+        parser.add_argument('--no-influx', action='store_true', help='Do not log to influx')
+        parser.add_argument('--no-slack', action='store_true', help='Do not log to slack')
 
     def handle(self, *args, **options):
         logger.info('Monitor publisher whitelisting and blacklisting.')
@@ -53,9 +56,16 @@ class Command(ExceptionCommand):
         publishers_map = self.get_publishers_map(date_from)
         stats_map = self.get_stats(date_from, date_to, ad_groups)
 
-        self.check_ad_groups(ad_groups, publishers_map, stats_map)
+        self.check_ad_groups(
+            ad_groups=ad_groups,
+            publishers_map=publishers_map,
+            stats_map=stats_map,
+            date_from=date_from,
+            log_influx=not options['no_influx'],
+            log_slack=not options['no_slack'],
+        )
 
-    def check_ad_groups(self, ad_groups, publishers_map, stats_map):
+    def check_ad_groups(self, ad_groups, publishers_map, stats_map, date_from, log_influx=True, log_slack=True):
         # load necessary objects
         ad_groups = ad_groups.select_related('campaign', 'campaign__account')
         campaigns = models.Campaign.objects.filter(pk__in=ad_groups.values_list('campaign_id', flat=True))
@@ -80,9 +90,12 @@ class Command(ExceptionCommand):
             blacklisted_publishers = set(list_helper.flatten(publishers_map[x] for x in blacklist))
             whitelisted_publishers = set(list_helper.flatten(publishers_map[x] for x in whitelist))
 
+            # filter out recently blacklisted, because we can still have some data for blacklisted
             overall_blacklisted_entry_ids |= set(models.PublisherGroupEntry.objects
                                                  .filter(publisher_group_id__in=blacklist)
+                                                 .filter(modified_dt__lt=date_from)
                                                  .values_list('id', flat=True))
+            # do not filter out recent, because we can already have some data for whitelisted
             overall_whitelisted_entry_ids |= set(models.PublisherGroupEntry.objects
                                                  .filter(publisher_group_id__in=whitelist)
                                                  .values_list('id', flat=True))
@@ -103,9 +116,16 @@ class Command(ExceptionCommand):
                 violator_publisher_ids = self.get_whitelist_violators(
                     traffic_publisher_ids_wo_outbrain, whitelisted_publishers, blacklisted_publishers)
                 if violator_publisher_ids:
-                    logger.warning(
-                        'publisher_group_monitor: Found publisher statistics for non-whitelisted publishers in ad group %s',
-                        ad_group.id, extra={'publisher_ids': violator_publisher_ids})
+                    message = 'Found publisher statistics in ad group {ad_group_id} for non-whitelisted publishers: {publisher_ids}'.format(
+                        ad_group_id=ad_group.id,
+                        publisher_ids=', '.join(violator_publisher_ids),
+                    )
+                    logger.info(message)
+                    if log_slack:
+                        slack.publish(
+                            message,
+                            channel='z1-monitor-blacklist',
+                        )
 
                     ad_group_violators |= violator_publisher_ids
 
@@ -117,9 +137,16 @@ class Command(ExceptionCommand):
                 violator_publisher_ids = self.get_blacklist_violators(
                     traffic_publisher_ids_wo_outbrain, blacklisted_publishers)
                 if violator_publisher_ids:
-                    logger.warning(
-                        'publisher_group_monitor: Found publisher statistics for blacklisted publishers in ad group %s',
-                        ad_group.id, extra={'publisher_ids': violator_publisher_ids})
+                    message = 'Found publisher statistics in ad group {ad_group_id} for blacklisted publishers: {publisher_ids}'.format(
+                        ad_group_id=ad_group.id,
+                        publisher_ids=', '.join(violator_publisher_ids),
+                    )
+                    logger.info(message)
+                    if log_slack:
+                        slack.publish(
+                            message,
+                            channel='z1-monitor-blacklist',
+                        )
 
                     ad_group_violators |= violator_publisher_ids
 
@@ -131,9 +158,16 @@ class Command(ExceptionCommand):
                     violator_publisher_ids = self.get_blacklist_violators(
                         traffic_publisher_ids_outbrain, outbrain_blacklist)
                     if violator_publisher_ids:
-                        logger.warning(
-                            'publisher_group_monitor: Found Outbrain publisher statistics for blacklisted publishers in ad group %s',
-                            ad_group.id, extra={'publisher_ids': violator_publisher_ids})
+                        message = 'Found Outbrain publisher statistics in ad group {ad_group_id} for blacklisted publishers: {publisher_ids}'.format(
+                            ad_group_id=ad_group.id,
+                            publisher_ids=', '.join(violator_publisher_ids),
+                        )
+                        logger.info(message)
+                        if log_slack:
+                            slack.publish(
+                                message,
+                                channel='z1-monitor-blacklist',
+                            )
 
                         ad_group_violators |= violator_publisher_ids
 
@@ -142,22 +176,24 @@ class Command(ExceptionCommand):
 
         logger.info('Blacklisted publisher group entries count %s', len(overall_blacklisted_entry_ids))
         logger.info('Whitelisted publisher group entries count %s', len(overall_whitelisted_entry_ids))
-        influx.gauge('dash.blacklisted_publisher.status', len(overall_blacklisted_entry_ids), status='blacklisted')
-        influx.gauge('dash.blacklisted_publisher.status', len(overall_whitelisted_entry_ids), status='whitelisted')
+        if log_influx:
+            influx.gauge('dash.blacklisted_publisher.status', len(overall_blacklisted_entry_ids), status='blacklisted')
+            influx.gauge('dash.blacklisted_publisher.status', len(overall_whitelisted_entry_ids), status='whitelisted')
 
         logger.info('Overall clicks for violator publishers: %s', overall_vialotors_stats['clicks'])
         logger.info('Overall impressions for violator publishers: %s', overall_vialotors_stats['impressions'])
-        influx.gauge('dash.blacklisted_publisher.stats', overall_vialotors_stats['clicks'], type='clicks')
-        influx.gauge('dash.blacklisted_publisher.stats', overall_vialotors_stats['impressions'], type='impressions')
+        if log_influx:
+            influx.gauge('dash.blacklisted_publisher.stats', overall_vialotors_stats['clicks'], type='clicks')
+            influx.gauge('dash.blacklisted_publisher.stats', overall_vialotors_stats['impressions'], type='impressions')
 
     def get_whitelist_violators(self, publisher_ids, whitelisted_publishers, blacklisted_publishers):
         whitelist = (whitelisted_publishers - blacklisted_publishers)
         lookup = publisher_helpers.PublisherIdLookupMap(whitelist)
-        return set([publisher_id for publisher_id in publisher_ids if publisher_id not in lookup])
+        return set([publisher_id for publisher_id in publisher_ids if publisher_id not in lookup and publisher_id != 'all publishers__4'])
 
     def get_blacklist_violators(self, publisher_ids, blacklisted_publishers):
         lookup = publisher_helpers.PublisherIdLookupMap(blacklisted_publishers)
-        return set([publisher_id for publisher_id in publisher_ids if publisher_id in lookup])
+        return set([publisher_id for publisher_id in publisher_ids if publisher_id in lookup and publisher_id != 'all publishers__4'])
 
     def get_stats(self, date_from, date_to, ad_groups):
         params = {
