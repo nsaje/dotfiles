8eb30ebe3634657e2bb758dafe3f611e81b09700
Ziga Stopinsek <sigi.kajzer@gmail.com>
2017-09-18 12:24:07 +0200

    changed to prod salesforce user

--- server/serviceapi/salesforce/views.py
+++ server/serviceapi/salesforce/views.py
@@ -6,7 +6,7 @@ from rest_framework.serializers import ValidationError
 from .. import base, authentication
 from . import serializers, service
 
-SERVICE_NAME = 'salesforce-test'
+SERVICE_NAME = 'salesforce'
 
 
 class CreateClientView(base.ServiceAPIBaseView):
@@ -58,7 +58,7 @@ class CreateCreditLineView(base.ServiceAPIBaseView):
         influx.incr('create_credit', 1, status='ok')
         return self.response_ok({
             'z1_cli_id': cli.pk,
-            'z1_data': cli.get_settings_dict()
+            'z1_data': cli.get_settings_dict() # TODO: status to text
         })
 
 
