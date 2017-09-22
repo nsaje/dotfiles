c0d5fab93b936db46e0a81a3cbbc797693b21912
Jure Ham <jure@hamsworld.net>
2017-09-22 17:25:45 +0200

    K1api: content_ad_publishers stats view fix (#2525)

--- server/redshiftapi/internal_stats/content_ad_publishers.py
+++ server/redshiftapi/internal_stats/content_ad_publishers.py
@@ -1,5 +1,6 @@
+import backtosql
 from redshiftapi import db
-from redshiftapi import queries
+from redshiftapi import models as rsmodels
 from collections import namedtuple
 
 import dash.models
@@ -13,13 +14,18 @@ def query_content_ad_publishers(date_from, date_to, ad_group_ids=None):
     if ad_group_ids:
         constraints['ad_group_id'] = ad_group_ids
 
-    sql, params = queries.prepare_query_all_base(
-        breakdown=['ad_group_id', 'content_ad_id', 'publisher', 'source_id'],
-        constraints=constraints,
-        parents=None,
-        use_publishers_view=True
-    )
+    mvmaster = rsmodels.MVMasterPublishers()
+    breakdown = ['ad_group_id', 'content_ad_id', 'publisher', 'source_id']
+    context = {
+        'breakdown': mvmaster.select_columns(breakdown),
+        'aggregates': [x for x in mvmaster.get_aggregates() if x.alias != 'external_id'],  # this is some special publishers view column
+        'constraints': mvmaster.get_constraints(constraints, None),
+        'view': 'mv_master',
+        'orders': [mvmaster.get_column('-clicks').as_order('-clicks', nulls='last')],
+    }
 
+    sql = backtosql.generate_sql('breakdown.sql', context)
+    params = context['constraints'].get_params()
     cursor = db.get_stats_cursor()
     cursor.execute(sql, params)
     return _fetch_all_with_source_slug(cursor)
@@ -28,7 +34,7 @@ def query_content_ad_publishers(date_from, date_to, ad_group_ids=None):
 def _fetch_all_with_source_slug(cursor):
     desc = cursor.description
     stats = namedtuple('Stats', [col[0] for col in desc])
-    stats_out = namedtuple('StatsOutput', ['source_slug', 'ad_group_id', 'content_ad_id', 'publisher', 'clicks', 'cost', 'dataCost'])
+    stats_out = namedtuple('StatsOutput', ['source_slug', 'ad_group_id', 'content_ad_id', 'publisher', 'clicks', 'media_cost', 'data_cost', 'video_complete'])
 
     source_id_map = {s.id: s.bidder_slug for s in dash.models.Source.objects.all()}
 
@@ -41,6 +47,7 @@ def _fetch_all_with_source_slug(cursor):
             publisher=c.publisher,
             clicks=c.clicks,
             cost=c.media_cost,
-            dataCost=c.data_cost,
+            data_cost=c.data_cost,
+            video_complete=c.video_complete,
         )
         yield c_out
