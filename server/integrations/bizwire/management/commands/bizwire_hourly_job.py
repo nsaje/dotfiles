77434afb5084d2b3bd73d45515a92d076763f251
Luka Šilovinac <luka.silovinac@zemanta.com>
2017-09-27 14:13:55 +0200

    [bizwire] Don't stop ad groups and ads anymore

--- server/integrations/bizwire/management/commands/bizwire_hourly_job.py
+++ server/integrations/bizwire/management/commands/bizwire_hourly_job.py
@@ -11,9 +11,9 @@ class Command(ExceptionCommand):
 
     def handle(self, *args, **options):
         try:
-            actions.check_pacific_noon_and_stop_ads()
+            # actions.check_pacific_noon_and_stop_ads()
             actions.check_time_and_create_new_ad_groups()
-            actions.check_date_and_stop_old_ad_groups()
+            # actions.check_date_and_stop_old_ad_groups()
             actions.check_local_midnight_and_recalculate_daily_budgets()
             actions.reprocess_missing_articles()
         except:
