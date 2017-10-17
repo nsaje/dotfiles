45af42ef4b781eba61dafc4473102184fa1a8e73
Gregor Ratajc <me@gregorratajc.com>
2017-10-17 09:29:24 +0200

    use integers instead of int2

--- server/etl/templates/etl_create_temp_table_mvh_adgroup_structure.sql
+++ server/etl/templates/etl_create_temp_table_mvh_adgroup_structure.sql
@@ -1,6 +1,6 @@
 CREATE TEMP TABLE mvh_adgroup_structure (
-    agency_id int2 encode lzo,
-    account_id int2 encode lzo,
-    campaign_id int2 encode lzo,
-    ad_group_id int2 encode lzo
-) sortkey(ad_group_id, campaign_id, account_id, agency_id)
+    agency_id integer encode lzo,
+    account_id integer encode lzo,
+    campaign_id integer encode lzo,
+    ad_group_id integer encode lzo
+) sortkey(ad_group_id, campaign_id, account_id)
