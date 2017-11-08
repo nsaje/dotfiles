d11c2ae1d266d311890b4022bc3b8d709627b328
Nejc Saje <nejc@saje.info>
2017-11-08 13:26:56 +0100

    Patch psycopg2 for gevent (#2611)
    * psycogreen patch
    
    * update start-server.sh

--- docker/start-server.sh
+++ docker/start-server.sh
@@ -8,16 +8,31 @@ GUNICORN_TO=${GUNICORN_TIMEOUT:-60}
 GUNICORN_WC=${GUNICORN_WORKERS:-4}
 GUNICORN_THREADS=${GUNICORN_THREADS:-1}
 GUNICORN_WORKER_CLASS=${GUNICORN_WORKER_CLASS:-gevent}
+GUNICORN_WORKER_CONNECTIONS=${GUNICORN_WORKER_CONNECTIONS:-200}
 GUNICORN_LOG_LEVEL=${GUNICORN_LOG_LEVEL:-info}
 GUNICORN_BACKLOG=${GUNICORN_BACKLOG:-2048}
 GUNICORN_ACCESS_LOGFORMAT=${GUNICORN_ACCESS_LOGFORMAT:-'%(h)s %(l)s %(u)s %(t)s "%(r)s" elb=%({X-Amzn-Trace-Id}i)s traefik=%({X-Traefik-Reqid}i)s %(s)s %(b)s "%(f)s" "%(a)s"'}
+
+GUNICORN_ARGS=(
+    --timeout=$GUNICORN_TO
+    --workers=$GUNICORN_WC
+    --threads=$GUNICORN_THREADS
+    --worker-class=$GUNICORN_WORKER_CLASS
+    --worker-connections=$GUNICORN_WORKER_CONNECTIONS
+    --log-level=$GUNICORN_LOG_LEVEL
+    --backlog=$GUNICORN_BACKLOG
+    --access-logfile=-
+    --access-logformat="$GUNICORN_ACCESS_LOGFORMAT"
+    --bind=0.0.0.0:8000
+    server.wsgi:application
+)
 
 msg "booting container. ETCD: $ETCD"
 python manage.py collectstatic --noinput
 if [ -z "$NEW_RELIC_LICENSE_KEY" ]; then
     msg "starting app in $PWD"
-    gunicorn --timeout $GUNICORN_TO --workers=$GUNICORN_WC --threads=$GUNICORN_THREADS --worker-class=$GUNICORN_WORKER_CLASS --log-level=$GUNICORN_LOG_LEVEL --backlog=$GUNICORN_BACKLOG --access-logfile=- --access-logformat="$GUNICORN_ACCESS_LOGFORMAT" --bind 0.0.0.0:8000 server.wsgi:application
+    gunicorn "${GUNICORN_ARGS[@]}"
 else
     msg "starting app with NewRelic in $PWD"
-    /usr/local/bin/newrelic-admin run-program /usr/local/bin/gunicorn --timeout $GUNICORN_TO --workers=$GUNICORN_WC --threads=$GUNICORN_THREADS --worker-class=$GUNICORN_WORKER_CLASS  --log-level=$GUNICORN_LOG_LEVEL --backlog=$GUNICORN_BACKLOG --access-logfile=- --access-logformat="$GUNICORN_ACCESS_LOGFORMAT" --bind 0.0.0.0:8000 server.wsgi:application
+    /usr/local/bin/newrelic-admin run-program /usr/local/bin/gunicorn "${GUNICORN_ARGS[@]}"
 fi
--- server/server/wsgi.py
+++ server/server/wsgi.py
@@ -15,5 +15,11 @@ import cdecimal
 # Ensure any import of decimal gets cdecimal instead.
 sys.modules['decimal'] = cdecimal
 
+import gevent.socket
+import socket
+if socket.socket is gevent.socket.socket:  # if running in gevent
+    import psycogreen.gevent
+    psycogreen.gevent.patch_psycopg()
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
