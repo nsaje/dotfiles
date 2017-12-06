f766d0dbfaea30e4194d08fea0f9618274e4712a
Luka Šilovinac <luka.silovinac@zemanta.com>
2017-12-06 10:54:58 +0100

    Temporarily disable parallel tests

--- server/run_tests.sh
+++ server/run_tests.sh
@@ -4,5 +4,5 @@
 set -eo pipefail
 
 #coverage run manage.py test --failfast --noinput --parallel=4 --timing --keepdb && echo "PASSED" || echo "FAILED"
-python manage.py test --noinput --parallel=2 --timing && echo "PASSED" || echo "FAILED"
+python manage.py test --noinput --timing && echo "PASSED" || echo "FAILED"
 exit $?
