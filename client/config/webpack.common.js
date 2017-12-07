3c8c94bae8a8aa6c5e5259cf80d127fffc6f374e
Jure Bajt <jure.bajt@gmail.com>
2017-12-07 14:14:06 +0100

    Fea client libs upgrade (#2670)
    * Upgraded Angular and dependencies.
    
    * Upgraded dev dependencies.
    
    * Upgraded highcharts.
    
    * Update package-lock.json.
    
    * Revert highcharts-ng upgrade due to a bug.

--- client/config/webpack.common.js
+++ client/config/webpack.common.js
@@ -89,7 +89,7 @@ function generateMainConfig (appConfig) {
         // Workaround for angular/angular#11580
         new webpack.ContextReplacementPlugin(
             // The (\\|\/) piece accounts for path separators in *nix and Windows
-            /angular(\\|\/)core(\\|\/)@angular/,
+            /angular(\\|\/)core(\\|\/)(@angular|esm5)/,
             root('./one'), // location of your src
             {} // a map of your routes
         ),
