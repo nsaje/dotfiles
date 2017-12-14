71add49f87ae8cfb007c0c29359f915d91a75d9d
Ziga Stopinsek <sigi.kajzer@gmail.com>
2017-12-14 09:29:50 +0100

    renamed bcm slack channel

--- server/core/bcm/bcm_slack.py
+++ server/core/bcm/bcm_slack.py
@@ -7,6 +7,7 @@ logger = logging.getLogger(__name__)
 ACCOUNT_URL = "https://one.zemanta.com/v2/credit/account/{}"
 CAMPAIGN_URL = "https://one.zemanta.com/v2/analytics/campaign/{}?settings"
 
+SLACK_CHANNEL = 'z1-budget-feed'
 SLACK_SKIP_LOG_ACCOUNTS = (305, )
 SLACK_NEW_CREDIT_MSG = u"New credit #{credit_id} added on account <{url}|{account_name}> with amount ${amount} and end date {end_date}."
 SLACK_NEW_AGENCY_CREDIT_MSG = u"New agency credit #{credit_id} added to agency {agency} with amount ${amount} and end date {end_date}."
@@ -21,7 +22,7 @@ def log_to_slack(account_id, msg):
     try:
         slack.publish(
             msg,
-            channel='bcm',
+            channel=SLACK_CHANNEL,
             username='z1'
         )
     except Exception:
