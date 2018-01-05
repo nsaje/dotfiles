cc0700df18c68a9d0c2600dab367c0f7160e22b1
Luka Šilovinac <luka.silovinac@gmail.com>
2018-01-05 14:33:16 +0100

    Add indexes to real time data history models (#2709)

--- server/automation/campaignstop/real_time_data_history.py
+++ server/automation/campaignstop/real_time_data_history.py
@@ -7,23 +7,23 @@ import core.source
 class RealTimeDataHistory(models.Model):
     ad_group = models.ForeignKey(core.entity.AdGroup)
     source = models.ForeignKey(core.source.Source)
-    date = models.DateField()
+    date = models.DateField(db_index=True)
     etfm_spend = models.DecimalField(
         max_digits=14,
         decimal_places=4,
         default=0,
     )
 
-    created_dt = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+    created_dt = models.DateTimeField(auto_now_add=True, verbose_name='Created at', db_index=True)
 
 
 class RealTimeCampaignDataHistory(models.Model):
     campaign = models.ForeignKey(core.entity.Campaign)
-    date = models.DateField()
+    date = models.DateField(db_index=True)
     etfm_spend = models.DecimalField(
         max_digits=14,
         decimal_places=4,
         default=0,
     )
 
-    created_dt = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+    created_dt = models.DateTimeField(auto_now_add=True, verbose_name='Created at', db_index=True)
