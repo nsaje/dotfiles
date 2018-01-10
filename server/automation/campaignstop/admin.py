5abe7422321316162d5eac2aa4fa8ccff529dccb
Luka Šilovinac <luka.silovinac@zemanta.com>
2018-01-10 17:26:02 +0100

    Fix text placement

--- server/automation/campaignstop/admin.py
+++ server/automation/campaignstop/admin.py
@@ -41,12 +41,12 @@ class RealTimeCampaignStopLogAdmin(admin.ModelAdmin):
         desc = textwrap.dedent('''\
             The calculated campaign state is: {state}.
 
-            Max allowed end date: {max_allowed_end_date} (is in past: {is_max_end_date_past})
-            Available budget (until above date): ${budget_spends_until_date}''')
+            Max allowed end date: {max_allowed_end_date} (is in past: {is_max_end_date_past})''')
         if 'budget_spends_until_date' in obj.context:
             desc += textwrap.dedent('''
 
                 Spend from daily statements was taken until <b>{budget_spends_until_date}</b>. Real time data was used for dates after that.
+                Available budget (until above date): ${budget_spends_until_date}
 
                 Spend data taken into account:
                     - Real time spend (current): ${current_rt_spend}
