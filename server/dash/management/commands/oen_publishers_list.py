ae6e4389ae84cc8c51529a75104a687c0fac1987
matematik7 <domen@ipavec.net>
2018-01-25 15:52:52 +0100

    Order publisher list by num bid requests

--- server/dash/management/commands/oen_publishers_list.py
+++ server/dash/management/commands/oen_publishers_list.py
@@ -4,10 +4,11 @@ from utils import dates_helper
 from utils import s3helpers
 
 select_query = """
-SELECT publisher, exchange, sum(bid_reqs)
+SELECT publisher, exchange, sum(bid_reqs) AS num_bid_req,
 FROM supply_stats
 WHERE date >= %s
 GROUP BY exchange, publisher
+ORDER BY num_bid_req DESC
 """
 
 unload_query = """
