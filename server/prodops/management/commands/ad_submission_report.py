35aaf680c5f6872dfc76b388728c313d472903ab
Ziga Stopinsek <sigi.kajzer@gmail.com>
2018-02-22 08:20:06 +0100

    added new prodops report, removed spend integrity check because of false positives

---
+++ server/prodops/management/commands/ad_submission_report.py
@@ -0,0 +1,108 @@
+import datetime
+
+from django.db.models import Q
+
+import utils.command_helpers
+import prodops.audience_report
+import prodops.helpers
+import dash.models
+
+OEN = 305
+HEADER = (
+    'Ad ID',
+    'Brand / Agency',
+    'Image URL',
+    'Landing page',
+    'Title',
+    'Display URL',
+    'Brand name',
+    'Description',
+)
+
+
+class Command(utils.command_helpers.ExceptionCommand):
+    help = "Create ad submission report"
+
+    def add_arguments(self, parser):
+        parser.add_argument('source', type=str)
+        parser.add_argument('--premium-only', dest='is_premium', action='store_true', default=False)
+        parser.add_argument('--approved-only', dest='is_approved', action='store_true', default=False)
+        parser.add_argument('--pending-only', dest='is_pending', action='store_true', default=False)
+        parser.add_argument('--active-only', dest='is_active', action='store_true', default=False)
+        parser.add_argument('--ex-oen', dest='is_not_oen', action='store_true', default=False)
+
+    def _print(self, msg):
+        self.stdout.write('{}\n'.format(msg))
+
+    def handle(self, *args, **options):
+        cas_list = dash.models.ContentAdSource.objects.all()
+
+        source = options['source']
+        if source.isdigit():
+            cas_list = cas_list.filter(source_id=int(source))
+        elif source:
+            cas_list = cas_list.filter(source=dash.models.Source.objects.get(
+                Q(bidder_slug='b1_{}'.format(source)) | Q(bidder_slug=source)
+            ))
+        else:
+            self._print('No source.')
+            return
+
+        flags = []
+        if options['is_premium']:
+            cas_list = cas_list.filter(
+                content_ad__ad_group__campaign_id__in=dash.models.CustomHack.objects.filter(
+                    summary='Approved for msn.com',
+                    campaign_id__isnull=False
+                ).values_list('campaign_id', flat=True)
+            )
+            flags.append('premum')
+        if options['is_approved']:
+            cas_list = cas_list.filter(
+                submission_status=dash.constants.ContentAdSubmissionStatus.APPROVED
+            )
+            flags.append('approved')
+        if options['is_pending']:
+            cas_list = cas_list.filter(
+                submission_status__in=(dash.constants.ContentAdSubmissionStatus.PENDING, dash.constants.ContentAdSubmissionStatus.NOT_SUBMITTED, )
+            )
+            flags.append('pending')
+        if options['is_active']:
+            cas_list = cas_list.filter(
+                content_ad__ad_group_id__in=dash.models.AdGroup.objects.all().filter_active().values_list('pk', flat=True)
+            )
+            flags.append('active')
+        if options['is_not_oen']:
+            cas_list = cas_list.exclude(
+                content_ad__ad_group__campaign__account_id=OEN
+            )
+            flags.append('exoen')
+
+        cas_list = cas_list.select_related(
+            'content_ad',
+            'content_ad__ad_group',
+            'content_ad__ad_group__campaign',
+            'content_ad__ad_group__campaign__account',
+            'content_ad__ad_group__campaign__account__agency',
+        )
+
+        out = []
+        for content_ad_source in cas_list:
+            if content_ad_source.content_ad.video_asset:
+                continue
+            out.append((
+                content_ad_source.content_ad.pk,
+                content_ad_source.content_ad.ad_group.campaign.account.get_long_name(),
+                content_ad_source.content_ad.get_image_url(),
+                content_ad_source.content_ad.url,
+                content_ad_source.content_ad.title,
+                content_ad_source.content_ad.display_url,
+                content_ad_source.content_ad.brand_name,
+                content_ad_source.content_ad.description,
+            ))
+
+        self._print('Report: ' + prodops.helpers.generate_report('{}_ads_{}_{}'.format(
+            source,
+            '-'.join(flags),
+            datetime.date.today()
+        ), [HEADER] + out))
