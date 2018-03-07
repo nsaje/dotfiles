3ef8b3dd387ac0f74507938f6848babbe5998bdd
Luka Šilovinac <luka.silovinac@zemanta.com>
2018-03-07 17:01:29 +0100

    [campaignstop] Also run selection job for marked campaigns and minor renaming

--- server/automation/management/commands/campaignstop_selection.py
+++ server/automation/management/commands/campaignstop_selection.py
@@ -15,7 +15,6 @@ class Command(ExceptionCommand):
     def handle(self, *args, **options):
         campaigns = core.entity.Campaign.objects.filter(
             real_time_campaign_stop=True,
-            campaignstopstate__almost_depleted=False,
             campaignstopstate__state=automation.campaignstop.constants.CampaignStopState.ACTIVE,
             campaignstopstate__max_allowed_end_date__gte=dates_helper.local_today(),
         )
