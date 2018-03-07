471915006dd633ac126eb2351169c0ea155e3593
Domen Ipavec <domen@ipavec.net>
2018-03-07 16:53:31 +0100

    [autopilot] Add calculate campaign daily budget (#2805)
    Calculate daily budget for campaigns with active autopilot.
    https://trello.com/c/DLl1X4Xs/724-budget-optimization-autopilot

---
+++ server/automation/autopilot/campaign.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+from decimal import Decimal
+
+from dash import models
+from utils import dates_helper
+
+
+def calculate_campaigns_daily_budget(campaign=None):
+    today = dates_helper.local_today()
+
+    budgets = (
+        models.BudgetLineItem.objects.all()
+        .filter_active()
+        .annotate_spend_data()
+        .select_related('campaign')
+    )
+    if campaign is not None:
+        budgets = budgets.filter(campaign=campaign)
+    else:
+        budgets = budgets.filter(campaign__settings__autopilot=True)
+
+    daily_budgets = defaultdict(Decimal)
+    for budget in budgets:
+        remaining_days = (budget.end_date - today).days + 1
+        budget_daily_part = budget.get_available_etfm_amount() / remaining_days
+        daily_budgets[budget.campaign] += budget_daily_part.quantize(Decimal('1.'))
+
+    return daily_budgets
