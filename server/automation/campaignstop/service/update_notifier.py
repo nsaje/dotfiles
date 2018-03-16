766576f3b4ca4eeb6008433bcb69d21f6d989242
Luka Šilovinac <luka.silovinac@zemanta.com>
2018-03-16 10:45:27 +0100

    Notify only for campaigns with campaignstop flag

--- server/automation/campaignstop/service/update_notifier.py
+++ server/automation/campaignstop/service/update_notifier.py
@@ -17,23 +17,25 @@ def notify_ad_group_settings_change(ad_group_settings, changes):
     if 'state' in changes or\
        'b1_sources_group_state' in changes or\
        'b1_sources_group_daily_budget' in changes:
-        campaign_id = ad_group_settings.ad_group.campaign_id
-        _notify(campaign_id, constants.CampaignUpdateType.DAILY_CAP)
+        campaign = ad_group_settings.ad_group.campaign
+        _notify(campaign, constants.CampaignUpdateType.DAILY_CAP)
 
 
 def notify_ad_group_source_settings_change(ad_group_source_settings, changes):
     if 'daily_budget_cc' in changes or 'state' in changes:
-        campaign_id = ad_group_source_settings.ad_group_source.ad_group.campaign_id
-        _notify(campaign_id, constants.CampaignUpdateType.DAILY_CAP)
+        campaign = ad_group_source_settings.ad_group_source.ad_group.campaign
+        _notify(campaign, constants.CampaignUpdateType.DAILY_CAP)
 
 
 def notify_budget_line_item_change(campaign):
-    _notify(campaign.id, constants.CampaignUpdateType.BUDGET)
+    _notify(campaign, constants.CampaignUpdateType.BUDGET)
 
 
-def _notify(campaign_id, type_):
-    logger.info('Notify campaign update: campaign_id=%s, type=%s', campaign_id, type_)
+def _notify(campaign, type_):
+    if not campaign.real_time_campaign_stop:
+        return
+    logger.info('Notify campaign update: campaign_id=%s, type=%s', campaign.id, type_)
     sqs_helper.write_message_json(
         settings.CAMPAIGN_STOP_UPDATE_HANDLER_QUEUE,
-        {'campaign_id': campaign_id, 'type': type_}
+        {'campaign_id': campaign.id, 'type': type_}
     )
