1cd9ef2a82b9c8ea897c87e0875b67fb8df4aa1b
Nejc Saje <nejc@saje.info>
2018-03-16 12:22:59 +0100

    Revert "Add currency field to reports (#2832)"
    This reverts commit 9a347972a01d3046a1b081bfc09ba6b552cb3e1e.

--- server/utils/columns/__init__.py
+++ server/utils/columns/__init__.py
@@ -3,5 +3,4 @@ from .columns import custom_column_to_field_name_mapping, get_field_name
 from .columns import custom_field_to_column_name_mapping, get_column_name
 from .columns import get_conversion_goals_column_names_mapping
 from .columns import add_date_to_name
-from .columns import is_cost_column
 from .exceptions import *
