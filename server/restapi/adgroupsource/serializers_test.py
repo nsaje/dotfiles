7f716185569ceb2fe4544304badf924f89fd31b7
Nejc Saje <nejc@saje.info>
2018-03-26 10:00:52 +0200

    add test for num queries

--- server/restapi/adgroupsource/serializers_test.py
+++ server/restapi/adgroupsource/serializers_test.py
@@ -17,3 +17,18 @@ class SerializersTest(TestCase):
         serializer = serializers.AdGroupSourceSerializer(data=data, many=True)
         self.assertFalse(serializer.is_valid())
         self.assertEqual(serializer.errors, ["Invalid source in object '{'source': 'b', 'cpc': 0.6}'!"])
+
+    def test_num_queries(self):
+        magic_mixer.blend(dash.models.Source, bidder_slug='a')
+        magic_mixer.blend(dash.models.Source, bidder_slug='b')
+        magic_mixer.blend(dash.models.Source, bidder_slug='c')
+        magic_mixer.blend(dash.models.Source, bidder_slug='d')
+        data = [
+            {'source': 'a', 'cpc': 0.5},
+            {'source': 'b', 'cpc': 0.6},
+            {'source': 'c', 'cpc': 0.6},
+            {'source': 'd', 'cpc': 0.6},
+        ]
+        with self.assertNumQueries(1):
+            serializer = serializers.AdGroupSourceSerializer(data=data, many=True)
+            self.assertTrue(serializer.is_valid())
