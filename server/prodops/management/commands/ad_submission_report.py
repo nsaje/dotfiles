e47b6d79bc7b4bf64d95ef585e8e3e69fb9cfbd9
Ziga Stopinsek <sigi.kajzer@gmail.com>
2018-04-10 12:06:23 +0200

    improved ad submission report

--- server/prodops/management/commands/ad_submission_report.py
+++ server/prodops/management/commands/ad_submission_report.py
@@ -26,9 +26,10 @@ class Command(utils.command_helpers.ExceptionCommand):
     def add_arguments(self, parser):
         parser.add_argument('source', type=str)
         parser.add_argument('--premium-only', dest='is_premium', action='store_true', default=False)
+        parser.add_argument('--not-premium', dest='is_not_premium', action='store_true', default=False)
         parser.add_argument('--approved-only', dest='is_approved', action='store_true', default=False)
         parser.add_argument('--pending-only', dest='is_pending', action='store_true', default=False)
-        parser.add_argument('--active-only', dest='is_active', action='store_true', default=False)
+        parser.add_argument('--live-only', dest='is_live', action='store_true', default=False)
         parser.add_argument('--ex-oen', dest='is_not_oen', action='store_true', default=False)
 
     def _print(self, msg):
@@ -51,12 +52,20 @@ class Command(utils.command_helpers.ExceptionCommand):
         flags = []
         if options['is_premium']:
             cas_list = cas_list.filter(
-                content_ad__ad_group__campaign_id__in=dash.models.CustomHack.objects.filter(
-                    summary='Approved for msn.com',
-                    campaign_id__isnull=False
-                ).values_list('campaign_id', flat=True)
+                Q(content_ad__ad_group__custom_flags__b1_msn_approved=True) |
+                Q(content_ad__ad_group__campaign__custom_flags__b1_msn_approved=True) |
+                Q(content_ad__ad_group__campaign__account__custom_flags__b1_msn_approved=True) |
+                Q(content_ad__ad_group__campaign__account__agency__custom_flags__b1_msn_approved=True)
             )
-            flags.append('premum')
+            flags.append('premium')
+        if options['is_not_premium']:
+            cas_list = cas_list.exclude(
+                Q(content_ad__ad_group__custom_flags__b1_msn_approved=True) |
+                Q(content_ad__ad_group__campaign__custom_flags__b1_msn_approved=True) |
+                Q(content_ad__ad_group__campaign__account__custom_flags__b1_msn_approved=True) |
+                Q(content_ad__ad_group__campaign__account__agency__custom_flags__b1_msn_approved=True)
+            )
+            flags.append('notpremium')
         if options['is_approved']:
             cas_list = cas_list.filter(
                 submission_status=dash.constants.ContentAdSubmissionStatus.APPROVED
@@ -64,14 +73,15 @@ class Command(utils.command_helpers.ExceptionCommand):
             flags.append('approved')
         if options['is_pending']:
             cas_list = cas_list.filter(
-                submission_status__in=(dash.constants.ContentAdSubmissionStatus.PENDING, dash.constants.ContentAdSubmissionStatus.NOT_SUBMITTED, )
+                submission_status__in=(dash.constants.ContentAdSubmissionStatus.PENDING,
+                                       dash.constants.ContentAdSubmissionStatus.NOT_SUBMITTED, )
             )
             flags.append('pending')
-        if options['is_active']:
+        if options['is_live']:
             cas_list = cas_list.filter(
-                content_ad__ad_group_id__in=dash.models.AdGroup.objects.all().filter_active().values_list('pk', flat=True)
+                content_ad__ad_group_id__in=dash.models.AdGroup.objects.all().filter_running().values_list('pk', flat=True)
             )
-            flags.append('active')
+            flags.append('live')
         if options['is_not_oen']:
             cas_list = cas_list.exclude(
                 content_ad__ad_group__campaign__account_id=OEN
