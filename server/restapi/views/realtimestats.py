b3ace381573b71809ddca21cb6947984d60279a4
Luka Šilovinac <luka.silovinac@zemanta.com>
2018-04-10 15:41:47 +0200

    Add permission check

--- server/restapi/views/realtimestats.py
+++ server/restapi/views/realtimestats.py
@@ -35,7 +35,10 @@ class AdGroupSourcesRealtimeStatsView(RESTAPIBaseView):
     def get(self, request, ad_group_id):
         ad_group = helpers.get_ad_group(request.user, ad_group_id, select_related=True)
 
-        stats = dash.features.realtimestats.get_ad_group_sources_stats(ad_group)
+        stats = dash.features.realtimestats.get_ad_group_sources_stats(
+            ad_group,
+            use_local_currency=request.user.has_perm('zemauth.can_see_infobox_in_local_currency'),
+        )
         return self.response_ok(
             restapi.serializers.realtimestats.AdGroupSourcesRealtimeStatsSerializer(stats, many=True).data,
         )
