b5747f196ed2537b3f3ce012535ec60747486e84
Jure Bajt <jure.bajt@gmail.com>
2018-04-17 07:35:06 +0200

    Fix SQL for performance calculations on Redshift. (#2903)

--- server/redshiftapi/templates/part_performance.sql
+++ server/redshiftapi/templates/part_performance.sql
@@ -3,9 +3,9 @@
 CASE
   WHEN
       {{ is_cost_dependent }}
-      AND TRUNC(CAST(COALESCE({{ cost_column }}, 0) AS NUMERIC), 2) > 0  -- has cost
+      AND TRUNC(CAST(COALESCE({{ cost_column }}, 0) AS DECIMAL(22, 18)), 2) > 0  -- has cost
       AND COALESCE(
-            TRUNC(CAST(CASE WHEN {{ has_conversion_key }} THEN {{ cost_column }} / NULLIF({{ conversion_key }}, 0) ELSE {{ metric_column }} END AS NUMERIC), {{ metric_val_decimal_places }})  -- metric value
+            TRUNC(CAST(CASE WHEN {{ has_conversion_key }} THEN {{ cost_column }} / NULLIF({{ conversion_key }}, 0) ELSE {{ metric_column }} END AS DECIMAL(22, 18)), {{ metric_val_decimal_places }})  -- metric value
           , 0) = 0 -- no spend
     THEN 0 -- underperforming
   WHEN
@@ -14,9 +14,9 @@ CASE
     THEN NULL  -- not determined
   WHEN {{ is_inverse_performance }}
     THEN (2 * {{ planned_value }} -
-           TRUNC(CAST(CASE WHEN {{ has_conversion_key }} THEN {{ cost_column }} / NULLIF({{ conversion_key }}, 0) ELSE {{ metric_column }} END AS NUMERIC), {{ metric_val_decimal_places }}) -- metric value
+           TRUNC(CAST(CASE WHEN {{ has_conversion_key }} THEN {{ cost_column }} / NULLIF({{ conversion_key }}, 0) ELSE {{ metric_column }} END AS DECIMAL(22, 18)), {{ metric_val_decimal_places }}) -- metric value
          ) / NULLIF({{ planned_value }}, 0)::float
-  ELSE 
-       TRUNC(CAST(CASE WHEN {{ has_conversion_key }} THEN {{ cost_column }} / NULLIF({{ conversion_key }}, 0) ELSE {{ metric_column }} END AS NUMERIC), {{ metric_val_decimal_places }}) -- metric value
+  ELSE
+       TRUNC(CAST(CASE WHEN {{ has_conversion_key }} THEN {{ cost_column }} / NULLIF({{ conversion_key }}, 0) ELSE {{ metric_column }} END AS DECIMAL(22, 18)), {{ metric_val_decimal_places }}) -- metric value
        / NULLIF({{ planned_value }}, 0)::float
 END {{ alias }}
