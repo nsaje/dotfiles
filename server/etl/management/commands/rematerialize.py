0a34a155068b8a1b1bdb1d4b9e5561d283fc0233
Žiga Stopinšek <sigi.kajzer@gmail.com>
2018-06-03 10:32:39 +0200

    improved rematerialize

--- server/etl/management/commands/rematerialize.py
+++ server/etl/management/commands/rematerialize.py
@@ -20,13 +20,19 @@ class Command(ExceptionCommand):
         parser.add_argument('batch', type=int,
                             help='Batch number - out of {} batches'.format(NUM_OF_BATCHES))
         parser.add_argument('min_date', type=str)
+        parser.add_argument('--skip-accounts', type=str, default='', dest='skip_accounts',
+                            help='Comma separated account ids to skip')
 
     def handle(self, *args, **options):
+        skip_accounts = set(map(int, (
+            acc_id.strip() for acc_id in options.get('skip_accounts', '').split(',')
+            if acc_id.strip()
+        )))
         since = datetime.datetime.strptime(options['min_date'], '%Y-%m-%d')
         batch = options['batch']
-        accounts = Account.objects.filter(
+        accounts = set(Account.objects.filter(
             agency_id=options['agency_id']
-        ).values_list('pk', flat=True)
+        ).values_list('pk', flat=True)) - skip_accounts
         query = '''select account_id, min(date) from mv_account
         where cost_nano > 0 and account_id in ({})
         group by account_id;'''.format(', '.join(map(str, accounts)))
@@ -35,7 +41,7 @@ class Command(ExceptionCommand):
         with redshiftapi.db.get_stats_cursor() as cur:
             cur.execute(query)
             account_since = {
-                account_id: max(
+                int(account_id): max(
                     datetime.datetime.strptime(str(start_date), "%Y-%m-%d"),
                     since
                 )
