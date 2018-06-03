7f60e00bdf370cd321491de015715e99e1d4669d
Žiga Stopinšek <sigi.kajzer@gmail.com>
2018-06-03 22:34:25 +0200

    added slack notification to rematerialize command

--- server/etl/management/commands/rematerialize.py
+++ server/etl/management/commands/rematerialize.py
@@ -9,6 +9,7 @@ from core.entity.account import Account
 
 logger = logging.getLogger(__name__)
 NUM_OF_BATCHES = 4
+import utils.slack
 
 
 class Command(ExceptionCommand):
@@ -37,7 +38,6 @@ class Command(ExceptionCommand):
         where cost_nano > 0 and account_id in ({})
         group by account_id;'''.format(', '.join(map(str, accounts)))
         account_since = {}
-
         with redshiftapi.db.get_stats_cursor() as cur:
             cur.execute(query)
             account_since = {
@@ -49,9 +49,15 @@ class Command(ExceptionCommand):
             }
         if not account_since:
             return
+        n_accounts_processed, n_accounts_all = 0, len(accounts)
         for acc_id, since in account_since.items():
             if (acc_id % NUM_OF_BATCHES) != batch:
                 continue
+            utils.slack.publish(
+                "batch {}: {}/{}".format(batch, n_accounts_processed+1, n_accounts_all),
+                msg_type=utils.slack.MESSAGE_TYPE_INFO,
+                username='Rematerialize'
+            )
             print("reprocessing", acc_id, "since", since)
             try:
                 refresh_k1.refresh_k1_reports(since, acc_id,
@@ -61,3 +67,4 @@ class Command(ExceptionCommand):
             finally:
                 from django import db
                 db.connections.close_all()
+            n_accounts_processed += 1
