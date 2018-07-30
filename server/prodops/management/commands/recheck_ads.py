a9957c6f7b5b1fbfd6a28f4f6961d972dd2153db
Žiga Stopinšek <sigi.kajzer@gmail.com>
2018-07-30 13:12:21 +0200

    added option to recheck approved ads

--- server/prodops/management/commands/recheck_ads.py
+++ server/prodops/management/commands/recheck_ads.py
@@ -12,6 +12,8 @@ class Command(utils.command_helpers.ExceptionCommand):
         parser.add_argument(dest="source", nargs=1)
         parser.add_argument("--ad-groups", dest="ad_groups", default="")
         parser.add_argument("--campaigns", dest="campaigns", default="")
+        parser.add_argument("--from-approved", dest="from_approved", action="store_true",
+                            default=False, help="Revert from APPROVED to PENDING")
 
     def _print(self, msg):
         self.stdout.write("{}\n".format(msg))
@@ -37,7 +39,10 @@ class Command(utils.command_helpers.ExceptionCommand):
             return
 
         for cas in dash.models.ContentAdSource.objects.filter(
-            submission_status=dash.constants.ContentAdSubmissionStatus.REJECTED, **lookup
+            submission_status=(
+                options['from_approved'] and dash.constants.ContentAdSubmissionStatus.APPROVED
+                or dash.constants.ContentAdSubmissionStatus.REJECTED
+            ), **lookup
         ).select_related("source"):
             print("Updating ad {} on source {} to PENDING".format(cas.content_ad_id, cas.source))
             cas.submission_status = dash.constants.ContentAdSubmissionStatus.PENDING
