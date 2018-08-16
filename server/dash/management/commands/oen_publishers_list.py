e0969a9e619ad3c851554bd1044069002c56a22f
Jure Ham <jure@hamsworld.net>
2018-08-16 15:34:30 +0200

    oen publishers list fix escape and add min num of bid requests (#3119)

--- server/dash/management/commands/oen_publishers_list.py
+++ server/dash/management/commands/oen_publishers_list.py
@@ -8,6 +8,7 @@ SELECT publisher, exchange, sum(bid_reqs) AS num_bid_req
 FROM supply_stats
 WHERE date >= %s AND blacklisted = false
 GROUP BY exchange, publisher
+HAVING sum(bid_reqs) > %s
 ORDER BY num_bid_req DESC
 """
 
@@ -16,6 +17,7 @@ UNLOAD (%s)
 TO %s
 DELIMITER AS %s
 ADDQUOTES
+ESCAPE
 GZIP
 PARALLEL OFF
 CREDENTIALS AS %s
@@ -25,14 +27,17 @@ bucket = "z1-publishers"
 filename = "publishers-%s.csv.gz"
 time_format = "%Y-%m-%d_%H_%M_%S"
 
+min_bid_req = 100
+num_days = 7
+
 
 class Command(ExceptionCommand):
     def handle(self, *args, **options):
         today = dates_helper.utc_today()
-        date_from = dates_helper.days_before(today, 7)
+        date_from = dates_helper.days_before(today, num_days)
         s3 = s3helpers.S3Helper(bucket)
         with redshiftapi.db.get_stats_cursor() as cur:
-            subquery = cur.mogrify(select_query, (date_from,)).decode("utf-8")
+            subquery = cur.mogrify(select_query, (date_from, min_bid_req)).decode("utf-8")
             cur.execute(unload_query, (subquery, "s3://%s/tmp.csv" % bucket, ",", s3helpers.get_credentials_string()))
             dest_file = filename % dates_helper.utc_now().strftime(time_format)
             s3.move("tmp.csv000.gz", dest_file)
