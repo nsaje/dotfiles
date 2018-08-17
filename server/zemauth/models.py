f660d2ef545ab367ad0557945da8a0a97ecc79a7
Matjaz Suber <matjaz.suber@gmail.com>
2018-08-17 11:55:42 +0200

    Add user permission fea_can_change_campaign_type. (#3125)

--- server/zemauth/models.py
+++ server/zemauth/models.py
@@ -368,6 +368,7 @@ class User(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
             ("can_use_publisher_bid_modifiers_in_ui", "User can see and adjust publisher bid modifiers in the UI"),
             ("can_edit_advanced_custom_flags", "User can modify the advanced custom flags in admin"),
             ("can_see_amplify_review_link", "User can see link to Amplify review campaign"),
+            ("fea_can_change_campaign_type", "User can change campaign type"),
         )
 
     def get_full_name(self):
