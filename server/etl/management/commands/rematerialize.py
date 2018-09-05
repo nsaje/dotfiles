04c2252c87da244260e85b58ce95a53122375f95
Žiga Stopinšek <sigi.kajzer@gmail.com>
2018-09-05 13:29:36 +0200

    ...

--- server/etl/management/commands/rematerialize.py
+++ server/etl/management/commands/rematerialize.py
@@ -21,6 +21,7 @@ class Command(ExceptionCommand):
         parser.add_argument("agency_id", type=int)
         parser.add_argument("batch", type=int, help="Batch number - out of {} batches".format(NUM_OF_BATCHES))
         parser.add_argument("min_date", type=str)
+        parser.add_argument("batch_size", type=int)
         parser.add_argument(
             "--skip-accounts", type=str, default="", dest="skip_accounts", help="Comma separated account ids to skip"
         )
@@ -31,11 +32,12 @@ class Command(ExceptionCommand):
         )
         since = datetime.datetime.strptime(options["min_date"], "%Y-%m-%d")
         batch = options["batch"]
+        num_of_batches = options["batch_size"] or NUM_OF_BATCHES
         accounts = (
             set(Account.objects.filter(agency_id=options["agency_id"]).values_list("pk", flat=True)) - skip_accounts
         )
         query = """select account_id, min(date) from mv_account
-        where and date >= '{}' cost_nano > 0 and account_id in ({})
+        where date >= '{}' and cost_nano > 0 and account_id in ({})
         group by account_id;""".format(
             options["min_date"], ", ".join(map(str, accounts))
         )
@@ -50,10 +52,10 @@ class Command(ExceptionCommand):
             return
         n_accounts_processed, n_accounts_all = (
             0,
-            len([acc_id for acc_id in account_since.keys() if (acc_id % NUM_OF_BATCHES) == batch]),
+            len([acc_id for acc_id in account_since.keys() if (acc_id % num_of_batches) == batch]),
         )
         for acc_id, since in account_since.items():
-            if (acc_id % NUM_OF_BATCHES) != batch:
+            if (acc_id % num_of_batches) != batch:
                 continue
             utils.slack.publish(
                 "batch {}: {}/{}".format(batch, n_accounts_processed + 1, n_accounts_all),
