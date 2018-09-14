cf1d48bf0eeccb3f4928192701fdc336d5c560d2
tibz7 <26058141+tibz7@users.noreply.github.com>
2018-09-14 13:47:59 +0200

    Change campaign default type for RCS and set AMMET default location (#3169)
    https://trello.com/c/vVEWDOxZ/2936-rcs-default-campaign-type-to-native-content-distribution
    https://trello.com/c/vVEWDOxZ/2936-rcs-default-campaign-type-to-native-content-distribution

--- server/dash/features/native_server/service.py
+++ server/dash/features/native_server/service.py
@@ -4,13 +4,19 @@ from django.contrib.auth import models as authmodels
 
 import dash.constants
 
-
 AGENCY_RCS = 220
 AGENCY_MEDIAMOND = 196
+AGENCY_AMMET = 289
 
-AD_GROUP_SETTINGS_CREATE_HACKS_PER_AGENCY = {AGENCY_RCS: {"target_regions": [], "exclusion_target_regions": []}}
+AD_GROUP_SETTINGS_CREATE_HACKS_PER_AGENCY = {
+    AGENCY_RCS: {"target_regions": [], "exclusion_target_regions": []},
+    AGENCY_AMMET: {"target_regions": ["AU"], "exclusion_target_regions": []},
+    AGENCY_MEDIAMOND: {"tracking_code": "utm_source=Zemanta&utm_medium=referral"},
+}
 AD_GROUP_SETTINGS_HACKS_PER_AGENCY = {AGENCY_RCS: {"delivery_type": dash.constants.AdGroupDeliveryType.ACCELERATED}}
+
 CAMPAIGN_SETTINGS_HACKS_PER_AGENCY = {AGENCY_RCS: {"language": "it", "autopilot": True}}
+FIXED_CAMPAIGN_TYPE_PER_AGENCY = {AGENCY_RCS: {"type": dash.constants.CampaignType.CONTENT}}
 
 
 def _update_ad_group_sources_cpc(request, ad_group, cpc_cc):
@@ -23,9 +29,9 @@ def apply_ad_group_create_hacks(request, ad_group):
         ad_group.settings.update(
             request, **AD_GROUP_SETTINGS_CREATE_HACKS_PER_AGENCY[ad_group.campaign.account.agency_id]
         )
-        _update_ad_group_sources_cpc(request, ad_group, _get_cpc_goal_value(ad_group.campaign).value)
-    if ad_group.campaign.account.agency_id == AGENCY_MEDIAMOND:
-        ad_group.settings.update(request, tracking_code="utm_source=Zemanta&utm_medium=referral")
+    if ad_group.campaign.account.agency_id == AGENCY_RCS:
+        goal_cpc_value = _get_cpc_goal_value(ad_group.campaign)
+        _update_ad_group_sources_cpc(request, ad_group, goal_cpc_value.value)
 
 
 def transform_ad_group_settings(ad_group, form_data):
@@ -36,6 +42,8 @@ def transform_ad_group_settings(ad_group, form_data):
 
 def _get_cpc_goal_value(campaign):
     cpc_goal = campaign.campaigngoal_set.filter(type=dash.constants.CampaignGoalKPI.CPC).first()
+    if not cpc_goal:
+        raise Exception("No CPC goal on the RCS campaign.")
     return cpc_goal.get_current_value()
 
 
@@ -79,6 +87,8 @@ def apply_create_user_hacks(user, account):
 def apply_campaign_create_hacks(request, campaign):
     if campaign.account.agency_id in CAMPAIGN_SETTINGS_HACKS_PER_AGENCY:
         campaign.settings.update(request, **CAMPAIGN_SETTINGS_HACKS_PER_AGENCY[campaign.account.agency_id])
+    if campaign.account.agency_id in FIXED_CAMPAIGN_TYPE_PER_AGENCY:
+        campaign.update_type(**FIXED_CAMPAIGN_TYPE_PER_AGENCY[campaign.account.agency_id])
 
 
 def apply_campaign_change_hacks(request, campaign, goal_changes):
