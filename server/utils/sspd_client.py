77250f0da87546ec822ed75293ec1e69e90f3035
Luka Šilovinac <luka.silovinac@gmail.com>
2018-10-16 10:55:01 +0200

    Revert sspd client (#3222)
    This reverts commit 21a198c099d5dcc620e6df183a663c62e398f7b5.

---
+++ server/utils/sspd_client.py
@@ -0,0 +1,66 @@
+import datetime
+
+import jwt
+import requests
+
+from django.conf import settings
+
+from dash import constants
+from dash import models
+from utils import dates_helper
+
+APPROVAL_STATUS_URL = "/service/approvalstatus"
+CONTENT_AD_STATUS_URL = "/service/contentadstatus"
+
+
+def get_approval_status(content_ad_source_ids):
+    url = settings.SSPD_BASE_URL + APPROVAL_STATUS_URL
+    approval_statuses = _make_request(
+        "get", url, params={"contentAdSourceIds": ",".join(str(id_) for id_ in content_ad_source_ids)}
+    )
+    return _map_approval_statuses(approval_statuses)
+
+
+def _map_approval_statuses(approval_statuses):
+    return {int(id_): getattr(constants.ContentAdSspdStatus, status) for id_, status in approval_statuses.items()}
+
+
+def get_content_ad_status(content_ad_ids):
+    url = settings.SSPD_BASE_URL + CONTENT_AD_STATUS_URL
+    content_ad_statuses = _make_request(
+        "get", url, params={"contentAdIds": ",".join(str(id_) for id_ in content_ad_ids)}
+    )
+    return _map_content_ad_statuses(content_ad_statuses)
+
+
+def _map_content_ad_statuses(content_ad_statuses):
+    source_map = {source.id: source for source in models.Source.objects.all()}
+    return {
+        int(id_): {
+            source_map[source_status["sourceId"]].id: {
+                "reason": source_status["reason"],
+                "status": getattr(constants.ContentAdSspdStatus, source_status["status"]),
+            }
+            for source_status in per_source_statuses
+        }
+        for id_, per_source_statuses in content_ad_statuses.items()
+    }
+
+
+def _make_request(method, url, data=None, params=None, headers=None):
+    if not headers:
+        headers = {}
+    _augment_with_auth_headers(headers)
+    response = requests.request(
+        method, url, data=data if data else {}, params=params if params else {}, headers=headers
+    )
+    if not response.ok:
+        raise Exception("Failed sspd request")
+    return response.json()
+
+
+def _augment_with_auth_headers(headers):
+    payload = {"iss": "Z1", "exp": dates_helper.utc_now() + datetime.timedelta(seconds=60)}
+    token = jwt.encode(payload, settings.SSPD_AUTH_SECRET, algorithm="HS256")
+    auth_header = "Bearer " + token.decode("utf-8")
+    headers.update({"Authorization": auth_header})
