c20e60dc39271dc5c3389f80f935d54132e070a5
Anej Placer <aplacer@outbrain.com>
2018-11-15 16:03:51 +0100

    Aglio parameters to render null as null

--- server/restapi/docs/build-docker.sh
+++ server/restapi/docs/build-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR="$(dirname $(readlink -f $0))"
 
-docker run -v $DIR:/tmp -t -e "DRAFTER_EXAMPLES=true aglio -i apiary.apib" -e "NOCACHE=1" zemanta/z1-aglio -i "/tmp/api_blueprint.md" --theme-style "/tmp/theme/style.less" --theme-variables "/tmp/theme/variables.less" --theme-template "/tmp/theme/template.jade" -o "/tmp/$1"
+docker run -v $DIR:/tmp -t -e "DRAFTER_EXAMPLES=true" -e "NOCACHE=1" zemanta/z1-aglio -i "/tmp/api_blueprint.md" --theme-style "/tmp/theme/style.less" --theme-variables "/tmp/theme/variables.less" --theme-template "/tmp/theme/template.jade" -o "/tmp/$1"
--- server/restapi/docs/build.sh
+++ server/restapi/docs/build.sh
@@ -8,4 +8,4 @@ trap cleanup SIGINT
 DIR="$(dirname $(readlink -f $0))"
 
 cleanup
-docker run --name z1-rest-aglio -p 3000:3000 -v $DIR:/tmp -t -e "NOCACHE=1" zemanta/z1-aglio -i "/tmp/api_blueprint.md" --theme-style "/tmp/theme/style.less" --theme-variables "/tmp/theme/variables.less" --theme-template "/tmp/theme/template.jade" -s -h 0.0.0.0 -p 3000
+docker run --name z1-rest-aglio -p 3000:3000 -v $DIR:/tmp -t -e "DRAFTER_EXAMPLES=true" -e "NOCACHE=1" zemanta/z1-aglio -i "/tmp/api_blueprint.md" --theme-style "/tmp/theme/style.less" --theme-variables "/tmp/theme/variables.less" --theme-template "/tmp/theme/template.jade" -s -h 0.0.0.0 -p 3000
