dc4e6724b6b6ffc54430f4c0b1d4cd8ca25a61c1
Tilen Kusterle <tkusterle@outbrain.com>
2018-11-16 11:22:26 +0100

    Fix broken check dcron jobs command

--- server/dcron/management/commands/check_dcron_jobs.py
+++ server/dcron/management/commands/check_dcron_jobs.py
@@ -6,5 +6,5 @@ from dcron.alerts import handle_alerts
 class Command(BaseCommand):
     help = "Check execution of cron jobs defined in DCronJob model."
 
-    def _handle(self, *args, **options):
+    def handle(self, *args, **options):
         handle_alerts()
