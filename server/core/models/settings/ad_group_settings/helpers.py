c8589f1127c31166b4e4588e3fab8a46016bd578
Luka Šilovinac <luka.silovinac@gmail.com>
2018-11-30 19:17:13 +0100

    Fix updating cpc when budget autopilot is enabled (#3360)

--- server/core/models/settings/ad_group_settings/helpers.py
+++ server/core/models/settings/ad_group_settings/helpers.py
@@ -26,7 +26,7 @@ def get_adjusted_ad_group_sources_bids(ad_group, ad_group_settings):
         ags_settings = ad_group_source.get_current_settings()
         proposed_bid = ags_settings.cpm if ad_group.bidding_type == constants.BiddingType.CPM else ags_settings.cpc_cc
         adjusted_bid = _get_adjusted_ad_group_source_bid(
-            proposed_bid, ad_group_source, ad_group_settings, ad_group.campaign.settings
+            proposed_bid, ad_group_source, ad_group_settings, ad_group.campaign, ad_group.campaign.settings
         )
         adjusted_bid = _adjust_ad_group_source_bid_to_max(ad_group, ad_group_settings, adjusted_bid)
         adjusted_bids[ad_group_source] = adjusted_bid
@@ -45,7 +45,7 @@ def set_ad_group_sources_cpcs(ad_group_sources_cpcs, ad_group, ad_group_settings
     rules_per_source = cpc_constraints.get_rules_per_source(ad_group)
     for ad_group_source, proposed_cpc in list(ad_group_sources_cpcs.items()):
         adjusted_cpc = _get_adjusted_ad_group_source_bid(
-            proposed_cpc, ad_group_source, ad_group_settings, ad_group.campaign.settings
+            proposed_cpc, ad_group_source, ad_group_settings, ad_group.campaign, ad_group.campaign.settings
         )
         if adjusted_cpc:
             adjusted_cpc = cpc_constraints.adjust_cpc(adjusted_cpc, rules=rules_per_source[ad_group_source.source])
@@ -60,7 +60,7 @@ def set_ad_group_sources_cpcs(ad_group_sources_cpcs, ad_group, ad_group_settings
 def set_ad_group_sources_cpms(ad_group_sources_cpms, ad_group, ad_group_settings, skip_validation=False):
     for ad_group_source, proposed_cpm in list(ad_group_sources_cpms.items()):
         adjusted_cpm = _get_adjusted_ad_group_source_bid(
-            proposed_cpm, ad_group_source, ad_group_settings, ad_group.campaign.settings
+            proposed_cpm, ad_group_source, ad_group_settings, ad_group.campaign, ad_group.campaign.settings
         )
         ad_group_source_settings = ad_group_source.get_current_settings()
         if ad_group_source_settings.cpm == adjusted_cpm:
@@ -68,11 +68,18 @@ def set_ad_group_sources_cpms(ad_group_sources_cpms, ad_group, ad_group_settings
         ad_group_source.settings.update(cpm=adjusted_cpm, k1_sync=False, skip_validation=skip_validation)
 
 
-def _get_adjusted_ad_group_source_bid(proposed_bid, ad_group_source, ad_group_settings, campaign_settings):
+def _get_adjusted_ad_group_source_bid(proposed_bid, ad_group_source, ad_group_settings, campaign, campaign_settings):
+    try:
+        campaign_goal = campaign.campaigngoal_set.get(primary=True)
+    except Exception:
+        campaign_goal = None
     if (
         ad_group_settings.b1_sources_group_enabled
         and ad_group_source.source.source_type.type == constants.SourceType.B1
-        and ad_group_settings.autopilot_state != constants.AdGroupSettingsAutopilotState.ACTIVE_CPC_BUDGET
+        and (
+            ad_group_settings.autopilot_state != constants.AdGroupSettingsAutopilotState.ACTIVE_CPC_BUDGET
+            or (campaign_goal and campaign_goal.type != constants.CampaignGoalKPI.CPA)
+        )
         and not campaign_settings.autopilot
     ):
         proposed_bid = (
