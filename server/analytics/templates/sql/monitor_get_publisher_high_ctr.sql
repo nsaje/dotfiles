9e43bdc5a2d1573b61720ccb443ff3a6819543b6
rblush <blazres@gmail.com>
2019-02-20 11:38:28 +0100

    fixed high ctr query (#3507)
    * fixed high ctr query
    
    * changed ctr threshold for publisher ctr monitor

--- server/analytics/templates/sql/monitor_get_publisher_high_ctr.sql
+++ server/analytics/templates/sql/monitor_get_publisher_high_ctr.sql
@@ -1,13 +1,11 @@
 SELECT *
-FROM (SELECT publisher,
-             date,
-             sum(impressions) as impressions,
-             sum(clicks) as clicks,
-             ((sum(clicks) :: decimal / nullif(sum(impressions) :: decimal, 0)) * 100) :: decimal as ctr
-      FROM stats
-      WHERE date = '{{ date }}'
-      AND clicks > {{ max_clicks }}
-      AND impressions > {{ max_impressions }}
-      group by publisher, date
-     ) ctr_query
-WHERE ctr >= {{ ctr_threshold }};
+FROM
+  (SELECT publisher, date, sum(impressions) AS impressions,
+                           sum(clicks) AS clicks,
+                           ((sum(clicks) :: decimal / nullif(sum(impressions) :: decimal, 0)) * 100) :: decimal AS ctr
+   FROM mv_master_pubs
+   GROUP BY publisher, date ) ctr_query
+WHERE date = '{{ date }}'
+  AND clicks > {{ max_clicks }}
+  AND impressions > {{ max_impressions }}
+  AND ctr >= {{ ctr_threshold }};
