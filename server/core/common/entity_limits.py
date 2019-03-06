915b36c04e7ef0a3487e987dac918e7638f76823
Matjaz Suber <msuber@outbrain.com>
2019-03-06 10:06:29 +0100

    Increase entity limit for PublisherGroup.

--- server/core/common/entity_limits.py
+++ server/core/common/entity_limits.py
@@ -26,7 +26,7 @@ ACCOUNT_EXCEPTIONS = defaultdict(
             305: 100000,  # OEN
             950: 20000,  # MatchesFashion
         },
-        "PublisherGroup": {305: 200000},  # OEN
+        "PublisherGroup": {305: 2000000},  # OEN
         "BudgetLineItem": {305: 1000},  # OEN
     },
 )
