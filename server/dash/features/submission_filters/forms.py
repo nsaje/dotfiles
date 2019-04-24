be227dc51cd0b2dc26870d65794f4b6e0017625f
Tilen Kusterle <tkusterle@outbrain.com>
2019-04-24 14:44:10 +0200

    Fix SubmissionFilter cleaning problem

--- server/dash/features/submission_filters/forms.py
+++ server/dash/features/submission_filters/forms.py
@@ -7,7 +7,7 @@ from . import models
 class SubmissionFilterForm(forms.ModelForm):
     def clean(self):
         obj = models.SubmissionFilter(**self.cleaned_data)
-        check_existing = False if set(self.changed_data.keys()) == {"description"} else True
+        check_existing = False if set(self.changed_data) == {"description"} else True
 
         possible_keys = [obj.agency_id, obj.account_id, obj.campaign_id, obj.ad_group_id, obj.content_ad_id]
         if len([key for key in possible_keys if key is not None]) != 1:
