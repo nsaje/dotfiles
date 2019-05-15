4044d04c81d381071a3d5f66d382772cf89e8e50
Anej Placer <aplacer@outbrain.com>
2019-05-15 10:49:48 +0200

    Revert "Revert "Revert "ETL: joint query MVJointMaster dynamic columns (#3621)"" (#3712)"
    This reverts commit 95a5cc8dcd7d5f7abeecb36f3c721ba3a2ae1251.

--- server/redshiftapi/templates/part_total_conversion_value.sql
+++ server/redshiftapi/templates/part_total_conversion_value.sql
@@ -1,13 +1,1 @@
-SUM(
-  CASE
-    WHEN {{ p }}slug='{{ slug }}'
-    AND {{ p }}account_id={{ account_id }}
-    AND {{ p }}conversion_window<={{ window }}
-    AND (
-        {{ type }}=1 AND ({{ p }}type=1 OR {{ p }}type IS NULL)
-        OR {{ type }}=2 AND {{ p }}type=2
-    )
-    THEN conversion_value_nano
-    ELSE 0
-  END
-)/1000000000.0 {{ alias }}
+SUM(CASE WHEN {{ p }}slug='{{ slug }}' AND {{ p }}account_id={{ account_id }} AND {{ p }}conversion_window<={{ window }} THEN conversion_value_nano ELSE 0 END)/1000000000.0 {{ alias }}
