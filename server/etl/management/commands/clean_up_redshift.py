b7eaaae942dc798c9220c0d52fdffd4c4c0b69c3
Nejc Saje <nejc@saje.info>
2019-05-29 11:31:15 +0200

    clean up redshift stats periodically (#3728)
    * clean up redshift stats
    
    * adjust crontab

--- server/etl/management/commands/clean_up_redshift.py
+++ server/etl/management/commands/clean_up_redshift.py
@@ -12,21 +12,23 @@ from utils.command_helpers import ExceptionCommand
 logger = logging.getLogger(__name__)
 
 
-SUPPLY_STATS_KEEP_DAYS = 31
-SUPPLY_STATS_TABLE = "supply_stats"
+CONFIG = [{"table_name": "supply_stats", "keep_days": 31}, {"table_name": "stats", "keep_days": 93}]
 
 
 class Command(ExceptionCommand):
-    @influx.timer("etl.clean_up_supply_stats")
+    @influx.timer("etl.clean_up_redshift")
     def handle(self, *args, **options):
-        self._delete_old_data(SUPPLY_STATS_TABLE)
-        maintenance.vacuum(SUPPLY_STATS_TABLE, delete_only=True)
-        maintenance.analyze(SUPPLY_STATS_TABLE)
+        for config in CONFIG:
+            table_name = config["table_name"]
+            keep_days = config["keep_days"]
+            self._delete_old_data(table_name, keep_days)
+            maintenance.vacuum(table_name, delete_only=True)
+            maintenance.analyze(table_name)
 
     @staticmethod
-    def _delete_old_data(table):
+    def _delete_old_data(table, keep_days):
         date_from = datetime.date(1970, 1, 1)
-        date_to = utils.dates_helper.days_before(utils.dates_helper.local_today(), SUPPLY_STATS_KEEP_DAYS)
+        date_to = utils.dates_helper.days_before(utils.dates_helper.local_today(), keep_days)
         with db.get_write_stats_cursor() as c:
             logger.info('Deleting old data from table "%s"', table)
             sql, params = redshift.prepare_date_range_delete_query(table, date_from, date_to, None)
