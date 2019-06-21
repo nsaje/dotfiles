4e36df5626cb581e3b33b04c307c6eafa4908d93
Luka Šilovinac <luka.silovinac@zemanta.com>
2019-06-21 09:39:49 +0200

    Add pagination to ga_accounts endpoint (#3732)
    * Add pagination to ga_accounts endpoint
    
    * Paginate by account id
    
    * Fix tests
    
    * Don't sort in tests so that sorting is also tested

--- server/k1api/views/ga_accounts.py
+++ server/k1api/views/ga_accounts.py
@@ -18,20 +18,26 @@ class GAAccountsView(K1APIView):
     @db_router.use_read_replica()
     def get(self, request):
         date_since = request.GET.get("date_since")
+        limit = int(request.GET.get("limit", 100))
+        marker = request.GET.get("marker")
 
         cache = caches["dash_db_cache"]
-        cache_key = cache_helper.get_cache_key("ga-accounts", date_since)
+        cache_key = cache_helper.get_cache_key("ga-accounts", date_since, limit, marker)
         response = cache.get(cache_key, None)
         if response is not None:
             return self.response_ok(response)
 
-        all_active_campaign_ids = set(
-            dash.models.Campaign.objects.all().exclude_archived().values_list("id", flat=True)
-        )
+        accounts = dash.models.Account.objects.all().exclude_archived()
+        if marker:
+            accounts = accounts.filter(pk__gt=int(marker))
+        accounts = accounts[:limit]
+        campaigns = dash.models.Campaign.objects.filter(account__in=accounts).exclude_archived()
+
+        active_campaign_ids = set(campaigns.values_list("id", flat=True))
         if "campaigns" in request.GET:
-            all_active_campaign_ids = set(request.GET.get("campaigns", "").split(","))
+            active_campaign_ids = set(request.GET.get("campaigns", "").split(","))
         all_current_settings = (
-            dash.models.CampaignSettings.objects.filter(campaign_id__in=all_active_campaign_ids)
+            dash.models.CampaignSettings.objects.filter(campaign_id__in=active_campaign_ids)
             .group_current_settings()
             .select_related("campaign")
         )
@@ -41,7 +47,7 @@ class GAAccountsView(K1APIView):
         if date_since:
             valid_previous_settings = (
                 dash.models.CampaignSettings.objects.filter(
-                    campaign_id__in=all_active_campaign_ids,
+                    campaign_id__in=active_campaign_ids,
                     created_dt__lte=datetime.datetime.strptime(date_since, "%Y-%m-%d").date(),
                 )
                 .order_by("campaign_id", "-created_dt")
@@ -50,17 +56,17 @@ class GAAccountsView(K1APIView):
             )
             for previous_settings in valid_previous_settings:
                 self._extract_ga_settings(ga_accounts, previous_settings)
-            # all_intermediate_settings = (
-            #     dash.models.CampaignSettings.objects.filter(
-            #         campaign_id__in=all_active_campaign_ids,
-            #         created_dt__gte=datetime.datetime.strptime(date_since, "%Y-%m-%d").date(),
-            #     )
-            #     .exclude(pk__in=set(s.pk for s in all_current_settings) | set(s.pk for s in valid_previous_settings))
-            #     .exclude(ga_property_id__in=set(ga_property_id for _, _, ga_property_id in ga_accounts))
-            #     .select_related("campaign")
-            # )
-            # for previous_settings in all_intermediate_settings:
-            #     self._extract_ga_settings(ga_accounts, previous_settings)
+            all_intermediate_settings = (
+                dash.models.CampaignSettings.objects.filter(
+                    campaign_id__in=active_campaign_ids,
+                    created_dt__gte=datetime.datetime.strptime(date_since, "%Y-%m-%d").date(),
+                )
+                .exclude(pk__in=set(s.pk for s in all_current_settings) | set(s.pk for s in valid_previous_settings))
+                .exclude(ga_property_id__in=set(ga_property_id for _, _, ga_property_id in ga_accounts))
+                .select_related("campaign")
+            )
+            for previous_settings in all_intermediate_settings:
+                self._extract_ga_settings(ga_accounts, previous_settings)
         ga_accounts_dicts = [
             {"account_id": account_id, "ga_account_id": ga_account_id, "ga_web_property_id": ga_web_property_id}
             for account_id, ga_account_id, ga_web_property_id in sorted(ga_accounts)
