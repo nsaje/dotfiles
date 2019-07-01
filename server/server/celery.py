d82d056c38b855bf45d78ae6d9243e8c947c07e2
Nejc Saje <nejc@saje.info>
2019-07-01 16:44:02 +0200

    tmp disable celery outbrainwsgi

--- server/server/celery.py
+++ server/server/celery.py
@@ -1,6 +1,7 @@
 # isort:skip_file
 import os
-import swinfra.celery
+
+# import swinfra.celery
 
 from celery import Celery  # noqa
 
@@ -16,4 +17,4 @@ app = Celery("eins")
 app.config_from_object("django.conf:settings")
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-swinfra.celery.outbrain_celery_setup()
+# swinfra.celery.outbrain_celery_setup()
