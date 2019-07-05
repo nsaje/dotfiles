c0c94fd6ed9647b2030f232e4333d53741f2bf2b
Luka Šilovinac <luka.silovinac@zemanta.com>
2019-07-05 10:41:58 +0200

    Re revert ga accounts pagination and change response structure (#3795)
    * Revert "Revert "Add pagination to ga_accounts endpoint (#3732)""
    
    This reverts commit b62dd4e16eb6dcb30b43f6aa2d8a9c0e73027472.
    
    * Return a list instead of dict

--- server/k1api/views/ga_accounts_test.py
+++ server/k1api/views/ga_accounts_test.py
@@ -21,7 +21,7 @@ logger.setLevel(logging.INFO)
 class GAAccountsTest(K1APIBaseTest):
     def test_get_ga_accounts(self):
         dash.features.ga.GALinkedAccounts.objects.create(
-            customer_ga_account_id="123", zem_ga_account_email="a1@gapps.com", has_read_and_analyze=True
+            customer_ga_account_id="124", zem_ga_account_email="a1@gapps.com", has_read_and_analyze=True
         )
 
         response = self.client.get(reverse("k1api.ga_accounts"))
@@ -30,17 +30,21 @@ class GAAccountsTest(K1APIBaseTest):
         self.assert_response_ok(response, data)
         data = data["response"]
 
-        self.assertEqual(len(data["ga_accounts"]), 2)
-        self.assertEqual(data["ga_accounts"][0]["account_id"], 1)
-        self.assertEqual(data["ga_accounts"][0]["ga_account_id"], "123")
-        self.assertEqual(data["ga_accounts"][0]["ga_web_property_id"], "UA-123-2")
-        self.assertEqual(data["ga_accounts"][1]["account_id"], 2)
-        self.assertEqual(data["ga_accounts"][1]["ga_account_id"], "123")
-        self.assertEqual(data["ga_accounts"][1]["ga_web_property_id"], "UA-123-3")
-
-        self.assertEqual(data["service_email_mapping"], {"123": "a1@gapps.com"})
+        self.assertEqual(len(data), 2)
+        self.assertEqual(data[0]["account_id"], 1)
+        self.assertEqual(data[0]["ga_account_id"], "123")
+        self.assertEqual(data[0]["ga_web_property_id"], "UA-123-2")
+        self.assertEqual(data[0]["service_email"], None)
+        self.assertEqual(data[1]["account_id"], 2)
+        self.assertEqual(data[1]["ga_account_id"], "124")
+        self.assertEqual(data[1]["ga_web_property_id"], "UA-124-3")
+        self.assertEqual(data[1]["service_email"], "a1@gapps.com")
 
     def test_get_ga_accounts_since_ever(self):
+        dash.features.ga.GALinkedAccounts.objects.create(
+            customer_ga_account_id="124", zem_ga_account_email="a1@gapps.com", has_read_and_analyze=True
+        )
+
         campaign_settings = dash.models.Campaign.objects.get(pk=1).get_current_settings().copy_settings()
         campaign_settings.ga_property_id = "UA-123-4"
         campaign_settings.save(None)
@@ -53,14 +57,30 @@ class GAAccountsTest(K1APIBaseTest):
         self.assert_response_ok(response, data)
         data = data["response"]
 
-        self.assertEqual(len(data["ga_accounts"]), 4)
+        self.assertEqual(len(data), 5)
         self.assertEqual(
-            list(sorted(data["ga_accounts"], key=lambda x: x["ga_web_property_id"])),
+            data,
             [
-                {"ga_web_property_id": "UA-123-1", "account_id": 2, "ga_account_id": "123"},
-                {"ga_web_property_id": "UA-123-2", "account_id": 1, "ga_account_id": "123"},
-                {"ga_web_property_id": "UA-123-3", "account_id": 2, "ga_account_id": "123"},
-                {"ga_web_property_id": "UA-123-4", "account_id": 1, "ga_account_id": "123"},
+                {"ga_web_property_id": "UA-123-2", "account_id": 1, "ga_account_id": "123", "service_email": None},
+                {"ga_web_property_id": "UA-123-4", "account_id": 1, "ga_account_id": "123", "service_email": None},
+                {
+                    "ga_web_property_id": "UA-124-0",
+                    "account_id": 2,
+                    "ga_account_id": "124",
+                    "service_email": "a1@gapps.com",
+                },
+                {
+                    "ga_web_property_id": "UA-124-1",
+                    "account_id": 2,
+                    "ga_account_id": "124",
+                    "service_email": "a1@gapps.com",
+                },
+                {
+                    "ga_web_property_id": "UA-124-3",
+                    "account_id": 2,
+                    "ga_account_id": "124",
+                    "service_email": "a1@gapps.com",
+                },
             ],
         )
 
@@ -76,16 +96,19 @@ class GAAccountsTest(K1APIBaseTest):
         self.assert_response_ok(response, data)
         data = data["response"]
 
-        self.assertEqual(len(data["ga_accounts"]), 3)
-        self.assertEqual(data["ga_accounts"][0]["account_id"], 1)
-        self.assertEqual(data["ga_accounts"][0]["ga_account_id"], "123")
-        self.assertEqual(data["ga_accounts"][0]["ga_web_property_id"], "UA-123-2")
-        self.assertEqual(data["ga_accounts"][1]["account_id"], 1)
-        self.assertEqual(data["ga_accounts"][1]["ga_account_id"], "123")
-        self.assertEqual(data["ga_accounts"][1]["ga_web_property_id"], "UA-123-4")
-        self.assertEqual(data["ga_accounts"][2]["account_id"], 2)
-        self.assertEqual(data["ga_accounts"][2]["ga_account_id"], "123")
-        self.assertEqual(data["ga_accounts"][2]["ga_web_property_id"], "UA-123-3")
+        self.assertEqual(len(data), 3)
+        self.assertEqual(data[0]["account_id"], 1)
+        self.assertEqual(data[0]["ga_account_id"], "123")
+        self.assertEqual(data[0]["ga_web_property_id"], "UA-123-2")
+        self.assertEqual(data[0]["service_email"], None)
+        self.assertEqual(data[1]["account_id"], 1)
+        self.assertEqual(data[1]["ga_account_id"], "123")
+        self.assertEqual(data[1]["ga_web_property_id"], "UA-123-4")
+        self.assertEqual(data[1]["service_email"], None)
+        self.assertEqual(data[2]["account_id"], 2)
+        self.assertEqual(data[2]["ga_account_id"], "124")
+        self.assertEqual(data[2]["ga_web_property_id"], "UA-124-3")
+        self.assertEqual(data[2]["service_email"], None)
 
     def test_get_ga_accounts_since_yesterday_with_additional_campaigns(self):
         self.campaign.settings.update(None, ga_property_id="UA-123-4")
@@ -98,7 +121,8 @@ class GAAccountsTest(K1APIBaseTest):
         self.assert_response_ok(response, data)
         data = data["response"]
 
-        self.assertEqual(len(data["ga_accounts"]), 1)
-        self.assertEqual(data["ga_accounts"][0]["account_id"], 1)
-        self.assertEqual(data["ga_accounts"][0]["ga_account_id"], "123")
-        self.assertEqual(data["ga_accounts"][0]["ga_web_property_id"], "UA-123-4")
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0]["account_id"], 1)
+        self.assertEqual(data[0]["ga_account_id"], "123")
+        self.assertEqual(data[0]["ga_web_property_id"], "UA-123-4")
+        self.assertEqual(data[0]["service_email"], None)
