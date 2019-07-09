c010ab0bb11e1dec760910408dabb453e23fa163
Katrca <katja.golobic@gmail.com>
2019-07-09 09:58:09 +0200

    Remove dump files after local database reset (#3811)

--- scripts/reset_local_database.sh
+++ scripts/reset_local_database.sh
@@ -6,3 +6,4 @@ LATEST_DEMO_DUMP=$(aws s3 cp s3://z1-demo/latest.txt -)
 DUMP_URL=$(aws s3 presign s3://z1-demo/$LATEST_DEMO_DUMP/dump.tar)
 docker-compose run --rm -e DUMP_URL="$DUMP_URL" server /prepare-demo.sh
 docker-compose run --rm server /create-dev-user.sh
+rm server/dump*
