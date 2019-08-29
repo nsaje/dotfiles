20219dfc0f470c6276284270c4bf91b8cad9dbf6
tfischer <26058141+tibz7@users.noreply.github.com>
2019-08-29 14:36:34 +0200

    Validate campaign budget while moving account between agencies. (#3927)
    https://trello.com/c/PBT0BDEG/4487-agency-switching-validation-prevent-if-any-budgets-exist-on-any-campaigns

--- server/core/models/account/validation.py
+++ server/core/models/account/validation.py
@@ -1,3 +1,5 @@
+import core.features.bcm
+
 from . import exceptions
 
 OUTBRAIN_SALESFORCE_SERVICE_USER = "outbrain-salesforce@service.zemanta.com"
@@ -6,6 +8,7 @@ OUTBRAIN_SALESFORCE_SERVICE_USER = "outbrain-salesforce@service.zemanta.com"
 class AccountValidatorMixin:
     def validate(self, changes, request=None):
         self._validate_externally_managed_fields(request, changes)
+        self._validate_agency(request, changes)
 
     def _validate_externally_managed_fields(self, request, changes):
         if (
@@ -20,3 +23,11 @@ class AccountValidatorMixin:
                         ", ".join(externally_managed_fields)
                     )
                 )
+
+    def _validate_agency(self, request, changes):
+        if changes.get("agency"):
+            if self.is_agency() and self.agency != changes["agency"]:
+                if core.features.bcm.BudgetLineItem.objects.filter(campaign__account=self):
+                    raise exceptions.EditingAccountNotAllowed(
+                        "Cannot switch Agency if Account has campaigns with budgets."
+                    )
--- server/core/models/account/validation_test.py
+++ server/core/models/account/validation_test.py
@@ -1,6 +1,12 @@
-from django.test import TestCase
+import datetime
+from decimal import Decimal
 
+from django.test import TestCase
+from mock import patch
+
+import core.features.bcm
 import core.models
+import dash.constants
 from utils.magic_mixer import magic_mixer
 
 from . import exceptions
@@ -10,6 +16,7 @@ class ValidationTestCase(TestCase):
     def setUp(self):
         self.account = magic_mixer.blend(core.models.Account, name="account 1", id=1)
         self.agency = magic_mixer.blend(core.models.Agency, name="agency 1", id=1)
+        self.agency2 = magic_mixer.blend(core.models.Agency, name="agency 2", id=2)
         self.source1 = magic_mixer.blend(core.models.Source, name="source1")
         self.source2 = magic_mixer.blend(core.models.Source, name="source2")
         self.request = magic_mixer.blend_request_user()
@@ -84,3 +91,32 @@ class ValidationTestCase(TestCase):
         self.account.update(self.request, allowed_sources=[], entity_tags=[])
         self.assertEqual(self.account.allowed_sources.count(), 0)
         self.assertEqual(self.account.entity_tags.count(), 0)
+
+    @patch("utils.dates_helper.local_today", return_value=datetime.date(2016, 12, 3))
+    def test_validate_agency(self, mock_today):
+
+        self.account.agency = self.agency
+        self.account.save(None)
+        self.assertEqual(self.account.agency, self.agency)
+
+        campaign = magic_mixer.blend(core.models.Campaign, name="Camp1", account=self.account)
+        credit = magic_mixer.blend(
+            core.features.bcm.CreditLineItem,
+            account=self.account,
+            start_date=datetime.date(2016, 12, 1),
+            end_date=datetime.date(2016, 12, 30),
+            status=dash.constants.CreditLineItemStatus.SIGNED,
+            amount=500,
+            license_fee=Decimal("0.10"),
+        )
+
+        core.features.bcm.BudgetLineItem.objects.create(
+            None, campaign, credit, mock_today.return_value, datetime.date(2016, 12, 30), 100
+        )
+
+        with self.assertRaisesMessage(
+            exceptions.EditingAccountNotAllowed, "Cannot switch Agency if Account has campaigns with budgets."
+        ):
+            self.account.update(None, agency=self.agency2)
+
+        self.assertEqual(self.account.agency, self.agency)
