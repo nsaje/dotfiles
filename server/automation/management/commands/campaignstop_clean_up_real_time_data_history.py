da6bd8dc5caab540caf39babbb50613613ac5a23
Anej <anej.placer@gmail.com>
2019-09-10 16:41:55 +0200

    Clean up realtime campaignstop data (#3941)
    * Clean up realtime campaignstop data

---
+++ server/automation/management/commands/campaignstop_clean_up_real_time_data_history.py
@@ -0,0 +1,34 @@
+import logging
+
+from django.db import connection
+from django.db import transaction
+
+import automation.campaignstop
+import utils.dates_helper
+from utils import metrics_compat
+from utils.command_helpers import Z1Command
+
+logger = logging.getLogger(__name__)
+
+
+REALTIME_DATA_KEEP_DAYS = 7
+
+
+class Command(Z1Command):
+    @metrics_compat.timer("campaignstop.job_run", job="clean_up")
+    def handle(self, *args, **options):
+        logger.info("Start: deleting old realtime campaign stop data")
+        self._delete_old_data()
+        logger.info("Finish: old campaign stop realtime data deleted and tables vacuumed/analyzed")
+
+    @staticmethod
+    @transaction.atomic
+    def _delete_old_data():
+        date_to = utils.dates_helper.days_before(utils.dates_helper.local_today(), REALTIME_DATA_KEEP_DAYS)
+
+        automation.campaignstop.RealTimeCampaignDataHistory.objects.filter(date__lt=date_to).delete()
+        automation.campaignstop.RealTimeDataHistory.objects.filter(date__lt=date_to).delete()
+
+        with connection.cursor() as cursor:
+            cursor.execute("VACUUM ANALYZE automation_realtimecampaigndatahistory;")
+            cursor.execute("VACUUM ANALYZE automation_realtimedatahistory;")
