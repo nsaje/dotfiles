8abfed01d7826154c25b4ae97948668174f95388
Nejc Saje <nejc@saje.info>
2019-09-19 12:45:41 +0200

    fix prometheus test

--- server/swinfra/metrics/push_test.py
+++ server/swinfra/metrics/push_test.py
@@ -7,8 +7,10 @@ from . import registry
 
 
 class PushTest(TestCase):
-    @mock.patch("prometheus_client.pushadd_to_gateway")
-    def test_basic(self, mock_pushadd):
+    @mock.patch("prometheus_client.push_to_gateway")
+    def test_basic(self, mock_push):
         push.start_push_mode("mygateway:8000", 15, "my-job", push_periodically=False)
         push.flush_push_metrics()
-        mock_pushadd.assert_called_once_with("mygateway:8000", job="my-job", registry=registry.get_registry())
+        mock_push.assert_called_once_with(
+            "mygateway:8000", job="my-job", registry=registry.get_registry(), grouping_key={"job": "my-job"}
+        )
