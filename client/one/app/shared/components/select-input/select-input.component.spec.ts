7bd24115d11a57fb4da046eb45b79b0af8cf5e00
Matjaz Suber <msuber@outbrain.com>
2019-09-25 15:50:46 +0200

    Add tests to select-input component. (#3976)

--- client/one/app/shared/components/select-input/select-input.component.spec.ts
+++ client/one/app/shared/components/select-input/select-input.component.spec.ts
@@ -3,6 +3,7 @@ import {FormsModule} from '@angular/forms';
 import {NgSelectModule} from '@ng-select/ng-select';
 import {SelectInputComponent} from './select-input.component';
 import {FocusDirective} from '../../directives/focus/focus.directive';
+import {SimpleChange} from '@angular/core';
 
 describe('SelectInputComponent', () => {
     let component: SelectInputComponent;
@@ -24,12 +25,41 @@ describe('SelectInputComponent', () => {
         expect(component).toBeDefined();
     });
 
+    it('should be correctly initialized on input change', () => {
+        const bindLabel = 'name';
+        const bindValue = 'value';
+        const items: any[] = [
+            {name: 'Nejc', value: 1},
+            {name: 'Anej', value: 2},
+            {name: 'Jure', value: 3},
+        ];
+        const orderByValue = 'name';
+
+        component.bindLabel = bindLabel;
+        component.bindValue = bindValue;
+        component.items = items;
+        component.orderByValue = orderByValue;
+
+        component.ngOnChanges({
+            bindLabel: new SimpleChange(null, bindLabel, false),
+            bindValue: new SimpleChange(null, bindValue, false),
+            items: new SimpleChange(null, items, false),
+            orderByValue: new SimpleChange(null, orderByValue, false),
+        });
+
+        expect(component.formattedItems).toEqual([
+            {name: 'Anej', value: 2},
+            {name: 'Jure', value: 3},
+            {name: 'Nejc', value: 1},
+        ]);
+    });
+
     it('should correctly emit model on change event', () => {
-        component.bindValue = 'customValue';
+        component.bindValue = 'value';
 
         spyOn(component.valueChange, 'emit').and.stub();
 
-        component.onChange({customValue: 'TEST'});
+        component.onChange({value: 'TEST'});
         expect(component.valueChange.emit).toHaveBeenCalledWith('TEST');
         (<any>component.valueChange.emit).calls.reset();
 
