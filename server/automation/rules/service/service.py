02ded196124d176d03d46a6016894251885bd282
Anej <anej.placer@gmail.com>
2019-10-08 15:34:47 +0200

    Campaign automation stats formatting v1 (#3982)
    * Stats format loop
    
    * Stat formatting test
    
    * Extra data for publishers

--- server/automation/rules/service/service.py
+++ server/automation/rules/service/service.py
@@ -1,10 +1,13 @@
 
-import collections
+from collections import defaultdict
+from typing import Callable
 from typing import DefaultDict
 from typing import Dict
 from typing import List
 from typing import Sequence
+from typing import Union
 
+import automation.rules.constants
 import core.models
 import redshiftapi.api_rules
 
@@ -20,17 +23,17 @@ def process_rules() -> None:
 
         ad_groups = list(rules_map.keys())
         raw_stats = redshiftapi.api_rules.query(target, ad_groups)
-        stats = format_stats(raw_stats)
+        stats = _format_stats(target, raw_stats)
 
         for ad_group in ad_groups:
             relevant_rules = rules_map[ad_group]
 
             for rule in relevant_rules:
-                apply_rule(ad_group, stats[ad_group.id], rule)
+                apply_rule(ad_group, stats.get(ad_group.id), rule)
 
 
 def _get_rules_by_ad_group_map(rules: Sequence[Rule]) -> DefaultDict[core.models.AdGroup, List[Rule]]:
-    rules_map: DefaultDict[core.models.AdGroup, List[Rule]] = collections.defaultdict(list)
+    rules_map: DefaultDict[core.models.AdGroup, List[Rule]] = defaultdict(list)
 
     for rule in rules:
         for ad_group in rule.ad_groups_included.all():
@@ -39,5 +42,28 @@ def _get_rules_by_ad_group_map(rules: Sequence[Rule]) -> DefaultDict[core.models
     return rules_map
 
 
-def format_stats(stats: Sequence[Dict]) -> Dict:
-    return {}
+def _format_stats(
+    target: int, stats: Sequence[Dict]
+) -> DefaultDict[int, DefaultDict[str, DefaultDict[str, DefaultDict[int, Union[None, int, float]]]]]:
+
+    dict_tree: Callable[[], DefaultDict] = lambda: defaultdict(dict_tree)  # noqa
+    formatted_stats = dict_tree()
+
+    target_column_keys = automation.rules.constants.TARGET_MV_COLUMNS_MAPPING[target]
+
+    for row in stats:
+        ad_group_id = row.pop("ad_group_id", None)
+        target_key = row.pop(target_column_keys[0], None)
+        target_extra_keys = {key: row.pop(key, None) for key in target_column_keys[1:]}
+        window_key = row.pop("window_key", None)
+
+        if None in (ad_group_id, window_key, target_key):
+            continue
+
+        for metric, value in row.items():
+            formatted_stats[ad_group_id][target_key][metric][window_key] = value
+
+        for key, value in target_extra_keys.items():
+            formatted_stats[ad_group_id][target_key][key] = value
+
+    return formatted_stats
--- server/automation/rules/service/service_test.py
+++ server/automation/rules/service/service_test.py
@@ -11,7 +11,7 @@ from . import service
 
 class ServiceTest(TestCase):
     @mock.patch("automation.rules.service.service.apply_rule")
-    @mock.patch("automation.rules.service.service.format_stats")
+    @mock.patch("automation.rules.service.service._format_stats")
     @mock.patch("redshiftapi.api_rules.query")
     def test_process_rules(self, mock_stats, mock_format, mock_apply):
         ad_groups = magic_mixer.cycle(5).blend(core.models.AdGroup)
@@ -27,7 +27,7 @@ class ServiceTest(TestCase):
             [ad_groups[0], ad_groups[1], ad_groups[2], ad_groups[3]], mock_stats.call_args_list[0][0][1]
         )
 
-        self.assertEqual([mock.call([123])], mock_format.call_args_list)
+        self.assertEqual([mock.call(constants.TargetType.PUBLISHER, [123])], mock_format.call_args_list)
 
         self.assertEqual(4, mock_apply.call_count)
 
@@ -47,4 +47,104 @@ class ServiceTest(TestCase):
                 ad_groups[4]: [rule_2],
             },
             rules_map,
+        )
+
+    def test_format_stats(self):
+        target = constants.TargetType.PUBLISHER
+        stats = [
+            {
+                "ad_group_id": 123,
+                "publisher_id": "pub1",
+                "publisher": "pub1.com",
+                "window_key": constants.MetricWindow.LAST_3_DAYS,
+                "cpc": 0.5,
+                "cpm": 0.7,
+            },
+            {
+                "ad_group_id": 123,
+                "publisher_id": "pub1",
+                "publisher": "pub1.com",
+                "window_key": constants.MetricWindow.LAST_30_DAYS,
+                "cpc": 0.3,
+                "cpm": 0.1,
+            },
+            {
+                "ad_group_id": 123,
+                "publisher_id": "pub2",
+                "publisher": "pub2.com",
+                "window_key": constants.MetricWindow.LAST_DAY,
+                "cpc": 0.5,
+                "cpm": 0.2,
+            },
+            {
+                "ad_group_id": 123,
+                "publisher_id": "pub2",
+                "publisher": "pub2.com",
+                "window_key": constants.MetricWindow.LIFETIME,
+                "cpc": None,
+                "cpm": 0.8,
+            },
+            {
+                "ad_group_id": 321,
+                "publisher_id": "pub1",
+                "publisher": "pub1.com",
+                "window_key": constants.MetricWindow.LAST_3_DAYS,
+                "cpc": 0.1,
+                "cpm": 0.2,
+            },
+            {
+                "ad_group_id": 321,
+                "publisher_id": "pub3",
+                "publisher": "pub3.com",
+                "window_key": constants.MetricWindow.LAST_DAY,
+                "cpc": 0.2,
+                "cpm": 0.1,
+            },
+            {
+                "ad_group_id": None,
+                "publisher_id": "pub4",
+                "publisher": "pub4.com",
+                "window_key": constants.MetricWindow.LAST_3_DAYS,
+                "cpc": 0.5,
+                "cpm": 0.7,
+            },
+            {
+                "ad_group_id": 123,
+                "publisher_id": None,
+                "publisher": "None",
+                "window_key": constants.MetricWindow.LAST_7_DAYS,
+                "cpc": 0.5,
+                "cpm": 0.7,
+            },
+            {"ad_group_id": 321, "publisher": "pub5", "window_key": None, "cpc": 0.5, "cpm": 0.7},
+        ]
+        formatted_stats = service._format_stats(target, stats)
+        self.assertEqual(
+            {
+                123: {
+                    "pub1": {
+                        "publisher": "pub1.com",
+                        "cpc": {constants.MetricWindow.LAST_3_DAYS: 0.5, constants.MetricWindow.LAST_30_DAYS: 0.3},
+                        "cpm": {constants.MetricWindow.LAST_3_DAYS: 0.7, constants.MetricWindow.LAST_30_DAYS: 0.1},
+                    },
+                    "pub2": {
+                        "publisher": "pub2.com",
+                        "cpc": {constants.MetricWindow.LAST_DAY: 0.5, constants.MetricWindow.LIFETIME: None},
+                        "cpm": {constants.MetricWindow.LAST_DAY: 0.2, constants.MetricWindow.LIFETIME: 0.8},
+                    },
+                },
+                321: {
+                    "pub1": {
+                        "publisher": "pub1.com",
+                        "cpc": {constants.MetricWindow.LAST_3_DAYS: 0.1},
+                        "cpm": {constants.MetricWindow.LAST_3_DAYS: 0.2},
+                    },
+                    "pub3": {
+                        "publisher": "pub3.com",
+                        "cpc": {constants.MetricWindow.LAST_DAY: 0.2},
+                        "cpm": {constants.MetricWindow.LAST_DAY: 0.1},
+                    },
+                },
+            },
+            formatted_stats,
         )
