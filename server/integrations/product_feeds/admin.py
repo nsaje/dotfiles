9d47d429da770cfece6987dcd4d2bee5a4a7d710
Thiebaut Fischer <26058141+tibz7@users.noreply.github.com>
2019-11-26 08:18:35 +0100

    allow delete permission on product feeds

--- server/integrations/product_feeds/admin.py
+++ server/integrations/product_feeds/admin.py
@@ -21,9 +21,6 @@ class SyncAttemptAdmin(admin.ModelAdmin):
     def has_add_permission(self, request, obj=None):
         return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
-
     def has_change_permission(self, request, obj=None):
         return False
 
