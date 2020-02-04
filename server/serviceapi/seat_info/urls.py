06a580ddf40aef0f9f415a04832c95ef45b2b5d2
tfischer <26058141+tibz7@users.noreply.github.com>
2020-02-04 09:03:04 +0100

    Revert "OPA entities mapping endpoint (#4257)" (#4277)
    This reverts commit 47be0e53886651b20fba89d02c0a99ef51ffd45f.

--- server/serviceapi/seat_info/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r"^seatinfo/accounts/(?P<seat_id>\d+)$", views.SeatInfoView.as_view(), name="service.seatinfo.seats")
-]
--- server/serviceapi/urls.py
+++ server/serviceapi/urls.py
@@ -1,8 +1,6 @@
 import serviceapi.salesforce.urls
-import serviceapi.seat_info.urls
 import serviceapi.video_upload_callback.urls
 
 urlpatterns = []
 urlpatterns += serviceapi.salesforce.urls.urlpatterns
 urlpatterns += serviceapi.video_upload_callback.urls.urlpatterns
-urlpatterns += serviceapi.seat_info.urls.urlpatterns
