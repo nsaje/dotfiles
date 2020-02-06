8cd9b02055ddd1209724508f5cf3db97d7f32500
Anej Placer <anej.placer@gmail.com>
2020-02-06 19:04:37 +0100

    Revert "Revert "RESTAPI docs constants templating (#4258)""
    This reverts commit 9368d4ab74ceb32a3ab92ec2871f7d0b38a0eed7.

--- server/restapi/docs/build-docker.sh
+++ server/restapi/docs/build-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR="$(dirname $(readlink -f $0))"
-
-docker run -v $DIR:/tmp -t -e "DRAFTER_EXAMPLES=true" -e "NOCACHE=1" zemanta/z1-aglio -i "/tmp/api_blueprint.md" --theme-style "/tmp/theme/style.less" --theme-variables "/tmp/theme/variables.less" --theme-template "/tmp/theme/template.jade" -o "/tmp/$1"
+docker run --rm -v $DIR:/tmp 569683728510.dkr.ecr.us-east-1.amazonaws.com/zemanta/z1 bash -c "python manage.py api_blueprint_generate_constants /tmp/api_blueprint.md  > /tmp/api_blueprint_generated.md"
+docker run --rm -v $DIR:/tmp -t -e "DRAFTER_EXAMPLES=true" -e "NOCACHE=1" zemanta/z1-aglio -i "/tmp/api_blueprint_generated.md" --theme-style "/tmp/theme/style.less" --theme-variables "/tmp/theme/variables.less" --theme-template "/tmp/theme/template.jade" -o "/tmp/$1"
