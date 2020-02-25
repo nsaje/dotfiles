40aae90dca5ce25a9811b2c85fd2286fbb718f99
jgosar <36840705+jgosar@users.noreply.github.com>
2020-02-25 07:29:25 +0100

    Created wrapper component for scope selector in order to add it to publisher groups AJS page (#4337)

--- client/one/app/features/publisher-groups-library/publisher-groups-library.module.ts
+++ client/one/app/features/publisher-groups-library/publisher-groups-library.module.ts
@@ -3,10 +3,18 @@ import {NgModule} from '@angular/core';
 import {SharedModule} from '../../shared/shared.module';
 import {PublisherGroupsComponent} from './components/publisher-groups/publisher-groups.component';
 import {PublisherGroupsLibraryView} from './views/publisher-groups-library/publisher-groups-library.view';
+import {PublisherGroupsScopeSelectorComponent} from './components/publisher-groups/publisher-groups-scope-selector.component';
 
 @NgModule({
-    declarations: [PublisherGroupsComponent, PublisherGroupsLibraryView],
+    declarations: [
+        PublisherGroupsComponent,
+        PublisherGroupsLibraryView,
+        PublisherGroupsScopeSelectorComponent,
+    ],
     imports: [SharedModule],
-    entryComponents: [PublisherGroupsLibraryView],
+    entryComponents: [
+        PublisherGroupsLibraryView,
+        PublisherGroupsScopeSelectorComponent,
+    ],
 })
 export class PublisherGroupsLibraryModule {}
