1f76f6a5fccfef8b3864242c08afc9f9eb45afc6
Luka Šilovinac <luka.silovinac@gmail.com>
2020-03-04 16:22:08 +0100

    Rules: settings fetch (#4348)
    * Add settings fetching queryset
    
    * Add tests and content ad handling
    
    * Split up ad group and content ad functions
    
    * Extract a function
    
    * Include goals and daily cap optimizations
    
    * Extract campaign goal and daily cap annotations
    
    * Extract ad group bid annotation
    
    * Actually use date fields and add a comment
    
    * Minor whitespace fixes
    
    * Add a comment

---
+++ server/automation/rules/service/fetch.py
@@ -0,0 +1,166 @@
+from typing import Dict
+from typing import Sequence
+from typing import Union
+
+from django.db.models import Case
+from django.db.models import DateField
+from django.db.models import DecimalField
+from django.db.models import F
+from django.db.models import OuterRef
+from django.db.models import Q
+from django.db.models import Subquery
+from django.db.models import Sum
+from django.db.models import When
+from django.db.models.functions import Cast
+from django.db.models.functions import Coalesce
+from django.db.models.functions import ExtractDay
+from django.db.models.functions import Now
+
+import core.models
+import dash.constants
+
+from .. import constants
+
+
+def prepare_content_ad_settings(ad_groups: Sequence[core.models.AdGroup]) -> Dict[int, Dict[str, Union[int, str]]]:
+    settings_by_ad_group = prepare_ad_group_settings(ad_groups)
+    content_ad_annotate_mappings = _prepare_content_ad_annotations()
+    content_ad_settings_qs = (
+        core.models.ContentAd.objects.filter(ad_group__in=settings_by_ad_group.keys())
+        .annotate(content_ad_id=F("id"), **content_ad_annotate_mappings)
+        .values("content_ad_id", "ad_group_id", *content_ad_annotate_mappings.keys())
+    )
+    settings_by_content_ad = {el["content_ad_id"]: el for el in content_ad_settings_qs}
+    return _merge_content_ad_with_ad_group_settings(settings_by_ad_group, settings_by_content_ad)
+
+
+def prepare_ad_group_settings(
+    ad_groups: Sequence[core.models.AdGroup],
+    include_campaign_goals: bool = False,
+    include_ad_group_daily_cap: bool = False,
+) -> Dict[int, Dict[str, Union[int, str]]]:
+    annotate_mappings = _prepare_ad_group_settings_annotations(
+        include_campaign_goals=include_campaign_goals, include_ad_group_daily_cap=include_ad_group_daily_cap
+    )
+    ad_group_settings_qs = (
+        core.models.AdGroup.objects.filter(id__in=[ag.id for ag in ad_groups])
+        .annotate(ad_group_id=F("id"), **annotate_mappings)
+        .values("ad_group_id", *annotate_mappings.keys())
+    )
+    return {el["ad_group_id"]: el for el in ad_group_settings_qs}
+
+
+def _prepare_content_ad_annotations():
+    metric_mappings = {
+        constants.MetricType.AD_TITLE: F("title"),
+        constants.MetricType.AD_LABEL: F("label"),
+        constants.MetricType.AD_CREATED_DATE: _cast_datetime_field_to_date("created_dt"),
+        constants.MetricType.DAYS_SINCE_AD_CREATED: _get_days_since_created_field("created_dt"),
+    }
+    annotate_mappings = _map_keys_from_constant_to_qs_string_representation(metric_mappings)
+    return annotate_mappings
+
+
+def _merge_content_ad_with_ad_group_settings(settings_by_ad_group, settings_by_content_ad):
+    updated_settings_by_content_ad = {}
+    for content_ad_id, content_ad_settings in settings_by_content_ad.items():
+        updated_settings_by_content_ad[content_ad_id] = _get_merged_content_ad_settings(
+            settings_by_ad_group, content_ad_settings
+        )
+    return updated_settings_by_content_ad
+
+
+def _get_merged_content_ad_settings(settings_by_ad_group, content_ad_settings):
+    ad_group_id = content_ad_settings["ad_group_id"]
+    ad_group_settings = settings_by_ad_group[ad_group_id]
+
+    updated_content_ad_settings = dict(content_ad_settings.items())
+    updated_content_ad_settings.update(ad_group_settings)
+    return updated_content_ad_settings
+
+
+def _prepare_ad_group_settings_annotations(*, include_campaign_goals: bool, include_ad_group_daily_cap: bool):
+    metric_mappings = {
+        constants.MetricType.ACCOUNT_NAME: F("campaign__account__name"),
+        constants.MetricType.ACCOUNT_CREATED_DATE: _cast_datetime_field_to_date("campaign__account__created_dt"),
+        constants.MetricType.DAYS_SINCE_ACCOUNT_CREATED: _get_days_since_created_field("campaign__account__created_dt"),
+        constants.MetricType.CAMPAIGN_NAME: F("campaign__name"),
+        constants.MetricType.CAMPAIGN_CREATED_DATE: _cast_datetime_field_to_date("campaign__created_dt"),
+        constants.MetricType.DAYS_SINCE_CAMPAIGN_CREATED: _get_days_since_created_field("campaign__created_dt"),
+        constants.MetricType.CAMPAIGN_TYPE: F("campaign__type"),
+        constants.MetricType.CAMPAIGN_MANAGER: F("campaign__settings__campaign_manager__email"),
+        constants.MetricType.CAMPAIGN_CATEGORY: F("campaign__settings__iab_category"),
+        constants.MetricType.CAMPAIGN_LANGUAGE: F("campaign__settings__language"),
+        constants.MetricType.AD_GROUP_NAME: F("name"),
+        constants.MetricType.AD_GROUP_CREATED_DATE: _cast_datetime_field_to_date("created_dt"),
+        constants.MetricType.DAYS_SINCE_AD_GROUP_CREATED: _get_days_since_created_field("created_dt"),
+        constants.MetricType.AD_GROUP_START_DATE: F("settings__start_date"),
+        constants.MetricType.AD_GROUP_END_DATE: F("settings__end_date"),
+        constants.MetricType.AD_GROUP_BIDDING_TYPE: F("bidding_type"),
+        constants.MetricType.AD_GROUP_BID: _get_ad_group_bid_field(),
+        constants.MetricType.AD_GROUP_DELIVERY_TYPE: F("settings__delivery_type"),
+    }
+    if include_campaign_goals:
+        metric_mappings.update(_constuct_campaign_goals_annotations())
+    if include_ad_group_daily_cap:
+        metric_mappings.update(_construct_ad_group_daily_cap_annotations())
+    annotate_mappings = _map_keys_from_constant_to_qs_string_representation(metric_mappings)
+    return annotate_mappings
+
+
+def _construct_ad_group_daily_cap_annotations():
+    # NOTE: possible optimization - materialize daily cap
+    ad_group_source_caps_subquery = (
+        core.models.AdGroupSource.objects.filter(ad_group_id=OuterRef("id"))
+        .filter(settings__state=dash.constants.AdGroupSourceSettingsState.ACTIVE)
+        .filter(Q(ad_group__settings__b1_sources_group_enabled=False) | ~Q(source__source_type__type="b1"))
+        .values("ad_group__pk")
+        .annotate(daily_cap=Sum("settings__local_daily_budget_cc"))
+    )
+    return {
+        constants.MetricType.AD_GROUP_DAILY_CAP: Case(
+            When(settings__b1_sources_group_enabled=True, then=F("settings__local_b1_sources_group_daily_budget")),
+            default=0,
+        )
+        + Coalesce(Subquery(ad_group_source_caps_subquery.values("daily_cap")[:1], output_field=DecimalField()), 0)
+    }
+
+
+def _constuct_campaign_goals_annotations():
+    # NOTE: possible optimization - store primary goal fk on campaign itself
+    campaign_goals_subquery = core.features.goals.CampaignGoal.objects.filter(
+        campaign_id=OuterRef("campaign_id"), primary=True
+    )
+    campaign_goals_values_subquery = core.features.goals.CampaignGoalValue.objects.filter(
+        campaign_goal_id=OuterRef("id")
+    ).order_by("-created_dt")
+    return {
+        constants.MetricType.CAMPAIGN_PRIMARY_GOAL: Subquery(campaign_goals_subquery.values("type")),
+        constants.MetricType.CAMPAIGN_PRIMARY_GOAL_VALUE: Subquery(
+            campaign_goals_subquery.annotate(
+                local_value=Subquery(campaign_goals_values_subquery.values("local_value")[:1])
+            )[:1].values("local_value"),
+            output_field=DecimalField(),
+        ),
+    }
+
+
+def _get_ad_group_bid_field():
+    return Case(
+        When(bidding_type=dash.constants.BiddingType.CPC, then=F("settings__local_cpc")),
+        When(bidding_type=dash.constants.BiddingType.CPM, then=F("settings__local_cpm")),
+    )
+
+
+def _get_days_since_created_field(field):
+    return ExtractDay(Cast(Now(), DateField()) - _cast_datetime_field_to_date(field))
+
+
+def _cast_datetime_field_to_date(field):
+    return Cast(field, DateField())
+
+
+def _map_keys_from_constant_to_qs_string_representation(metric_mappings):
+    # NOTE: django expects strings as keys in annotations.
+    # They are mapped to descriptive names for clarity.
+    return {constants.METRIC_SETTINGS_MAPPING[metric]: field for metric, field in metric_mappings.items()}
---
+++ server/automation/rules/service/fetch_test.py
@@ -0,0 +1,184 @@
+import datetime
+import decimal
+
+import mock
+from django.test import TestCase
+
+import core.features.goals
+import core.models
+import dash.constants
+from utils import dates_helper
+from utils import test_helper
+from utils.magic_mixer import magic_mixer
+
+from . import fetch
+
+
+class PrepareSettingsTestCase(TestCase):
+    def setUp(self):
+        # NOTE: patching the class with a decorator doesn't work because of import timing
+        patcher = mock.patch("automation.autopilot.recalculate_budgets_ad_group")
+        patcher.start()
+        self.addCleanup(patcher.stop)
+
+        now = dates_helper.utc_now()
+        self.utc_today = dates_helper.utc_today()
+        self.request = magic_mixer.blend_request_user()
+        self.user = self.request.user
+        with test_helper.disable_auto_now_add(core.models.Account, field_name="created_dt"):
+            self.account = magic_mixer.blend(core.models.Account, created_dt=now - datetime.timedelta(days=3))
+        with test_helper.disable_auto_now_add(core.models.Campaign, field_name="created_dt"):
+            self.campaign = magic_mixer.blend(
+                core.models.Campaign, account=self.account, created_dt=now - datetime.timedelta(days=2)
+            )
+        with test_helper.disable_auto_now_add(core.models.AdGroup, field_name="created_dt"):
+            self.ad_group = magic_mixer.blend(
+                core.models.AdGroup, campaign=self.campaign, created_dt=now - datetime.timedelta(days=1)
+            )
+        b1_source_type = magic_mixer.blend(core.models.SourceType, type="b1")
+        self.ad_group_sources = magic_mixer.cycle(3).blend(
+            core.models.AdGroupSource, ad_group=self.ad_group, source__source_type=b1_source_type
+        )
+        for ad_group_source in self.ad_group_sources:
+            ad_group_source.settings.update(
+                None, state=dash.constants.AdGroupSourceSettingsState.ACTIVE, daily_budget_cc=decimal.Decimal("10")
+            )
+        self.campaign.settings.update(None, campaign_manager=self.user)
+        self.ad_group.settings.update(None, end_date=self.utc_today + datetime.timedelta(days=7))
+
+    def test_prepare_ad_group_settings(self):
+        self.assertEqual(
+            fetch.prepare_ad_group_settings([self.ad_group]),
+            {
+                self.ad_group.id: {
+                    "account_created_date": self.utc_today - datetime.timedelta(days=3),
+                    "account_name": self.account.name,
+                    "ad_group_bid": decimal.Decimal("0.4500"),
+                    "ad_group_bidding_type": dash.constants.BiddingType.CPC,
+                    "ad_group_created_date": self.utc_today - datetime.timedelta(days=1),
+                    "ad_group_delivery_type": dash.constants.AdGroupDeliveryType.STANDARD,
+                    "ad_group_end_date": self.utc_today + datetime.timedelta(days=7),
+                    "ad_group_id": self.ad_group.id,
+                    "ad_group_name": self.ad_group.name,
+                    "ad_group_start_date": self.utc_today,
+                    "campaign_category": dash.constants.IABCategory.IAB24,
+                    "campaign_created_date": self.utc_today - datetime.timedelta(days=2),
+                    "campaign_language": dash.constants.Language.ENGLISH,
+                    "campaign_manager": self.user.email,
+                    "campaign_name": self.campaign.name,
+                    "campaign_type": dash.constants.CampaignType.CONTENT,
+                    "days_since_account_created": 3,
+                    "days_since_ad_group_created": 1,
+                    "days_since_campaign_created": 2,
+                }
+            },
+        )
+
+    def test_prepare_content_ad_settings(self):
+        content_ad = magic_mixer.blend(core.models.ContentAd, ad_group=self.ad_group, label="test")
+        self.assertEqual(
+            fetch.prepare_content_ad_settings([self.ad_group]),
+            {
+                content_ad.id: {
+                    "account_created_date": self.utc_today - datetime.timedelta(days=3),
+                    "account_name": self.account.name,
+                    "ad_created_date": self.utc_today,
+                    "ad_group_bid": decimal.Decimal("0.4500"),
+                    "ad_group_bidding_type": dash.constants.BiddingType.CPC,
+                    "ad_group_created_date": self.utc_today - datetime.timedelta(days=1),
+                    "ad_group_delivery_type": dash.constants.AdGroupDeliveryType.STANDARD,
+                    "ad_group_end_date": self.utc_today + datetime.timedelta(days=7),
+                    "ad_group_id": self.ad_group.id,
+                    "ad_group_name": self.ad_group.name,
+                    "ad_group_start_date": self.utc_today,
+                    "ad_title": content_ad.title,
+                    "ad_label": content_ad.label,
+                    "campaign_category": dash.constants.IABCategory.IAB24,
+                    "campaign_created_date": self.utc_today - datetime.timedelta(days=2),
+                    "campaign_language": dash.constants.Language.ENGLISH,
+                    "campaign_manager": self.user.email,
+                    "campaign_name": self.campaign.name,
+                    "campaign_type": dash.constants.CampaignType.CONTENT,
+                    "content_ad_id": content_ad.id,
+                    "days_since_account_created": 3,
+                    "days_since_ad_created": 0,
+                    "days_since_ad_group_created": 1,
+                    "days_since_campaign_created": 2,
+                }
+            },
+        )
+
+    def test_primary_campaign_goal(self):
+        core.features.goals.CampaignGoal.objects.create(
+            self.request, self.campaign, dash.constants.CampaignGoalKPI.TIME_ON_SITE, 30, primary=False
+        )
+        primary_goal = core.features.goals.CampaignGoal.objects.create(
+            self.request, self.campaign, dash.constants.CampaignGoalKPI.CPC, decimal.Decimal("0.5"), primary=True
+        )
+        primary_goal.update(self.request, value=decimal.Decimal("0.7"))
+
+        ad_group_settings = fetch.prepare_ad_group_settings([self.ad_group], include_campaign_goals=True)[
+            self.ad_group.id
+        ]
+        self.assertEqual(dash.constants.CampaignGoalKPI.CPC, ad_group_settings["campaign_primary_goal"])
+        self.assertEqual(decimal.Decimal("0.7"), ad_group_settings["campaign_primary_goal_value"])
+
+    def test_non_primary_campaign_goal(self):
+        non_primary_goal = core.features.goals.CampaignGoal.objects.create(
+            self.request, self.campaign, dash.constants.CampaignGoalKPI.TIME_ON_SITE, 30, primary=False
+        )
+        core.features.goals.CampaignGoal.objects.create(
+            self.request, self.campaign, dash.constants.CampaignGoalKPI.CPC, decimal.Decimal("0.5"), primary=True
+        )
+        non_primary_goal.update(self.request, primary=True)
+
+        ad_group_settings = fetch.prepare_ad_group_settings([self.ad_group], include_campaign_goals=True)[
+            self.ad_group.id
+        ]
+        self.assertEqual(dash.constants.CampaignGoalKPI.TIME_ON_SITE, ad_group_settings["campaign_primary_goal"])
+        self.assertEqual(30, ad_group_settings["campaign_primary_goal_value"])
+
+    def test_ad_group_bid_cpc(self):
+        self.ad_group.update(None, bidding_type=dash.constants.BiddingType.CPC)
+        self.ad_group.settings.update(None, cpc=decimal.Decimal("0.7"))
+
+        ad_group_settings = fetch.prepare_ad_group_settings([self.ad_group])[self.ad_group.id]
+        self.assertEqual(decimal.Decimal("0.7"), ad_group_settings["ad_group_bid"])
+
+    def test_ad_group_bid_cpm(self):
+        self.ad_group.update(None, bidding_type=dash.constants.BiddingType.CPM)
+        self.ad_group.settings.update(None, cpm=decimal.Decimal("1.2"))
+
+        ad_group_settings = fetch.prepare_ad_group_settings([self.ad_group])[self.ad_group.id]
+        self.assertEqual(decimal.Decimal("1.2"), ad_group_settings["ad_group_bid"])
+
+    def test_ad_group_daily_caps_manual_rtb(self):
+        self.ad_group.settings.update(
+            None, b1_sources_group_enabled=False, autopilot_state=dash.constants.AdGroupSettingsAutopilotState.INACTIVE
+        )
+
+        ad_group_settings = fetch.prepare_ad_group_settings([self.ad_group], include_ad_group_daily_cap=True)[
+            self.ad_group.id
+        ]
+        self.assertEqual(decimal.Decimal("30"), ad_group_settings["ad_group_daily_cap"])
+
+    def test_ad_group_daily_caps_all_rtb(self):
+        ad_group_settings = fetch.prepare_ad_group_settings([self.ad_group], include_ad_group_daily_cap=True)[
+            self.ad_group.id
+        ]
+        self.assertEqual(decimal.Decimal("50"), ad_group_settings["ad_group_daily_cap"])
+
+    def test_ad_group_daily_caps_all_rtb_api_soures(self):
+        non_b1_source_type = magic_mixer.blend(core.models.SourceType, type="notb1")
+        non_b1_ad_group_sources = magic_mixer.cycle(2).blend(
+            core.models.AdGroupSource, ad_group=self.ad_group, source__source_type=non_b1_source_type
+        )
+        for ad_group_source in non_b1_ad_group_sources:
+            ad_group_source.settings.update(
+                None, state=dash.constants.AdGroupSourceSettingsState.ACTIVE, daily_budget_cc=decimal.Decimal("10")
+            )
+
+        ad_group_settings = fetch.prepare_ad_group_settings([self.ad_group], include_ad_group_daily_cap=True)[
+            self.ad_group.id
+        ]
+        self.assertEqual(decimal.Decimal("70"), ad_group_settings["ad_group_daily_cap"])
