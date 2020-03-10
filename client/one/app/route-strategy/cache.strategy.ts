02d5fb28f1eb66b6037c4626dfcf29078488890f
Matjaz Suber <msuber@outbrain.com>
2020-03-10 08:26:46 +0100

    Optimize memory management in routing strategy.

--- client/one/app/route-strategy/cache.strategy.ts
+++ client/one/app/route-strategy/cache.strategy.ts
@@ -6,9 +6,10 @@ import {
 } from '@angular/router';
 import * as commonHelpers from '../shared/helpers/common.helpers';
 import {RoutePathName} from '../app.constants';
+import {ComponentRef} from '@angular/core';
 
-const ROUTE_CACHE_TTL_MINUTES = 5;
-const MAX_ROUTE_CACHE_MAP_SIZE = 10;
+const ROUTE_CACHE_TTL_MINUTES = 4;
+const MAX_ROUTE_CACHE_MAP_SIZE = 5;
 
 interface RouteCache {
     routeHandle: DetachedRouteHandle;
@@ -20,7 +21,6 @@ export class CacheRouteReuseStrategy implements RouteReuseStrategy {
         `${RoutePathName.APP_BASE}/${RoutePathName.ANALYTICS}`,
         `${RoutePathName.APP_BASE}/${RoutePathName.CREDITS_LIBRARY}`,
         `${RoutePathName.APP_BASE}/${RoutePathName.PIXELS_LIBRARY}`,
-        `${RoutePathName.APP_BASE}/${RoutePathName.DEALS_LIBRARY}`,
         `${RoutePathName.APP_BASE}/${RoutePathName.REPORTS_LIBRARY}`,
         `${RoutePathName.APP_BASE}/${RoutePathName.USERS_LIBRARY}`,
         `${RoutePathName.APP_BASE}/${RoutePathName.INVENTORY_PLANNING}`,
@@ -40,14 +40,17 @@ export class CacheRouteReuseStrategy implements RouteReuseStrategy {
             const routePath = this.getRoutePath(route);
             if (this.routeCacheMap.has(routePath)) {
                 if (this.hasRouteExpired(routePath)) {
-                    this.routeCacheMap.set(routePath, {
-                        routeHandle: handle,
-                        expireDate: this.getNewExpireDate(),
-                    });
+                    const routeCache = this.routeCacheMap.get(routePath);
+                    this.deactivateOutlet(routeCache.routeHandle);
+                    this.routeCacheMap.delete(routePath);
                 }
             } else {
                 if (this.routeCacheMap.size >= MAX_ROUTE_CACHE_MAP_SIZE) {
                     const routePathToDelete: string = this.getRoutePathToDelete();
+                    const routeCacheToDelete = this.routeCacheMap.get(
+                        routePathToDelete
+                    );
+                    this.deactivateOutlet(routeCacheToDelete.routeHandle);
                     this.routeCacheMap.delete(routePathToDelete);
                 }
 
@@ -131,5 +134,15 @@ export class CacheRouteReuseStrategy implements RouteReuseStrategy {
             }
         });
         return routePath;
+    }
+
+    // If a snapshot is detached from the routing and re-attached later, there should be no memory leak.
+    // But if a snapshot is detached from the routing and never re-attached to the router,
+    // we need to take care about the memory management.
+    private deactivateOutlet(handle: DetachedRouteHandle): void {
+        const componentRef: ComponentRef<any> = (<any>handle).componentRef;
+        if (commonHelpers.isDefined(componentRef)) {
+            componentRef.destroy();
+        }
     }
 }
