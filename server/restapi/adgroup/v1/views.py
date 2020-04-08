e13c084b8f9ab21bd80d0e2c9ba48b10b30c0ef8
Tilen <tilen970@gmail.com>
2020-04-08 15:56:51 +0200

    changed to get validated data in adgroup

--- server/restapi/adgroup/v1/views.py
+++ server/restapi/adgroup/v1/views.py
@@ -33,7 +33,7 @@ class AdGroupViewSet(RESTAPIBaseViewSet):
     def list(self, request):
         qpe = serializers.AdGroupQueryParams(data=request.query_params)
         qpe.is_valid(raise_exception=True)
-        campaign_id = request.query_params.get("campaignId", None)
+        campaign_id = qpe.validated_data.get("campaign_id", None)
         if campaign_id:
             campaign = restapi.access.get_campaign(request.user, campaign_id)
             ad_groups = core.models.AdGroup.objects.filter(campaign=campaign)
