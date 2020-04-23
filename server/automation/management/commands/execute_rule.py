6bbf53455dd40ca5537be8d53edc38f087b6e20f
Luka Šilovinac <luka.silovinac@zemanta.com>
2020-04-23 16:53:16 +0200

    Rules: simplify execute rule command

--- server/automation/management/commands/execute_rule.py
+++ server/automation/management/commands/execute_rule.py
@@ -1,5 +1,3 @@
-import sys
-
 import automation.rules
 from utils import metrics_compat
 from utils import zlogging
@@ -12,13 +10,9 @@ class Command(Z1Command):
     help = "Execute specified automation rule."
 
     def add_arguments(self, parser):
-        parser.add_argument("--rule_id", dest="rule_id", default=None, help="Rule id")
+        parser.add_argument("rule_id", type=int)
 
     @metrics_compat.timer("automation.rules.run_rules_job")
     def handle(self, *args, **options):
-        if not options["rule_id"]:
-            self.stderr.write("Missing rule id.")
-            sys.exit(1)
-
         self.stdout.write(f"Executing rule with id: {options['rule_id']}")
         automation.rules.execute_rules([automation.rules.Rule.objects.get(id=options["rule_id"])])
