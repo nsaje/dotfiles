5f66e0cf30dcfba7bc3a8bd6cf8b11ab775a0ee8
Matjaz Suber <msuber@outbrain.com>
2020-04-28 08:56:54 +0200

    User roles: improve types and filtering of entity_permissions. (#4543)

--- server/zemauth/features/entity_permission/queryset.py
+++ server/zemauth/features/entity_permission/queryset.py
@@ -6,10 +6,18 @@ class EntityPermissionQuerySet(models.QuerySet):
         return self.filter(agency=agency)
 
     def filter_by_account(self, account):
-        deal_qs = self.filter(account=account)
+        entity_permission_qs = self.filter(account=account)
         if account.agency is not None:
-            deal_qs |= self.filter_by_agency(account.agency)
-        return deal_qs
+            entity_permission_qs |= self.filter_by_agency(account.agency)
+        return entity_permission_qs
+
+    def filter_by_campaign(self, campaign):
+        return self.filter(models.Q(agency__account__campaign=campaign) | models.Q(account__campaign=campaign))
+
+    def filter_by_adgroup(self, adgroup):
+        return self.filter(
+            models.Q(agency__account__campaign__adgroup=adgroup) | models.Q(account__campaign__adgroup=adgroup)
+        )
 
     def filter_by_user(self, user):
         return self.filter(user=user)
--- server/zemauth/models/user/entity_permission.py
+++ server/zemauth/models/user/entity_permission.py
@@ -1,65 +1,47 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
 from typing import Any
 from typing import Union
 
-from django.db.models import Q
-
 import core.models
 import zemauth.features.entity_permission
+
+if TYPE_CHECKING:
+    Entity = Union[core.models.Agency, core.models.Account, core.models.Campaign, core.models.AdGroup]
 
 
 class EntityPermissionMixin(object):
     entitypermission_set: Any
 
-    def has_read_perm_on(
-        self, entity: Union[core.models.Agency, core.models.Account, core.models.Campaign, core.models.AdGroup]
-    ) -> bool:
+    def has_read_perm_on(self, entity: Entity) -> bool:
         return self._has_perm_on(zemauth.features.entity_permission.Permission.READ, entity)
 
-    def has_write_perm_on(
-        self, entity: Union[core.models.Agency, core.models.Account, core.models.Campaign, core.models.AdGroup]
-    ) -> bool:
+    def has_write_perm_on(self, entity: Entity) -> bool:
         return self._has_perm_on(zemauth.features.entity_permission.Permission.WRITE, entity)
 
-    def has_user_perm_on(
-        self, entity: Union[core.models.Agency, core.models.Account, core.models.Campaign, core.models.AdGroup]
-    ) -> bool:
+    def has_user_perm_on(self, entity: Entity) -> bool:
         return self._has_perm_on(zemauth.features.entity_permission.Permission.USER, entity)
 
-    def has_budget_perm_on(
-        self, entity: Union[core.models.Agency, core.models.Account, core.models.Campaign, core.models.AdGroup]
-    ) -> bool:
+    def has_budget_perm_on(self, entity: Entity) -> bool:
         return self._has_perm_on(zemauth.features.entity_permission.Permission.BUDGET, entity)
 
-    def has_budget_margin_perm_on(
-        self, entity: Union[core.models.Agency, core.models.Account, core.models.Campaign, core.models.AdGroup]
-    ) -> bool:
+    def has_budget_margin_perm_on(self, entity: Entity) -> bool:
         return self._has_perm_on(zemauth.features.entity_permission.Permission.BUDGET_MARGIN, entity)
 
-    def has_agency_spend_and_margin_perm_on(
-        self, entity: Union[core.models.Agency, core.models.Account, core.models.Campaign, core.models.AdGroup]
-    ) -> bool:
+    def has_agency_spend_and_margin_perm_on(self, entity: Entity) -> bool:
         return self._has_perm_on(zemauth.features.entity_permission.Permission.AGENCY_SPEND_MARGIN, entity)
 
-    def has_media_cost_data_cost_and_licence_fee_perm_on(
-        self, entity: Union[core.models.Agency, core.models.Account, core.models.Campaign, core.models.AdGroup]
-    ) -> bool:
+    def has_media_cost_data_cost_and_licence_fee_perm_on(self, entity: Entity) -> bool:
         return self._has_perm_on(zemauth.features.entity_permission.Permission.MEDIA_COST_DATA_COST_LICENCE_FEE, entity)
 
-    def has_rest_api_perm_on(
-        self, entity: Union[core.models.Agency, core.models.Account, core.models.Campaign, core.models.AdGroup]
-    ) -> bool:
+    def has_rest_api_perm_on(self, entity: Entity) -> bool:
         return self._has_perm_on(zemauth.features.entity_permission.Permission.RESTAPI, entity)
 
     def refresh_entity_permissions(self):
         zemauth.features.entity_permission.refresh_entity_permissions_for_user(self)
 
-    def _has_perm_on(
-        self,
-        permission: str,
-        entity: Union[core.models.Agency, core.models.Account, core.models.Campaign, core.models.AdGroup],
-    ) -> bool:
+    def _has_perm_on(self, permission: str, entity: Entity) -> bool:
         if entity is None:
             return False
 
@@ -72,21 +54,11 @@ class EntityPermissionMixin(object):
                 return True
             return self._has_perm_on_wildcard(permission)
         elif isinstance(entity, core.models.Campaign):
-            has_perm = zemauth.features.entity_permission.EntityPermission.objects.filter(
-                Q(user=self)
-                & Q(permission=permission)
-                & (Q(agency__account__campaign=entity) | Q(account__campaign=entity))
-            ).exists()
-            if has_perm:
+            if self.entitypermission_set.filter(permission=permission).filter_by_campaign(entity).exists():
                 return True
             return self._has_perm_on_wildcard(permission)
         elif isinstance(entity, core.models.AdGroup):
-            has_perm = zemauth.features.entity_permission.EntityPermission.objects.filter(
-                Q(user=self)
-                & Q(permission=permission)
-                & (Q(agency__account__campaign__adgroup=entity) | Q(account__campaign__adgroup=entity))
-            ).exists()
-            if has_perm:
+            if self.entitypermission_set.filter(permission=permission).filter_by_adgroup(entity).exists():
                 return True
             return self._has_perm_on_wildcard(permission)
         else:
