a95112ea38769eb6cae0a4d939cecbd1c453ca06
Matjaz Suber <msuber@outbrain.com>
2020-05-25 11:19:49 +0200

    [Z1-640] User roles: add entity_permission to restapi adgroupstats. (#4635)

--- server/restapi/adgroupstats/v1/views.py
+++ server/restapi/adgroupstats/v1/views.py
@@ -1,14 +1,15 @@
 import dash.features.realtimestats
-import restapi.access
+import zemauth.access
 from restapi.adgroupstats.v1 import serializers
 from restapi.common.views_base import RESTAPIBaseViewSet
+from zemauth.features.entity_permission import Permission
 
 
 class AdGroupRealtimeStatsViewSet(RESTAPIBaseViewSet):
     """ Outbrain only """
 
     def get(self, request, ad_group_id):
-        ad_group = restapi.access.get_ad_group(request.user, ad_group_id)
+        ad_group = zemauth.access.get_ad_group(request.user, Permission.READ, ad_group_id)
 
         stats = dash.features.realtimestats.get_ad_group_stats(ad_group)
         return self.response_ok(serializers.AdGroupRealtimeStatsSerializer(stats).data)
--- server/restapi/adgroupstats/v1/views_test.py
+++ server/restapi/adgroupstats/v1/views_test.py
@@ -3,15 +3,27 @@ from django.urls import reverse
 
 import core.models.ad_group
 from restapi.common.views_base_test import RESTAPITest
+from restapi.common.views_base_test import RESTAPITestCase
+from utils.magic_mixer import magic_mixer
+from zemauth.features.entity_permission import Permission
 
 
-class RealtimestatsViewsTest(RESTAPITest):
+class LegacyRealtimestatsViewsTest(RESTAPITest):
     @mock.patch("dash.features.realtimestats.get_ad_group_stats")
     def test_adgroups_realtimestats(self, mock_get):
+        account = self.mix_account(self.user, permissions=[Permission.READ])
+        ad_group = magic_mixer.blend(core.models.AdGroup, campaign__account=account)
+
         mock_get.return_value = {"clicks": 12321, "spend": 12.3}
-        r = self.client.get(reverse("restapi.adgroupstats.v1:adgroups_realtimestats", kwargs={"ad_group_id": 2040}))
+        r = self.client.get(
+            reverse("restapi.adgroupstats.v1:adgroups_realtimestats", kwargs={"ad_group_id": ad_group.id})
+        )
 
         resp_json = self.assertResponseValid(r)
         self.assertEqual(resp_json["data"], {"spend": "12.30", "clicks": 12321})
 
-        mock_get.assert_called_with(core.models.ad_group.AdGroup.objects.get(pk=2040))
+        mock_get.assert_called_with(ad_group)
+
+
+class RealtimestatsViewsTest(RESTAPITestCase, LegacyRealtimestatsViewsTest):
+    pass
