68824d094fd48ae20245caea6b2730bf74099073
Matjaz Suber <msuber@outbrain.com>
2020-05-25 11:19:24 +0200

    [Z1-639] User roles: add entity_permission to restapi adgroupsourcestats. (#4634)

--- server/restapi/adgroupsourcestats/v1/views.py
+++ server/restapi/adgroupsourcestats/v1/views.py
@@ -1,16 +1,17 @@
 from rest_framework import permissions
 
 import dash.features.realtimestats
-import restapi.access
+import zemauth.access
 from restapi.adgroupsourcestats.v1 import serializers
 from restapi.common.views_base import RESTAPIBaseViewSet
+from zemauth.features.entity_permission import Permission
 
 
 class AdGroupSourcesRealtimeStatsViewSet(RESTAPIBaseViewSet):
     permission_classes = (permissions.IsAuthenticated,)
 
     def list(self, request, ad_group_id):
-        ad_group = restapi.access.get_ad_group(request.user, ad_group_id)
+        ad_group = zemauth.access.get_ad_group(request.user, Permission.READ, ad_group_id)
 
         stats = dash.features.realtimestats.get_ad_group_sources_stats(ad_group, use_local_currency=True)
         return self.response_ok(serializers.AdGroupSourcesRealtimeStatsSerializer(stats, many=True).data)
--- server/restapi/adgroupsourcestats/v1/views_test.py
+++ server/restapi/adgroupsourcestats/v1/views_test.py
@@ -1,22 +1,23 @@
 import decimal
 
 import mock
-from django.contrib.auth.models import Permission
 from django.urls import reverse
 
 import core.models
 import core.models.ad_group
 from restapi.common.views_base_test import RESTAPITest
+from restapi.common.views_base_test import RESTAPITestCase
+from utils import test_helper
 from utils.magic_mixer import magic_mixer
-from zemauth.models import User
+from zemauth.features.entity_permission import Permission
 
 
-class RealtimestatsViewsTest(RESTAPITest):
+class LegacyRealtimestatsViewsTest(RESTAPITest):
     @mock.patch("dash.features.realtimestats.get_ad_group_sources_stats")
     def test_adgroup_sources_realtimestats(self, mock_get):
-        permission = Permission.objects.get(codename="can_use_restapi")
-        user = User.objects.get(pk=1)
-        user.user_permissions.remove(permission)
+        test_helper.remove_permissions(self.user, permissions=["can_use_restapi"])
+        account = self.mix_account(self.user, permissions=[Permission.READ])
+        ad_group = magic_mixer.blend(core.models.AdGroup, campaign__account=account)
 
         sources = magic_mixer.cycle(2).blend(core.models.Source, bidder_slug=magic_mixer.RANDOM)
         data = [
@@ -26,7 +27,7 @@ class RealtimestatsViewsTest(RESTAPITest):
 
         mock_get.return_value = data
         r = self.client.get(
-            reverse("restapi.adgroupsourcestats.v1:adgroups_realtimestats_sources", kwargs={"ad_group_id": 2040})
+            reverse("restapi.adgroupsourcestats.v1:adgroups_realtimestats_sources", kwargs={"ad_group_id": ad_group.id})
         )
 
         resp_json = self.assertResponseValid(r, data_type=list)
@@ -34,4 +35,8 @@ class RealtimestatsViewsTest(RESTAPITest):
         expected = [{"source": sources[0].name, "spend": "12.35"}, {"source": sources[1].name, "spend": "0.11"}]
         self.assertEqual(resp_json["data"], expected)
 
-        mock_get.assert_called_with(core.models.ad_group.AdGroup.objects.get(pk=2040), use_local_currency=True)
+        mock_get.assert_called_with(ad_group, use_local_currency=True)
+
+
+class RealtimestatsViewsTest(RESTAPITestCase, LegacyRealtimestatsViewsTest):
+    pass
