aa0eec2064e12c0807305f0c9e850c7a2c32f063
Matjaz Suber <msuber@outbrain.com>
2020-06-01 12:46:23 +0200

    Revert "[Z1-662] User roles: add entity_permission to restapi agency. (#4668)"
    This reverts commit 46f02b4b10aba330db00da06dc0197ce3aec8453.

--- server/restapi/agency/internal/views.py
+++ server/restapi/agency/internal/views.py
@@ -1,9 +1,7 @@
 import rest_framework.permissions
 
 import core.models
-import zemauth.features.entity_permission.helpers
 from restapi.common.views_base import RESTAPIBaseViewSet
-from zemauth.features.entity_permission import Permission
 
 from . import serializers
 
@@ -13,17 +11,8 @@ class AgencyViewSet(RESTAPIBaseViewSet):
     serializer = serializers.AgencySerializer
 
     def list(self, request):
-        accounts_user_perm = core.models.Account.objects.filter_by_user(request.user)
-        accounts_entity_perm = core.models.Account.objects.filter_by_entity_permission(request.user, Permission.READ)
-
-        agencies_user_perm = core.models.Agency.objects.filter_by_user(request.user)
-        agencies_user_perm |= core.models.Agency.objects.filter(account__in=accounts_user_perm).distinct()
-
-        agencies_entity_perm = core.models.Agency.objects.filter_by_entity_permission(request.user, Permission.READ)
-        agencies_entity_perm |= core.models.Agency.objects.filter(account__in=accounts_entity_perm).distinct()
-
-        agencies = zemauth.features.entity_permission.helpers.log_differences_and_get_queryset(
-            request.user, Permission.READ, agencies_user_perm, agencies_entity_perm
+        accounts = core.models.Account.objects.filter_by_user(request.user)
+        agencies = core.models.Agency.objects.filter_by_user(request.user).union(
+            core.models.Agency.objects.filter(account__in=accounts)
         )
-
         return self.response_ok(self.serializer(agencies, many=True, context={"request": request}).data)
