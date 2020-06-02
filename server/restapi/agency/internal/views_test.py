dac36fb311a5da29ffc9c5dca0f646493a012a83
Matjaz Suber <msuber@outbrain.com>
2020-06-02 08:35:57 +0200

    [Z1-662] User roles: fix user roles restapi agency (#4679)

--- server/restapi/agency/internal/views_test.py
+++ server/restapi/agency/internal/views_test.py
@@ -2,15 +2,52 @@ from django.urls import reverse
 
 import core.models
 from restapi.common.views_base_test import RESTAPITest
+from restapi.common.views_base_test import RESTAPITestCase
+from utils import test_helper
 from utils.magic_mixer import magic_mixer
+from zemauth.features.entity_permission import Permission
 
 
-class AgencyViewSetTest(RESTAPITest):
+class LegacyAgencyViewSetTest(RESTAPITest):
+    def setUp(self):
+        super().setUp()
+        self.user.account_set.remove(*self.user.account_set.all())
+        self.user.agency_set.remove(*self.user.agency_set.all())
+
     def test_agency_list(self):
-        agency = magic_mixer.blend(core.models.Agency, users=[self.user])
+        agency_read_access = self.mix_agency(self.user, permissions=[Permission.READ])
+
+        agency_no_read_access = magic_mixer.blend(core.models.Agency)
+        self.mix_account(self.user, permissions=[Permission.READ], agency=agency_no_read_access)
+        self.mix_account(self.user, permissions=[Permission.READ], agency=agency_no_read_access)
+        self.mix_account(self.user, permissions=[Permission.READ], agency=agency_no_read_access)
+
         magic_mixer.cycle(5).blend(core.models.Agency)
 
         r = self.client.get(reverse("restapi.agency.internal:agencies_list"))
         resp_json = self.assertResponseValid(r, data_type=list)
-        self.assertGreater(len(resp_json["data"]), 1)
-        self.assertTrue(any(item.get("name") == agency.name for item in resp_json["data"]))
+
+        resp_json_ids = [int(x.get("id")) for x in resp_json["data"]]
+
+        self.assertEqual(len(resp_json_ids), 2)
+        self.assertTrue(agency_read_access.id in resp_json_ids)
+        self.assertTrue(agency_no_read_access.id in resp_json_ids)
+
+    def test_agency_list_internal_user(self):
+        internal_user = magic_mixer.blend_user()
+        test_helper.add_permissions(internal_user, permissions=["can_see_all_accounts"])
+        test_helper.add_entity_permissions(internal_user, permissions=[Permission.READ], entity=None)
+        self.client.force_authenticate(user=internal_user)
+
+        agencies = magic_mixer.cycle(5).blend(core.models.Agency)
+
+        r = self.client.get(reverse("restapi.agency.internal:agencies_list"))
+        resp_json = self.assertResponseValid(r, data_type=list)
+
+        resp_json_ids = [int(x.get("id")) for x in resp_json["data"]]
+        for agency in agencies:
+            self.assertTrue(agency.id in resp_json_ids)
+
+
+class AgencyViewSetTest(RESTAPITestCase, LegacyAgencyViewSetTest):
+    pass
