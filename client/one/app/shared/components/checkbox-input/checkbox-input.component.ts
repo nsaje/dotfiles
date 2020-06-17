b6d0f6dd6f75bc5f14a85f1505c132e7bb954fb2
jgosar <36840705+jgosar@users.noreply.github.com>
2020-06-17 11:48:44 +0200

    [Z1-741] Three state checkbox (#4743)

--- client/one/app/shared/components/checkbox-input/checkbox-input.component.ts
+++ client/one/app/shared/components/checkbox-input/checkbox-input.component.ts
@@ -6,24 +6,47 @@ import {
     EventEmitter,
     Output,
     Input,
+    SimpleChanges,
+    OnChanges,
 } from '@angular/core';
+import {CheckboxInputDisplayState} from './types/checkbox-input-display-state';
 
 @Component({
     selector: 'zem-checkbox-input',
     templateUrl: './checkbox-input.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CheckboxInputComponent {
+export class CheckboxInputComponent implements OnChanges {
     @Input()
     isChecked: boolean = false;
     @Input()
     isDisabled: boolean = false;
     @Input()
     hasError: boolean = false;
+    @Input()
+    enableIndeterminateState: boolean = false;
     @Output()
     toggle = new EventEmitter<boolean>();
 
+    displayState: CheckboxInputDisplayState = 'unchecked';
+
     onChange() {
-        this.toggle.emit(!this.isChecked);
+        this.toggle.emit(this.displayState !== 'checked');
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes.isChecked) {
+            if (
+                this.enableIndeterminateState &&
+                this.isChecked !== true &&
+                this.isChecked !== false
+            ) {
+                this.displayState = 'indeterminate';
+            } else if (this.isChecked) {
+                this.displayState = 'checked';
+            } else {
+                this.displayState = 'unchecked';
+            }
+        }
     }
 }
---
+++ client/one/app/shared/components/checkbox-input/types/checkbox-input-display-state.ts
@@ -0,0 +1,4 @@
+export type CheckboxInputDisplayState =
+    | 'checked'
+    | 'unchecked'
+    | 'indeterminate';
