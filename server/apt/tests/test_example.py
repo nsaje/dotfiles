3ac71905107f0ce2c5ef3f9bf0dda696ddffd6df
Luka Šilovinac <luka.silovinac@gmail.com>
2020-07-02 09:10:40 +0200

    APT: Add an example test (#4829)

--- server/apt/tests/test_example.py
+++ server/apt/tests/test_example.py
@@ -1,9 +1,9 @@
+import core.models
+
 from ..base.test_case import APTTestCase
 
 
 class ExampleAPTTest(APTTestCase):
-    def test_example_restapi(self):
-        pass
-
-    def test_example_b1_db(self):
-        pass
+    def test_example_db(self):
+        acc = core.models.Account.objects.get(id=92)
+        self.assertEqual("Zemanta Product Group", acc.name)
