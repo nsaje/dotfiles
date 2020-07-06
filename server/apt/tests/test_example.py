f9cd4282e44e888cdf0f6a7409cf6038be6612f5
Luka Šilovinac <luka.silovinac@zemanta.com>
2020-07-06 15:14:52 +0200

    APT: move an id to constant

--- server/apt/tests/test_example.py
+++ server/apt/tests/test_example.py
@@ -2,8 +2,10 @@ import core.models
 
 from ..base.test_case import APTTestCase
 
+ZEMANTA_PRODUCT_GROUP_ACCOUNT_ID = 92
+
 
 class ExampleAPTTest(APTTestCase):
     def test_example_db(self):
-        acc = core.models.Account.objects.get(id=92)
+        acc = core.models.Account.objects.get(id=ZEMANTA_PRODUCT_GROUP_ACCOUNT_ID)
         self.assertEqual("Zemanta Product Group", acc.name)
