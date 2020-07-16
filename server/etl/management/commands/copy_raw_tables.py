ec4a91ba61cfe46f45ad19a2c67323cbae408432
Anej <anej.placer@gmail.com>
2020-07-16 14:37:17 +0200

    Copy raw tables command updated; preparation for redshift migration (#4900)
    * raw table copy command updated, new clusters prepared

--- server/etl/management/commands/copy_raw_tables.py
+++ server/etl/management/commands/copy_raw_tables.py
@@ -10,17 +10,27 @@ logger = zlogging.getLogger(__name__)
 
 
 class Command(Z1Command):
-    help = (
-        "Copy raw tables to a specified Redshift cluster. NOTE: audience_report and pixie_sample tables are not copied."
-    )
+    help = "Copy raw tables from and to a specified Redshift cluster. NOTE: audience_report and pixie_sample tables are not copied."
 
     def add_arguments(self, parser):
+        parser.add_argument("from_db_alias", type=str)
         parser.add_argument("to_db_alias", type=str)
         parser.add_argument("num_days", type=int)
 
     def handle(self, *args, **options):
-        tables = ["conversions", "postclickstats", "outbrainpublisherstats", "stats", "supply_stats"]
+        tables = [
+            "conversions",
+            "postclickstats",
+            "outbrainpublisherstats",
+            "supply_stats",
+            "partnerstats",
+            "stats",
+            "stats_diff",
+            "stats_placement",
+            "stats_placement_diff",
+        ]
 
+        from_db_alias = options["from_db_alias"]
         to_db_alias = options["to_db_alias"]
         num_days = options["num_days"]
         date_to = dates_helper.local_today()
@@ -28,6 +38,6 @@ class Command(Z1Command):
 
         job_id = refresh.generate_job_id(None)
         for table in tables:
-            logger.info("Copying table", table=table, into=to_db_alias)
-            s3_path = etl.redshift.unload_table(job_id, table, date_from, date_to)
+            logger.info("Copying table", table=table, from_db=from_db_alias, to_db=to_db_alias)
+            s3_path = etl.redshift.unload_table(job_id, table, date_from, date_to, db_name=from_db_alias)
             etl.redshift.update_table_from_s3(to_db_alias, s3_path, table, date_from, date_to)
