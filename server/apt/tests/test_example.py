cf80f8daf077b9608db2b35c73da81ad2b8a0bc9
Luka Šilovinac <luka.silovinac@zemanta.com>
2020-07-22 16:56:20 +0200

    APT: temp fail a test

--- server/apt/tests/test_example.py
+++ server/apt/tests/test_example.py
@@ -8,4 +8,4 @@ ZEMANTA_PRODUCT_GROUP_ACCOUNT_ID = 92
 class ExampleAPTTest(APTTestCase):
     def test_example_db(self):
         acc = core.models.Account.objects.get(id=ZEMANTA_PRODUCT_GROUP_ACCOUNT_ID)
-        self.assertEqual("Zemanta Product Group", acc.name)
+        self.assertEqual("", acc.name)
