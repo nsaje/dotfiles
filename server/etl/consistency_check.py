49864d582e60645d01f2d6f837c08b1e0703513b
Nejc Saje <nejc@saje.info>
2020-07-23 13:38:43 +0200

    hack: check_consistency _pubs tables

--- server/etl/consistency_check.py
+++ server/etl/consistency_check.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.conf import settings
 
 from etl import materialize
@@ -22,6 +24,8 @@ def consistency_check(date_from, date_to):
 
     for view in derived_views:
         for db_name in [settings.STATS_DB_HOT_CLUSTER] + settings.STATS_DB_COLD_CLUSTERS + settings.STATS_DB_POSTGRES:
+            if date_from <= datetime.date(2020, 7, 7) and view.TABLE_NAME.endswith("_pubs"):
+                continue  # HACK(nsaje): dont check pubs tables before that date since they werent consistent with mv_master before mv_master_pubs removal
             check_counts(master_counts, view, date_from, date_to, db_name)
 
     logger.info("Consistency check finished")
