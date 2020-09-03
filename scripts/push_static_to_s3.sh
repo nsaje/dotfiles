813301cf8f0a73c34460a97e9165515cc6eed66a
Matija Vižintin <matija.vizintin@gmail.com>
2020-09-03 07:54:23 +0200

    stop pushing static to z1-static bucket (#5072)

--- scripts/push_static_to_s3.sh
+++ scripts/push_static_to_s3.sh
@@ -11,25 +11,6 @@ else
 fi
 
 if [ -d client/dist ]; then
-	S3_PATH="s3://z1-static/build-${BRANCH}${BUILD_NUM}"
-
-	# need to set content-disposition for attachments sync assets separately
-	aws s3 sync --acl public-read --content-disposition 'attachment' client/dist/one/assets "${S3_PATH}/client/one/assets"
-
-	aws s3 sync --acl public-read --exclude 'assets/' client/dist "${S3_PATH}/client"
-	aws s3 sync --acl public-read server/static "${S3_PATH}/server"
-
-
-  if [ "$BRANCH_NAME" == "master" ]; then
-	    echo ${BUILD_NUM} | aws s3 cp - s3://z1-static/latest.txt
-  fi
-
-	# Push styleguides to well-known URL
-	aws s3 cp --acl public-read client/dist/one/zemanta-one.css s3://z1-static/styleguides/
-	aws s3 cp --acl public-read client/dist/one/zemanta-one.lib.css s3://z1-static/styleguides/
-fi
-
-if [ -d client/dist ]; then
 	S3_PATH="s3://one-static.zemanta.com/build-${BRANCH}${BUILD_NUM}"
 
 	# need to set content-disposition for attachments sync assets separately
