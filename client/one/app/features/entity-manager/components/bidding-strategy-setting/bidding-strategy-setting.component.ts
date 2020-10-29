e6486245d134b87d88b90c7c0cc9a078946c0176
jgosar <36840705+jgosar@users.noreply.github.com>
2020-10-29 06:23:12 +0100

    Real-time autopilot: show autopilot state ACTIVE_CPC_BUDGET or ACTIVE_CPC as Optimal bid (#5275)

--- client/one/app/features/entity-manager/components/bidding-strategy-setting/bidding-strategy-setting.component.ts
+++ client/one/app/features/entity-manager/components/bidding-strategy-setting/bidding-strategy-setting.component.ts
@@ -1,11 +1,13 @@
 import './bidding-strategy-setting.component.less';
 
 import {
-    Component,
     ChangeDetectionStrategy,
+    Component,
     EventEmitter,
-    Output,
     Input,
+    OnChanges,
+    Output,
+    SimpleChanges,
 } from '@angular/core';
 import {AdGroupAutopilotState, BiddingType} from '../../../../app.constants';
 
@@ -14,7 +16,7 @@ import {AdGroupAutopilotState, BiddingType} from '../../../../app.constants';
     templateUrl: './bidding-strategy-setting.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BiddingStrategySettingComponent {
+export class BiddingStrategySettingComponent implements OnChanges {
     @Input()
     biddingType: BiddingType;
     @Input()
@@ -24,5 +26,17 @@ export class BiddingStrategySettingComponent {
     @Output()
     autopilotStateChange = new EventEmitter<AdGroupAutopilotState>();
 
+    formattedAutopilotState: AdGroupAutopilotState;
+
     AdGroupAutopilotState = AdGroupAutopilotState;
+
+    // We want to display ad groups with autopilot state set to the legacy options of ACTIVE_CPC_BUDGET and ACTIVE_CPC as ACTIVE
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes.autopilotState) {
+            this.formattedAutopilotState =
+                this.autopilotState === AdGroupAutopilotState.INACTIVE
+                    ? AdGroupAutopilotState.INACTIVE
+                    : AdGroupAutopilotState.ACTIVE;
+        }
+    }
 }
