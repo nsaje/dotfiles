9cf2b9ff15b23cdeca400ebaf50fba8bc2d9f724
Jan Holas <jholas@outbrain.com>
2020-11-16 17:37:24 +0100

    Revert "Revert "Revert "3rd Party JS Tracking: BE: Trackers & trackers_status field insert/update & validation logic (#5270)"""
    This reverts commit 2853195de760e0bde22284597844ff104152d693.

--- server/dash/features/contentupload/__init__.py
+++ server/dash/features/contentupload/__init__.py
@@ -1,5 +1,1 @@
 from . import upload
-from .trackers_helpers import convert_legacy_trackers
-from .trackers_helpers import get_privacy_frameworks
-from .trackers_helpers import get_tracker
-from .trackers_helpers import get_tracker_status_key
