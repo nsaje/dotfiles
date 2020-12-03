8c90ddf18d9809d3cf5220274d6f70115e119cf0
Nejc Saje <nejc@saje.info>
2020-12-03 09:53:29 +0100

    fix bulkupload url matching

--- server/restapi/bulkupload/beta/urls.py
+++ server/restapi/bulkupload/beta/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = "restapi.bulkupload"
 urlpatterns = [
-    url(r"^(?P<task_id>.+)$", views.BulkAdGroupsViewSet.as_view({"get": "get"}), name="bulkadgroups_details"),
+    url(r"^(?P<task_id>\S+)$", views.BulkAdGroupsViewSet.as_view({"get": "get"}), name="bulkadgroups_details"),
     url(r"^$", views.BulkAdGroupsViewSet.as_view({"post": "create"}), name="bulkadgroups_list"),
 ]
