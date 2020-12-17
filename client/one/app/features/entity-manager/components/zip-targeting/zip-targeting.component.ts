cdd9cadae35e3129326a63f923249422951f3a1a
Jan Holas <1366866+janholas@users.noreply.github.com>
2020-12-17 12:35:27 +0100

    ZIP code not saved: ZMS Pilot Munchen MVV (#5467)

--- client/one/app/features/entity-manager/components/zip-targeting/zip-targeting.component.ts
+++ client/one/app/features/entity-manager/components/zip-targeting/zip-targeting.component.ts
@@ -125,7 +125,7 @@ export class ZipTargetingComponent implements OnChanges {
         location: Geolocation,
         zipCodesText: string
     ): void {
-        if (!isDefined(this.location)) {
+        if (!isDefined(location)) {
             return;
         }
 
