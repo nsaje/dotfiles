84e1e6698ab3592c9a8d6fb49c84961258a090db
Voltarox <tilen970@gmail.com>
2020-12-22 08:36:54 +0100

    Fixed and Simplified APT GET tests (#5440)
    * Added retry
    
    * tweaks to get_id_with_retries
    
    * type hinting
    
    * change order and rename
    
    * mypy ignore secretcrypt
    
    * Revert "mypy ignore secretcrypt"
    
    This reverts commit 4ceef62f5219e7686d5cd169c4e6f7f24a1d9808.
    
    * type ignore for mypy
    
    * added another type ignore
    
    * add additional output
    
    * additiona logging
    
    * added time measurement
    
    * added counter
    
    * comment publisher tests
    
    * change method
    
    * reintroduce publishergroups
    
    * fix function call
    
    * changed from class variable to local
    
    * Change publisher query
    
    * move publisher to setup
    
    * test with choice
    
    * unify method
    
    * Revert "unify method"
    
    This reverts commit f4845df0a60ee0a84f97bea6e88b7c12709251f9.
    
    * take latest element
    
    * fetch latest object everywhere
    
    * remove teardown

--- server/apt/common/db_queries.py
+++ server/apt/common/db_queries.py
@@ -1,7 +1,7 @@
 from django.db import connection
 
 
-def get_count_estimate(table):
+def get_count_estimate(table: str) -> int:
     with connection.cursor() as c:
         c.execute(f"SELECT reltuples as approximate_row_count FROM pg_class WHERE relname = '{table}';")
         return int(c.fetchall()[0][0])
