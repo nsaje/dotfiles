520df8cb93742dc84dc2b40a100e3552d5e4a118
Anej Placer <anej.placer@gmail.com>
2021-01-18 13:58:28 +0100

    Ironsource campaign limit raised to 5000

--- server/core/common/entity_limits.py
+++ server/core/common/entity_limits.py
@@ -21,7 +21,7 @@ DEFAULT_LIMITS = {
 ACCOUNT_EXCEPTIONS = defaultdict(
     dict,
     {
-        "Campaign": {settings.HARDCODED_ACCOUNT_ID_OEN: 5000000, 2323: 600},  # OEN
+        "Campaign": {settings.HARDCODED_ACCOUNT_ID_OEN: 5000000, 2323: 600, 5921: 5000},  # OEN  # ironsource
         "AdGroup": {
             settings.HARDCODED_ACCOUNT_ID_INPOWERED_1: 1000,
             settings.HARDCODED_ACCOUNT_ID_INPOWERED_2: 1000,
