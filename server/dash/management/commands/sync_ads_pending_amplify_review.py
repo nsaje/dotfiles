e13b4341648b1884639efe5e714b156bcc6f8a7e
Nejc Saje <nejc@saje.info>
2021-02-18 09:57:17 +0100

    check pending amplify ads more often

--- server/dash/management/commands/sync_ads_pending_amplify_review.py
+++ server/dash/management/commands/sync_ads_pending_amplify_review.py
@@ -1,25 +1,26 @@
+import datetime
 from core import models
 from dash import constants
 from utils import k1_helper
 from utils import zlogging
 from utils.command_helpers import Z1Command
-from utils.dates_helper import utc_yesterday
 
 logger = zlogging.getLogger(__name__)
 OUTBRAIN_SOURCE_ID = 3
+DAYS_BACK = 7
 
 
 class Command(Z1Command):
     help = "Sync ads pending amplify review periodically"
 
     def handle(self, *args, **options):
-        yesterday = utc_yesterday()
+        from_date = datetime.date.today() - datetime.timedelta(days=DAYS_BACK)
         content_ads = models.ContentAd.objects.filter(
             id__in=(
                 models.ContentAdSource.objects.filter(
                     source_id=OUTBRAIN_SOURCE_ID,
                     submission_status=constants.ContentAdSubmissionStatus.PENDING,
-                    created_dt__gte=yesterday,
+                    created_dt__gte=from_date,
                 )
                 .select_related("content_ad")
                 .distinct("content_ad_id")
