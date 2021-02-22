1a9baa6d73425fffa3ce89f95387c10f7b580f83
Anej <anej.placer@gmail.com>
2021-02-22 16:01:32 +0100

    OutbrainRTB: K1API: bid modifiers (#5545)
    * Bid modifier expansion and tests
    
    * exclude deprecated bid modifiers
    
    * Deprecated bid modifiers filtering fix

--- server/k1api/views/bid_modifiers.py
+++ server/k1api/views/bid_modifiers.py
@@ -1,4 +1,8 @@
+from django.conf import settings
+from django.db.models import Q
+
 import core.features.bid_modifiers
+import core.features.source_groups
 import core.models
 from utils import db_router
 
@@ -13,7 +17,19 @@ class BidModifiersView(K1APIView):
         ad_group_ids = request.GET.get("ad_group_ids")
         type_ = request.GET.get("type")
 
-        qs = core.features.bid_modifiers.BidModifier.objects.all().order_by("pk")
+        source_groups_id_slugs_mapping = core.features.source_groups.get_source_id_slugs_mapping()
+
+        qs = core.features.bid_modifiers.BidModifier.objects.exclude(
+            (
+                Q(type=core.features.bid_modifiers.constants.BidModifierType.SOURCE)
+                & Q(target__in=[str(sid) for sid in source_groups_id_slugs_mapping.keys()])
+                | Q(type=core.features.bid_modifiers.constants.BidModifierType.PUBLISHER)
+                & Q(source_id__in=source_groups_id_slugs_mapping.keys())
+                | Q(type=core.features.bid_modifiers.constants.BidModifierType.PLACEMENT)
+                & Q(source_id__in=source_groups_id_slugs_mapping.keys())
+            ),
+            ad_group__campaign__account__agency__uses_source_groups=True,
+        ).order_by("pk")
 
         if ad_group_ids:
             ad_group_ids = ad_group_ids.split(",")
@@ -25,16 +41,52 @@ class BidModifiersView(K1APIView):
             qs = qs.filter(pk__gt=int(marker))
         qs = qs[:limit]
 
-        return self.response_ok(
-            [
-                {
-                    "id": item.id,
-                    "ad_group_id": item.ad_group_id,
-                    "target": item.target,
-                    "type": item.type,
-                    "source": item.source_slug,
-                    "modifier": item.modifier,
-                }
-                for item in qs
-            ]
-        )
+        response = []
+        for bid_modifier in qs:
+            item = {
+                "ad_group_id": bid_modifier.ad_group_id,
+                "target": bid_modifier.target,
+                "type": bid_modifier.type,
+                "source": bid_modifier.source_slug,
+                "modifier": bid_modifier.modifier,
+            }
+
+            if bid_modifier.type == core.features.bid_modifiers.constants.BidModifierType.SOURCE:
+                response += self._generate_source_groups_bid_modifiers(
+                    bid_modifier, item, int(bid_modifier.target), "target", str
+                )
+
+            elif bid_modifier.type in (
+                core.features.bid_modifiers.constants.BidModifierType.PUBLISHER,
+                core.features.bid_modifiers.constants.BidModifierType.PLACEMENT,
+            ):
+                response += self._generate_source_groups_bid_modifiers(
+                    bid_modifier,
+                    item,
+                    bid_modifier.source_id,
+                    "source",
+                    lambda sid: source_groups_id_slugs_mapping[sid]["bidder_slug"],
+                )
+
+            else:
+                response.append(item)
+
+        return self.response_ok(response)
+
+    def _generate_source_groups_bid_modifiers(self, bid_modifier, item, source_id, field, update_fn):
+        response = []
+
+        source_group = settings.SOURCE_GROUPS.get(source_id)
+        if source_group and bid_modifier.ad_group.campaign.account.agency.uses_source_groups:
+            for grouped_source_id in source_group:
+                grouped_item = item.copy()
+                grouped_item[field] = update_fn(grouped_source_id)
+                response.append(grouped_item)
+
+        if (
+            source_id != settings.HARDCODED_SOURCE_ID_OUTBRAINRTB
+            or bid_modifier.ad_group.campaign.account_id != settings.HARDCODED_ACCOUNT_ID_OEN
+        ):
+            response.append(item)
+
+        return response
--- server/k1api/views/bid_modifiers_test.py
+++ server/k1api/views/bid_modifiers_test.py
@@ -1,5 +1,7 @@
 import json
 
+import mock
+from django.conf import settings
 from django.urls import reverse
 
 import core.features.bid_modifiers
@@ -11,13 +13,12 @@ from .base_test import K1APIBaseTest
 
 class BidModifiersTest(K1APIBaseTest):
     def setUp(self):
-        self.source = magic_mixer.blend(core.models.Source, bidder_slug="test_source")
+        self.source = magic_mixer.blend(core.models.Source, id=1, bidder_slug="test_source")
         self.bid_modifier_types = list(core.features.bid_modifiers.constants.BidModifierType.get_all())
         super(BidModifiersTest, self).setUp()
 
     def repr(self, obj):
         return {
-            "id": obj.id,
             "ad_group_id": obj.ad_group_id,
             "type": obj.type,
             "target": obj.target,
@@ -30,6 +31,7 @@ class BidModifiersTest(K1APIBaseTest):
             core.features.bid_modifiers.BidModifier,
             source=None,
             source_slug="",
+            target="123",
             type=(t for t in self.bid_modifier_types),
         )
         test_objs.append(
@@ -37,6 +39,7 @@ class BidModifiersTest(K1APIBaseTest):
                 core.features.bid_modifiers.BidModifier,
                 source=self.source,
                 source_slug=self.source.bidder_slug,
+                target="321",
                 type=(t for t in self.bid_modifier_types),
             )
         )
@@ -68,3 +71,228 @@ class BidModifiersTest(K1APIBaseTest):
         self.assert_response_ok(response, data)
         self.assertEqual(1, len(data["response"]))
         self.assertEqual(data["response"], [self.repr(obj) for obj in test_objs if obj.type == ad_type])
+
+
+@mock.patch("django.conf.settings.SOURCE_GROUPS", {settings.HARDCODED_SOURCE_ID_OUTBRAINRTB: [81]})
+class BidModifiersGroupsTest(K1APIBaseTest):
+    def setUp(self):
+        super().setUp()
+
+        self.ad_group = magic_mixer.blend(core.models.AdGroup, campaign__account__agency__uses_source_groups=True)
+
+        self.parent_source = magic_mixer.blend(
+            core.models.Source,
+            id=settings.HARDCODED_SOURCE_ID_OUTBRAINRTB,
+            bidder_slug="main_slug",
+            tracking_slug="main_tracking",
+        )
+        self.grouped_source = magic_mixer.blend(
+            core.models.Source, id=81, bidder_slug="sub_slug", tracking_slug="sub_tracking"
+        )
+
+        self.source_bid_modifier = magic_mixer.blend(
+            core.features.bid_modifiers.BidModifier,
+            ad_group=self.ad_group,
+            type=core.features.bid_modifiers.constants.BidModifierType.SOURCE,
+            target=str(self.parent_source.id),
+            modifier=1.7,
+        )
+        self.publisher_bid_modifier = magic_mixer.blend(
+            core.features.bid_modifiers.BidModifier,
+            ad_group=self.ad_group,
+            type=core.features.bid_modifiers.constants.BidModifierType.PUBLISHER,
+            source=self.parent_source,
+            source_slug=self.parent_source.bidder_slug,
+            modifier=1.8,
+        )
+        self.placement_bid_modifier = magic_mixer.blend(
+            core.features.bid_modifiers.BidModifier,
+            ad_group=self.ad_group,
+            type=core.features.bid_modifiers.constants.BidModifierType.PLACEMENT,
+            source=self.parent_source,
+            source_slug=self.parent_source.bidder_slug,
+            modifier=1.9,
+        )
+
+        # deprecated bid modifiers
+        self.deprecated_source_bid_modifier = magic_mixer.blend(
+            core.features.bid_modifiers.BidModifier,
+            ad_group=self.ad_group,
+            type=core.features.bid_modifiers.constants.BidModifierType.SOURCE,
+            target=str(self.grouped_source.id),
+            modifier=2.7,
+        )
+        self.deprecated_publisher_bid_modifier = magic_mixer.blend(
+            core.features.bid_modifiers.BidModifier,
+            ad_group=self.ad_group,
+            type=core.features.bid_modifiers.constants.BidModifierType.PUBLISHER,
+            source=self.grouped_source,
+            source_slug=self.grouped_source.bidder_slug,
+            modifier=2.8,
+        )
+        self.deprecated_placement_bid_modifier = magic_mixer.blend(
+            core.features.bid_modifiers.BidModifier,
+            ad_group=self.ad_group,
+            type=core.features.bid_modifiers.constants.BidModifierType.PLACEMENT,
+            source=self.grouped_source,
+            source_slug=self.grouped_source.bidder_slug,
+            modifier=2.9,
+        )
+
+    def test_get_grouped_bid_modifiers(self):
+        response = self.client.get(reverse("k1api.bidmodifiers"))
+
+        data = json.loads(response.content)
+        self.assert_response_ok(response, data)
+        data = data["response"]
+
+        self.assertEqual(len(data), 6)
+
+        self.assertCountEqual(
+            [
+                {
+                    "ad_group_id": self.ad_group.id,
+                    "target": self.source_bid_modifier.target,
+                    "type": self.source_bid_modifier.type,
+                    "source": self.source_bid_modifier.source_slug,
+                    "modifier": self.source_bid_modifier.modifier,
+                },
+                {
+                    "ad_group_id": self.ad_group.id,
+                    "target": str(self.grouped_source.id),
+                    "type": self.source_bid_modifier.type,
+                    "source": self.source_bid_modifier.source_slug,
+                    "modifier": self.source_bid_modifier.modifier,
+                },
+                {
+                    "ad_group_id": self.ad_group.id,
+                    "target": self.publisher_bid_modifier.target,
+                    "type": self.publisher_bid_modifier.type,
+                    "source": self.publisher_bid_modifier.source_slug,
+                    "modifier": self.publisher_bid_modifier.modifier,
+                },
+                {
+                    "ad_group_id": self.ad_group.id,
+                    "target": self.publisher_bid_modifier.target,
+                    "type": self.publisher_bid_modifier.type,
+                    "source": self.grouped_source.bidder_slug,
+                    "modifier": self.publisher_bid_modifier.modifier,
+                },
+                {
+                    "ad_group_id": self.ad_group.id,
+                    "target": self.placement_bid_modifier.target,
+                    "type": self.placement_bid_modifier.type,
+                    "source": self.placement_bid_modifier.source_slug,
+                    "modifier": self.placement_bid_modifier.modifier,
+                },
+                {
+                    "ad_group_id": self.ad_group.id,
+                    "target": self.placement_bid_modifier.target,
+                    "type": self.placement_bid_modifier.type,
+                    "source": self.grouped_source.bidder_slug,
+                    "modifier": self.placement_bid_modifier.modifier,
+                },
+            ],
+            data,
+        )
+
+    def test_get_grouped_bid_modifiers_no_flag(self):
+        self.ad_group.campaign.account.agency.uses_source_groups = False
+        self.ad_group.campaign.account.agency.save(None)
+
+        response = self.client.get(reverse("k1api.bidmodifiers"))
+
+        data = json.loads(response.content)
+        self.assert_response_ok(response, data)
+        data = data["response"]
+
+        self.assertEqual(len(data), 6)
+
+        self.assertCountEqual(
+            [
+                {
+                    "ad_group_id": self.ad_group.id,
+                    "target": self.source_bid_modifier.target,
+                    "type": self.source_bid_modifier.type,
+                    "source": self.source_bid_modifier.source_slug,
+                    "modifier": self.source_bid_modifier.modifier,
+                },
+                {
+                    "ad_group_id": self.ad_group.id,
+                    "target": self.publisher_bid_modifier.target,
+                    "type": self.publisher_bid_modifier.type,
+                    "source": self.publisher_bid_modifier.source_slug,
+                    "modifier": self.publisher_bid_modifier.modifier,
+                },
+                {
+                    "ad_group_id": self.ad_group.id,
+                    "target": self.placement_bid_modifier.target,
+                    "type": self.placement_bid_modifier.type,
+                    "source": self.placement_bid_modifier.source_slug,
+                    "modifier": self.placement_bid_modifier.modifier,
+                },
+                {
+                    "ad_group_id": self.ad_group.id,
+                    "target": str(self.grouped_source.id),
+                    "type": self.deprecated_source_bid_modifier.type,
+                    "source": self.deprecated_source_bid_modifier.source_slug,
+                    "modifier": self.deprecated_source_bid_modifier.modifier,
+                },
+                {
+                    "ad_group_id": self.ad_group.id,
+                    "target": self.deprecated_publisher_bid_modifier.target,
+                    "type": self.deprecated_publisher_bid_modifier.type,
+                    "source": self.grouped_source.bidder_slug,
+                    "modifier": self.deprecated_publisher_bid_modifier.modifier,
+                },
+                {
+                    "ad_group_id": self.ad_group.id,
+                    "target": self.deprecated_placement_bid_modifier.target,
+                    "type": self.deprecated_placement_bid_modifier.type,
+                    "source": self.grouped_source.bidder_slug,
+                    "modifier": self.deprecated_placement_bid_modifier.modifier,
+                },
+            ],
+            data,
+        )
+
+    def test_get_grouped_bid_modifiers_oen(self):
+        self.ad_group.campaign.account = magic_mixer.blend(
+            core.models.Account, id=settings.HARDCODED_ACCOUNT_ID_OEN, agency__uses_source_groups=True
+        )
+        self.ad_group.campaign.save(None)
+
+        response = self.client.get(reverse("k1api.bidmodifiers"))
+
+        data = json.loads(response.content)
+        self.assert_response_ok(response, data)
+        data = data["response"]
+
+        self.assertEqual(len(data), 3)
+
+        self.assertCountEqual(
+            [
+                {
+                    "ad_group_id": self.ad_group.id,
+                    "target": str(self.grouped_source.id),
+                    "type": self.source_bid_modifier.type,
+                    "source": self.source_bid_modifier.source_slug,
+                    "modifier": self.source_bid_modifier.modifier,
+                },
+                {
+                    "ad_group_id": self.ad_group.id,
+                    "target": self.publisher_bid_modifier.target,
+                    "type": self.publisher_bid_modifier.type,
+                    "source": self.grouped_source.bidder_slug,
+                    "modifier": self.publisher_bid_modifier.modifier,
+                },
+                {
+                    "ad_group_id": self.ad_group.id,
+                    "target": self.placement_bid_modifier.target,
+                    "type": self.placement_bid_modifier.type,
+                    "source": self.grouped_source.bidder_slug,
+                    "modifier": self.placement_bid_modifier.modifier,
+                },
+            ],
+            data,
+        )
