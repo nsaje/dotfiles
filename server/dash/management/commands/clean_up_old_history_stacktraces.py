8d8c97fc53125fd17784134ef1c89ec1ac647a13
Tilen Kusterle <tkusterle@outbrain.com>
2021-03-02 08:22:19 +0100

    Add retrying to drop table statement and unify retrying for all cases (#5660)
    * Add retrying to drop table statement and unify retrying for all cases
    
    * Add missing cursor parameter
    
    * Fix leftover cursor execute call
    
    * Reintroduce programming error handling

--- server/dash/management/commands/clean_up_old_history_stacktraces.py
+++ server/dash/management/commands/clean_up_old_history_stacktraces.py
@@ -48,17 +48,9 @@ class Command(Z1Command):
         for i in range(1, PARTITION_CHECK_DAYS + 1):
             date_from = today + datetime.timedelta(i)
             date_to = today + datetime.timedelta(i + 1)
-            self._create_partition_with_retry(cursor, date_from, date_to)
+            self._create_partition(cursor, date_from, date_to)
 
-    @retrying.retry(
-        retry_on_exception=lambda e: isinstance(e, OperationalError)
-        and "canceling statement due to lock timeout" in str(e)
-        or "deadlock detected" in str(e),
-        stop_max_attempt_number=50,
-        wait_exponential_multiplier=1000,
-        wait_exponential_max=10000,
-    )
-    def _create_partition_with_retry(self, cursor, date_from, date_to):
+    def _create_partition(self, cursor, date_from, date_to):
         partition_name = "dash_historystacktrace_{date_postfix}".format(date_postfix=date_from.strftime("%Y%m%d"))
         sql = """
             CREATE TABLE IF NOT EXISTS
@@ -67,11 +59,12 @@ class Command(Z1Command):
                 dash_historystacktrace
             FOR VALUES FROM ('{date_from}') TO ('{date_to}');
         """
-        try:
-            cursor.execute(sql.format(partition_name=partition_name, date_from=date_from, date_to=date_to))
-        except OperationalError as e:
-            logger.info("Failed creating partiton", partition=partition_name)
-            raise e
+        self._execute_sql_with_retry(
+            cursor,
+            sql.format(partition_name=partition_name, date_from=date_from, date_to=date_to),
+            "Failed creating partiton",
+            partition=partition_name,
+        )
 
     def _delete_old_partitions(self, today):
         cursor = connection.cursor()
@@ -83,8 +76,16 @@ class Command(Z1Command):
             self.delete_partition(cursor, partition_name)
 
     def delete_partition(self, cursor, partition_name):
-        self._detach_partition_with_retry(cursor, partition_name)
-        cursor.execute(f"DROP TABLE IF EXISTS {partition_name};")
+        self._detach_partition(cursor, partition_name)
+        self._delete_partition(cursor, partition_name)
+
+    def _detach_partition(self, cursor, partition_name):
+        sql = f"ALTER TABLE dash_historystacktrace DETACH PARTITION {partition_name};"
+        self._execute_sql_with_retry(cursor, sql, "Failed detaching partiton", partition=partition_name)
+
+    def _delete_partition(self, cursor, partition_name):
+        sql = f"DROP TABLE IF EXISTS {partition_name};"
+        self._execute_sql_with_retry(cursor, sql, "Failed deleting partiton", partition=partition_name)
 
     @retrying.retry(
         retry_on_exception=lambda e: isinstance(e, OperationalError)
@@ -94,14 +95,14 @@ class Command(Z1Command):
         wait_exponential_multiplier=1000,
         wait_exponential_max=10000,
     )
-    def _detach_partition_with_retry(self, cursor, partition_name):
+    def _execute_sql_with_retry(self, cursor, sql, log_message, **log_kwargs):
         try:
-            cursor.execute(f"ALTER TABLE dash_historystacktrace DETACH PARTITION IF EXISTS {partition_name};")
+            cursor.execute(sql)
         except ProgrammingError:
-            # skipping due to non-existing partition
+            # something wrong - e.g. partition does not exist
             pass
         except OperationalError as e:
-            logger.info("Failed detaching partiton", partition=partition_name)
+            logger.info(log_message, **log_kwargs)
             raise e
 
     def _set_lock_timeout(self, cursor, timeout):
--- server/dash/management/commands/clean_up_old_history_stacktraces_test.py
+++ server/dash/management/commands/clean_up_old_history_stacktraces_test.py
@@ -34,13 +34,9 @@ class CleanUpOldHistoryStackTracesTest(TestCase):
         """
                 ),
                 mock.call("SET lock_timeout TO '2s';"),
-                mock.call(
-                    "ALTER TABLE dash_historystacktrace DETACH PARTITION IF EXISTS dash_historystacktrace_20200909;"
-                ),
+                mock.call("ALTER TABLE dash_historystacktrace DETACH PARTITION dash_historystacktrace_20200909;"),
                 mock.call("DROP TABLE IF EXISTS dash_historystacktrace_20200909;"),
-                mock.call(
-                    "ALTER TABLE dash_historystacktrace DETACH PARTITION IF EXISTS dash_historystacktrace_20200908;"
-                ),
+                mock.call("ALTER TABLE dash_historystacktrace DETACH PARTITION dash_historystacktrace_20200908;"),
                 mock.call("DROP TABLE IF EXISTS dash_historystacktrace_20200908;"),
             ],
         )
